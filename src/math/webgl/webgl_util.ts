58086821669afbaa91878d48767fec58b51fe9b8
Daniel Smilkov <dsmilkov@gmail.com>
2017-10-03 18:25:23 -0400

    cache attribute locations in gpgpu binary (#169)
    * cache attribute locations in gpgpu binary
    
    * address review
    
    * fix typo

--- src/math/webgl/webgl_util.ts
+++ src/math/webgl/webgl_util.ts
@@ -257,14 +257,17 @@ export function createFramebuffer(gl: WebGLRenderingContext): WebGLFramebuffer {
 export function bindVertexBufferToProgramAttribute(
     gl: WebGLRenderingContext, program: WebGLProgram, attribute: string,
     buffer: WebGLBuffer, arrayEntriesPerItem: number, itemStrideInBytes: number,
-    itemOffsetInBytes: number) {
-  const loc = gl.getAttribLocation(program, attribute);
+    itemOffsetInBytes: number, attribLocations?: {[name: string]: number}) {
+  let loc = -1;
+  if ((attribLocations != null) && (attribute in attribLocations)) {
+    loc = attribLocations[attribute];
+  } else {
+    loc = gl.getAttribLocation(program, attribute);
+  }
   if (loc === -1) {
-    const error = new Error(
-        'Unable to get attribute "' + attribute + '" on WebGLProgram.');
-    // tslint:disable-next-line:no-any
-    (error as any).namedVertexAttributeNotFound = attribute;
-    throw error;
+    // The GPU compiler decided to strip out this attribute because it's unused,
+    // thus no need to bind.
+    return;
   }
   callAndCheck(gl, () => gl.bindBuffer(gl.ARRAY_BUFFER, buffer));
   callAndCheck(
