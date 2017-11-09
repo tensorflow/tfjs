29b756ecab02f42912c91a40737668a66cde45a4
Nikhil Thorat <nsthorat@google.com>
2017-11-09 10:52:51 -0500

    Allow all devices for teachable gaming so we can debug polymer. (#326)

--- demos/homepage/assets/support.js
+++ demos/homepage/assets/support.js
@@ -25,6 +25,7 @@ function isSafari() {
     }
   }
 }
+
 function isMobile() {
   var a = navigator.userAgent || navigator.vendor || window.opera;
   return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i
@@ -50,7 +51,8 @@ function isWebGLEnabled() {
 }
 
 function isNotSupported() {
-  if (location.pathname.startsWith('/demos/playground/')) {
+  if (location.pathname.startsWith('/demos/playground/') ||
+      location.pathname.startsWith('/demos/teachable_gaming/')) {
     return false;
   }
   return isMobile() || isSafari();
