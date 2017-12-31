1a29e8e226ae8bc447d2d5228fb93038a51e1a02
Manraj Singh <manrajsinghgrover@gmail.com>
2017-12-31 06:58:13 +0530

    Add a dev tsconfig and use it when building poi demos. (#432)

--- demos/poi.config.js
+++ demos/poi.config.js
@@ -1,9 +1,15 @@
-module.exports = {
+const path = require('path');
+
+module.exports = options => ({
   webpack(config) {
     config.output.publicPath = '';
     return config;
   },
-  presets: [
-    require('poi-preset-typescript')(/* options */)
-  ],
-};
+  presets: [require('poi-preset-typescript')({
+    loaderOptions: {
+      configFile: options.mode === 'development' ?
+          path.join('..', 'tsconfig-dev.json') :
+          path.join('..', 'tsconfig.json')
+    }
+  })],
+});
