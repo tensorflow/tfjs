06b5fd9bc9d5d0e9e88168be5e3198d265d8b1c8
Nick Kreeger <nick.kreeger@gmail.com>
2018-03-01 09:30:57 -0800

    Let core TF set the output Tensor. (#10)
    * Add source file.
    
    * Add an optional default constructor.
    
    * Change backend to allow output to be set by core TF.
    
    * Cleanup.

--- binding/tf_node_binding.cc
+++ binding/tf_node_binding.cc
@@ -61,6 +61,12 @@ static napi_value NewTensorHandle(napi_env env, napi_callback_info info) {
   nstatus = napi_get_cb_info(env, info, &argc, args, &js_this, nullptr);
   ENSURE_NAPI_OK(env, nstatus);
 
+  if (argc == 0) {
+    // A zero param was passed, simply wrap the struct with nullptr.
+    InitPlaceholderTensorHandle(env, js_this);
+    return js_this;
+  }
+
   napi_value shape_value = args[0];
   ENSURE_VALUE_IS_ARRAY(env, shape_value);
 
