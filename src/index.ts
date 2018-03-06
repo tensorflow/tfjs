d937cda7ce0361d9d6923933aab2f12cd9b97c07
Daniel Smilkov <dsmilkov@gmail.com>
2018-03-06 10:19:20 -0500

    save

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2018 Google Inc. All Rights Reserved.
+ * Copyright 2018 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,5 +15,5 @@
  * =============================================================================
  */
 
-export * from 'deeplearn';
-export * from 'tfjs-layers';
+export * from '@tensorflow/tfjs-core';
+export * from '@tensorflow/tfjs-layers';
