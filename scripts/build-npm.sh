ad6715d70bf3f61d8653b60e589130533c96922a
Daniel Smilkov <dsmilkov@gmail.com>
2018-03-09 15:36:58 -0500

    Fix build (move underscore to dep) and add build-npm script (#7)

---
+++ scripts/build-npm.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Copyright 2018 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+rimraf dist/
+yarn
+tsc --sourceMap false
+browserify --standalone tf src/index.ts -p [tsify] > dist/tf-layers.js
+uglifyjs dist/tf-layers.js -c -m -o dist/tf-layers.min.js
+echo "Stored standalone library at dist/tf-layers(.min).js"
+npm pack
