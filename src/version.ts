11d77f12cdab5893d742771c5b6afb48c1e9a621
Daniel Smilkov <dsmilkov@gmail.com>
2018-03-13 10:25:35 -0400

    update version to 0.6.0-alpha5

--- src/version.ts
+++ src/version.ts
@@ -1,5 +1,5 @@
 /** @license See the LICENSE file. */
 
 // This code is auto-generated, do not modify this file!
-const version = '0.6.0-alpha4';
+const version = '0.6.0-alpha5';
 export {version};
