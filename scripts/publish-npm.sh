71445b96acd8b9d0809f7a5182c9a1f405d0a8b2
Daniel Smilkov <dsmilkov@gmail.com>
2018-03-20 14:04:41 -0400

    save

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -17,12 +17,21 @@
 # Before you run this script, do this:
 # 1) Update the version in package.json
 # 2) Run ./scripts/make-version from the base dir of the project.
-# 3) Commit to the master branch.
+# 3) Run `yarn` to update `yarn.lock`, in case you updated dependencies
+# 4) Commit to the master branch.
 
 # Then:
-# 4) Run this script as `./scripts/publish-npm.sh` from the project base dir.
+# 5) Checkout the master branch of this repo.
+# 6) Run this script as `./scripts/publish-npm.sh` from the project base dir.
 
 set -e
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+if [ "$BRANCH" != "master" ]; then
+  echo "Error: Switch to the master branch before publishing to npm."
+  exit
+fi
 
 yarn build-npm
 ./scripts/make-version # This is for safety in case you forgot to do 2).
