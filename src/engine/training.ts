51147ba4fe5086d0107266b9246e3a8857bd9127
ericdnielsen <ericdnielsen@gmail.com>
2018-03-20 14:47:33 -0400

    Split model/model creation docs (#59)
    * Split model/model creation docs

--- src/engine/training.ts
+++ src/engine/training.ts
@@ -614,35 +614,6 @@ export interface ModelCompileConfig {
  * A `Model` is the basic unit of training, inference and evaluation in
  * TensorFlow.js. To create a `Model, use `model`.
  *
- * When creating a `Model`, specify its input(s) and output(s). Inputs are
- * `SymbolicTensor`s provided by `Input` layers. Outputs are `SymbolicTensor`s
- * provided by other layers that perform mathematical and neural-network
- * operations.
- *
- * For example, the following code snippet defines a model consisting of
- * two `dense` layers, with of 10 and 4 units, respectively.
- *
- * ```js
- * // Define input, which has a size of 5 (not including batch dimension).
- * const input = tf.input({shape: [5]});
- *
- * // First dense layer uses relu activation.
- * const denseLayer1 = tf.layers.dense({units: 10, activation: 'relu'});
- * // Second dense layer uses softmax activation.
- * const denseLayer2 = tf.layers.dense({units: 2, activation: 'softmax'});
- *
- * // Obtain the output symbolic tensor by applying the layers on the input.
- * const output = denseLayer2.apply(denseLayer1.apply(input));
- *
- * // Create the model based on the inputs.
- * const model = tf.model({inputs: input, outputs: output});
- *
- * // The model can be used for training, evaluation and prediction.
- * // For example, the following line runs prediction with the model on
- * // some fake data.
- * model.predict(tf.ones([2, 5])).print();
- * ```
- *
  * See also:
  *   `Sequential`, `loadModel`.
  */
