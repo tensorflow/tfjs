b77f6ecf5fa381772882aa611409fccd4e7559b8
Nick Kreeger <nick.kreeger@gmail.com>
2018-03-26 20:45:20 -0700

    Add mnist data loader file for Node.js (#43)
    * Add mnist-data and test file.
    
    * more wip
    
    * batch wip
    
    * wip
    
    * yarn bump
    
    * bump demo yarn.lock
    
    * wip
    
    * Concat on axis=1.
    
    * Some timing
    
    * cleanup
    
    * Cleanup.

---
+++ demo/src/mnist_data_demo.ts
@@ -0,0 +1,66 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as dl from 'deeplearn';
+import * as tf from 'tfjs-node';
+
+import {MnistDataset} from './mnist_data';
+import {Timer} from './timer';
+
+tf.bindTensorFlowBackend();
+
+function testPrint(image: dl.Tensor, label: dl.Tensor) {
+  const data = image.dataSync();
+  console.log(`--- Label: ${label.dataSync()}`);
+  let test = '';
+  for (let i = 0; i < data.length; i++) {
+    if (i !== 0 && i % 28 === 0) {
+      console.log(test);
+      test = '';
+    }
+
+    if (data[i] === 0) {
+      test += '     ';
+    } else {
+      test += ' ' + data[i].toFixed(1);
+    }
+  }
+}
+
+async function loadTest() {
+  const dataset = new MnistDataset();
+  await dataset.loadData();
+
+  const testBatch = dataset.nextTrainBatch(3);
+  testPrint(testBatch.image, testBatch.label);
+
+  dataset.reset();
+  const timer = new Timer();
+  timer.start();
+  for (let i = 0; i < 2000 && dataset.hasMoreData(); i++) {
+    dl.tidy(() => {
+      dataset.nextTrainBatch(100);
+      if (i % 100 === 0) {
+        console.log(`Batch ${i} of 2000`);
+      }
+    });
+  }
+  timer.end();
+  console.log(`Looped through 2000 batches at 100: ${timer.seconds()} seconds`);
+}
+
+loadTest();
