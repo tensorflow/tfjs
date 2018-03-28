cb1aba1521f0366014fe1bd6687e99b4085921c5
Shanqing Cai <cais@google.com>
2018-03-28 13:27:22 -0400

    Update license of setup.py for pip package (#56)
    * Update license of setup.py for pip package
    
    * Also: Add tensorflow as dependency

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,17 @@
-# Copyright 2018 Google LLC
+# Copyright 2018 Google LLC. All Rights Reserved.
 #
-# Use of this source code is governed by an MIT-style
-# license that can be found in the LICENSE file or at
-# https://opensource.org/licenses/MIT.
-# =============================================================================
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 """Build pip wheel for model_converter."""
 
 import setuptools
@@ -14,6 +22,7 @@ REQUIRED_PACKAGES = [
     'keras >= 2.1.4',
     'numpy >= 1.14.1',
     'six >= 1.11.0',
+    'tensorflow >= 1.6.0'
 ]
 
 CONSOLE_SCRIPTS = [
@@ -32,7 +41,7 @@ setuptools.setup(
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: JavaScript',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
@@ -61,6 +70,6 @@ setuptools.setup(
     entry_points={
         'console_scripts': CONSOLE_SCRIPTS,
     },
-    license='MIT',
+    license='Apache 2.0',
     keywords='tensorflow javascript machine deep learning converter',
 )
