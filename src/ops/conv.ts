567accbbb312fccd636b1c60c08d3e27bc3d1505
Shanqing Cai <cais@google.com>
2018-04-09 14:47:55 -0400

    Add op: separableConv2d (#934)
    * Add op: separableConv2d
    
    A step toward fixing:
    https://github.com/tensorflow/tfjs/issues/91
    
    * Fix doc string error for pointwise filter

--- src/ops/conv.ts
+++ src/ops/conv.ts
@@ -479,6 +479,109 @@ export class ConvOps {
     }
     return res as T;
   }
+
+  /**
+   * 2-D convolution with separable filters.
+   *
+   * Performs a depthwise convolution that acts separately on channels followed
+   * by a pointwise convolution that mixes channels. Note that this is
+   * separability between dimensions [1, 2] and 3, not spatial separability
+   * between dimensions 1 and 2.
+   *
+   * See
+   * [https://www.tensorflow.org/api_docs/python/tf/nn/separable_conv2d](
+   *     https://www.tensorflow.org/api_docs/python/tf/nn/separable_conv2d)
+   * for more details.
+   *
+   * @param input The input tensor, of rank 4 or rank 3, of shape
+   *     `[batch, height, width, inChannels]`. If rank 3, batch of 1 is
+   * assumed.
+   * @param depthwiseFilter The depthwise filter tensor, rank 4, of shape
+   *     `[filterHeight, filterWidth, inChannels, channelMultiplier]`. This is
+   *     the filter used in the first step.
+   * @param pointwiseFilter The pointwise filter tensor, rank 4, of shape
+   *     `[1, 1, inChannels * channelMultiplier, outChannels]`. This is
+   *     the filter used in the second step.
+   * @param strides The strides of the convolution: `[strideHeight,
+   * strideWidth]`. If strides is a single number, then `strideHeight ==
+   * strideWidth`.
+   * @param pad The type of padding algorithm.
+   *   - `same` and stride 1: output will be of same size as input,
+   *       regardless of filter size.
+   *   - `valid`: output will be smaller than input if filter is larger
+   *       than 1x1.
+   *   - For more info, see this guide:
+   *     [https://www.tensorflow.org/api_guides/python/nn#Convolution](
+   *          https://www.tensorflow.org/api_guides/python/nn#Convolution)
+   * @param dilations The dilation rates: `[dilationHeight, dilationWidth]`
+   *     in which we sample input values across the height and width dimensions
+   *     in atrous convolution. Defaults to `[1, 1]`. If `rate` is a single
+   *     number, then `dilationHeight == dilationWidth`. If it is greater than
+   *     1, then all values of `strides` must be 1.
+   * @param dataFormat: An optional string from: "NHWC", "NCHW". Defaults to
+   *     "NHWC". Specify the data format of the input and output data. With the
+   *     default format "NHWC", the data is stored in the order of: [batch,
+   *     height, width, channels]. Only "NHWC" is currently supported.
+   */
+  @doc({heading: 'Operations', subheading: 'Convolution'})
+  @operation
+  static separableConv2d<T extends Tensor3D|Tensor4D>(
+      input: T, depthwiseFilter: Tensor4D, pointwiseFilter: Tensor4D,
+      strides: [number, number]|number, pad: 'valid'|'same',
+      dilation: [number, number]|number = [1, 1],
+      dataFormat: 'NHWC'|'NCHW' = 'NHWC'): T {
+    let input4D = input as Tensor4D;
+    let reshapedTo4D = false;
+    if (input.rank === 3) {
+      reshapedTo4D = true;
+      input4D = input.as4D(1, input.shape[0], input.shape[1], input.shape[2]);
+    }
+
+    if (dataFormat === 'NCHW') {
+      throw new Error(
+          'separableConv2d currently does not support dataFormat NCHW; only ' +
+          'NHWC is supported');
+    }
+
+    util.assert(
+        input4D.rank === 4,
+        `Error in separableConv2d: input must be rank 4, but got ` +
+            `rank ${input4D.rank}.`);
+    util.assert(
+        depthwiseFilter.rank === 4,
+        `Error in separableConv2d: depthwise filter must be rank 4, but got ` +
+            `rank ${depthwiseFilter.rank}.`);
+    util.assert(
+        pointwiseFilter.rank === 4,
+        `Error in separableConv2d: pointwise filter must be rank 4, but got ` +
+            `rank ${depthwiseFilter.rank}.`);
+    util.assert(
+        pointwiseFilter.shape[0] === 1,
+        `Error in separableConv2d: the first dimension of pointwise filter ` +
+            ` must be 1, but got ${pointwiseFilter.shape[0]}.`);
+    util.assert(
+        pointwiseFilter.shape[1] === 1,
+        `Error in separableConv2d: the second dimension of pointwise filter ` +
+            ` must be 1, but got ${pointwiseFilter.shape[1]}.`);
+
+    const inChannels = depthwiseFilter.shape[2];
+    const channelMultiplier = depthwiseFilter.shape[3];
+    util.assert(
+        pointwiseFilter.shape[2] === inChannels * channelMultiplier,
+        `Error in separableConv2d: the third dimension of pointwise filter ` +
+            `must be ${inChannels * channelMultiplier}, ` +
+            `but got ${pointwiseFilter.shape[2]}.`);
+
+    const depthwise = ConvOps.depthwiseConv2d(
+        input4D, depthwiseFilter, strides, pad, dataFormat, dilation);
+    const pointwiseStride = 1;
+    const res = ConvOps.conv2d(
+        depthwise, pointwiseFilter, pointwiseStride, 'valid', dataFormat);
+    if (reshapedTo4D) {
+      return res.as3D(res.shape[1], res.shape[2], res.shape[3]) as T;
+    }
+    return res as T;
+  }
 }
 
 function parseTupleParam(param: number|[number, number]): [number, number] {
