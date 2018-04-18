ad5ef1ec1f8c69a6eb5295b3bf255e3ea2fbede8
Ryan Dahl <ry@tinyclouds.org>
2018-04-18 16:25:57 -0400

    Add slice ergonomics. (#964)

--- src/ops/slice_test.ts
+++ src/ops/slice_test.ts
@@ -17,7 +17,7 @@
 
 import * as tf from '../index';
 // tslint:disable-next-line:max-line-length
-import {ALL_ENVS, describeWithFlags, expectArraysClose, expectNumbersClose} from '../test_util';
+import {ALL_ENVS, CPU_ENVS, describeWithFlags, expectArraysClose, expectNumbersClose} from '../test_util';
 import {Rank} from '../types';
 
 describeWithFlags('slice1d', ALL_ENVS, () => {
@@ -120,6 +120,43 @@ describeWithFlags('slice3d', ALL_ENVS, () => {
 
     expect(result.shape).toEqual([2, 1, 1]);
     expectArraysClose(result, [4, 8]);
+  });
+});
+
+describeWithFlags('slice ergonomics', CPU_ENVS, () => {
+  it('slices 2x2x2 array into 2x1x1 no size', () => {
+    const a = tf.tensor3d([1, 2, 3, 4, 5, 6, 7, 8], [2, 2, 2]);
+    const result = a.slice([0, 1, 1]);
+    expect(result.shape).toEqual([2, 1, 1]);
+    expectArraysClose(result, [4, 8]);
+  });
+
+  it('slices 2x2x2 array into 1x2x2 with scalar begin no size', () => {
+    const a = tf.tensor3d([1, 2, 3, 4, 5, 6, 7, 8], [2, 2, 2]);
+    const result = a.slice(1);
+    expect(result.shape).toEqual([1, 2, 2]);
+    expectArraysClose(result, [5, 6, 7, 8]);
+  });
+
+  it('slices 2x2x2 array using 2d size and 2d size', () => {
+    const a = tf.tensor3d([1, 2, 3, 4, 5, 6, 7, 8], [2, 2, 2]);
+    const result = a.slice([0, 1]);
+    expect(result.shape).toEqual([2, 1, 2]);
+    expectArraysClose(result, [3, 4, 7, 8]);
+  });
+
+  it('slices 2x2x2 array using negative size', () => {
+    const a = tf.tensor3d([1, 2, 3, 4, 5, 6, 7, 8], [2, 2, 2]);
+    const result = a.slice([0, 1], [-1, 1]);
+    expect(result.shape).toEqual([2, 1, 2]);
+    expectArraysClose(result, [3, 4, 7, 8]);
+  });
+
+  it('slices 2x2x2 array using 1d size', () => {
+    const a = tf.tensor3d([1, 2, 3, 4, 5, 6, 7, 8], [2, 2, 2]);
+    const result = a.slice(0, 1);
+    expect(result.shape).toEqual([1, 2, 2]);
+    expectArraysClose(result, [1, 2, 3, 4]);
   });
 });
 
