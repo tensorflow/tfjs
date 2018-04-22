2de8cf4040a5def33a7d0ef74b0d782601f7f455
Nick Kreeger <nick.kreeger@gmail.com>
2018-04-22 13:57:27 -0700

    Fix linux build and add a TODO for darwin builds. (#71)

--- scripts/download-libtensorflow.sh
+++ scripts/download-libtensorflow.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
+# TODO(kreeger): Use TF 1.8 builds.
 CPU_DARWIN="http://ci.tensorflow.org/view/Nightly/job/nightly-libtensorflow/TYPE=mac-slave/lastSuccessfulBuild/artifact/lib_package/libtensorflow-cpu-darwin-x86_64.tar.gz"
-GPU_LINUX="http://ci.tensorflow.org/view/Nightly/job/nightly-libtensorflow/TYPE=gpu-linux/lastSuccessfulBuild/artifact/lib_package/libtensorflow-gpu-linux-x86_64.tar.gz"
-# TODO(kreeger): Use nightly links when Jenkins TF uses an updated bazel.
+
 # Build new package:
-# `bazel build --config opt //tensorflow/tools/lib_package:libtensorflow`
-CPU_LINUX="https://storage.googleapis.com/tf-buiilds/libtensorflow.tar.gz"
-# CPU_LINUX="http://ci.tensorflow.org/view/Nightly/job/nightly-libtensorflow/TYPE=cpu-slave/lastSuccessfulBuild/artifact/lib_package/libtensorflow-cpu-linux-x86_64.tar.gz"
+# `bazel build //tensorflow/tools/lib_package:libtensorflow`
+CPU_LINUX="https://storage.googleapis.com/tf-buiilds/libtensorflow_r1_8.tar.gz"
 
 target=""
 platform=$1
 if [ "$platform" = "linux-cpu" ]
 then
   target=$CPU_LINUX
-elif [ "$platform" = "linux-gpu" ]
-then
-  target=$GPU_LINUX
 elif [ "$platform" = "darwin" ]
 then
   target=$CPU_DARWIN
