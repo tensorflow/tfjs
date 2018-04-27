e57e5ad04dda7cefedd1e3c04ba3e34ff8529b44
Nikhil Thorat <nsthorat@google.com>
2018-04-27 09:39:15 -0400

    Add release notes script. (#228)

--- DEVELOPMENT.md
+++ DEVELOPMENT.md
@@ -12,3 +12,67 @@ To develop:
 - `tfjs-core`, see [this doc](https://github.com/tensorflow/tfjs-core/blob/master/DEVELOPMENT.md).
 - `tfjs-layers` see [this doc](https://github.com/tensorflow/tfjs-layers/blob/master/DEVELOPMENT.md).
 - `tfjs` with locally modified `tfjs-layers` (or `tfjs-core`) see [this section](https://github.com/tensorflow/tfjs-layers/blob/master/DEVELOPMENT.md#changing-tensorflowtfjs-layers-and-testing-tensorflowtfjs) in [tfjs-layers/DEVELOPMENT.md](https://github.com/tensorflow/tfjs-layers) repo.
+
+## For repository owners: commit style guide
+
+When merging commits into master, it is important to follow a few conventions
+so that we can automatically generate release notes and have a uniform commit
+history.
+
+1. When you squash and merge, the default commit body will be all of the
+commits on your development branch (not the PR description). These are usually
+not very useful, so you should remove them, or replace them with the PR
+description.
+
+2. Release notes are automatically generated from commits. We have introduced a
+few tags which help sort commits into categories for release notes:
+
+- FEATURE (when new functionality / API is added)
+- BREAKING (when there is API breakage)
+- BUG (bug fixes)
+- PERF (performance improvements)
+- DEV (development flow changes)
+- DOC (documentation changes)
+- SECURITY (security changes)
+
+If no tag is specified, it will be put under a "MISC" tag.
+
+You can tag a commit with these tags by putting the tag at the beginning of a
+new line. Any text after the tag on that line will show up in the release notes
+next to the subject.
+
+A typical commit may look something like:
+
+```
+Subject: Add tf.toPixels. (#900)
+Body:
+FEATURE
+
+tf.toPixels is the inverse of tf.fromPixels, writing a tensor to a canvas.
+
+```
+
+This will show up under "Features" as:
+- Add tf.toPixels. (#900). Thanks, @externalcontributor.
+
+
+You can also use multiple tags for the same commit if you want it to show up in
+two sections. You can add clarifying text on the line of the tags.
+
+You can add clarifying messages on the line of the tag as well.
+
+For example:
+
+```
+Subject: Improvements to matMul. (#900)
+Body:
+
+FEATURE Add transpose bits to matmul.
+PERFORMANCE Improve matMul CPU speed by 100%.
+```
+
+This will show up under "Features" as:
+- Add transpose bits to matmul (Improvements to matMul.) (#900). Thanks, @externalcontributor.
+
+This will also show up under "Performance" as:
+- Improve matMul CPU speed by 100%. (Improvements to matMul.) (#900). Thanks, @externalcontributor.
---
+++ scripts/release_notes/util.ts
@@ -0,0 +1,174 @@
+#!/usr/bin/env node
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as shell from 'shelljs';
+
+/**
+ * A wrapper around shell.exec for readability.
+ * @param cmd The bash command to execute.
+ * @returns stdout returned by the executed bash script.
+ */
+export function $(cmd) {
+  const result = shell.exec(cmd, {silent: true});
+  if (result.code > 0) {
+    console.log('$', cmd);
+    console.log(result.stderr);
+    process.exit(1);
+  }
+  return result.stdout.trim();
+}
+
+export interface Repo {
+  name: string;
+  identifier: string;
+}
+
+export interface RepoCommits {
+  repo: Repo;
+  startVersion: string;
+  endVersion: string;
+  startCommit: string;
+  commits: Commit[];
+}
+
+export interface Commit {
+  subject: string;
+  body: string;
+  authorEmail: string;
+  sha: string;
+}
+
+export interface OctokitGetCommit {
+  repos: {
+    getCommit:
+        (config: {owner: string, repo: string, sha: string}) => {
+          data: {author: {login: string}}
+        }
+  };
+}
+
+interface SectionTag {
+  section: string;
+  tag: string;
+}
+
+const SECTION_TAGS: SectionTag[] = [
+  {section: 'Features', tag: 'FEATURE'},
+  {section: 'Breaking changes', tag: 'BREAKING'},
+  {section: 'Bug fixes', tag: 'BUG'}, {section: 'Performance', tag: 'PERF'},
+  {section: 'Development', tag: 'DEV'}, {section: 'Documentation', tag: 'DOC'},
+  {section: 'Security', tag: 'SECURITY'}, {section: 'Misc', tag: 'MISC'}
+];
+
+/**
+ * Assembles the release note drafts from a set of commits.
+ *
+ * @param octokit An authenticated octokit object (to make github API requests).
+ * It only needs to satisfy the OctokitGetCommit interface which gets commit
+ * metadata.
+ * @param repoCommits An object representing the metadata for commits to
+ * assmemble into release notes.
+ * @returns The release notes markdown draft as a string.
+ */
+export async function getReleaseNotesDraft(
+    octokit: OctokitGetCommit, repoCommits: RepoCommits[]): Promise<string> {
+  const repoNotes = [];
+  for (let i = 0; i < repoCommits.length; i++) {
+    const repoCommit = repoCommits[i];
+
+    const getUsernameForCommit = async sha => {
+      const result = await octokit.repos.getCommit(
+          {owner: 'tensorflow', repo: repoCommit.repo.identifier, sha});
+      return result.data.author.login;
+    };
+
+    const tagEntries: {[tag: string]: string[]} = {};
+    SECTION_TAGS.forEach(({tag}) => tagEntries[tag] = []);
+
+    for (let j = 0; j < repoCommit.commits.length; j++) {
+      const commit = repoCommit.commits[j];
+
+      const tagsFound: Array<{tag: string, tagMessage: string}> = [];
+      const bodyLines = commit.body.split('\n').map(line => line.trim());
+      // Get tags for the body by finding lines that start with tags. Do this
+      // without a regex for readability.
+      SECTION_TAGS.forEach(({tag}) => {
+        bodyLines.forEach(line => {
+          // Split by word boundaries, and make sure the first word is the tag.
+          const split = line.split(/\b/);
+          if (split[0] === tag) {
+            const tagMessage = line.substring(tag.length).trim();
+            tagsFound.push({tag, tagMessage});
+          }
+        });
+      });
+      // If no explicit tags, put this under misc.
+      if (tagsFound.length === 0) {
+        tagsFound.push({tag: 'MISC', tagMessage: ''});
+      }
+
+      const isExternalContributor = !commit.authorEmail.endsWith('@google.com');
+
+      const pullRequestRegexp = /\(#([0-9]+)\)/;
+      const pullRequestNumber = commit.subject.match(pullRequestRegexp)[1];
+      const subject = commit.subject.replace(pullRequestRegexp, '').trim();
+
+      for (let k = 0; k < tagsFound.length; k++) {
+        const {tag, tagMessage} = tagsFound[k];
+
+        // When the tag has no message, use the subject.
+        let entry;
+        if (tagMessage === '') {
+          entry = '- ' + subject;
+        } else {
+          entry = '- ' + tagMessage + ' [' + subject + ']';
+        }
+
+        // Attach the link to the pull request.
+        entry = entry.trim() +
+            ` ([#${pullRequestNumber}](https://github.com/tensorflow/` +
+            `${repoCommit.repo.identifier}/pull/${pullRequestNumber})).`;
+
+        // For external contributors, we need to query github because git does
+        // not contain github username metadatea.
+        if (isExternalContributor) {
+          const username = await getUsernameForCommit(commit.sha);
+          entry += (!entry.endsWith('.') ? '.' : '') + ` Thanks, @${username}.`;
+        }
+
+        tagEntries[tag].push(entry);
+      }
+    }
+
+    const repoLines = [];
+    SECTION_TAGS.forEach(({tag, section}) => {
+      if (tagEntries[tag].length !== 0) {
+        const sectionNotes = tagEntries[tag].join('\n');
+        repoLines.push(`### ${section}`);
+        repoLines.push(sectionNotes);
+      }
+    });
+
+    const repoSection = `## ${repoCommit.repo.name} ` +
+        `(${repoCommit.startVersion} ==> ` +
+        `${repoCommit.endVersion})\n\n` + repoLines.join('\n');
+    repoNotes.push(repoSection);
+  }
+
+  return repoNotes.join('\n\n');
+}
