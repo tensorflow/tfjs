316d5daf50d3b4532213515397179f69eb4b6e61
Nicola Pezzotti <nicola.pezzotti@gmail.com>
2018-05-03 16:59:38 +0200

    Fix the import/export of gpgpu_util. (#1009)
    BUG

--- src/webgl.ts
+++ src/webgl.ts
@@ -16,7 +16,7 @@
  */
 
 import * as webgl_util from './kernels/webgl/webgl_util';
-import * as gpgpu_util from './kernels/webgl/webgl_util';
+import * as gpgpu_util from './kernels/webgl/gpgpu_util';
 
 export {MathBackendWebGL, WebGLTimingInfo} from './kernels/backend_webgl';
 export {GPGPUContext} from './kernels/webgl/gpgpu_context';
