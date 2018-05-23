cf85ffadf9cf119aac10c5a449ce710f5446e294
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-05-23 14:20:30 -0700

    added converter to the union package (#325)
    * added converter to the union package
    
    * add test for adding converter

--- src/index.ts
+++ src/index.ts
@@ -17,14 +17,17 @@
 
 export * from '@tensorflow/tfjs-core';
 export * from '@tensorflow/tfjs-layers';
+export * from '@tensorflow/tfjs-converter';
 
 // Import versions of all sub-packages.
 import {version_core} from '@tensorflow/tfjs-core';
 import {version_layers} from '@tensorflow/tfjs-layers';
+import {version_converter} from '@tensorflow/tfjs-converter';
 import {version as version_union} from './version';
 
 export const version = {
   'tfjs-core': version_core,
   'tfjs-layers': version_layers,
+  'tfjs-converter': version_converter,
   'tfjs': version_union
 };
