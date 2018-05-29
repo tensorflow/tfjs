748ef0986fb14108032334f3072556d2ed626cfd
David Soergel <dev@davidsoergel.com>
2018-05-29 16:57:09 -0400

    Mention node bindings in setBackend docstring. (#1065)
    DOC

--- src/environment.ts
+++ src/environment.ts
@@ -220,7 +220,8 @@ export class Environment {
    * associated with it.  A new backend is initialized, even if it is of the
    * same type as the previous one.
    *
-   * @param backendType The backend type. Currently supports `'webgl'|'cpu'`.
+   * @param backendType The backend type. Currently supports `'webgl'|'cpu'` in
+   *     the browser, and `'tensorflow'` under node.js (requires tfjs-node).
    * @param safeMode Defaults to false. In safe mode, you are forced to
    *     construct tensors and call math operations inside a `tidy()` which
    *     will automatically clean up intermediate tensors.
