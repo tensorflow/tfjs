4ebb179fb4a148074eab85b2c828c9c3d29c378c
Ali Torki <ali_4286@live.com>
2018-06-17 16:37:04 +0430

    remove: resolve module in the rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -18,7 +18,6 @@ import babel from 'rollup-plugin-babel';
 import node from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
 import json from 'rollup-plugin-json';
 import uglify from 'rollup-plugin-uglify';
 
