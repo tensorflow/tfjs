86ec27312cdcdff33a905aa3b7bbe16888a7e781
Shanqing Cai <cais@google.com>
2018-06-28 18:15:42 -0400

    Add tf.linalg.qr() (#1128)
    * Add tf.linag.qr()

--- src/ops/linalg_ops_test.ts
+++ src/ops/linalg_ops_test.ts
@@ -20,6 +20,8 @@ import {describeWithFlags} from '../jasmine_util';
 import {Tensor1D, Tensor2D} from '../tensor';
 import {ALL_ENVS, expectArraysClose, WEBGL_ENVS} from '../test_util';
 
+import {scalar, tensor1d, tensor2d, tensor3d, tensor4d} from './ops';
+
 describeWithFlags('gramSchmidt-tiny', ALL_ENVS, () => {
   it('2x2, Array of Tensor1D', () => {
     const xs: Tensor1D[] = [tf.randomNormal([2]), tf.randomNormal([2])];
@@ -83,5 +85,153 @@ describeWithFlags('gramSchmidt-non-tiny', WEBGL_ENVS, () => {
     const xs = tf.randomUniform([32, 512]) as Tensor2D;
     const y = tf.linalg.gramSchmidt(xs) as Tensor2D;
     expectArraysClose(y.matMul(y.transpose()), tf.eye(32));
+  });
+});
+
+describeWithFlags('qr', ALL_ENVS, () => {
+  it('1x1', () => {
+    const x = tensor2d([[10]], [1, 1]);
+    const [q, r] = tf.linalg.qr(x);
+    expectArraysClose(q, tensor2d([[-1]], [1, 1]));
+    expectArraysClose(r, tensor2d([[-10]], [1, 1]));
+  });
+
+  it('2x2', () => {
+    const x = tensor2d([[1, 3], [-2, -4]], [2, 2]);
+    const [q, r] = tf.linalg.qr(x);
+    expectArraysClose(
+        q, tensor2d([[-0.4472, -0.8944], [0.8944, -0.4472]], [2, 2]));
+    expectArraysClose(r, tensor2d([[-2.2361, -4.9193], [0, -0.8944]], [2, 2]));
+  });
+
+  it('2x2x2', () => {
+    const x = tensor3d([[[-1, -3], [2, 4]], [[1, 3], [-2, -4]]], [2, 2, 2]);
+    const [q, r] = tf.linalg.qr(x);
+    expectArraysClose(
+        q,
+        tensor3d(
+            [
+              [[-0.4472, -0.8944], [0.8944, -0.4472]],
+              [[-0.4472, -0.8944], [0.8944, -0.4472]]
+            ],
+            [2, 2, 2]));
+    expectArraysClose(
+        r,
+        tensor3d(
+            [
+              [[2.2361, 4.9193], [0, 0.8944]],
+              [[-2.2361, -4.9193], [0, -0.8944]]
+            ],
+            [2, 2, 2]));
+  });
+
+  it('2x1x2x2', () => {
+    const x =
+        tensor4d([[[[-1, -3], [2, 4]]], [[[1, 3], [-2, -4]]]], [2, 1, 2, 2]);
+    const [q, r] = tf.linalg.qr(x);
+    expectArraysClose(
+        q,
+        tensor4d(
+            [
+              [[[-0.4472, -0.8944], [0.8944, -0.4472]]],
+              [[[-0.4472, -0.8944], [0.8944, -0.4472]]],
+            ],
+            [2, 1, 2, 2]));
+    expectArraysClose(
+        r,
+        tensor4d(
+            [
+              [[[2.2361, 4.9193], [0, 0.8944]]],
+              [[[-2.2361, -4.9193], [0, -0.8944]]]
+            ],
+            [2, 1, 2, 2]));
+  });
+
+  it('3x3', () => {
+    const x = tensor2d([[1, 3, 2], [-2, 0, 7], [8, -9, 4]], [3, 3]);
+    const [q, r] = tf.linalg.qr(x);
+    expectArraysClose(
+        q,
+        tensor2d(
+            [
+              [-0.1204, 0.8729, 0.4729], [0.2408, -0.4364, 0.8669],
+              [-0.9631, -0.2182, 0.1576]
+            ],
+            [3, 3]));
+    expectArraysClose(
+        r,
+        tensor2d(
+            [[-8.3066, 8.3066, -2.4077], [0, 4.5826, -2.1822], [0, 0, 7.6447]],
+            [3, 3]));
+  });
+
+  it('3x2, fullMatrices = default false', () => {
+    const x = tensor2d([[1, 2], [3, -3], [-2, 1]], [3, 2]);
+    const [q, r] = tf.linalg.qr(x);
+    expectArraysClose(
+        q,
+        tensor2d(
+            [[-0.2673, 0.9221], [-0.8018, -0.3738], [0.5345, -0.0997]],
+            [3, 2]));
+    expectArraysClose(r, tensor2d([[-3.7417, 2.4054], [0, 2.8661]], [2, 2]));
+  });
+
+  it('3x2, fullMatrices = true', () => {
+    const x = tensor2d([[1, 2], [3, -3], [-2, 1]], [3, 2]);
+    const [q, r] = tf.linalg.qr(x, true);
+    expectArraysClose(
+        q,
+        tensor2d(
+            [
+              [-0.2673, 0.9221, 0.2798], [-0.8018, -0.3738, 0.4663],
+              [0.5345, -0.0997, 0.8393]
+            ],
+            [3, 3]));
+    expectArraysClose(
+        r, tensor2d([[-3.7417, 2.4054], [0, 2.8661], [0, 0]], [3, 2]));
+  });
+
+  it('2x3, fullMatrices = default false', () => {
+    const x = tensor2d([[1, 2, 3], [-3, -2, 1]], [2, 3]);
+    const [q, r] = tf.linalg.qr(x);
+    expectArraysClose(
+        q,
+        tensor2d([[-0.3162278, -0.9486833], [0.9486833, -0.31622773]], [2, 2]));
+    expectArraysClose(
+        r,
+        tensor2d(
+            [[-3.162, -2.5298, -2.3842e-07], [0, -1.2649, -3.162]], [2, 3]),
+    );
+  });
+
+  it('2x3, fullMatrices = true', () => {
+    const x = tensor2d([[1, 2, 3], [-3, -2, 1]], [2, 3]);
+    const [q, r] = tf.linalg.qr(x, true);
+    expectArraysClose(
+        q,
+        tensor2d([[-0.3162278, -0.9486833], [0.9486833, -0.31622773]], [2, 2]));
+    expectArraysClose(
+        r,
+        tensor2d(
+            [[-3.162, -2.5298, -2.3842e-07], [0, -1.2649, -3.162]], [2, 3]),
+    );
+  });
+
+  it('Does not leak memory', () => {
+    const x = tensor2d([[1, 3], [-2, -4]], [2, 2]);
+    // The first call to qr creates and keeps internal singleton tensors.
+    // Subsequent calls should always create exactly two tensors.
+    tf.linalg.qr(x);
+    // Count before real call.
+    const numTensors = tf.memory().numTensors;
+    tf.linalg.qr(x);
+    expect(tf.memory().numTensors).toEqual(numTensors + 2);
+  });
+
+  it('Insuffient input tensor rank leads to error', () => {
+    const x1 = scalar(12);
+    expect(() => tf.linalg.qr(x1)).toThrowError(/rank >= 2.*got rank 0/);
+    const x2 = tensor1d([12]);
+    expect(() => tf.linalg.qr(x2)).toThrowError(/rank >= 2.*got rank 1/);
   });
 });
