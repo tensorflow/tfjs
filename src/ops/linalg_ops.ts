7e86ecc5a166c1258f5a61b8ca7474352a3d9473
Shanqing Cai <cais@google.com>
2018-06-28 18:15:42 -0400

    Add tf.linalg.qr() (#1128)
    * Add tf.linag.qr()

--- src/ops/linalg_ops.ts
+++ src/ops/linalg_ops.ts
@@ -21,12 +21,15 @@
 
 import {doc} from '../doc';
 import {ENV} from '../environment';
-import {Tensor1D, Tensor2D} from '../tensor';
+import {dispose} from '../globals';
+import {Tensor, Tensor1D, Tensor2D} from '../tensor';
 import {assert} from '../util';
+
 import {ArrayOps} from './array_ops';
 import {NormOps} from './norm';
 import {operation} from './operation';
 import {ReductionOps} from './reduction_ops';
+import {TensorOps} from './tensor_ops';
 
 export class LinalgOps {
   /**
@@ -92,4 +95,141 @@ export class LinalgOps {
       return ys;
     }
   }
+
+  /**
+   * Compute QR decomposition of m-by-n matrix using Householder transformation.
+   *
+   * Implementation based on
+   *   [http://www.cs.cornell.edu/~bindel/class/cs6210-f09/lec18.pdf]
+   * (http://www.cs.cornell.edu/~bindel/class/cs6210-f09/lec18.pdf)
+   *
+   * @param x The `Tensor` to be QR-decomposed. Must have rank >= 2. Suppose
+   *   it has the shape `[..., M, N]`.
+   * @param fullMatrices An optional boolean parameter. Defaults to `false`.
+   *   If `true`, compute full-sized `Q`. If `false` (the default),
+   *   compute only the leading N columns of `Q` and `R`.
+   * @return An `Array` of two `Tensor`s: `[Q, R]`. `Q` is a unitary matrix,
+   *   i.e., its columns all have unit norm and are mutually orthogonal.
+   *   If `M >= N`,
+   *     If `fullMatrices` is `false` (default),
+   *       - `Q` has a shape of `[..., M, N]`,
+   *       - `R` has a shape of `[..., N, N]`.
+   *     If `fullMatrices` is `true` (default),
+   *       - `Q` has a shape of `[..., M, M]`,
+   *       - `R` has a shape of `[..., M, N]`.
+   *   If `M < N`,
+   *     - `Q` has a shape of `[..., M, M]`,
+   *     - `R` has a shape of `[..., M, N]`.
+   * @throws If the rank of `x` is less than 2.
+   */
+  @doc({heading: 'Operations', subheading: 'Linear Algebra'})
+  @operation
+  static qr(x: Tensor, fullMatrices = false): [Tensor, Tensor] {
+    if (x.rank < 2) {
+      throw new Error(
+          `qr() requires input tensor to have a rank >= 2, but got rank ${
+              x.rank}`);
+    } else if (x.rank === 2) {
+      return qr2d(x as Tensor2D, fullMatrices);
+    } else {
+      // Rank > 2.
+      // TODO(cais): Below we split the input into individual 2D tensors,
+      //   perform QR decomposition on them and then stack the results back
+      //   together. We should explore whether this can be parallelized.
+      const outerDimsProd = x.shape.slice(0, x.shape.length - 2)
+                                .reduce((value, prev) => value * prev);
+      const x2ds = ArrayOps.unstack(
+          x.reshape([
+            outerDimsProd, x.shape[x.shape.length - 2],
+            x.shape[x.shape.length - 1]
+          ]),
+          0);
+      const q2ds: Tensor2D[] = [];
+      const r2ds: Tensor2D[] = [];
+      x2ds.forEach(x2d => {
+        const [q2d, r2d] = qr2d(x2d as Tensor2D, fullMatrices);
+        q2ds.push(q2d);
+        r2ds.push(r2d);
+      });
+      const q = ArrayOps.stack(q2ds, 0).reshape(x.shape);
+      const r = ArrayOps.stack(r2ds, 0).reshape(x.shape);
+      return [q, r];
+    }
+  }
+}
+
+function qr2d(x: Tensor2D, fullMatrices = false): [Tensor2D, Tensor2D] {
+  return ENV.engine.tidy(() => {
+    if (x.shape.length !== 2) {
+      throw new Error(
+          `qr2d() requires a 2D Tensor, but got a ${x.shape.length}D Tensor.`);
+    }
+
+    const m = x.shape[0];
+    const n = x.shape[1];
+
+    let q = ArrayOps.eye(m) as Tensor2D;  // Orthogonal transform so far.
+    let r = x.clone();                    // Transformed matrix so far.
+
+    const one2D = TensorOps.tensor2d([[1]], [1, 1]);
+    let w: Tensor2D = one2D.clone();
+
+    const iters = m >= n ? n : m;
+    for (let j = 0; j < iters; ++j) {
+      // This tidy within the for-loop ensures we clean up temporary
+      // tensors as soon as they are no longer needed.
+      const rTemp = r;
+      const wTemp = w;
+      const qTemp = q;
+      [w, r, q] = ENV.engine.tidy((): [Tensor2D, Tensor2D, Tensor2D] => {
+        // Find H = I - tau * w * w', to put zeros below R(j, j).
+        const rjEnd1 = r.slice([j, j], [m - j, 1]);
+        const normX = rjEnd1.norm();
+        const rjj = r.slice([j, j], [1, 1]);
+        const s = rjj.sign().neg() as Tensor2D;
+        const u1 = rjj.sub(s.mul(normX)) as Tensor2D;
+        const wPre = rjEnd1.div(u1);
+        if (wPre.shape[0] === 1) {
+          w = one2D.clone();
+        } else {
+          w = one2D.concat(
+                  wPre.slice([1, 0], [wPre.shape[0] - 1, wPre.shape[1]]), 0) as
+              Tensor2D;
+        }
+        const tau = s.matMul(u1).div(normX).neg() as Tensor2D;
+
+        // -- R := HR, Q := QH.
+        const rjEndAll = r.slice([j, 0], [m - j, n]);
+        const tauTimesW = tau.mul(w) as Tensor2D;
+        if (j === 0) {
+          r = rjEndAll.sub(tauTimesW.matMul(w.transpose().matMul(rjEndAll)));
+        } else {
+          r = r.slice([0, 0], [j, n])
+                  .concat(
+                      rjEndAll.sub(
+                          tauTimesW.matMul(w.transpose().matMul(rjEndAll))),
+                      0) as Tensor2D;
+        }
+        const qAllJEnd = q.slice([0, j], [m, q.shape[1] - j]);
+        if (j === 0) {
+          q = qAllJEnd.sub(qAllJEnd.matMul(w).matMul(tauTimesW.transpose()));
+        } else {
+          q = q.slice([0, 0], [m, j])
+                  .concat(
+                      qAllJEnd.sub(
+                          qAllJEnd.matMul(w).matMul(tauTimesW.transpose())),
+                      1) as Tensor2D;
+        }
+        return [w, r, q];
+      });
+      dispose([rTemp, wTemp, qTemp]);
+    }
+
+    if (!fullMatrices && m > n) {
+      q = q.slice([0, 0], [m, n]);
+      r = r.slice([0, 0], [n, n]);
+    }
+
+    return [q, r];
+  }) as [Tensor2D, Tensor2D];
 }
