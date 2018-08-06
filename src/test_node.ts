3c203ddb1297b8e67df93b15b744aba7ea0c7047
Tyler Hill <5706227+reighlan@users.noreply.github.com>
2018-08-06 10:09:56 -0700

    add node testing (#10)

---
+++ src/test_node.ts
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import {setTestEnvs} from '@tensorflow/tfjs-core/dist/jasmine_util';
+import {MathBackendCPU} from '@tensorflow/tfjs-core/dist/kernels/backend_cpu';
+
+// tslint:disable-next-line:no-require-imports
+const jasmine = require('jasmine');
+
+process.on('unhandledRejection', e => { throw e; });
+
+setTestEnvs(
+    [{name: 'node', factory: () => new MathBackendCPU(), features: {}}]);
+
+const runner = new jasmine();
+runner.loadConfig({spec_files: ['src/**/*_test.ts'], random: false});
+runner.execute();
