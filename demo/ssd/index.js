8e220a93aa79294dba363af2fe1c0634c4b5dc32
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-08-17 10:10:21 -0700

    add coco ssd demo (#199)
    * add coco ssd demo
    
    * updated license header
    
    * updated the model name on the page
    
    * removed unused constants
    
    * refactor the graph async execution to parallize the ops

---
+++ demo/ssd/index.js
@@ -0,0 +1,66 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as tf from '@tensorflow/tfjs';
+import imageURL from './image1.jpg';
+
+const image = document.getElementById('image');
+  image.onload =
+    async function run() {
+      const GOOGLE_CLOUD_STORAGE_DIR =
+      'https://storage.googleapis.com/tfjs-models/savedmodel/';
+      const MODEL_URL = GOOGLE_CLOUD_STORAGE_DIR + 'coco-ssd-mobilenet_v1/tensorflowjs_model.pb';
+      const WEIGHTS_URL = GOOGLE_CLOUD_STORAGE_DIR + 'coco-ssd-mobilenet_v1/weights_manifest.json';
+      tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL).then(async model => {
+        const pixels = tf.fromPixels(image);
+        console.log('model loaded');
+        console.time('predict1');
+        const res1 = await model.executeAsync(pixels.reshape([1, ...pixels.shape]));
+        res1.map(t => t.dataSync());
+        console.timeEnd('predict1');
+        console.time('predict2');
+        const res2 = await model.executeAsync(pixels.reshape([1, ...pixels.shape]));
+        const count = res2[3].dataSync()[0];
+        const boxes = res2[0].dataSync();
+        const scores = res2[1].dataSync();
+        const classes = res2[2].dataSync();
+        console.timeEnd('predict2');
+
+
+        const c = document.getElementById("canvas");
+        const context = c.getContext("2d");
+        context.drawImage(image, 0, 0);
+        context.font = "10px Arial";
+
+        console.log('number of detections: ', count);
+        for (let i = 0; i < count; i++) {
+          const min_y = boxes[i * 4] * 399;
+          const min_x = boxes[i * 4 + 1] * 600;
+          const max_y = boxes[i * 4 + 2] * 399;
+          const max_x = boxes[i * 4 + 3] * 600;
+
+          context.beginPath();
+          context.rect(min_x, min_y, max_x - min_x, max_y - min_y);
+          context.lineWidth = 1;
+          context.strokeStyle = 'black';
+          context.stroke();
+          context.fillText(scores[i].toFixed(3) + ' ' + (classes[i] === 1 ? 'person'
+            : 'kite'), min_x, min_y - 5);
+        }
+      });
+    };
+  image.src = imageURL;
