b329412553601b177e94baf82c98fd05e8c5d020
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-08-21 17:39:18 -0700

    Coco ssd demo (#200)
    * add coco ssd demo
    
    * updated license header
    
    * updated the model name on the page
    
    * removed unused constants
    
    * refactor the graph async execution to parallize the ops
    
    * added run and toggle buttons to allow investigating dynamic tensor shape effects on WebGL shader caching performance
    
    * fixed merge issue
    
    * added coco class label array
    
    * remove logs

--- demo/ssd/index.js
+++ demo/ssd/index.js
@@ -16,51 +16,67 @@
  */
 
 import * as tf from '@tensorflow/tfjs';
+import {CLASSES} from './classes';
 import imageURL from './image1.jpg';
+import image2URL from './image2.jpg';
+
+const GOOGLE_CLOUD_STORAGE_DIR =
+    'https://storage.googleapis.com/tfjs-models/savedmodel/';
+const MODEL_URL =
+    GOOGLE_CLOUD_STORAGE_DIR + 'coco-ssd-mobilenet_v1/tensorflowjs_model.pb';
+const WEIGHTS_URL =
+    GOOGLE_CLOUD_STORAGE_DIR + 'coco-ssd-mobilenet_v1/weights_manifest.json';
+
+let modelPromise;
+
+window.onload = () => modelPromise = tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL);
+
+const button = document.getElementById('toggle');
+button.onclick = () => {
+  image.src = image.src.endsWith(imageURL) ? image2URL : imageURL;
+};
 
 const image = document.getElementById('image');
-  image.onload =
-    async function run() {
-      const GOOGLE_CLOUD_STORAGE_DIR =
-      'https://storage.googleapis.com/tfjs-models/savedmodel/';
-      const MODEL_URL = GOOGLE_CLOUD_STORAGE_DIR + 'coco-ssd-mobilenet_v1/tensorflowjs_model.pb';
-      const WEIGHTS_URL = GOOGLE_CLOUD_STORAGE_DIR + 'coco-ssd-mobilenet_v1/weights_manifest.json';
-      tf.loadFrozenModel(MODEL_URL, WEIGHTS_URL).then(async model => {
-        const pixels = tf.fromPixels(image);
-        console.log('model loaded');
-        console.time('predict1');
-        const res1 = await model.executeAsync(pixels.reshape([1, ...pixels.shape]));
-        res1.map(t => t.dataSync());
-        console.timeEnd('predict1');
-        console.time('predict2');
-        const res2 = await model.executeAsync(pixels.reshape([1, ...pixels.shape]));
-        const count = res2[3].dataSync()[0];
-        const boxes = res2[0].dataSync();
-        const scores = res2[1].dataSync();
-        const classes = res2[2].dataSync();
-        console.timeEnd('predict2');
+image.src = imageURL;
+
+const runButton = document.getElementById('run');
+runButton.onclick = async () => {
+  const model = await modelPromise;
+  const pixels = tf.fromPixels(image);
+  console.log('model loaded');
+  console.time('predict1');
+  const res1 = await model.executeAsync(pixels.reshape([1, ...pixels.shape]));
+  res1.map(t => t.dataSync());
+  console.timeEnd('predict1');
+  console.time('predict2');
+  const res2 = await model.executeAsync(pixels.reshape([1, ...pixels.shape]));
+  const count = res2[3].dataSync()[0];
+  const boxes = res2[0].dataSync();
+  const scores = res2[1].dataSync();
+  const classes = res2[2].dataSync();
+  console.timeEnd('predict2');
 
 
-        const c = document.getElementById("canvas");
-        const context = c.getContext("2d");
-        context.drawImage(image, 0, 0);
-        context.font = "10px Arial";
+  const c = document.getElementById('canvas');
+  const context = c.getContext('2d');
+  context.drawImage(image, 0, 0);
+  context.font = '10px Arial';
 
-        console.log('number of detections: ', count);
-        for (let i = 0; i < count; i++) {
-          const min_y = boxes[i * 4] * 399;
-          const min_x = boxes[i * 4 + 1] * 600;
-          const max_y = boxes[i * 4 + 2] * 399;
-          const max_x = boxes[i * 4 + 3] * 600;
+  console.log('number of detections: ', count);
+  for (let i = 0; i < count; i++) {
+    const min_y = boxes[i * 4] * 399;
+    const min_x = boxes[i * 4 + 1] * 600;
+    const max_y = boxes[i * 4 + 2] * 399;
+    const max_x = boxes[i * 4 + 3] * 600;
 
-          context.beginPath();
-          context.rect(min_x, min_y, max_x - min_x, max_y - min_y);
-          context.lineWidth = 1;
-          context.strokeStyle = 'black';
-          context.stroke();
-          context.fillText(scores[i].toFixed(3) + ' ' + (classes[i] === 1 ? 'person'
-            : 'kite'), min_x, min_y - 5);
-        }
-      });
-    };
-  image.src = imageURL;
+    context.beginPath();
+    context.rect(min_x, min_y, max_x - min_x, max_y - min_y);
+    context.lineWidth = 1;
+    context.strokeStyle = 'black';
+    context.stroke();
+    context.fillText(
+        scores[i].toFixed(3) + ' ' + CLASSES.find(label => label.id === classes[i]).display_name,
+        min_x, min_y - 5);
+  }
+};
+
