88709cb75b3b8ff7541723eeb436f0da30b6b13a
Kim Kangho <cimfalab@gmail.com>
2018-10-05 23:48:30 +0900

    Prevent TypeError of 'arraysEqual()' for null arguments. (#1281)
    DEV

--- src/util.ts
+++ src/util.ts
@@ -163,6 +163,13 @@ export function isScalarShape(shape: number[]): boolean {
 }
 
 export function arraysEqual(n1: FlatVector, n2: FlatVector) {
+  if (n1 === n2) {
+    return true;
+  }
+  if (n1 == null || n2 == null) {
+    return false;
+  }
+
   if (n1.length !== n2.length) {
     return false;
   }
