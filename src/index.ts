b92c457dde32a103a11c1cbe2538449f2c8a9722
Kangyi Zhang <kangyizhang@google.com>
2018-10-10 13:52:28 -0700

    Update default exports (#61)
    * update default exports
    
    * cleanup types.ts

--- src/index.ts
+++ src/index.ts
@@ -20,5 +20,8 @@ export {Dataset} from './dataset';
 export {datasetFromElements} from './dataset';
 export {CSVDataset} from './datasets/csv_dataset';
 export {TextLineDataset} from './datasets/text_line_dataset';
+export {csv} from './readers';
 export {FileDataSource} from './sources/file_data_source';
 export {URLDataSource} from './sources/url_data_source';
+export * from './types';
+export * from './statistics';
