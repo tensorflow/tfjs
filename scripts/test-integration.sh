7090a7c8cc99c16b6be3609ac5327d6d5758b1db
Daniel Smilkov <dsmilkov@gmail.com>
2018-11-05 11:11:14 -0500

    Add integration test with layers/node/converter when we modify `version.ts` (#1372)
    Run a 3rd travis job (integration) when the `src/version.ts` file is modified.
    
    The integration job clones `layers`/`node`/`converter` and tests those repos against `core@master`, giving the PR author a heads up before they make a new release of `core`.
    
    https://travis-ci.org/tensorflow/tfjs-core/builds/449534806 is an example of a Travis run. See the 3rd job (TO_TEST=integration).
    
    ![status](https://user-images.githubusercontent.com/2294279/47921751-c1520b00-de8b-11e8-8a5a-d7366f3700d6.png)
    
    
    Details:
     - rename `yarn test-travis` to `yarn run-browserstack` to better reflect the task
     - move all travis-related scripts under `yarn test-travis` to align core with the other repos (layers & node).
    
    Towards tensorflow/tfjs#848
    
    DEV

---
+++ scripts/test-integration.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+# Copyright 2018 Google LLC. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
+function print_status() {
+  name=$1
+  code=$2
+  echo -e "$name:" $([[ $code -eq 0 ]] && echo 'PASS' || echo 'FAIL') $3
+}
+
+function test () {
+  echo '######################'
+  echo 'version.ts was modified.'
+  echo 'Testing layers/converter/node againts tfjs-core@master.'
+  echo '######################'
+  yarn build && yarn run yalc publish
+
+  echo 'Cloning layers'
+  git clone https://github.com/tensorflow/tfjs-layers.git --depth 5
+  cd tfjs-layers
+  yarn && yarn link-local '@tensorflow/tfjs-core' && ./scripts/test-travis.sh
+  LAYERS_EXIT_CODE=$?
+
+  cd ..
+  echo 'Cloning node'
+  git clone https://github.com/tensorflow/tfjs-node.git --depth 5
+  cd tfjs-node
+  yarn && yarn link-local '@tensorflow/tfjs-core' && ./scripts/test-travis.sh
+  NODE_EXIT_CODE=$?
+
+  cd ..
+  echo 'Cloning converter'
+  git clone https://github.com/tensorflow/tfjs-converter.git --depth 5
+  cd tfjs-converter
+  yarn && yarn link-local '@tensorflow/tfjs-core'
+  yarn build && yarn lint && yarn test-travis
+  CONVERTER_EXIT_CODE=$?
+
+  echo '==== INTEGRATION TEST RESULTS ===='
+  print_status "tfjs-layers" "$LAYERS_EXIT_CODE"
+  print_status "tfjs-node" "$NODE_EXIT_CODE"
+  print_status "tfjs-converter" "$CONVERTER_EXIT_CODE"
+  echo '=================================='
+
+  RED='\033[0;31m'
+  BLUE='\e[34m'
+  NC='\033[0m' # No Color
+  FINAL_EXIT_CODE=$(($LAYERS_EXIT_CODE+$NODE_EXIT_CODE+$CONVERTER_EXIT_CODE))
+  [[ $FINAL_EXIT_CODE -eq 0 ]] && COLOR=$BLUE || COLOR=$RED
+  print_status "${COLOR}Final result" "$FINAL_EXIT_CODE" "$NC"
+  exit $FINAL_EXIT_CODE
+}
+
+
+readarray -t files_changed <<< "$(git diff --name-only $TRAVIS_COMMIT_RANGE)"
+
+for file in "${files_changed[@]}"
+do
+  if [ "$file" = "src/version.ts" ]; then test; break; fi
+done
