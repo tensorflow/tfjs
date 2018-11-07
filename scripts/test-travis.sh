10ebb9889612b4a2c592f12e04cb9cf2d9992aab
Daniel Smilkov <dsmilkov@gmail.com>
2018-11-07 09:38:01 -0500

    Fix failing test in travis (#1381)
    https://github.com/tensorflow/tfjs-core/pull/1379 added a unit test that failed in travis due to numerical precision: https://travis-ci.org/tensorflow/tfjs-core/jobs/451654174#L801
    
    This PR:
    - fixes that test
    - Also fixes `test-travis.sh` to make sure we don't forward webgl --> cpu except for the last browser run -- probably caused during conflict resolving of https://github.com/tensorflow/tfjs-core/pull/1371 and https://github.com/tensorflow/tfjs-core/pull/1372 (https://reviewable.io/reviews/tensorflow/tfjs-core/1371#-LQ_a2y92YYLRMiYWqhC)
    
    DEV

--- scripts/test-travis.sh
+++ scripts/test-travis.sh
@@ -25,15 +25,15 @@ if [[ $(node -v) = *v10* ]]; then
 
   # Run the first karma separately so it can download the BrowserStack binary
   # without conflicting with others.
-  yarn run-browserstack --browsers=bs_safari_mac --backend webgl
+  yarn run-browserstack --browsers=bs_safari_mac --backend webgl --features '{"WEBGL_CPU_FORWARD": false}'
 
   # Run the rest of the karma tests in parallel. These runs will reuse the
   # already downloaded binary.
-  npm-run-all -p -c --aggregate-output \
-    "run-browserstack --browsers=bs_safari_mac --features '{\"HAS_WEBGL\": false}' --backend cpu" \
-    "run-browserstack --browsers=bs_ios_11 --backend webgl" \
-    "run-browserstack --browsers=bs_ios_11 --features '{\"HAS_WEBGL\": false}' --backend cpu" \
-    "run-browserstack --browsers=bs_firefox_mac" \
-    "run-browserstack --browsers=bs_chrome_mac" \
-    "run-browserstack --browsers=bs_chrome_mac --features '{\"WEBGL_CPU_FORWARD\": true}' --backend webgl"
+npm-run-all -p -c --aggregate-output \
+  "run-browserstack --browsers=bs_safari_mac --features '{\"HAS_WEBGL\": false}' --backend cpu" \
+  "run-browserstack --browsers=bs_ios_11 --backend webgl --features '{\"WEBGL_CPU_FORWARD\": false}'" \
+  "run-browserstack --browsers=bs_ios_11 --features '{\"HAS_WEBGL\": false}' --backend cpu" \
+  "run-browserstack --browsers=bs_firefox_mac" \
+  "run-browserstack --browsers=bs_chrome_mac" \
+  "run-browserstack --browsers=bs_chrome_mac --features '{\"WEBGL_CPU_FORWARD\": true}' --backend webgl"
 fi
