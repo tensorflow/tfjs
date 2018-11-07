88f2fcbd9f79848e09b12dc214e4cdaf08d58f92
Daniel Smilkov <dsmilkov@gmail.com>
2018-11-07 09:38:01 -0500

    Fix failing test in travis (#1381)
    https://github.com/tensorflow/tfjs-core/pull/1379 added a unit test that failed in travis due to numerical precision: https://travis-ci.org/tensorflow/tfjs-core/jobs/451654174#L801
    
    This PR:
    - fixes that test
    - Also fixes `test-travis.sh` to make sure we don't forward webgl --> cpu except for the last browser run -- probably caused during conflict resolving of https://github.com/tensorflow/tfjs-core/pull/1371 and https://github.com/tensorflow/tfjs-core/pull/1372 (https://reviewable.io/reviews/tensorflow/tfjs-core/1371#-LQ_a2y92YYLRMiYWqhC)
    
    DEV

--- src/ops/matmul_test.ts
+++ src/ops/matmul_test.ts
@@ -293,31 +293,40 @@ describeWithFlags('matmul', ALL_ENVS, () => {
   it('batched matmul with the matrices being vectors', () => {
     const batch = 3;
     const sharedDim = 10000;
-    const a = tf.fill([batch, 1, sharedDim], 0.1);
-    const b = tf.fill([batch, sharedDim, 1], 0.1);
+    const values = new Float32Array(batch * sharedDim);
+    values[10] = 2;
+
+    const a = tf.tensor(values, [batch, 1, sharedDim]);
+    const b = tf.tensor(values, [batch, sharedDim, 1]);
     const result = tf.matMul(a, b);
     expect(result.shape).toEqual([batch, 1, 1]);
-    expectArraysClose(result, [100, 100, 100]);
+    expectArraysClose(result, [4, 0, 0]);
   });
 
   it('batched matmul with matrix x vector', () => {
     const batch = 3;
     const sharedDim = 10000;
-    const a = tf.fill([batch, 2, sharedDim], 0.1);
-    const b = tf.fill([batch, sharedDim, 1], 0.1);
+    const values = new Float32Array(batch * sharedDim);
+    values[10] = 2;
+
+    const a = tf.ones([batch, 2, sharedDim]);
+    const b = tf.tensor(values, [batch, sharedDim, 1]);
     const result = tf.matMul(a, b);
     expect(result.shape).toEqual([batch, 2, 1]);
-    expectArraysClose(result, [100, 100, 100, 100, 100, 100]);
+    expectArraysClose(result, [2, 2, 0, 0, 0, 0]);
   });
 
   it('batched matmul with vector x matrix', () => {
     const batch = 3;
     const sharedDim = 10000;
-    const a = tf.fill([batch, 1, sharedDim], 0.1);
-    const b = tf.fill([batch, sharedDim, 2], 0.1);
+    const values = new Float32Array(batch * sharedDim);
+    values[10] = 2;
+
+    const a = tf.tensor(values, [batch, 1, sharedDim]);
+    const b = tf.ones([batch, sharedDim, 2]);
     const result = tf.matMul(a, b);
     expect(result.shape).toEqual([batch, 1, 2]);
-    expectArraysClose(result, [100, 100, 100, 100, 100, 100]);
+    expectArraysClose(result, [2, 2, 0, 0, 0, 0]);
   });
 
   it('Matrix * vector propagates NaNs', () => {
