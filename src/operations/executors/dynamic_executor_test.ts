c60fa1ad98611916a7cb8d93f60cb3b3d38997c2
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-11-14 17:16:04 -0800

    Add support for ListDiff op (#247)
    * added support for setdiff1dAsync
    
    * remove the console logs
    
    * address comments

--- src/operations/executors/dynamic_executor_test.ts
+++ src/operations/executors/dynamic_executor_test.ts
@@ -17,11 +17,12 @@
 import * as tfc from '@tensorflow/tfjs-core';
 
 import {ExecutionContext} from '../../executor/execution_context';
-import {Node} from '../types';
+import * as dynamic from '../op_list/dynamic';
+import {Node, OpMapper} from '../types';
 
 import {executeOp} from './dynamic_executor';
 // tslint:disable-next-line:max-line-length
-import {createNumberAttr, createTensorAttr} from './test_helper';
+import {createNumberAttrFromIndex, createTensorAttr, validateParam} from './test_helper';
 
 describe('dynamic', () => {
   let node: Node;
@@ -32,7 +33,7 @@ describe('dynamic', () => {
     node = {
       name: 'input1',
       op: '',
-      category: 'image',
+      category: 'dynamic',
       inputNames: ['input1'],
       inputs: [],
       params: {},
@@ -46,33 +47,74 @@ describe('dynamic', () => {
         node.op = 'nonMaxSuppression';
         node.params['boxes'] = createTensorAttr(0);
         node.params['scores'] = createTensorAttr(1);
-        node.params['maxOutputSize'] = createTensorAttr(2);
-        node.params['iouThreshold'] = createTensorAttr(3);
-        node.params['scoreThreshold'] = createNumberAttr(1);
-        node.inputNames = ['input1', 'input2', 'input3', 'input4'];
+        node.params['maxOutputSize'] = createNumberAttrFromIndex(2);
+        node.params['iouThreshold'] = createNumberAttrFromIndex(3);
+        node.params['scoreThreshold'] = createNumberAttrFromIndex(4);
+        node.inputNames = ['input1', 'input2', 'input3', 'input4', 'input5'];
         const input2 = [tfc.tensor1d([1])];
         const input3 = [tfc.tensor1d([1])];
         const input4 = [tfc.tensor1d([1])];
+        const input5 = [tfc.tensor1d([1])];
         spyOn(tfc.image, 'nonMaxSuppressionAsync').and.callThrough();
         const result =
-            executeOp(node, {input1, input2, input3, input4}, context);
+            executeOp(node, {input1, input2, input3, input4, input5}, context);
         expect(tfc.image.nonMaxSuppressionAsync)
-            .toHaveBeenCalledWith(
-                input1[0], input2[0], input3[0], input4[0], 1);
+            .toHaveBeenCalledWith(input1[0], input2[0], 1, 1, 1);
         expect(result instanceof Promise).toBeTruthy();
+      });
+      it('should match json def', () => {
+        node.op = 'nonMaxSuppression';
+        node.params['boxes'] = createTensorAttr(0);
+        node.params['scores'] = createTensorAttr(1);
+        node.params['maxOutputSize'] = createNumberAttrFromIndex(2);
+        node.params['iouThreshold'] = createNumberAttrFromIndex(3);
+        node.params['scoreThreshold'] = createNumberAttrFromIndex(4);
+        node.inputNames = ['input1', 'input2', 'input3', 'input4', 'input5'];
+
+        expect(validateParam(
+                   node, dynamic.json as OpMapper[], 'NonMaxSuppressionV3'))
+            .toBeTruthy();
       });
     });
 
     describe('whereAsync', () => {
       it('should call tfc.whereAsync', async () => {
         node.op = 'whereAsync';
-        const input1 = [tfc.scalar(1)];
         node.params = {'condition': createTensorAttr(0)};
+        const input1 = [tfc.scalar(1)];
         spyOn(tfc, 'whereAsync').and.callThrough();
 
         const result = executeOp(node, {input1}, context);
         expect(tfc.whereAsync).toHaveBeenCalledWith(input1[0]);
         expect(result instanceof Promise).toBeTruthy();
+      });
+      it('should match json def', () => {
+        node.op = 'whereAsync';
+        node.params = {'condition': createTensorAttr(0)};
+
+        expect(validateParam(node, dynamic.json as OpMapper[])).toBeTruthy();
+      });
+    });
+
+    describe('setdiff1dAsync', () => {
+      it('should call tfc.setdiff1dAsync', async () => {
+        node.op = 'setdiff1dAsync';
+        node.inputNames = ['input1', 'input2'];
+        node.params = {'x': createTensorAttr(0), 'y': createTensorAttr(1)};
+        const input1 = [tfc.scalar(1)];
+        const input2 = [tfc.scalar(1)];
+        spyOn(tfc, 'setdiff1dAsync').and.callThrough();
+
+        const result = executeOp(node, {input1, input2}, context);
+        expect(tfc.setdiff1dAsync).toHaveBeenCalledWith(input1[0], input2[0]);
+        expect(result instanceof Promise).toBeTruthy();
+      });
+      it('should match json def', () => {
+        node.op = 'setdiff1dAsync';
+        node.inputNames = ['input1', 'input2'];
+        node.params = {'x': createTensorAttr(0), 'y': createTensorAttr(1)};
+
+        expect(validateParam(node, dynamic.json as OpMapper[])).toBeTruthy();
       });
     });
   });
