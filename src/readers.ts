7eb0f1c183190ff197383c761f1ee841b92413ea
Kangyi Zhang <kangyizhang@google.com>
2018-11-19 10:00:08 -0800

    Update readme example (#86)
    * update readme
    
    * update readme
    
    * update readers

--- src/readers.ts
+++ src/readers.ts
@@ -26,29 +26,54 @@ import {CSVConfig} from './types';
  * ```js
  * const csvUrl =
  * 'https://storage.googleapis.com/tfjs-examples/multivariate-linear-regression/data/boston-housing-train.csv';
- * // We want to predict the column "medv", which represents a median value of a
- * // home (in $1000s), so we mark it as a label.
- * const csvDataset = tf.data.csv(
- *   csvUrl, {columnConfigs: {medv: {isLabel: true}}});
  *
- * const flattenedDataset =
+ * async function run() {
+ *   // We want to predict the column "medv", which represents a median value of
+ *   // a home (in $1000s), so we mark it as a label.
+ *   const csvDataset = tf.data.csv(
+ *     csvUrl, {
+ *       columnConfigs: {
+ *         medv: {
+ *           isLabel: true
+ *         }
+ *       }
+ *     });
+ *
+ *   // Number of features is the number of column names minus one for the label
+ *   // column.
+ *   const numOfFeatures = (await csvDataset.columnNames()).length - 1;
+ *
+ *   // Prepare the Dataset for training.
+ *   const flattenedDataset =
  *     csvDataset
- *         .map(row => {
- *           const [rawFeatures, rawLabel] = row;
- *           const features = Object.values(rawFeatures);
- *           const label = rawLabel['medv'];
- *           return [features, label];
- *         })
- *         .batch(10).repeat();
+ *     .map(([rawFeatures, rawLabel]) =>
+ *       // Convert rows from object form (keyed by column name) to array form.
+ *       [Object.values(rawFeatures), Object.values(rawLabel)])
+ *     .batch(10);
  *
- * const model = tf.sequential();
+ *   // Define the model.
+ *   const model = tf.sequential();
+ *   model.add(tf.layers.dense({
+ *     inputShape: [numOfFeatures],
+ *     units: 1
+ *   }));
+ *   model.compile({
+ *     optimizer: tf.train.sgd(0.000001),
+ *     loss: 'meanSquaredError'
+ *   });
  *
- * const numOfFeatures = (await csvDataset.columnNames()).length - 1;
- * model.add(tf.layers.dense(
- *       {inputShape: [numOfFeatures], units: 1}));
- * model.compile({optimizer: tf.train.sgd(0.000001), loss: 'meanSquaredError'});
+ *   // Fit the model using the prepared Dataset
+ *   return model.fitDataset(flattenedDataset, {
+ *     epochs: 10,
+ *     callbacks: {
+ *       onEpochEnd: async (epoch, logs) => {
+ *         console.log(epoch, logs.loss);
+ *       }
+ *     }
+ *   });
+ * }
  *
- * await model.fitDataset(flattenedDataset, {epochs: 10, batchesPerEpoch: 3});
+ * run().then(() => console.log('Done'));
  * ```
  *
  * @param source URL to fetch CSV file.
