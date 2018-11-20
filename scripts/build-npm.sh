1a0899d179b8110895c8922d029a294d091cbd40
Kangyi Zhang <kangyizhang@google.com>
2018-11-20 10:31:28 -0800

    Add scripts for publish package (#92)
    * add scripts for publish package
    
    * fix nit in .travis.yml
    
    * remove os in travis
    
    * fix nit
    
    * update travis test
    
    * update travis
    
    * add permission to test-travis.sh
    
    * fix nit

--- scripts/build-npm.sh
+++ scripts/build-npm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Copyright 2017 Google Inc. All Rights Reserved.
+# Copyright 2018 Google LLC. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
