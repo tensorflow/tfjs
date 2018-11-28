e533dca70970c14ed3805e800fbc974e31cf3150
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-11-28 14:13:02 -0800

    Add api for loading models directly from tfhub http server (#254)
    * add load tfhub model api
    
    * loadTfHubModule will call loadFrozenModel
    
    * clean up the tsconfig.json
    
    * addressed comments

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,5 @@
  * limitations under the License.
  * =============================================================================
  */
-export {FrozenModel, loadFrozenModel} from './executor/frozen_model';
+export {FrozenModel, loadFrozenModel, loadTfHubModule} from './executor/frozen_model';
 export {version as version_converter} from './version';
