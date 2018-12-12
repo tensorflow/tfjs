3db0c97a2c4889fb9ad7e8e738855b031d7abc74
Nikhil Thorat <nsthorat@google.com>
2018-12-12 13:16:59 -0500

    Run tests in Node 8 instead of Node 10. (#1450)
    DEV

--- scripts/test-travis.sh
+++ scripts/test-travis.sh
@@ -21,7 +21,7 @@ yarn lint
 # Test in node (headless environment).
 yarn test-node
 
-if [[ $(node -v) = *v10* ]]; then
+if [[ $(node -v) = *v8* ]]; then
   # Run the first karma separately so it can download the BrowserStack binary
   # without conflicting with others.
   yarn run-browserstack --browsers=bs_safari_mac --backend webgl --features '{"WEBGL_CPU_FORWARD": false, "WEBGL_SIZE_UPLOAD_UNIFORM": 0}'
