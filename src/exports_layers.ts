56ad115c0df5ac211dd28002db0fe27ee69d1987
Shanqing Cai <cais@google.com>
2019-01-07 14:13:59 -0500

    Add advanced activation: PReLU (tf.layers.prelu) (#401)
    FEATURE
    
    Fixes: https://github.com/tensorflow/tfjs/issues/629

--- src/exports_layers.ts
+++ src/exports_layers.ts
@@ -11,7 +11,7 @@
 import {InputLayer, InputLayerConfig} from './engine/input_layer';
 import {Layer, LayerConfig} from './engine/topology';
 import {input} from './exports';
-import {ELU, ELULayerConfig, LeakyReLU, LeakyReLULayerConfig, ReLU, ReLULayerConfig, Softmax, SoftmaxLayerConfig, ThresholdedReLU, ThresholdedReLULayerConfig} from './layers/advanced_activations';
+import {ELU, ELULayerConfig, LeakyReLU, LeakyReLULayerConfig, ReLU, ReLULayerConfig, Softmax, SoftmaxLayerConfig, ThresholdedReLU, ThresholdedReLULayerConfig, PReLULayerConfig, PReLU} from './layers/advanced_activations';
 import {Conv1D, Conv2D, Conv2DTranspose, ConvLayerConfig, Cropping2D, Cropping2DLayerConfig, SeparableConv2D, SeparableConvLayerConfig, UpSampling2D, UpSampling2DLayerConfig} from './layers/convolutional';
 import {DepthwiseConv2D, DepthwiseConv2DLayerConfig} from './layers/convolutional_depthwise';
 import {Activation, ActivationLayerConfig, Dense, DenseLayerConfig, Dropout, DropoutLayerConfig, Flatten, Permute, PermuteLayerConfig, RepeatVector, RepeatVectorLayerConfig, Reshape, ReshapeLayerConfig} from './layers/core';
@@ -81,6 +81,19 @@ export function reLU(config?: ReLULayerConfig): Layer {
  */
 export function leakyReLU(config?: LeakyReLULayerConfig): Layer {
   return new LeakyReLU(config);
+}
+
+/**
+ * @doc {
+ *   heading: 'Layers',
+ *   subheading: 'Advanced Activation',
+ *   namespace: 'layers',
+ *   useDocsFrom: 'PReLU',
+ *   configParamIndices: [0]
+ * }
+ */
+export function prelu(config?: PReLULayerConfig): Layer {
+  return new PReLU(config);
 }
 
 /**
