768799114580c967d2460d7b4932ac30d2f14302
Colle <piercus@users.noreply.github.com>
2019-01-16 15:33:25 +0100

    fix: Dropout Memory Leak (#428)
    Related to https://github.com/tensorflow/tfjs/issues/995
    BUG

--- src/layers/core.ts
+++ src/layers/core.ts
@@ -18,7 +18,7 @@ import {Activation as ActivationFn, getActivation, serializeActivation} from '..
 import {getScalar} from '../backend/state';
 import * as K from '../backend/tfjs_backend';
 import {Constraint, ConstraintIdentifier, getConstraint, serializeConstraint} from '../constraints';
-import {InputSpec, Layer, LayerArgs} from '../engine/topology';
+import {DisposeResult, InputSpec, Layer, LayerArgs} from '../engine/topology';
 import {NotImplementedError, ValueError} from '../errors';
 import {getInitializer, Initializer, InitializerIdentifier, serializeInitializer} from '../initializers';
 import {ActivationIdentifier} from '../keras_format/activation_config';
@@ -124,6 +124,15 @@ export class Dropout extends Layer {
     const baseConfig = super.getConfig();
     Object.assign(config, baseConfig);
     return config;
+  }
+  
+  dispose(): DisposeResult {
+    const result = super.dispose();
+    if (!this.rateScalar.isDisposed) {
+      this.rateScalar.dispose();
+      result.numDisposedVariables++;
+    }
+    return result;
   }
 }
 serialization.registerClass(Dropout);
