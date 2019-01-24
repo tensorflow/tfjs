759b4466a38a25dec2f4ebc8ca8f3b61b374676a
Kai Sasaki <lewuathe@me.com>
2019-01-25 03:36:54 +0900

    Add util to get the non-flattened tensor data (#1483)
    INTERNAL
    #### Description
    <!--
    Please describe the pull request here.
    Also, if this is an issue/bug fix, please add the issue link for reference here.
    -->
    
    MISC
    
    Provide a utility to get the non-flattened Tensor data. See https://github.com/tensorflow/tfjs/issues/979

--- src/util.ts
+++ src/util.ts
@@ -517,6 +517,42 @@ export function toTypedArray(
   }
 }
 
+function createNestedArray(offset: number, shape: number[], a: TypedArray) {
+  const ret = new Array();
+  if (shape.length === 1) {
+    const d = shape[0];
+    for (let i = 0; i < d; i++) {
+      ret[i] = a[offset + i];
+    }
+  } else {
+    const d = shape[0];
+    const rest = shape.slice(1);
+    const len = rest.reduce((acc, c) => acc * c);
+    for (let i = 0; i < d; i++) {
+      ret[i] = createNestedArray(offset + i * len, rest, a);
+    }
+  }
+  return ret;
+}
+
+// Provide a nested array of TypedArray in given shape.
+export function toNestedArray(shape: number[], a: TypedArray) {
+  if (shape.length === 0) {
+    // Scalar type should be empty list.
+    return [];
+  }
+  const size = shape.reduce((acc, c) => acc * c);
+  if (size === 0) {
+    // A tensor with shape zero should be turned into empty list.
+    return [];
+  }
+  if (size !== a.length) {
+    throw new Error(`[${shape}] does not match the input size.`);
+  }
+
+  return createNestedArray(0, shape, a);
+}
+
 function noConversionNeeded(a: TensorLike, dtype: DataType): boolean {
   return (a instanceof Float32Array && dtype === 'float32') ||
       (a instanceof Int32Array && dtype === 'int32') ||
--- src/util_test.ts
+++ src/util_test.ts
@@ -17,6 +17,7 @@
 
 import {inferShape} from './tensor_util_env';
 import * as util from './util';
+import {scalar, tensor2d} from './ops/ops';
 
 describe('Util', () => {
   it('Correctly gets size from shape', () => {
@@ -452,6 +453,47 @@ describe('util.hasEncodingLoss', () => {
 
   it('bool to bool', () => {
     expect(util.hasEncodingLoss('bool', 'bool')).toBe(false);
+  });
+});
+
+describe('util.toNestedArray', () => {
+  it('2 dimensions', () => {
+    const a = new Float32Array([1, 2, 3, 4, 5, 6]);
+    expect(util.toNestedArray([2, 3], a))
+      .toEqual([[1,2,3], [4,5,6]]);
+  });
+
+  it('3 dimensions (2x2x3)', () => {
+    const a = new Float32Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+    expect(util.toNestedArray([2, 2, 3], a))
+      .toEqual([[[0, 1, 2], [3, 4, 5]], [[6, 7, 8], [9, 10, 11]]]);
+  });
+
+  it('3 dimensions (3x2x2)', () => {
+    const a = new Float32Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
+    expect(util.toNestedArray([3, 2, 2], a))
+      .toEqual([[[0, 1],[2, 3]],[[4, 5],[6, 7]],[[8, 9],[10, 11]]]);
+  });
+
+  it('invalid dimension', () => {
+    const a = new Float32Array([1, 2, 3]);
+    expect(() => util.toNestedArray([2, 2], a)).toThrowError();
+  });
+
+  it('tensor to nested array', () => {
+    const x = tensor2d([1, 2, 3, 4], [2, 2]);
+    expect(util.toNestedArray(x.shape, x.dataSync()))
+      .toEqual([[1, 2], [3, 4]]);
+  });
+
+  it('scalar to nested array', () => {
+    const x = scalar(1);
+    expect(util.toNestedArray(x.shape, x.dataSync())).toEqual([]);
+  });
+
+  it('tensor with zero shape', () => {
+    const a = new Float32Array([0, 1]);
+    expect(util.toNestedArray([1, 0, 2], a)).toEqual([]);
   });
 });
 
