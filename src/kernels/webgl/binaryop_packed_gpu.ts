a71964b483c224a23984dfc38365279abb48eebc
Ann Yuan <annyuan@google.com>
2019-01-25 14:54:49 -0500

    Replace vectorized packed div with channel-wise implementation to fix layers tests. (#1513)
    BUG

--- src/kernels/webgl/binaryop_packed_gpu.ts
+++ src/kernels/webgl/binaryop_packed_gpu.ts
@@ -20,9 +20,17 @@ import {GPGPUContext} from './gpgpu_context';
 import {GPGPUProgram} from './gpgpu_math';
 
 // We do the same as in ./binaryop_gpu, with vec4 and ivec4.
+// TODO(https://github.com/tensorflow/tfjs/issues/1141): Understand why
+// vectorized implementation causes layers tests to break.
 export const PACKED_DIV = `
-  vec4 one = vec4(equal(a, b));
-  return one + (vec4(1.0) - one) * a / b;
+  // vec4 one = vec4(equal(a, b));
+  // return one + (vec4(1.0) - one) * a / b;
+  vec4 result = a / b;
+  result.x = a.x == b.x ? 1. : result.x;
+  result.y = a.y == b.y ? 1. : result.y;
+  result.z = a.z == b.z ? 1. : result.z;
+  result.w = a.w == b.w ? 1. : result.w;
+  return result;
 `;
 
 export const PACKED_INT_DIV = `
