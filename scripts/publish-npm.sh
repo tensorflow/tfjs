5ea5e9dcc65ceb4e697d0953896424e4829bcf8d
Nikhil Thorat <nsthorat@google.com>
2019-02-04 14:56:26 -0500

    Update tfjs-data to 1.0.0-alpha1 at master and update core version. (#131)

--- scripts/publish-npm.sh
+++ scripts/publish-npm.sh
@@ -49,6 +49,6 @@ fi
 
 yarn build-npm
 ./scripts/make-version # This is for safety in case you forgot to do 2).
-npm publish
+npm publish --tag next # Remove --tag next when prereleases are done.
 ./scripts/tag-version
 echo 'Yay! Published a new package to npm.'
