20f1fd953275c38fa07d6f6c939f30346e0d07f7
Nikhil Thorat <nsthorat@google.com>
2019-02-08 12:14:24 -0500

    Update tfjs-converter to 1.0.0-alpha2 at master and point to tfjs-core@1.0.0-alpha2. Also update to tfc.batchNorm. (#292)
    * Update tfjs-converter to 1.0.0-alph2 at master.
    
    * Update to tfc.batchNorm
    
    * update unit test

--- src/operations/executors/normalization_executor.ts
+++ src/operations/executors/normalization_executor.ts
@@ -29,13 +29,13 @@ export let executeOp: OpExecutor = (node: Node, tensorMap: NamedTensorsMap,
                                        tfc.Tensor[] => {
   switch (node.op) {
     case 'batchNormalization': {
-      return [tfc.batchNormalization(
+      return [tfc.batchNorm(
           getParamValue('x', node, tensorMap, context) as tfc.Tensor,
           getParamValue('mean', node, tensorMap, context) as tfc.Tensor,
           getParamValue('variance', node, tensorMap, context) as tfc.Tensor,
-          getParamValue('epsilon', node, tensorMap, context) as number,
+          getParamValue('offset', node, tensorMap, context) as tfc.Tensor,
           getParamValue('scale', node, tensorMap, context) as tfc.Tensor,
-          getParamValue('offset', node, tensorMap, context) as tfc.Tensor)];
+          getParamValue('epsilon', node, tensorMap, context) as number)];
     }
     case 'localResponseNormalization': {
       return [tfc.localResponseNormalization(
