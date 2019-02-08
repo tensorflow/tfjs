165e616b0335994cc0dc144290d77a8e006c1885
Daniel Smilkov <dsmilkov@gmail.com>
2019-02-08 15:55:21 -0500

    Fix license in the auto-generated `version.ts` (#455)

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,12 @@
-/** @license See the LICENSE file. */
+/**
+ * @license
+ * Copyright 2019 Google LLC
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ * =============================================================================
+ */
 
 // This code is auto-generated, do not modify this file!
 const version = '1.0.0-alpha2';
