1f65ca8daeb83e67284f4cafafabd3693d0a0461
Shanqing Cai <cais@google.com>
2019-02-11 14:52:20 -0500

    [io] Add new interface LoadOptions (#1555)
    Towards https://github.com/tensorflow/tfjs/issues/1175
    
    FEATURE

--- src/io/types.ts
+++ src/io/types.ts
@@ -239,3 +239,69 @@ export interface ModelStoreManager {
    */
   removeModel(path: string): Promise<ModelArtifactsInfo>;
 }
+
+/**
+ * Callback for the progress of a long-running action such as an HTTP
+ * request for a large binary object.
+ *
+ * `fraction` should be a number in the [0, 1] interval, indicating how
+ * much of the action has completed.
+ */
+export type OnProgressCallback = (fraction: number) => void;
+
+export interface LoadOptions {
+  /**
+   * RequestInit (options) for HTTP requests.
+   *
+   * For detailed information on the supported fields, see
+   * https://developer.mozilla.org/en-US/docs/Web/API/Request/Request
+   */
+  requestInit?: RequestInit;
+
+  /**
+   * Progress callback.
+   */
+  onProgress?: OnProgressCallback;
+
+  /**
+   * A function used to override the `window.fetch` function.
+   */
+  fetchFunc?: Function;
+
+  /**
+   * Strict loading model: whether extraneous weights or missing
+   * weights should trigger an `Error`.
+   *
+   * If `true`, require that the provided weights exactly match those
+   * required by the layers. `false` means that both extra weights
+   * and missing weights will be silently ignored.
+   *
+   * Default: `true`.
+   */
+  strict?: boolean;
+
+  /**
+   * Path prefix for weight files, by default this is calculated from the
+   * path of the model JSON file.
+   *
+   * For instance, if the path to the model JSON file is
+   * 'http://localhost/foo/model.json', then the default path prefix will be
+   * 'http://localhost/foo/'. If a weight file has the path value
+   * 'group1-shard1of2' in the weight manifest, then the weight file will be
+   * loaded from 'http://localhost/foo/group1-shard1of2' by default. However,
+   * if you provide a `weightPathPrefix` value of
+   * 'http://localhost/foo/alt-weights', then the weight file will be loaded
+   * from the path 'http://localhost/foo/alt-weights/group1-shard1of2' instead.
+   */
+  weightPathPrefix?: string;
+
+  /**
+   * Whether the module or model is to be loaded from TF Hub.
+   * 
+   * Setting this to `true` allows passing a TF-Hub module URL, omitting the
+   * standard model file name and the query parameters.
+   *
+   * Default: `false`.
+   */
+  fromTFHub?: boolean;
+}
