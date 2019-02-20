6858d1164938c9d54a54de3c6f0d556d2f6b0ace
Ann Yuan <annyuan@google.com>
2019-02-20 14:02:48 -0500

    Cut off negative values for padAlongDim in computing convInfo.padInfo (#1581)
    BUG

--- src/ops/conv_util.ts
+++ src/ops/conv_util.ts
@@ -338,8 +338,9 @@ function getPadAndOutInfo(
     outHeight = Math.ceil(inHeight / strideHeight);
     outWidth = Math.ceil(inWidth / strideWidth);
     const padAlongHeight =
-        (outHeight - 1) * strideHeight + filterHeight - inHeight;
-    const padAlongWidth = (outWidth - 1) * strideWidth + filterWidth - inWidth;
+        Math.max(0, (outHeight - 1) * strideHeight + filterHeight - inHeight);
+    const padAlongWidth =
+        Math.max(0, (outWidth - 1) * strideWidth + filterWidth - inWidth);
     const top = Math.floor(padAlongHeight / 2);
     const bottom = padAlongHeight - top;
     const left = Math.floor(padAlongWidth / 2);
