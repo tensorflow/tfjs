49bf10c6fb3af61090d90e2d9544287538b8b1be
Ann Yuan <annyuan@google.com>
2019-03-15 11:27:14 -0400

    how bout now

--- integration_tests/benchmarks/benchmark_travis.sh
+++ integration_tests/benchmarks/benchmark_travis.sh
@@ -52,6 +52,7 @@ then
   yarn link-local '@tensorflow/tfjs-converter'
 
   yarn run-browserstack --travis --browsers=bs_safari_mac
+  yarn run-browserstack --travis --browsers=bs_ios_11
   yarn run-browserstack --travis --browsers=bs_chrome_mac
 
   # npm-run-all -p -c --aggregate-output \
