30914c456b1f7b6c939c28432f0cd12cad8ecf4c
Ann Yuan <annyuan@google.com>
2019-03-18 12:07:20 -0400

    try nonparallel

--- integration_tests/benchmarks/benchmark_travis.sh
+++ integration_tests/benchmarks/benchmark_travis.sh
@@ -52,11 +52,11 @@ then
   yarn link-local '@tensorflow/tfjs-converter'
 
   # yarn run-browserstack --travis --browsers=bs_ios_11
-  # yarn run-browserstack --travis --browsers=bs_safari_mac
-  # yarn run-browserstack --travis --browsers=bs_chrome_mac
+  yarn run-browserstack --travis --browsers=bs_safari_mac
+  yarn run-browserstack --travis --browsers=bs_chrome_mac
     # "run-browserstack --travis --browsers=bs_ios_11" \
 
-  npm-run-all -p -c --aggregate-output \
-    "run-browserstack --travis --browsers=bs_safari_mac" \
-    "run-browserstack --travis --browsers=bs_chrome_mac"
+  # npm-run-all -p -c --aggregate-output \
+  #   "run-browserstack --travis --browsers=bs_safari_mac" \
+  #   "run-browserstack --travis --browsers=bs_chrome_mac"
 fi
