f6bc0eb6c5a72ef34ce8fa2cfdf1a8c3316c53b9
Ann Yuan <annyuan@google.com>
2019-03-18 17:26:42 -0400

    trigger build

--- integration_tests/benchmarks/benchmark_travis.sh
+++ integration_tests/benchmarks/benchmark_travis.sh
@@ -19,8 +19,8 @@ set -e
 yarn
 yarn lint
 
-if [ "$TRAVIS_EVENT_TYPE" = cron ] && [[ $(node -v) = *v10* ]]
-# if [[ $(node -v) = *v10* ]]
+# if [ "$TRAVIS_EVENT_TYPE" = cron ] && [[ $(node -v) = *v10* ]]
+if [[ $(node -v) = *v10* ]]
 then
   # Run the first karma separately so it can download the BrowserStack binary
   # without conflicting with others.
@@ -60,10 +60,7 @@ then
   yarn link-local '@tensorflow/tfjs-data'
 
   npm-run-all -p -c --aggregate-output \
-    "run-browserstack --travis --browsers=bs_safari_mac --grep=models" \
+    "run-browserstack --travis --browsers=bs_safari_mac,bs_chrome_mac --grep=models" \
     "run-browserstack --travis --browsers=bs_ios_11 --grep=mobilenet" \
-    "run-browserstack --travis --browsers=bs_chrome_mac --grep=models" \
-    "run-browserstack --travis --browsers=bs_ios_11 --grep=ops" \
-    "run-browserstack --travis --browsers=bs_safari_mac --grep=ops" \
-    "run-browserstack --travis --browsers=bs_chrome_mac --grep=ops"
+    "run-browserstack --travis --browsers=bs_safari_mac,bs_ios_11,bs_chrome_mac --grep=ops"
 fi
