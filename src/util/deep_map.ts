53a0554c8e32c36253c3fdb24f4c26208c534ed6
Kangyi Zhang <kangyizhang@google.com>
2019-04-02 11:37:21 -0700

    Make `seedrandom` peerDependency and cleanup up node util.isPrimitive (#173)
    INTERNAL
    * save
    
    * save

--- src/util/deep_map.ts
+++ src/util/deep_map.ts
@@ -17,7 +17,6 @@
  */
 
 import * as tf from '@tensorflow/tfjs-core';
-import {isPrimitive} from 'util';
 
 // tslint:disable:no-any
 
@@ -265,4 +264,14 @@ export function canTensorify(obj: any): boolean {
   return obj == null || isPrimitive(obj) || Array.isArray(obj) ||
       (typeof obj === 'object' && (obj instanceof tf.Tensor)) ||
       tf.util.isTypedArray(obj);
+}
+
+/**
+ * Returns true if the given `value` is a primitive type. Otherwise returns
+ * false. This is equivalant to node util.isPrimitive
+ */
+function isPrimitive(value: any): boolean {
+  return (
+      value === null ||
+      (typeof value !== 'object' && typeof value !== 'function'));
 }
