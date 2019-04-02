044d9f351220ad1e2d02b238328cebaf6b75a596
Nikhil Thorat <nsthorat@google.com>
2019-04-02 11:11:54 -0700

    Remove firefox from local testing to speed up local dev. (#177)
    DEV
    Firefox is covered on travis.

--- karma.conf.js
+++ karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function (config) {
       }
     },
     reporters: ['progress', 'karma-typescript'],
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['Chrome'],
     browserStack: {
       username: process.env.BROWSERSTACK_USERNAME,
       accessKey: process.env.BROWSERSTACK_KEY
