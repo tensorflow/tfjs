a2d517426126e020bb9d9a272ff07f53221da529
Nikhil Thorat <nsthorat@google.com>
2019-04-22 10:14:41 -0400

    Add tf.io.http and deprecate tf.io.browserHTTPRequest (#1684)
    FEATURE
    
    `tf.io.http` supports node and the browser as well as overriding the global fetch and using it.
    
    Node will use this registry instead of registering its own.

--- src/io/io.ts
+++ src/io/io.ts
@@ -21,7 +21,7 @@ import './indexed_db';
 import './local_storage';
 
 import {browserFiles} from './browser_files';
-import {browserHTTPRequest, isHTTPScheme} from './browser_http';
+import {browserHTTPRequest, http, isHTTPScheme} from './http';
 import {concatenateArrayBuffers, decodeWeights, encodeWeights, getModelArtifactsInfoForJSON} from './io_utils';
 import {fromMemory, withSaveHandler} from './passthrough';
 import {getLoadHandlers, getSaveHandlers, registerLoadRouter, registerSaveRouter} from './router_registry';
@@ -39,6 +39,7 @@ export {
   getLoadHandlers,
   getModelArtifactsInfoForJSON,
   getSaveHandlers,
+  http,
   IOHandler,
   isHTTPScheme,
   LoadHandler,
