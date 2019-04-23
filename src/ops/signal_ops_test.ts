3ecae911c05dbdc9bdd733cfb37ea071bff801e7
Kai Sasaki <lewuathe@gmail.com>
2019-04-23 23:53:59 +0900

    Add hannWindow and hammingWindow op (#1671)
    FEATURE
    Add [Hann window op](https://www.tensorflow.org/versions/r2.0/api_docs/python/tf/signal/hann_window) and [hamming window op](https://www.tensorflow.org/api_docs/python/tf/signal/hamming_window) since this window function is necessary to implement stft and isftf as well as hamming window. 
    
    See: https://github.com/tensorflow/tfjs/issues/1362

---
+++ src/ops/signal_ops_test.ts
@@ -0,0 +1,70 @@
+/**
+ * @license
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as tf from '../index';
+import {describeWithFlags, ALL_ENVS} from '../jasmine_util';
+import {expectArraysClose} from '../test_util';
+
+describeWithFlags('hannWindow', ALL_ENVS, () => {
+  it('length=3', () => {
+    const ret = tf.hannWindow(3);
+    expectArraysClose(ret, [0, 1, 0]);
+  });
+
+  it('length=7', () => {
+    const ret = tf.hannWindow(7);
+    expectArraysClose(ret, [0, 0.25, 0.75, 1, 0.75, 0.25, 0]);
+  });
+
+  it('length=6', () => {
+    const ret = tf.hannWindow(6);
+    expectArraysClose(ret, [0., 0.25, 0.75, 1., 0.75, 0.25]);
+  });
+
+  it('length=20', () => {
+    const ret = tf.hannWindow(20);
+    expectArraysClose(ret, [0., 0.02447176, 0.09549153, 0.20610738, 0.34549153,
+       0.5, 0.65450853, 0.79389274, 0.9045085, 0.97552824, 1., 0.97552824,
+       0.9045085, 0.7938925, 0.65450835, 0.5, 0.34549144, 0.20610726,
+       0.09549153, 0.02447173]);
+  });
+});
+
+describeWithFlags('hammingWindow', ALL_ENVS, () => {
+  it('length=3', () => {
+    const ret = tf.hammingWindow(3);
+    expectArraysClose(ret, [0.08, 1, 0.08]);
+  });
+
+  it('length=6', () => {
+    const ret = tf.hammingWindow(6);
+    expectArraysClose(ret, [0.08, 0.31, 0.77, 1., 0.77, 0.31]);
+  });
+
+  it('length=7', () => {
+    const ret = tf.hammingWindow(7);
+    expectArraysClose(ret, [0.08, 0.31,  0.77, 1, 0.77, 0.31, 0.08]);
+  });
+
+  it('length=20', () => {
+    const ret = tf.hammingWindow(20);
+    expectArraysClose(ret, [0.08000001, 0.10251403, 0.16785222, 0.2696188,
+      0.3978522,  0.54, 0.68214786, 0.8103813, 0.9121479, 0.977486, 1.,
+      0.977486, 0.9121478, 0.8103812, 0.6821477, 0.54, 0.39785212, 0.2696187,
+      0.16785222, 0.102514]);
+  });
+});
