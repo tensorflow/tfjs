00ead5c66edb915a4d18cfec2431bd867d69535b
Daniel Smilkov <dsmilkov@gmail.com>
2019-04-24 11:29:45 -0400

    Update test-ci.sh (#182)
    INTERNAL
    Remove the nightly logic to avoid double-linking, since the linking happens by the central nightly test: https://github.com/tensorflow/tfjs-core/blob/master/scripts/test-integration.sh#L54

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -9,19 +9,6 @@
 
 set -e
 
-# If this is nightly, use tfjs-core at master.
-if [ "$NIGHTLY" = true ]
-then
-  echo '########### Testing against tfjs-core@master ###########'
-  yarn run rimraf tfjs-core/
-  # depth is an optimization for faster clone (don't clone all of history).
-  git clone https://github.com/tensorflow/tfjs-core.git --depth=5
-  cd tfjs-core
-  yarn && yarn build && yarn publish-local
-  cd ..
-  yarn link-local '@tensorflow/tfjs-core'
-fi
-
 # Regular testing.
 yarn build
 yarn lint
