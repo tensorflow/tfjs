bc7d6ee0dff870c30e0b9c1dac210fe3b43827de
Shanqing Cai <cais@google.com>
2019-05-07 11:04:52 -0400

    apt-get update

--- integration_tests/benchmarks/benchmark_layers.sh
+++ integration_tests/benchmarks/benchmark_layers.sh
@@ -138,6 +138,7 @@ if [[ "${SKIP_PY_BENCHMAKRS}" == 0 ]]; then
   echo "Installing virtualenv..."
   if [[ -z "$(which pip)" ]]; then
     echo "pip is not on path. Attempting to install it..."
+    apt-get update
     apt-get install python-pip
   fi
   pip install virtualenv
