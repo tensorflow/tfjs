e49dfbd0caa05dd443ee53ecacc2da77c2028382
Shanqing Cai <cais@google.com>
2019-05-28 16:35:25 -0400

    Pin FireFox version at 66 to avoid timeout on OS X (#549)
    This is temporary. It should be reverted to latest after the
    tooling failure is fixed.
    
    https://github.com/tensorflow/tfjs/issues/1620
    
    DEV

--- karma.conf.js
+++ karma.conf.js
@@ -57,7 +57,9 @@ module.exports = function(config) {
       bs_firefox_mac: {
         base: 'BrowserStack',
         browser: 'firefox',
-        browser_version: 'latest',
+        // TODO(cais): Change to latest after browser stack infrastructure
+        // stabilizes. https://github.com/tensorflow/tfjs/issues/1620
+        browser_version: '66.0',
         os: 'OS X',
         os_version: 'High Sierra'
       },
