8b8e99b7bf0d582859814553ad0934726378a1aa
Kangyi Zhang <kangyizhang@google.com>
2019-07-19 15:03:39 -0700

    Add tf.node.decodeImage (#285)
    FEATURE
    * Add decodeImage
    
    * Format
    
    * Fix lint
    
    * format
    
    * fix lint
    
    * address comments
    
    * fix lint
    
    * update api;
    
    * fix nits
    
    * fix nit

--- src/tfjs_binding.ts
+++ src/tfjs_binding.ts
@@ -26,7 +26,7 @@ export declare class TensorMetadata {
 export declare class TFEOpAttr {
   name: string;
   type: number;
-  value: boolean|number|object|string|number[];
+  value: boolean | number | object | string | number[];
 }
 
 export interface TFJSBinding {
@@ -40,12 +40,12 @@ export interface TFJSBinding {
   deleteTensor(tensorId: number): void;
 
   // Reads data-sync from a tensor on the backend:
-  tensorDataSync(tensorId: number): Float32Array|Int32Array|Uint8Array;
+  tensorDataSync(tensorId: number): Float32Array | Int32Array | Uint8Array;
 
   // Executes an Op on the backend, returns an array of output TensorMetadata:
   executeOp(
-      opName: string, opAttrs: TFEOpAttr[], inputTensorIds: number[],
-      numOutputs: number): TensorMetadata[];
+    opName: string, opAttrs: TFEOpAttr[], inputTensorIds: number[],
+    numOutputs: number): TensorMetadata[];
 
   // TF Types
   TF_FLOAT: number;
@@ -55,6 +55,7 @@ export interface TFJSBinding {
   TF_COMPLEX64: number;
   TF_STRING: number;
   TF_RESOURCE: number;
+  TF_UINT8: number;
 
   // TF OpAttrTypes
   TF_ATTR_STRING: number;
