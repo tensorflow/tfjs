eb2e989beee0a1b1ca8d517792985c3073b972bb
WenheLI <wl1508@nyu.edu>
2019-08-06 01:19:59 +0800

    Add exports to remove importing from dist in other packages. (#1856)
    DEV
    
    This pr is to fix dist importing happens in tfjs-node.
    A corresponding pr is here, tensorflow/tfjs-node#289

--- src/backends/backend_util.ts
+++ src/backends/backend_util.ts
@@ -28,7 +28,9 @@ export * from '../ops/axis_util';
 export * from '../ops/broadcast_util';
 export * from '../ops/concat_util';
 export * from '../ops/conv_util';
-export {TypedArray, upcastType, PixelData} from '../types';
+export {Activation} from '../ops/fused_util';
+
+export {BackendValues, TypedArray, upcastType, PixelData} from '../types';
 export {MemoryInfo, TimingInfo} from '../engine';
 
 export function castTensor<T extends Tensor>(
