707a66949dd035117a0910158de52df8ef4d0dc3
kedevked <edoh.dev@gmail.com>
2019-08-07 14:15:45 +0200

    add tf.diag (#1256)
    cla: yes
    
    Add tf.diag

---
+++ src/ops/diag.ts
@@ -0,0 +1,50 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import {ENGINE} from '../engine';
+import {Tensor} from '../tensor';
+import {convertToTensor} from '../tensor_util_env';
+import {op} from './operation';
+
+/**
+ * Returns a diagonal tensor with a given diagonal values.
+ *
+ * Given a diagonal, this operation returns a tensor with the diagonal and
+ * everything else padded with zeros.
+ *
+ * Assume the input has dimensions `[D1,..., Dk]`, then the output is a tensor
+ * of rank 2k with dimensions `[D1,..., Dk, D1,..., Dk]`
+ *
+ * ```js
+ * const x = tf.tensor1d([1, 2, 3, 4]);
+ *
+ * tf.diag(x).print()
+ * ```
+ * ```js
+ * const x = tf.tensor1d([1, 2, 3, 4, 5, 6, 6, 8], [4, 2])
+ *
+ * tf.diag(x).print()
+ * ```
+ * @param x The input tensor.
+ */
+function diag_(x: Tensor): Tensor {
+  const $x = convertToTensor(x, 'x', 'diag').flatten();
+  const outShape = [...x.shape, ...x.shape];
+  return ENGINE.runKernel(backend => backend.diag($x), {$x}).reshape(outShape);
+}
+
+export const diag = op({diag_});
