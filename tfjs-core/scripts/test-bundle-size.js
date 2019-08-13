dff12fc3fc22ae9dbf0517d72e63e38e0cf8b3f5
Nikhil Thorat <nsthorat@google.com>
2019-08-13 11:41:22 -0400

    save

--- tfjs-core/scripts/test-bundle-size.js
+++ tfjs-core/scripts/test-bundle-size.js
@@ -32,13 +32,15 @@ exec(`yarn rollup -c --ci`, {silent: true});
 const minSize = getFileSizeBytes('dist/tf-core.min.js');
 
 // Clone master and get the bundle size from master.
-const dirName = '/tmp/tfjs-core-bundle/tfjs-core';
+const dirName = '/tmp/tfjs-core-bundle';
+const coreDirName = 'tfjs-core';
 exec(
     `git clone --depth=1 --single-branch ` +
-        `https://github.com/tensorflow/tfjs.git ${dirName}`,
+        `https://github.com/tensorflow/tfjs ${dirName}`,
     {silent: true});
 
 shell.cd(dirName);
+shell.cd(coreDirName);
 exec(`yarn && yarn rollup -c --ci`, {silent: true});
 
 const masterMinSize = getFileSizeBytes('dist/tf-core.min.js');
