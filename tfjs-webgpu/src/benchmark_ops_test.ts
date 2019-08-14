fecfe40a8f40098ba7594c655c44fda39d1b30e4
Ann Yuan <annyuan@gmail.com>
2019-08-14 10:46:52 -0400

    shorten tests

--- tfjs-webgpu/src/benchmark_ops_test.ts
+++ tfjs-webgpu/src/benchmark_ops_test.ts
@@ -28,8 +28,9 @@ describeWebGPU('Ops benchmarks', () => {
   // avoided by using fences, but we don't have a common abstraction over
   // WebGL and WebGPU fences at the moment.
   async function time(
-      doRep: (r: number) => tf.Tensor[], endTrial: () => Promise<void>,
-      disposeAfterEachTrial = false, trials = 50, reps = 1) {
+      doRep: (r: number) => tf.Tensor[] | tf.Tensor,
+      endTrial?: () => Promise<void>, disposeAfterEachTrial = false,
+      trials = 50, reps = 1) {
     const times = [];
 
     let toDispose: tf.Tensor[] = [];
@@ -41,10 +42,18 @@ describeWebGPU('Ops benchmarks', () => {
     };
 
     const trial = async () => {
+      let result;
       for (let r = 0; r < reps; ++r) {
-        toDispose = toDispose.concat(doRep(r));
+        result = doRep(r);
+
+        toDispose = toDispose.concat(Array.isArray(result) ? result : [result]);
       }
-      await endTrial();
+
+      if (endTrial != null) {
+        await endTrial();
+      } else {
+        await (Array.isArray(result) ? result[0] : result).data();
+      }
     };
 
     // Warm-up. Specifically, this pre-allocates enough memory for an entire
@@ -98,53 +107,26 @@ describeWebGPU('Ops benchmarks', () => {
   }, 60000);
 
   // tslint:disable-next-line:ban
-  xit('matMul', async () => {
-    let a = tf.randomNormal([500, 500]);
+  fit('matMul', async () => {
+    const a = tf.randomNormal([500, 500]);
     const b = tf.randomNormal([500, 500]);
 
-    await time(
-        () => {
-          const c = tf.matMul(a, b);
-          const toDispose = a;
-          a = c;
-          return [toDispose];
-        },
-        async () => {
-          await a.data();
-        });
-  }, 60000);
-
-  // tslint:disable-next-line:ban
-  xit('add', async () => {
-    let a = tf.randomNormal([1, 65, 65, 256]);
-    const b = tf.randomNormal([1, 65, 65, 256]);
-
-    await time(
-        () => {
-          const c = tf.add(a, b);
-          const toDispose = a;
-          a = c;
-          return [toDispose];
-        },
-        async () => {
-          await a.data();
-        });
+    await time(() => tf.matMul(a, b));
   });
 
   // tslint:disable-next-line:ban
-  xit('conv2d', async () => {
-    let a = tf.randomNormal<tf.Rank.R4>([1, 128, 128, 4]);
+  fit('add', async () => {
+    const a = tf.randomNormal([1, 65, 65, 256]);
+    const b = tf.randomNormal([1, 65, 65, 256]);
+
+    await time(() => tf.add(a, b));
+  });
+
+  // tslint:disable-next-line:ban
+  fit('conv2d', async () => {
+    const a = tf.randomNormal<tf.Rank.R4>([1, 128, 128, 4]);
     const b = tf.randomNormal<tf.Rank.R4>([25, 25, 4, 4]);
 
-    await time(
-        () => {
-          const c = tf.conv2d(a, b, 1, 'same');
-          const toDispose = a;
-          a = c;
-          return [toDispose];
-        },
-        async () => {
-          await a.data();
-        });
-  }, 60000);
+    await time(() => tf.conv2d(a, b, 1, 'same'));
+  });
 });
