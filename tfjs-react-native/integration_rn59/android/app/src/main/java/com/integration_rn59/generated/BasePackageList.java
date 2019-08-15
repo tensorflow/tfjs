a4d3b24e28c607e50ad192c1cb88a5826e2e67d3
Yannick Assogba <yassogba@google.com>
2019-08-15 14:36:08 -0700

    [tfjs-react-native] add rn integration app (#1840)
    INTERNAL
    
    Adds a react native app that can be used for integration testing.

---
+++ tfjs-react-native/integration_rn59/android/app/src/main/java/com/integration_rn59/generated/BasePackageList.java
@@ -0,0 +1,16 @@
+package com.integration_rn59.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.gl.GLPackage(),
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.permissions.PermissionsPackage()
+    );
+  }
+}
