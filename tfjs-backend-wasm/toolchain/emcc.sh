50a8445344f27f5c7225b38864083e880e685923
Daniel Smilkov <dsmilkov@gmail.com>
2019-08-20 23:34:55 -0400

    save

--- tfjs-backend-wasm/toolchain/emcc.sh
+++ tfjs-backend-wasm/toolchain/emcc.sh
@@ -3,15 +3,15 @@
 set -euo pipefail
 
 export LLVM_ROOT='external/emsdk/emsdk/fastcomp/fastcomp/bin'
-export BINARYEN_ROOT='external/emsdk/fastcomp'
+export BINARYEN_ROOT='external/emsdk/emsdk/fastcomp'
 export EMSCRIPTEN_NATIVE_OPTIMIZER='external/emsdk/emsdk/fastcomp/bin/optimizer'
-export NODE_JS='external/emsdk/node/8.9.1_64bit/bin/node'
+export NODE_JS='external/emsdk/emsdk/node/8.9.1_64bit/bin/node'
 export SPIDERMONKEY_ENGINE=''
 export V8_ENGINE=''
 export TEMP_DIR="tmp"
 export COMPILER_ENGINE=$NODE_JS
 export JS_ENGINES=$NODE_JS
-export EMSCRIPTEN_ROOT='external/emsdk/fastcomp'
+export EMSCRIPTEN_ROOT='external/emsdk/emsdk/fastcomp'
 
 export EM_EXCLUSIVE_CACHE_ACCESS=1
 export EMCC_SKIP_SANITY_CHECK=1
@@ -24,4 +24,4 @@ export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 python2 external/emsdk/emsdk/fastcomp/emscripten/emcc.py "$@"
 
 # Remove the first line of .d file
-find . -name "*.d" -exec gsed -i '2d' {} \;
+# find . -name "*.d" -exec gsed -i '2d' {} \;
