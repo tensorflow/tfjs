2ecc9f7a5d4ceefd619030aa6e29be6a2229f223
Daniel Smilkov <dsmilkov@gmail.com>
2019-08-21 14:53:38 -0400

    Add Bazel build system for the WASM backend (#1877)
    DEV
    
    Add a bazel build system for our WASM backend using the emscripten toolkit. This opens the door to depend on other TF code that uses bazel rules and allows for easier syncing back to google3.
    
    Details:
    - Move all c++ files to `src/cc`
    - Add a custom bazel toolkit that uses emsdk to compile and link (emscripten).
    - Add the correct emsdk flags
    - Bazel look for an emsdk installation available in the home directory, under the path `~/emsdk`

--- tfjs-backend-wasm/toolchain/emcc.sh
+++ tfjs-backend-wasm/toolchain/emcc.sh
@@ -14,11 +14,22 @@
 # limitations under the License.
 # =============================================================================
 
-set -e
+set -euo pipefail
 
-yarn bazel build -c opt //src/cc:tfjs-backend-wasm.js
-# The typescript code and karma config expect the output of emscripten to be in
-# wasm-out/ so we copy the bazel output there.
-cp -f bazel-bin/src/cc/tfjs-backend-wasm.js \
-      bazel-bin/src/cc/tfjs-backend-wasm.wasm \
-      wasm-out/
+export LLVM_ROOT='external/emsdk/emsdk/fastcomp/fastcomp/bin'
+export BINARYEN_ROOT='external/emsdk/emsdk/fastcomp'
+export EMSCRIPTEN_NATIVE_OPTIMIZER='external/emsdk/emsdk/fastcomp/bin/optimizer'
+export NODE_JS='external/emsdk/emsdk/node/8.9.1_64bit/bin/node'
+export SPIDERMONKEY_ENGINE=''
+export V8_ENGINE=''
+export TEMP_DIR="tmp"
+export COMPILER_ENGINE=$NODE_JS
+export JS_ENGINES=$NODE_JS
+export EMSCRIPTEN_ROOT='external/emsdk/emsdk/fastcomp'
+
+export EM_EXCLUSIVE_CACHE_ACCESS=1
+export EMCC_SKIP_SANITY_CHECK=1
+export EMCC_WASM_BACKEND=0
+
+# Run emscripten to compile and link
+python2 external/emsdk/emsdk/fastcomp/emscripten/emcc.py "$@"
