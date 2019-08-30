c3316bcc2ad9040450ab35b6c86025b1ab455a64
Nikhil Thorat <nsthorat@google.com>
2019-08-30 13:23:19 -0400

    save

--- tfjs-core/src/ops/array_ops_test.ts
+++ tfjs-core/src/ops/array_ops_test.ts
@@ -1607,17 +1607,6 @@ describeWithFlags('fromPixels', BROWSER_ENVS, () => {
     const data = await res.data();
     expect(data.length).toEqual(10 * 10 * 3);
   });
-  it('fromPixels for HTMLVideolement', async () => {
-    const video = document.createElement('video');
-    video.width = 1;
-    video.height = 1;
-    video.src = 'data:image/gif;base64' +
-        ',R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
-    const res = tf.browser.fromPixels(video);
-    expect(res.shape).toEqual([1, 1, 3]);
-    const data = await res.data();
-    expect(data.length).toEqual(1 * 1 * 3);
-  });
 
   it('throws when passed a primitive number', () => {
     const msg = /pixels passed to tf.browser.fromPixels\(\) must be either/;
