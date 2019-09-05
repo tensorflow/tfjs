9b59c53432e0de2629a1372ed74f99cf7c75c98a
Yannick Assogba <yassogba@google.com>
2019-09-05 17:20:39 -0400

    [tfjs-vis] Upgrade vega and fix tooltip displays (#1980)
    BUG
    
    * upgrade vega-embed
    * improve line chart tooltips. Fixes #1850
    * Set default dimensions for surfaces.
    * add version export. Fixes #1703

--- tfjs-vis/src/render/scatterplot.ts
+++ tfjs-vis/src/render/scatterplot.ts
@@ -18,7 +18,7 @@
 import embed, {Mode, VisualizationSpec} from 'vega-embed';
 
 import {Drawable, Point2D, XYPlotData, XYPlotOptions} from '../types';
-
+import {getDefaultHeight, getDefaultWidth} from '../util/dom';
 import {getDrawArea} from './render_utils';
 
 /**
@@ -89,8 +89,8 @@ export async function scatterplot(
   };
 
   const spec: VisualizationSpec = {
-    'width': options.width || drawArea.clientWidth,
-    'height': options.height || drawArea.clientHeight,
+    'width': options.width || getDefaultWidth(drawArea),
+    'height': options.height || getDefaultHeight(drawArea),
     'padding': 0,
     'autosize': {
       'type': 'fit',
