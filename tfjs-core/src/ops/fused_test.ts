c3fbb18bfdb02cd6fcc46858cfb45d36e042a00c
Ann Yuan <annyuan@gmail.com>
2019-09-13 10:35:21 -0400

    remove fit

--- tfjs-core/src/ops/fused_test.ts
+++ tfjs-core/src/ops/fused_test.ts
@@ -56,7 +56,7 @@ describeWithFlags('fused matmul', ALL_ENVS, () => {
     expectArraysClose(await c.data(), [0, 8, -0.9502, 20]);
   });
 
-  fit('A x B with relu6', async () => {
+  it('A x B with relu6', async () => {
     const a = tf.tensor2d([1, 2, 3, 4, 5, 6], [2, 3]);
     const b = tf.tensor2d([0, 1, -3, 2, 2, 1], [3, 2]);
     const transposeA = false;
