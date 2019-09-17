a7eff0036a7bc825fba87983ddaa2dc712942236
Yannick Assogba <yassogba@google.com>
2019-09-17 10:58:43 -0400

    fix types

--- tfjs-core/src/backends/webgl/unaryop_gpu.ts
+++ tfjs-core/src/backends/webgl/unaryop_gpu.ts
@@ -167,14 +167,14 @@ export const COS = CHECK_NAN_SNIPPET + `
 export const TAN = `return tan(x);`;
 
 export const ASIN = CHECK_NAN_SNIPPET + `
-if (abs(x) > 1) {
+if (abs(x) > 1.) {
     return NAN;
   }
   return asin(x);
 `;
 
 export const ACOS = CHECK_NAN_SNIPPET + `
-  if (abs(x) > 1) {
+  if (abs(x) > 1.) {
     return NAN;
   }
   return acos(x);
