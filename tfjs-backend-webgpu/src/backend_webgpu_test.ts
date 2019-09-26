824d50c542e4468f6ba18cb1cc010be8888afeb1
Ann Yuan <annyuan@gmail.com>
2019-09-26 10:54:07 -0400

    add test

--- tfjs-backend-webgpu/src/backend_webgpu_test.ts
+++ tfjs-backend-webgpu/src/backend_webgpu_test.ts
@@ -210,4 +210,20 @@ describeWebGPU('backend webgpu', () => {
         await backend.read(t.dataId), new Float32Array([4, 5, 6]));
     expect(bufferManager.getNumUsedBuffers()).toBe(0);
   });
+
+  it('should be possible to move data from webgl to webgpu', async () => {
+    tf.setBackend('webgl');
+    const a = tf.randomNormal([1, 65, 65, 256]);
+    const b = tf.randomNormal([1, 65, 65, 256]);
+    const c = tf.add(a, b);
+    await c.data();
+
+    const f = async () => {
+      tf.setBackend('webgpu');
+      const d = tf.add(a, b);
+      await d.data();
+    };
+
+    expect(f).not.toThrow();
+  });
 });
