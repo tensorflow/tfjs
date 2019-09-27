940a40117c57e526eadd1fba45d86ac2bd02a522
Shanqing Cai <cais@google.com>
2019-09-27 17:11:08 -0400

    Back out unwanted changes

--- tfjs-layers/src/user_defined_metadata_test.ts
+++ tfjs-layers/src/user_defined_metadata_test.ts
@@ -12,7 +12,7 @@ import {io, zeros} from '@tensorflow/tfjs-core';
 
 import * as tfl from './index';
 import {Sequential} from './models';
-import {MAX_USER_DEFINED_METADATA_SERIALIZED_LENGTH, plainObjectCheck} from './user_defined_metadata';
+import {plainObjectCheck, MAX_USER_DEFINED_METADATA_SERIALIZED_LENGTH} from './user_defined_metadata';
 
 describe('plainObjectCheck', () => {
   it('Primitives', () => {
@@ -26,15 +26,19 @@ describe('plainObjectCheck', () => {
   });
   it('Complex objects lead to false', () => {
     expect(plainObjectCheck(new Date())).toEqual(false);
-    expect(plainObjectCheck(new Float32Array([1, 2]))).toEqual(false);
+    expect(plainObjectCheck(new Float32Array([1, 2])))
+        .toEqual(false);
     expect(plainObjectCheck(new ArrayBuffer(4))).toEqual(false);
     expect(plainObjectCheck(new Error())).toEqual(false);
     expect(plainObjectCheck(zeros([2, 3]))).toEqual(false);
   });
   it('POJOs lead to true', () => {
     expect(plainObjectCheck({})).toEqual(true);
-    expect(plainObjectCheck({'key1': 'foo', 'key2': 1337, 'key3': false}))
-        .toEqual(true);
+    expect(plainObjectCheck({
+      'key1': 'foo',
+      'key2': 1337,
+      'key3': false
+    })).toEqual(true);
     expect(plainObjectCheck({
       'key1': {
         'key1_1': [1, 3, 3, 7, [42]],
@@ -46,18 +50,33 @@ describe('plainObjectCheck', () => {
     })).toEqual(true);
   });
   it('POJOs with invalid value types lead to false', () => {
-    expect(plainObjectCheck({'key1': new Date(), 'key2': 1337})).toEqual(false);
-    expect(plainObjectCheck({'key1': new ArrayBuffer(3), 'key2': 1337}))
-        .toEqual(false);
-    expect(plainObjectCheck({'key1': 'foo', 'key2': undefined})).toEqual(false);
-    expect(plainObjectCheck({'key1': 'foo', 'key2': {'tensor': zeros([2, 3])}}))
-        .toEqual(false);
+    expect(plainObjectCheck({
+      'key1': new Date(),
+      'key2': 1337
+    })).toEqual(false);
+    expect(plainObjectCheck({
+      'key1': new ArrayBuffer(3),
+      'key2': 1337
+    })).toEqual(false);
+    expect(plainObjectCheck({
+      'key1': 'foo',
+      'key2': undefined
+    })).toEqual(false);
+    expect(plainObjectCheck({
+      'key1': 'foo',
+      'key2': {
+        'tensor': zeros([2, 3])
+      }
+    })).toEqual(false);
   });
   it('Arrays of POJO lead to true', () => {
     expect(plainObjectCheck([])).toEqual(true);
     expect(plainObjectCheck([{}, {}])).toEqual(true);
-    expect(plainObjectCheck([{'key1': 'foo', 'key2': 1337, 'key3': false}]))
-        .toEqual(true);
+    expect(plainObjectCheck([{
+      'key1': 'foo',
+      'key2': 1337,
+      'key3': false
+    }])).toEqual(true);
     expect(plainObjectCheck([{
       'key1': {
         'key1_1': [1, 3, 3, 7],
@@ -71,10 +90,14 @@ describe('plainObjectCheck', () => {
 });
 
 describe('Save and load model with metadata', () => {
+
   function createSequentialModelForTest(): Sequential {
     const model = tfl.sequential();
-    model.add(
-        tfl.layers.dense({units: 3, inputShape: [10], activation: 'softmax'}));
+    model.add(tfl.layers.dense({
+      units: 3,
+      inputShape: [10],
+      activation: 'softmax'
+    }));
     return model;
   }
 
@@ -85,16 +108,15 @@ describe('Save and load model with metadata', () => {
     const dense2 = tfl.layers.dense({units: 1, inputShape: [4]});
     const y1 = dense1.apply(input1) as tfl.SymbolicTensor;
     const y2 = dense2.apply(input2) as tfl.SymbolicTensor;
-    const output =
-        tfl.layers.concatenate().apply([y1, y2]) as tfl.SymbolicTensor;
+    const output = tfl.layers.concatenate().apply([y1, y2]) as
+        tfl.SymbolicTensor;
     return tfl.model({inputs: [input1, input2], outputs: output});
   }
 
   for (const modelType of ['sequential', 'functional']) {
     it(`Valid user-defined metadata round trip: ${modelType}`, async () => {
       const model = modelType === 'sequential' ?
-          createSequentialModelForTest() :
-          createFunctionalModelForTest();
+          createSequentialModelForTest() : createFunctionalModelForTest();
       const userDefinedMetadata = {'outputLabels': ['foo', 'bar', 'baz']};
       model.setUserDefinedMetadata(userDefinedMetadata);
       expect(model.getUserDefinedMetadata()).toEqual(userDefinedMetadata);
@@ -104,8 +126,8 @@ describe('Save and load model with metadata', () => {
             savedArtifacts = artifacts;
             return {modelArtifactsInfo: null};
           }));
-      const reloadedModel =
-          await tfl.loadLayersModel(io.fromMemory(savedArtifacts));
+      const reloadedModel = await tfl.loadLayersModel(
+          io.fromMemory(savedArtifacts));
       expect(reloadedModel.getUserDefinedMetadata())
           .toEqual(userDefinedMetadata);
     });
@@ -113,18 +135,16 @@ describe('Save and load model with metadata', () => {
   for (const modelType of ['sequential', 'functional']) {
     it(`Invalid user metadata leads to error: ${modelType}`, async () => {
       const model = modelType === 'sequential' ?
-          createSequentialModelForTest() :
-          createFunctionalModelForTest();
-      expect(() => model.setUserDefinedMetadata(JSON.stringify({
-        'outputLabels': ['foo', 'bar', 'baz']
-      }))).toThrowError(/is expected to be a JSON object, but is not/);
-      expect(() => model.setUserDefinedMetadata([
-        'foo', 'bar', 'baz'
-      ])).toThrowError(/is expected to be a JSON object, but is not/);
-      expect(() => model.setUserDefinedMetadata({
-        'foo': zeros([2, 3]),
-        'outputLabels': ['foo', 'bar', 'baz']
-      })).toThrowError(/is expected to be a JSON object, but is not/);
+          createSequentialModelForTest() : createFunctionalModelForTest();
+      expect(() => model.setUserDefinedMetadata(
+          JSON.stringify({'outputLabels': ['foo', 'bar', 'baz']})))
+          .toThrowError(/is expected to be a JSON object, but is not/);
+      expect(() => model.setUserDefinedMetadata(
+          ['foo', 'bar', 'baz']))
+          .toThrowError(/is expected to be a JSON object, but is not/);
+      expect(() => model.setUserDefinedMetadata(
+          {'foo': zeros([2, 3]), 'outputLabels': ['foo', 'bar', 'baz']}))
+          .toThrowError(/is expected to be a JSON object, but is not/);
       expect(() => model.setUserDefinedMetadata(undefined))
           .toThrowError(/is expected to be a JSON object, but is not/);
       expect(() => model.setUserDefinedMetadata(null))
@@ -145,8 +165,7 @@ describe('Save and load model with metadata', () => {
         'metadata': 'x'.repeat(MAX_USER_DEFINED_METADATA_SERIALIZED_LENGTH)
       };
       const model = modelType === 'sequential' ?
-          createSequentialModelForTest() :
-          createFunctionalModelForTest();
+          createSequentialModelForTest() : createFunctionalModelForTest();
       model.setUserDefinedMetadata(largeMetadata);
       await model.save(
           io.withSaveHandler(async (artifacts: io.ModelArtifacts) => {
@@ -160,5 +179,6 @@ describe('Save and load model with metadata', () => {
     expect(MAX_USER_DEFINED_METADATA_SERIALIZED_LENGTH).toBeGreaterThan(0);
     expect(Number.isInteger(MAX_USER_DEFINED_METADATA_SERIALIZED_LENGTH))
         .toEqual(true);
+
   });
 });
