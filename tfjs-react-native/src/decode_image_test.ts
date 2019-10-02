6c11ba8ae4f2941418ac1b7eec5b59b0746b5724
Kevin VanGelder <kevin@infinite.red>
2019-10-02 13:33:18 -0700

    fix quotemark and add tests

---
+++ tfjs-react-native/src/decode_image_test.ts
@@ -0,0 +1,126 @@
+/**
+ * @license
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import { memory, setBackend, test_util } from '@tensorflow/tfjs-core';
+import * as fs from 'fs';
+import { promisify } from 'util';
+import * as tf from './index';
+
+const readFile = promisify(fs.readFile);
+
+describe('decode images', () => {
+  it('decode jpg', async () => {
+    const beforeNumTensors: number = memory().numTensors;
+    const uint8array =
+      await getUint8ArrayFromImage('test_images/image_jpeg_test.jpeg');
+    const imageTensor = await tf.decodeJpeg(uint8array);
+    expect(imageTensor.dtype).toBe('int32');
+    expect(imageTensor.shape).toEqual([2, 2, 3]);
+    test_util.expectArraysEqual(
+      await imageTensor.data(),
+      [239, 100, 0, 46, 48, 47, 92, 49, 0, 194, 98, 47]);
+    expect(memory().numTensors).toBe(beforeNumTensors + 1);
+  });
+
+  it('decode jpg 1 channel', async () => {
+    const beforeNumTensors: number = memory().numTensors;
+    const uint8array =
+      await getUint8ArrayFromImage('test_images/image_jpeg_test.jpeg');
+    const imageTensor = await tf.decodeImage(uint8array, 1);
+    expect(imageTensor.dtype).toBe('int32');
+    expect(imageTensor.shape).toEqual([2, 2, 1]);
+    test_util.expectArraysEqual(await imageTensor.data(), [130, 47, 56, 121]);
+    expect(memory().numTensors).toBe(beforeNumTensors + 1);
+  });
+
+  it('decode jpg 3 channels', async () => {
+    const beforeNumTensors: number = memory().numTensors;
+    const uint8array =
+      await getUint8ArrayFromImage('test_images/image_jpeg_test.jpeg');
+    const imageTensor = await tf.decodeImage(uint8array, 3);
+    expect(imageTensor.dtype).toBe('int32');
+    expect(imageTensor.shape).toEqual([2, 2, 3]);
+    test_util.expectArraysEqual(
+      await imageTensor.data(),
+      [239, 100, 0, 46, 48, 47, 92, 49, 0, 194, 98, 47]);
+    expect(memory().numTensors).toBe(beforeNumTensors + 1);
+  });
+
+  it('decode jpg with 0 channels, use the number of channels in the ' +
+    'JPEG-encoded image',
+    async () => {
+      const beforeNumTensors: number = memory().numTensors;
+      const uint8array =
+        await getUint8ArrayFromImage('test_images/image_jpeg_test.jpeg');
+      const imageTensor = await tf.decodeImage(uint8array);
+      expect(imageTensor.dtype).toBe('int32');
+      expect(imageTensor.shape).toEqual([2, 2, 3]);
+      test_util.expectArraysEqual(
+        await imageTensor.data(),
+        [239, 100, 0, 46, 48, 47, 92, 49, 0, 194, 98, 47]);
+      expect(memory().numTensors).toBe(beforeNumTensors + 1);
+    });
+
+  it('throw error if request non int32 dtype', async done => {
+    try {
+      const uint8array =
+        await getUint8ArrayFromImage('test_images/image_png_test.png');
+      await tf.decodeImage(uint8array, 0, 'uint8');
+      done.fail();
+    } catch (error) {
+      expect(error.message)
+        .toBe(
+          'decodeImage could only return Tensor of type `int32` for now.');
+      done();
+    }
+  });
+
+  it('throw error if decode invalid image type', async done => {
+    try {
+      const uint8array = await getUint8ArrayFromImage('package.json');
+      await tf.decodeImage(uint8array);
+      done.fail();
+    } catch (error) {
+      expect(error.message)
+        .toBe(
+          'Expected image (JPEG, PNG, or GIF), ' +
+          'but got unsupported image type');
+      done();
+    }
+  });
+
+  it('throw error if backend is not tensorflow', async done => {
+    try {
+      setBackend('cpu');
+      const uint8array =
+        await getUint8ArrayFromImage('test_images/image_png_test.png');
+      await tf.decodeImage(uint8array);
+      done.fail();
+    } catch (err) {
+      expect(err.message)
+        .toBe('Expect the current backend to be "tensorflow", but got "cpu"');
+      setBackend('tensorflow');
+      done();
+    }
+  });
+});
+
+async function getUint8ArrayFromImage(path: string) {
+  const image = await readFile(path);
+  const buf = Buffer.from(image);
+  return new Uint8Array(buf);
+}
