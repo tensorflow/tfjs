08c9b030b444b5d3436d3dada9705ee6ef1423bc
Ann Yuan <annyuan@gmail.com>
2019-10-11 16:43:54 -0400

    rotate cpu

--- tfjs-core/src/backends/cpu/backend_cpu.ts
+++ tfjs-core/src/backends/cpu/backend_cpu.ts
@@ -3626,8 +3626,45 @@ export class MathBackendCPU extends KernelBackend {
     const output = ops.buffer(image.shape, image.dtype);
     const [batch, imageHeight, imageWidth, numChannels] = image.shape;
 
+    const centerX =
+        imageWidth * (typeof center === 'number' ? center : center[0]);
+    const centerY =
+        imageHeight * (typeof center === 'number' ? center : center[1]);
+
+    const sinFactor = Math.sin(radians);
+    const cosFactor = Math.cos(radians);
+    const imageVals = this.readSync(image.dataId) as TypedArray;
+
     for (let row = 0; row < imageHeight; row++) {
       for (let col = 0; col < imageWidth; col++) {
+        for (let channel = 0; channel < numChannels; channel++) {
+          const coords = [batch, row, col, channel];
+
+          const x = coords[2];
+          const y = coords[1];
+
+          let coordX = (x - centerX) * cosFactor - (y - centerY) * sinFactor;
+          let coordY = (x - centerX) * sinFactor + (y - centerY) * cosFactor;
+
+          coordX = Math.floor(coordX + centerX);
+          coordY = Math.floor(coordY + centerY);
+
+          let outputValue = fillValue;
+          if (typeof fillValue !== 'number') {
+            outputValue = fillValue[channel];
+          }
+
+          if (coordX > 0 && coordX < imageWidth && coordY > 0 &&
+              coordY < imageHeight) {
+            const imageIdx = coordY * (imageWidth * numChannels) +
+                coordX * numChannels + channel;
+            outputValue = imageVals[imageIdx];
+          }
+
+          const outIdx =
+              row * (imageWidth * numChannels) + col * numChannels + channel;
+          output.values[outIdx] = outputValue as number;
+        }
       }
     }
 
