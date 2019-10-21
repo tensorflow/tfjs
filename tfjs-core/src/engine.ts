3cc2cb959aaaa2b3a7321412648f4bb719df96f1
Daniel Smilkov <dsmilkov@gmail.com>
2019-10-21 16:21:45 -0400

    [Core] Remove `save` from kernels (#2248)
    INTERNAL
    
    Modular kernels shouldn't deal with saving results for back-prop since back-prop is backend-agnostic operation and should happen in the logical op.
    
    We generalize `runKernel()` to take `inputsToKeep: Tensor[]` and `outputsToKeep: boolean[]` as optional args which get passed to `runKernelFunc()`.

--- tfjs-core/src/engine.ts
+++ tfjs-core/src/engine.ts
@@ -17,7 +17,7 @@
 
 import {BackendTimingInfo, DataMover, KernelBackend} from './backends/backend';
 import {Environment, setEnvironmentGlobal} from './environment';
-import {getKernel, getKernelsForBackend, NamedAttrMap, NamedTensorInfoMap, TensorInfo} from './kernel_registry';
+import {getKernel, getKernelsForBackend, NamedAttrMap, TensorInfo} from './kernel_registry';
 import {Profiler} from './profiler';
 import {backpropagateGradients, getFilteredNodesXToY, NamedGradientMap, TapeNode} from './tape';
 import {DataId, setTensorTracker, Tensor, Tensor3D, TensorTracker, Variable} from './tensor';
@@ -469,23 +469,29 @@ export class Engine implements TensorTracker, DataMover {
   }
 
   /**
-   * Execute a kernel with the given name and return the output tensor info.
+   * Execute a kernel with the given name and return the output tensor.
    *
    * @param kernelName The name of the kernel to execute.
-   * @param inputs A map of input names to tensor infos.
+   * @param inputs A map of input names to tensors.
    * @param attrs A map of attribute names to their values. An attribute is a
    *     primitive (non-tensor) input to the kernel.
+   * @param inputsToSave A list of tensors, inputs to save for the backprop
+   *     computation.
+   * @param outputsToSave A list of booleans, specifying which output to save
+   *     for the backprop computation. These are booleans since the output
+   * tensors are not visible to the user.
    */
   runKernel(
-      kernelName: string, inputs: NamedTensorInfoMap,
-      attrs: NamedAttrMap): TensorInfo|TensorInfo[] {
+      kernelName: string, inputs: NamedTensorMap, attrs: NamedAttrMap,
+      inputsToSave?: Tensor[], outputsToSave?: boolean[]): Tensor|Tensor[] {
     const forwardFunc: null = null;
     const backwardsFunc: null = null;
     // Call runKernel as a stop-gap until we modularize all kernels.
-    // Once we modularize all kernels, we will remove the existing runKernel().
+    // Once we modularize all kernels, we will remove the existing
+    // `runKernelFunc`.
     return this.runKernelFunc(
-        forwardFunc, inputs as NamedTensorMap, backwardsFunc, kernelName,
-        attrs);
+        forwardFunc, inputs, backwardsFunc, kernelName, attrs, inputsToSave,
+        outputsToSave);
   }
 
   private shouldCheckForMemLeaks(): boolean {
@@ -528,7 +534,8 @@ export class Engine implements TensorTracker, DataMover {
   runKernelFunc<T extends Tensor|Tensor[], I extends NamedTensorMap>(
       forwardFunc: ForwardFunc<T>, inputs: I,
       backwardsFunc?: (dy: T, saved: Tensor[]) => {[P in keyof I]: () => I[P]},
-      kernelName?: string, attrs?: NamedAttrMap): T {
+      kernelName?: string, attrs?: NamedAttrMap, inputsToSave: Tensor[] = [],
+      outputsToSave: boolean[] = []): T {
     let outputs: Tensor[];
     let saved: Tensor[] = [];
     const isTapeOn = this.isTapeOn();
@@ -557,15 +564,18 @@ export class Engine implements TensorTracker, DataMover {
     if (kernel != null) {
       kernelFunc = () => {
         const numDataIdsBefore = this.backend.numDataIds();
-        out = kernel.kernelFunc(
-            {inputs, attrs, backend: this.backend, save: saveFunc});
+        out = kernel.kernelFunc({inputs, attrs, backend: this.backend});
         const outInfos = Array.isArray(out) ? out : [out];
         if (this.shouldCheckForMemLeaks()) {
           this.checkKernelForMemLeak(scopeName, numDataIdsBefore, outInfos);
         }
-        return outInfos.map(
+        const outTensors = outInfos.map(
             ({dataId, shape, dtype}) =>
                 this.makeTensorFromDataId(dataId, shape, dtype));
+        const outsToSave = outTensors.filter((_, i) => outputsToSave[i]);
+        // Save the inputs and outputs.
+        saveFunc(inputsToSave.slice().concat(outsToSave));
+        return outTensors;
       };
     } else {
       kernelFunc = () => {
