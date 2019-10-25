ffb38e202fdaa7d4e585147c74fab95b7bf898ab
Daniel Smilkov <dsmilkov@gmail.com>
2019-10-25 14:57:46 -0400

    save

--- tfjs/karma.conf.js
+++ tfjs/karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function(config) {
     },
     karmaTypescriptConfig: {
       tsconfig: 'tsconfig.json',
-      reports: {}  // Do not produce coverage html.
+      reports: {} // Do not produce coverage html.
     },
     reporters: ['progress', 'karma-typescript'],
     browsers: ['Chrome', 'Firefox'],
@@ -45,7 +45,10 @@ module.exports = function(config) {
       bs_firefox_mac: {
         base: 'BrowserStack',
         browser: 'firefox',
-        browser_version: 'latest',
+        // TODO(cais): Restore 'latest' to browser_version once ongoing
+        // instability in BrowserStack + OS X + FireFox is resolved.
+        // https://github.com/tensorflow/tfjs/issues/1620
+        browser_version: '66.0',
         os: 'OS X',
         os_version: 'Sierra'
       },
@@ -54,6 +57,8 @@ module.exports = function(config) {
         flags: ['--blacklist-accelerated-compositing', '--blacklist-webgl']
       }
     },
-    client: {args: ['--grep', config.grep || '']}
+    client: {
+      args: ['--grep', config.grep || '']
+    }
   });
 };
