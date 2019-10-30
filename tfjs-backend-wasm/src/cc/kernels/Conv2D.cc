81541e7f0d9e86aca4d876ee2349441b71057102
Nikhil Thorat <nsthorat@google.com>
2019-10-29 17:35:29 -0700

    save

--- tfjs-backend-wasm/src/cc/kernels/Conv2D.cc
+++ tfjs-backend-wasm/src/cc/kernels/Conv2D.cc
@@ -91,6 +91,7 @@ void Conv2D(int x_id, int batch_size, int input_height, int input_width,
     const int groups = 1;
 
     // TODO(nsthorat): Make this a nullptr when xnn pack supports a null bias.
+    // Actually this needs to be fixed because it's a memory leak.
     const float* bias_buf = new float[output_channels]();
 
     xnn_status status = xnn_create_convolution2d_nhwc_f32(
