9deda6fb9cff4c4d385b078dd1e7078f813d6257
Nikhil Thorat <nsthorat@google.com>
2019-10-31 10:18:45 -0700

    [wasm] Refactor setup_test to have hierarchical include / exclude.

--- tfjs-backend-wasm/src/setup_test.ts
+++ tfjs-backend-wasm/src/setup_test.ts
@@ -24,11 +24,109 @@ const env = jasmine.getEnv();
 // Account for --grep flag passed to karma by saving the existing specFilter.
 const grepFilter = env.specFilter;
 
+interface TestFilter {
+  include: string;
+  excludes?: string[];
+}
+
 /** Tests that have these substrings in their name will be included. */
-const INCLUDE_LIST: string[] = [
-  'add ', 'matmul ', 'prelu ', ' cast', 'sigmoid', 'abs ', 'sub ', 'mul ',
-  'div ', 'batchNorm', 'slice ', 'square '
+const INCLUDE_LIST: TestFilter[] = [
+  {
+    include: 'add ',
+    excludes: [
+      'gradient',                   // Gradient is missing.
+      'broadcast inner dim',        // Broadcast inner dim not yet supported.
+      'broadcast each with 1 dim',  // Same as above.
+      'broadcasting same rank Tensors different shape',  // Same as above.
+      'upcasts when dtypes dont match',  // Uses the 'complex' dtype.
+      'complex',                         // Complex numbers not supported yet
+    ]
+  },
+  {
+    include: 'matmul ',
+    excludes: [
+      'valueAndGradients',       // Gradients not defined yet
+      'gradient',                // Gradients not defined yet
+      'fused matmul',            // Fused kernels aren't ready yet
+      'zero in its shape',       // Zero in shapes aren't supported yet
+      'matmul followed by mul',  // mul not supported yet
+      'upcasts',                 // Upcasting not supported yet.
+    ]
+  },
+  {
+    include: 'prelu ',
+    excludes: [
+      'gradient',   // Gradient is missing.
+      'derivative'  // Missing gradient.
+    ]
+  },
+  {
+    include: ' cast ',
+    excludes: [
+      'complex',  // Complex numbers not yet implemented.
+      'shallow slice an input that was cast'  // Slice is not implemented.
+    ]
+  },
+  {
+    include: 'sigmoid ',
+    excludes: [
+      'sigmoidCrossEntropy',  // Not yet implemented.
+      'gradient'              // Not yet implemented.
+    ]
+  },
+  {
+    include: 'abs ',
+    excludes: [
+      'gradient',  // Not yet implemented.
+      'complex'    // Complex numbers not supported yet.
+    ]
+  },
+  {
+    include: 'sub ',
+    excludes: [
+      'complex',              // Complex numbers not yet implemented.
+      'gradient',             // Not yet implemented.
+      'upcasts',              // Upcasting not supported yet.
+      'broadcast inner dim',  //  Broadcasting along inner dims not supported.
+      'broadcast each with 1 dim',  //  Broadcasting along inner dims not
+                                    //  supported.
+      'broadcasting same rank Tensors different shape',  //  Broadcasting along
+                                                         //  inner dims not
+      //  supported.
+    ]
+  },
+  {
+    include: 'mul ',
+    excludes: [
+      'complex',   // Complex numbers not yet supported.
+      'gradient',  // Gradient not defined yet.
+      'broadcasting same rank Tensors different shape',  // Broadcasting along
+                                                         // inner dims not
+                                                         // supported yet.
+      'broadcast 5D + 2D',  // Broadcasting along inner dims not supported yet.
+      'broadcast 6D + 2D'   // Broadcasting along inner dims not supported yet.
+    ]
+  },
+  {
+    include: 'div ',
+    excludes: [
+      'gradient',          // Gradient not defined yet.
+      'integer division',  // FloorDiv not yet implemented.
+      'upcasts',           // Cast not supported yet.
+      'broadcasting same rank Tensors different shape',  // Broadcasting along
+                                                         // inner dims not
+                                                         // supported yet.
+    ]
+  },
+  {
+    include: 'batchNorm ',
+    excludes: [
+      'gradient'  // Gradient is missing.
+    ]
+  },
+  {include: 'slice '}, {include: 'square '}
 ];
+
 /** Tests that have these substrings in their name will be excluded. */
 const EXCLUDE_LIST: string[] = [
   'complex',                    // Complex numbers not yet implemented.
@@ -57,15 +155,17 @@ const EXCLUDE_LIST: string[] = [
   'integer division',  // FloorDiv not yet implemented.
 
   // Mul
-  'broadcast 5D + 2D',  // Broadcasting along inner dims not supported yet.
+  'broadcast 5D + 2D',  // Broadcasting along inner dims not supported
+                        // yet.
   'broadcast 6D + 2D'   // Broadcasting along inner dims not supported yet.
 ];
+console.log(EXCLUDE_LIST);
 
 /**
  * Filter method that returns boolean, if a given test should run or be
- * ignored based on its name. The exclude list has priority over the include
- * list. Thus, if a test matches both the exclude and the include list, it
- * will be exluded.
+ * ignored based on its name. The exclude list has priority over the
+ * include list. Thus, if a test matches both the exclude and the include
+ * list, it will be exluded.
  */
 env.specFilter = spec => {
   // Filter out tests if the --grep flag is passed.
@@ -74,12 +174,6 @@ env.specFilter = spec => {
   }
 
   const name = spec.getFullName();
-  // Return false (skip the test) if the test is in the exclude list.
-  for (let i = 0; i < EXCLUDE_LIST.length; ++i) {
-    if (name.indexOf(EXCLUDE_LIST[i]) > -1) {
-      return false;
-    }
-  }
 
   // Include all regular describe() tests.
   if (name.indexOf('test-wasm') < 0) {
@@ -91,10 +185,19 @@ env.specFilter = spec => {
     return true;
   }
 
-  // Include a describeWithFlags() test from tfjs-core only if the test is in
-  // the include list.
+  // Include a describeWithFlags() test from tfjs-core only if the test is
+  // in the include list.
   for (let i = 0; i < INCLUDE_LIST.length; ++i) {
-    if (name.indexOf(INCLUDE_LIST[i]) > -1) {
+    const testFilter = INCLUDE_LIST[i];
+    if (name.indexOf(testFilter.include) > -1) {
+      if (testFilter.excludes != null) {
+        for (let j = 0; j < testFilter.excludes.length; j++) {
+          if (name.indexOf(testFilter.excludes[j]) > -1) {
+            return false;
+          }
+        }
+      }
+      console.log(name);
       return true;
     }
   }
