18da0505c2f895bc7444e4e398cac7ff7e7feca5
Daniel Smilkov <dsmilkov@gmail.com>
2019-11-06 15:56:54 -0500

    save

--- tfjs-backend-wasm/src/cc/kernels/PadV2.cc
+++ tfjs-backend-wasm/src/cc/kernels/PadV2.cc
@@ -21,35 +21,59 @@
 
 namespace {
 
-// Optimized transpose 2D that uses direct pointer arithmetic instead of bracket
-// indexing.
+// Reference:
+// https://github.com/tensorflow/tensorflow/blob/a4190274142b70f3d5b7a27f93fc14abc7448240/tensorflow/lite/kernels/internal/optimized/optimized_ops.h#L4873
 template <typename T>
-void transpose_2d(const T* x_data, const std::vector<int>& x_shape,
-                  T* out_data) {
-  const int d0 = x_shape[0];
-  const int d1 = x_shape[1];
-  const T* input = x_data;
-  for (int i = 0; i < d0; ++i) {
-    T* output = out_data + i;
-    for (int j = 0; j < d1; ++j) {
-      *output = *input;
-      output += d0;
-      ++input;
+void typed_memset(void* ptr, T value, size_t num) {
+  // Optimization for common cases where memset() will suffice.
+  if (value == 0 || std::is_same<T, uint8_t>::value) {
+    memset(ptr, value, num * sizeof(T));
+  } else {
+    // Default implementation for cases where memset() will not preserve the
+    // bytes, e.g., typically when sizeof(T) > sizeof(uint8_t).
+    char* pos = static_cast<char*>(ptr);
+    for (size_t i = 0; i < num; ++i) {
+      memcpy(pos, &value, sizeof(T));
+      pos += sizeof(T);
     }
   }
 }
 
 template <typename T>
-void transpose_impl(const T* x_data, const std::vector<int>& x_shape,
-                    const std::vector<int>& perm, T* out_data) {
-  if (x_shape.size() == 2) {
-    transpose_2d(x_data, x_shape, out_data);
-  } else if (x_shape.size() == 3) {
-    transpose_3d(x_data, x_shape, perm, out_data);
-  } else if (x_shape.size() == 4) {
-    transpose_4d(x_data, x_shape, perm, out_data);
-  } else {
-    slow_transpose_nd(x_data, x_shape, perm, out_data);
+void pad_2d(const T* x_data, const std::vector<int>& x_shape,
+            const std::vector<int>& paddings, const int pad_value,
+            T* out_data) {
+  const int left_b_pad = paddings[0];
+  const int right_b_pad = paddings[1];
+  const int left_d_pad = paddings[2];
+  const int right_d_pad = paddings[3];
+
+  const int batch = x_shape[0];
+  const int depth = x_shape[1];
+
+  T* out_offset = out_data;
+  const T* x_offset = x_data;
+
+  if (left_b_pad != 0) {
+    typed_memset<T>(out_offset, pad_value, left_b_pad * depth);
+    out_offset += left_b_pad * depth;
+  }
+  for (int b = 0; b < batch; ++b) {
+    if (left_d_pad != 0) {
+      typed_memset<T>(out_offset, pad_value, left_d_pad);
+      out_offset += left_d_pad;
+    }
+    memcpy(out_offset, x_offset, depth * sizeof(T));
+    x_offset += depth;
+    out_offset += depth;
+
+    if (right_d_pad != 0) {
+      typed_memset<T>(out_offset, pad_value, right_d_pad);
+      out_offset += right_d_pad;
+    }
+  }
+  if (right_b_pad != 0) {
+    typed_memset<T>(out_offset, pad_value, right_b_pad * depth);
   }
 }
 
@@ -57,14 +81,11 @@ template <typename T>
 void pad(const T* x_data, const std::vector<int>& x_shape,
          const std::vector<int>& paddings, const int constant_value,
          T* out_data) {
-  std::vector<int> new_x_shape;
-  std::vector<int> new_perm;
-  // Try to reduce the rank of the transpose by flattening any outer-most
-  // dimensions.
-  const int non_flatten_size = flatten(x_shape, perm, &new_x_shape, &new_perm);
-  const int total_size = tfjs::util::size_from_shape(x_shape);
-  for (int offset = 0; offset < total_size; offset += non_flatten_size) {
-    transpose_impl(x_data + offset, new_x_shape, new_perm, out_data + offset);
+  if (x_shape.size() == 2) {
+    pad_2d(x_data, x_shape, paddings, constant_value, out_data);
+  } else {
+    tfjs::util::warn("Padding for rank %d is not yet supported",
+                     x_shape.size());
   }
 }
 
@@ -86,23 +107,22 @@ void PadV2(const int x_id, const int* x_shape_ptr, const int x_shape_length,
   auto paddings =
       std::vector<int>(paddings_ptr, paddings_ptr + paddings_length);
   auto& x_info = backend::get_tensor_info(x_id);
-  auto& out_info = backend::get_tensor_info(out_id);
+  auto& out_info = backend::get_tensor_info_out(out_id);
   switch (dtype) {
     case DType::float32:
-      pad<float>(reinterpret_cast<const float*>(x_info.memory_offset), x_shape,
-                 paddings, constant_value,
-                 reinterpret_cast<float*>(out_info.memory_offset));
+      pad<float>(x_info.f32(), x_shape, paddings, constant_value,
+                 out_info.f32_write());
       break;
     case DType::int32:
-      pad<int>(reinterpret_cast<const int*>(x_info.memory_offset), x_shape,
-               perm, reinterpret_cast<int*>(out_info.memory_offset));
+      pad<int>(x_info.i32(), x_shape, paddings, constant_value,
+               out_info.i32_write());
       break;
     case DType::boolean:
-      pad<bool>(reinterpret_cast<const bool*>(x_info.memory_offset), x_shape,
-                perm, reinterpret_cast<bool*>(out_info.memory_offset));
+      pad<bool>(x_info.b(), x_shape, paddings, constant_value,
+                out_info.b_write());
       break;
     default:
-      util::warn("Transpose for tensor id %d failed. Unknown dtype % d ", x_id,
+      util::warn("Pad for tensor id %d failed. Unknown dtype % d ", x_id,
                  dtype);
   }
 }
--- tfjs-backend-wasm/src/setup_test.ts
+++ tfjs-backend-wasm/src/setup_test.ts
@@ -159,6 +159,7 @@ const TEST_FILTERS: TestFilter[] = [
     include: 'transpose',
     excludes: ['oneHot']  // oneHot not yet implemented.
   },
+  {include: 'pad ', excludes: ['complex', 'zerosLike']},
 ];
 
 const customInclude = (testName: string) => {
