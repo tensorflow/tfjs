77058ec2b5ed5c50bf0a85c0d933d8db7fd08867
Ann Yuan <annyuan@gmail.com>
2019-11-26 07:32:59 -0500

    add

--- tfjs-backend-wasm/src/cc/kernels/ResizeBilinear.cc
+++ tfjs-backend-wasm/src/cc/kernels/ResizeBilinear.cc
@@ -34,8 +34,59 @@ EMSCRIPTEN_KEEPALIVE
 void ResizeBilinear(int x_id, int batch, int old_height, int old_width,
                     int num_channels, int new_height, int new_width,
                     int align_corners, int out_id) {
-  int effective_input_height =
+  auto& x_info = backend::get_tensor_info(x_id);
+  auto& out_info = backend::get_tensor_info_out(out_id);
+
+  const float* x_buf = x_info.f32();
+  float* out_buf = out_info.f32_write();
+
+  const std::vector<int> x_shape = {batch, old_height, old_width, num_channels};
+  const auto x_strides = util::compute_strides(x_shape);
+
+  const int effective_input_height =
       (align_corners > 0 && new_height > 1) ? old_height - 1 : old_height;
+  const int effective_input_width =
+      (align_corners > 0 && new_width > 1) ? old_width - 1 : old_width;
+
+  const int effective_output_height =
+      (align_corners > 0 && new_height > 1) ? new_height - 1 : new_height;
+  const int effective_output_width =
+      (align_corners > 0 && new_width > 1) ? new_width - 1 : new_width;
+
+  const float effective_row_size_ratio =
+      effective_input_height / effective_output_height;
+  const float effective_col_size_ratio =
+      effective_input_width / effective_output_width;
+
+  for (int b = 0; b < batch; ++b) {
+    for (int r = 0; r < new_height; ++r) {
+      const float source_frac_row = effective_row_size_ratio * r;
+      const int source_row_floor = std::floor(source_frac_row);
+      const float row_frac = source_frac_row - source_row_floor;
+      const float source_row_ceil =
+          std::min(float(old_height - 1), std::ceil(source_frac_row));
+      const int top_row_offset =
+          b * x_strides[0] + source_row_floor * x_strides[1];
+      const int bot_row_offset =
+          b * x_strides[0] + source_row_ceil * x_strides[1];
+
+      for (int c = 0; c < new_width; ++c) {
+        const float source_frac_col = effective_col_size_ratio * c;
+        const int source_col_floor = std::floor(source_frac_col);
+        const float col_frac = source_frac_col - source_col_floor;
+        const float source_col_ceil =
+            std::min(float(old_width - 1), std::ceil(source_frac_col));
+        const int top_left_offset =
+            top_row_offset + source_col_floor * x_strides[2];
+        const int bot_left_offset =
+            bot_row_offset + source_col_floor * x_strides[2];
+        const int top_right_offset =
+            top_row_offset + source_col_ceil * x_strides[2];
+        const int bot_right_offset =
+            bot_row_offset + source_col_ceil * x_strides[2];
+      }
+    }
+  }
 }
 
 }  // extern "C"
