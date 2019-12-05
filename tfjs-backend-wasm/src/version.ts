d22cb8a9267bda15f5b942f376526b98cdbeb0e8
Daniel Smilkov <dsmilkov@gmail.com>
2019-12-05 18:24:06 -0500

    Update tfjs-backend-wasm to 1.4.0-alpha2. (#2501)
    INTERNAL

--- tfjs-backend-wasm/src/version.ts
+++ tfjs-backend-wasm/src/version.ts
@@ -1,5 +1,5 @@
 /** @license See the LICENSE file. */
 
 // This code is auto-generated, do not modify this file!
-const version = '1.4.0-alpha1';
+const version = '1.4.0-alpha2';
 export {version};
