fd4ad853784310d7535672f7952f170436c50d40
Na Li <linazhao@google.com>
2019-12-10 10:44:06 -0800

    Make sorting stable for duplicates

--- tfjs-core/src/backends/non_max_suppression_impl.ts
+++ tfjs-core/src/backends/non_max_suppression_impl.ts
@@ -140,5 +140,10 @@ function suppressWeight(iouThreshold: number, scale: number, iou: number) {
 }
 
 function ascendingComparator(c1: Candidate, c2: Candidate) {
-  return c1.score - c2.score;
+  // For objects with same scores, we make the object with the larger index go
+  // first. In an array that pops from the end, this means that the object with
+  // the smaller index will be popped first. This ensures the same output as
+  // the TensorFlow python version.
+  return (c1.score - c2.score) ||
+      ((c1.score === c2.score) && (c2.boxIndex - c1.boxIndex));
 }
