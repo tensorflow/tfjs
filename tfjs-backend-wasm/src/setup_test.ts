32d69e88d1f22f06b4310dba83ccf77b15b276dc
翠 / green <green@sapphi.red>
2019-12-12 08:55:24 +0900

    [wasm] Add Minimum, Maximum kernels (#2528)
    FEATURE
    
    Added `Minimum`, `Maximum` kernels

--- tfjs-backend-wasm/src/setup_test.ts
+++ tfjs-backend-wasm/src/setup_test.ts
@@ -195,6 +195,22 @@ const TEST_FILTERS: TestFilter[] = [
   {include: 'argmax', excludes: ['gradient']},
   {include: 'exp '},
   {include: 'unstack'},
+  {
+    include: 'minimum',
+    excludes: [
+      'gradient',  // Not yet implemented.
+      'broadcasts 2x1 Tensor2D and 2x2 Tensor2D' // Broadcasting along inner
+                                                 // dims not supported yet.
+    ]
+  },
+  {
+    include: 'maximum',
+    excludes: [
+      'gradient',  // Not yet implemented.
+      'broadcasts 2x1 Tensor2D and 2x2 Tensor2D' // Broadcasting along inner
+                                                 // dims not supported yet.
+    ]
+  },
 ];
 
 const customInclude = (testName: string) => {
