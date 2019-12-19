0c3b8fe33e599ca88f2db79b48ab1068b431ba5f
Na Li <linazhao@google.com>
2019-12-18 17:55:10 -0800

    Add support to NonMaxSuppressionV5

---
+++ tfjs-backend-wasm/src/kernels/NonMaxSuppressionV5.ts
@@ -0,0 +1,118 @@
+/**
+ * @license
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import {NamedAttrMap, NamedTensorInfoMap, registerKernel, TensorInfo} from '@tensorflow/tfjs-core';
+
+import {BackendWasm} from '../backend_wasm';
+
+interface NonMaxSuppressionInputs extends NamedTensorInfoMap {
+  boxes: TensorInfo;
+  scores: TensorInfo;
+}
+
+interface NonMaxSuppressionAttrs extends NamedAttrMap {
+  maxOutputSize: number;
+  iouThreshold: number;
+  scoreThreshold: number;
+  softNmsSigma: number;
+}
+
+// Analogous to `struct Result` in `NonMaxSuppressionV5.cc`.
+interface Result {
+  pSelectedIndices: number;
+  selectedIndicesSize: number;
+  pSelectedScores: number;
+  selectedScoresSize: number;
+}
+
+/**
+ * Parse the result of the c++ method, which is a data structure with four ints
+ * for selected_indices pointer, selected_indices size, selected_scores pointer,
+ * selected_scores size.
+ */
+function parseResultStruct(backend: BackendWasm, resOffset: number): Result {
+  const result = new Int32Array(backend.wasm.HEAPU8.buffer, resOffset, 4);
+  const pSelectedIndices = result[0];
+  const selectedIndicesSize = result[1];
+  const pSelectedScores = result[2];
+  const selectedScoresSize = result[3];
+  // Since the result was allocated on the heap, we have to delete it.
+  backend.wasm._free(resOffset);
+  return {
+    pSelectedIndices,
+    selectedIndicesSize,
+    pSelectedScores,
+    selectedScoresSize
+  };
+}
+
+let wasmFunc:
+    (boxesId: number, scoresId: number, maxOutputSize: number,
+     iouThreshold: number, scoreThreshold: number, softNmsSigma: number) =>
+        number;
+
+function setup(backend: BackendWasm): void {
+  wasmFunc = backend.wasm.cwrap(
+      'NonMaxSuppressionV5',
+      'number',  // Result*
+      [
+        'number',  // boxesId
+        'number',  // scoresId
+        'number',  // maxOutputSize
+        'number',  // iouThreshold
+        'number',  // scoreThreshold
+        'number',  // softNmsSigma
+      ]);
+}
+
+function kernelFunc(args: {
+  backend: BackendWasm,
+  inputs: NonMaxSuppressionInputs,
+  attrs: NonMaxSuppressionAttrs
+}): TensorInfo[] {
+  const {backend, inputs, attrs} = args;
+  const {iouThreshold, maxOutputSize, scoreThreshold, softNmsSigma} = attrs;
+  const {boxes, scores} = inputs;
+
+  const boxesId = backend.dataIdMap.get(boxes.dataId).id;
+  const scoresId = backend.dataIdMap.get(scores.dataId).id;
+
+  const resOffset = wasmFunc(
+      boxesId, scoresId, maxOutputSize, iouThreshold, scoreThreshold,
+      softNmsSigma);
+
+  const {
+    pSelectedIndices,
+    selectedIndicesSize,
+    pSelectedScores,
+    selectedScoresSize
+  } = parseResultStruct(backend, resOffset);
+
+  const selectedIndices =
+      backend.makeOutput([selectedIndicesSize], 'int32', pSelectedIndices);
+  const selectedScores =
+      backend.makeOutput([selectedScoresSize], 'float32', pSelectedScores);
+
+  return [selectedIndices, selectedScores];
+}
+
+registerKernel({
+  kernelName: 'NonMaxSuppressionV5',
+  backendName: 'wasm',
+  setupFunc: setup,
+  kernelFunc,
+});
