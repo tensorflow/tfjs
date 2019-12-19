ffd7172694fcae1cfc099c91e7ab4a626617e801
Na Li <linazhao@google.com>
2019-12-19 13:37:56 -0800

    Add support for NonMaxSuppressionV5 using modular pattern for Node

---
+++ tfjs-node/src/non_max_suppression_v5.ts
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import {NamedAttrMap, NamedTensorInfoMap, registerKernel, TensorInfo, scalar} from '@tensorflow/tfjs-core';
+import { NodeJSKernelBackend, createTypeOpAttr } from './nodejs_kernel_backend';
+
+interface NonMaxSuppressionWithScoreInputs extends NamedTensorInfoMap {
+  boxes: TensorInfo;
+  scores: TensorInfo;
+}
+
+interface NonMaxSuppressionWithScoreAttrs extends NamedAttrMap {
+  maxOutputSize: number;
+  iouThreshold: number;
+  scoreThreshold: number;
+  softNmsSigma: number;
+}
+
+registerKernel({
+  kernelName: 'NonMaxSuppressionV5',
+  backendName: 'tensorflow',
+  kernelFunc: ({inputs, backend, attrs}) => {
+    const {boxes, scores} = inputs as NonMaxSuppressionWithScoreInputs;
+    const {maxOutputSize, iouThreshold, scoreThreshold, softNmsSigma} =
+        attrs as NonMaxSuppressionWithScoreAttrs;
+        const maxOutputSizeTensor = scalar(maxOutputSize, 'int32');
+        const iouThresholdTensor = scalar(iouThreshold);
+        const scoreThresholdTensor = scalar(scoreThreshold);
+        const softNmsSigmaTensor = scalar(softNmsSigma);
+    const opAttrs = [createTypeOpAttr('T', boxes.dtype)];
+
+    const nodeBackend = backend as NodeJSKernelBackend;
+
+    const outputMetadata = nodeBackend.binding.executeOp('NonMaxSuppressionV5', opAttrs, nodeBackend.getInputTensorIds([boxes, scores, maxOutputSizeTensor, iouThresholdTensor, scoreThresholdTensor, softNmsSigmaTensor]), 2);
+
+    return outputMetadata.map(m => nodeBackend.createOutputTensor(m));
+  }
+});
