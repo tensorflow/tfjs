a2b1e270f361a5d67d98fff5c8dc999c3c20af5e
Ann Yuan <annyuan@google.com>
2019-12-30 16:00:44 -0500

    Update backend_webgpu.ts

--- tfjs-backend-webgpu/src/backend_webgpu.ts
+++ tfjs-backend-webgpu/src/backend_webgpu.ts
@@ -26,7 +26,8 @@ import {backend_util, DataStorage, DataType, engine, env, findBackend, KernelBac
 import {FusedConv2DConfig} from '@tensorflow/tfjs-core/dist/ops/fused_util';
 // TODO: Import reduce_util from backend_util with next release of core.
 import {computeOptimalWindowSize} from '@tensorflow/tfjs-core/src/ops/reduce_util';
-// TODO: import sumOutType directly from '@tensorflow/tfjs-core' with next release of core.
+// TODO: import sumOutType directly from '@tensorflow/tfjs-core' with next 
+// release of core.
 import {sumOutType} from '@tensorflow/tfjs-core/src/types';
 import {Glslang} from '@webgpu/glslang/dist/web-devel/glslang.onefile';
 
