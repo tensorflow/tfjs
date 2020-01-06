86efe53deb7ace2aa8163061ede451806494f6c5
Yannick Assogba <yassogba@google.com>
2020-01-06 11:32:04 -0500

    restore webcam demo

--- tfjs-react-native/integration_rn59/components/webcam/webcam_demo.tsx
+++ tfjs-react-native/integration_rn59/components/webcam/webcam_demo.tsx
@@ -22,11 +22,6 @@ import { Camera } from 'expo-camera';
 import {StyleTranfer} from './style_transfer';
 import {base64ImageToTensor, tensorToImageUrl, resizeImage, toDataUri} from './image_utils';
 import * as tf from '@tensorflow/tfjs';
-import { GLView, ExpoWebGLRenderingContext } from 'expo-gl';
-import {fromTexture, toTexture, renderToGLView, decodeJpeg, fetch} from '@tensorflow/tfjs-react-native';
-import { Dimensions,PixelRatio, LayoutChangeEvent } from 'react-native';
-
-
 
 interface ScreenProps {
   returnToMain: () => void;
@@ -43,15 +38,9 @@ interface ScreenState {
   isLoading: boolean;
 }
 
-let cameraPreviewWidth: number;
-let cameraPreviewHeight: number;
-
 export class WebcamDemo extends React.Component<ScreenProps,ScreenState> {
   private camera?: Camera|null;
   private styler: StyleTranfer;
-  private glView?: GLView;
-  private texture?: WebGLTexture;
-  private _rafID?: number;
 
   constructor(props: ScreenProps) {
     super(props);
@@ -64,10 +53,8 @@ export class WebcamDemo extends React.Component<ScreenProps,ScreenState> {
   }
 
   async componentDidMount() {
-    // await this.styler.init();
+    await this.styler.init();
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    // this.camTexture = await GLView.createCameraTextureAsync();
-
     this.setState({
       hasCameraPermission: status === 'granted',
       isLoading: false
@@ -198,530 +185,6 @@ export class WebcamDemo extends React.Component<ScreenProps,ScreenState> {
     }
   }
 
-  async createCameraTexture(): Promise<WebGLTexture> {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    if (status !== 'granted') {
-      throw new Error('Denied camera permissions!');
-    }
-    //@ts-ignore
-    return this.glView!.createCameraTextureAsync(this.camera!);
-  }
-
-  onCameraLayout(event: LayoutChangeEvent) {
-    const {x, y, width, height} = event.nativeEvent.layout;
-    cameraPreviewHeight = height;
-    cameraPreviewWidth = width;
-    console.log('onCameraLayout', x, y, width, height);
-  }
-
-  onGLViewLayout(event: LayoutChangeEvent) {
-    const {x, y, width, height} = event.nativeEvent.layout;
-    console.log('onGLViewLayout', x, y, width, height);
-  }
-
-  async roundtrip(gl: WebGL2RenderingContext) {
-    const width = 2;
-    const height = 2;
-    const depth = 4;
-
-    const inTensor =
-      tf.truncatedNormal([width, height, depth], 127, 40, 'int32');
-    const texture = await toTexture(gl, inTensor as tf.Tensor3D);
-
-    const outTensor = fromTexture(gl, texture,
-      {width, height, depth},
-      {width, height, depth});
-
-    const inData = inTensor.dataSync();
-    const outData = outTensor.dataSync();
-    const matches = tf.util.arraysEqual(inData, outData);
-
-    if(matches) {
-      console.log('**toTexture -> fromTexture roundtrip success');
-    } else {
-      console.log('ERROR toTexture -> fromTexture roundtrip failed');
-      console.log('input', inTensor.shape, Array.from(inData));
-      console.log('output', outTensor.shape, Array.from(outData));
-    }
-
-    tf.dispose([inTensor, outTensor]);
-    return matches;
-  }
-
-  async resizeNNSameAspect(gl: WebGL2RenderingContext, alignCorners: boolean) {
-    // Same aspect ratio
-    const inShape: [number, number, number] = [4,4,4];
-    const input = tf.tensor3d([
-      [
-        [200, 201, 202, 255], // a
-        [190, 191, 192, 255], // b
-        [180, 181, 182, 255], // c
-        [170, 171, 172, 255], // d
-      ],
-      [
-        [160, 161, 162, 255], // e
-        [150, 151, 152, 255], // f
-        [140, 141, 142, 255], // g
-        [130, 131, 132, 255], // h
-      ],
-      [
-        [120, 121, 122, 255], // i
-        [110, 111, 112, 255], // j
-        [100, 101, 102, 255], // k
-        [90, 91, 92, 255],    // l
-      ],
-      [
-        [80, 81, 82, 255],    // m
-        [70, 71, 72, 255],    // n
-        [60, 61, 62, 255],    // o
-        [50, 51, 52, 255],    // p
-      ]
-    ], inShape, 'int32');
-
-    let expected: tf.Tensor3D;
-    if (alignCorners) {
-      expected = tf.tensor3d([
-        [
-          [200, 201, 202, 255],
-          [170, 171, 172, 255]
-        ],
-        [
-          [ 80,  81,  82, 255],
-          [ 50,  51,  52, 255]
-        ]
-      ], [2,2,4], 'int32');
-    } else {
-      expected = tf.tensor3d([
-        [
-          [200, 201, 202, 255],  // x
-          [180, 181, 182, 255],  // y
-        ],
-        [
-          [120, 121, 122, 255],  // z
-          [100, 101, 102, 255],  // w
-        ]
-      ], [2,2,4], 'int32');
-    }
-
-    const size: [number, number] = [2, 2];
-    const texture = await toTexture(gl, input);
-    const outTensor = fromTexture(
-      gl,
-      texture,
-      {width: inShape[0], height: inShape[1], depth: inShape[2]},
-      {width: size[0], height: size[1], depth: 4},
-      {alignCorners, interpolation: 'nearest_neighbor'});
-
-    const fromTexResizeMatch = tf.util.arraysEqual(
-      expected.dataSync(),
-      outTensor.dataSync()) &&
-      tf.util.arraysEqual(expected.shape, outTensor.shape);
-
-    if(fromTexResizeMatch) {
-      console.log(
-        `**fromTexture resizeNNSameAspect success alignCorners=${alignCorners}`);
-    } else {
-      console.log(
-        `ERROR fromTexture resizeNNSameAspect alignCorners=${alignCorners}`);
-      console.log('input', input.shape);
-      input.print();
-      console.log('expected', expected.shape);
-      expected.print();
-      console.log('outTensor', outTensor.shape);
-      outTensor.print();
-    }
-
-    tf.dispose([input, expected, outTensor]);
-    return fromTexResizeMatch;
-  }
-
-  async resizeBilinearSameAspect(gl: WebGL2RenderingContext,
-    alignCorners: boolean) {
-    // Same aspect ratio
-    const inShape: [number, number, number] = [4,4,4];
-    const input = tf.tensor3d([
-      [
-        [200, 201, 202, 255], // a
-        [190, 191, 192, 255], // b
-        [180, 181, 182, 255], // c
-        [170, 171, 172, 255], // d
-      ],
-      [
-        [160, 161, 162, 255], // e
-        [150, 151, 152, 255], // f
-        [140, 141, 142, 255], // g
-        [130, 131, 132, 255], // h
-      ],
-      [
-        [120, 121, 122, 255], // i
-        [110, 111, 112, 255], // j
-        [100, 101, 102, 255], // k
-        [90, 91, 92, 255],    // l
-      ],
-      [
-        [80, 81, 82, 255],    // m
-        [70, 71, 72, 255],    // n
-        [60, 61, 62, 255],    // o
-        [50, 51, 52, 255],    // p
-      ]
-    ], inShape, 'int32');
-
-    let expected: tf.Tensor3D;
-    if (alignCorners) {
-      expected = tf.tensor3d([
-        [
-          [200, 201, 202, 255],
-          [170, 171, 172, 255],
-        ],
-        [
-          [ 80,  81,  82, 255],
-          [ 50,  51,  52, 255],
-        ]
-      ], [2,2,4], 'int32');
-    } else {
-      expected = tf.tensor3d([
-        [
-          [200, 201, 202, 255],  // x
-          [180, 181, 182, 255],  // y
-        ],
-        [
-          [120, 121, 122, 255],  // z
-          [100, 101, 102, 255],  // w
-        ]
-      ], [2,2,4], 'int32');
-    }
-
-    const size: [number, number] = [2, 2];
-    const texture = await toTexture(gl, input);
-    const outTensor = fromTexture(
-      gl,
-      texture,
-      {width: inShape[0], height: inShape[1], depth: inShape[2]},
-      {width: size[0], height: size[1], depth: 4},
-      {alignCorners, interpolation: 'bilinear'});
-
-    const fromTexResizeMatch = tf.util.arraysEqual(
-      expected.dataSync(),
-      outTensor.dataSync()) &&
-      tf.util.arraysEqual(expected.shape, outTensor.shape);
-
-    if(fromTexResizeMatch) {
-      console.log(
-        `**fromTexture resizeBilinearSameAspect success alignCorners=${alignCorners}`);
-    } else {
-      console.log(
-        `ERROR fromTexture resizeBilinearSameAspect alignCorners=${alignCorners}`);
-      console.log('input', input.shape);
-      input.print();
-      console.log('expected', expected.shape);
-      expected.print();
-      console.log('outTensor', outTensor.shape);
-      outTensor.print();
-    }
-
-    tf.dispose([input, expected, outTensor]);
-    return fromTexResizeMatch;
-  }
-
-  async resizeNNWide(gl: WebGL2RenderingContext, alignCorners: boolean) {
-    const inHeight = 4;
-    const inWidth = 4;
-    const inDepth = 4;
-
-    const outHeight = 2;
-    const outWidth = 3;
-    const outDepth = 4;
-
-    const input = tf.tensor3d([
-      [
-        [200, 201, 202, 255], // a
-        [190, 191, 192, 255], // b
-        [180, 181, 182, 255], // c
-        [170, 171, 172, 255], // d
-      ],
-      [
-        [160, 161, 162, 255], // e
-        [150, 151, 152, 255], // f
-        [140, 141, 142, 255], // g
-        [130, 131, 132, 255], // h
-      ],
-      [
-        [120, 121, 122, 255], // i
-        [110, 111, 112, 255], // j
-        [100, 101, 102, 255], // k
-        [90, 91, 92, 255],    // l
-      ],
-      [
-        [80, 81, 82, 255],    // m
-        [70, 71, 72, 255],    // n
-        [60, 61, 62, 255],    // o
-        [50, 51, 52, 255],    // p
-      ]
-    ], [inHeight, inWidth, inDepth], 'int32');
-
-    let expected: tf.Tensor3D;
-    if (alignCorners) {
-      expected = tf.tensor3d([
-        [
-          [200, 201, 202, 255],
-          [180, 181, 182, 255],
-          [170, 171, 172, 255]],
-
-        [
-          [ 80,  81,  82, 255],
-          [ 60,  61,  62, 255],
-          [ 50,  51,  52, 255],
-        ]
-      ], [outHeight, outWidth, outDepth], 'int32');
-    } else {
-      expected = tf.tensor3d([
-        [
-          [200, 201, 202, 255],
-          [190, 191, 192, 255],
-          [180, 181, 182, 255],
-        ],
-        [
-          [120, 121, 122, 255],
-          [110, 111, 112, 255],
-          [100, 101, 102, 255],
-        ]
-      ], [outHeight, outWidth, outDepth], 'int32');
-    }
-
-    const texture = await toTexture(gl, input);
-    const outTensor = fromTexture(
-      gl,
-      texture,
-      {width: inWidth, height: inHeight, depth: inDepth},
-      {width: outWidth, height: outHeight, depth: outDepth},
-      {alignCorners, interpolation: 'nearest_neighbor'});
-
-    const fromTexResizeMatch = tf.util.arraysEqual(
-      expected.dataSync(),
-      outTensor.dataSync()) &&
-      tf.util.arraysEqual(expected.shape, outTensor.shape);
-
-    if(fromTexResizeMatch) {
-      console.log(
-        `**fromTexture resizeNNNarrow success alignCorners=${alignCorners}`);
-    } else {
-      console.log(
-        `ERROR fromTexture resizeNNNarrow. alignCorners=${alignCorners}`);
-      console.log('input', input.shape);
-      input.print();
-      console.log('expected', expected.shape);
-      expected.print();
-      console.log('outTensor', outTensor.shape);
-      outTensor.print();
-    }
-
-    tf.dispose([input, expected, outTensor]);
-    return fromTexResizeMatch;
-  }
-
-  async resizeBilinearWide(gl: WebGL2RenderingContext,
-    alignCorners: boolean) {
-    const inHeight = 4;
-    const inWidth = 4;
-    const inDepth = 4;
-
-    const outHeight = 2;
-    const outWidth = 3;
-    const outDepth = 4;
-
-    const input = tf.tensor3d([
-      [
-        [200, 201, 202, 255], // a
-        [190, 191, 192, 255], // b
-        [180, 181, 182, 255], // c
-        [170, 171, 172, 255], // d
-      ],
-      [
-        [160, 161, 162, 255], // e
-        [150, 151, 152, 255], // f
-        [140, 141, 142, 255], // g
-        [130, 131, 132, 255], // h
-      ],
-      [
-        [120, 121, 122, 255], // i
-        [110, 111, 112, 255], // j
-        [100, 101, 102, 255], // k
-        [90, 91, 92, 255],    // l
-      ],
-      [
-        [80, 81, 82, 255],    // m
-        [70, 71, 72, 255],    // n
-        [60, 61, 62, 255],    // o
-        [50, 51, 52, 255],    // p
-      ]
-    ], [inHeight, inWidth, inDepth], 'int32');
-
-    let expected: tf.Tensor3D;
-    if (alignCorners) {
-      expected = tf.tensor3d([
-        [
-          [200, 201, 202, 255],
-          [185, 186, 187, 255],
-          [170, 171, 172, 255],],
-        [
-          [80 , 81 , 82 , 255],
-          [65 , 66 , 67 , 255],
-          [50 , 51 , 52 , 255],
-        ]
-      ], [outHeight, outWidth, outDepth], 'int32');
-    } else {
-      expected = tf.tensor3d([
-        [
-          [200, 201, 202, 255],
-          [187, 188, 189, 255],
-          [173, 174, 175, 255],],
-        [
-          [120, 121, 122, 255],
-          [107, 108, 109, 255],
-          [93 , 94 , 95 , 255],
-        ]
-      ], [outHeight, outWidth, outDepth], 'int32');
-    }
-
-    const texture = await toTexture(gl, input);
-    const outTensor = fromTexture(
-      gl,
-      texture,
-      {width: inWidth, height: inHeight, depth: inDepth},
-      {width: outWidth, height: outHeight, depth: outDepth},
-      {alignCorners, interpolation: 'bilinear'});
-
-    const fromTexResizeMatch = tf.util.arraysEqual(
-      expected.dataSync(),
-      outTensor.dataSync()) &&
-      tf.util.arraysEqual(expected.shape, outTensor.shape);
-
-    if(fromTexResizeMatch) {
-      console.log(
-        `**fromTexture resizeBilinearWide success alignCorners=${alignCorners}`);
-    } else {
-      console.log(
-        `ERROR fromTexture resizeBilinearWide. alignCorners=${alignCorners}`);
-      console.log('input', input.shape);
-      input.print();
-      console.log('expected', expected.shape);
-      expected.print();
-      console.log('outTensor', outTensor.shape);
-      outTensor.print();
-    }
-
-    tf.dispose([input, expected, outTensor]);
-    return fromTexResizeMatch;
-  }
-
- async onContextCreate(gl: ExpoWebGLRenderingContext) {
-    console.log('onContextCreate texture tests');
-    await this.roundtrip(gl);
-    await this.resizeNNSameAspect(gl, false);
-    await this.resizeNNSameAspect(gl, true);
-    await this.resizeNNWide(gl, false);
-    await this.resizeNNSameAspect(gl, true);
-
-    await this.resizeBilinearSameAspect(gl, false);
-    await this.resizeBilinearSameAspect(gl, true);
-    await this.resizeBilinearWide(gl, false);
-    await this.resizeBilinearWide(gl, true);
-    console.log('------ END onContextCreate texture tests --------');
-
-
-    const ratios = await this.camera!.getSupportedRatiosAsync();
-    console.log('Supported aspect ratios', ratios);
-    const picSizes = await this.camera!.getAvailablePictureSizesAsync('4:3');
-    console.log('Available picSizes 4:3', picSizes);
-    const picSizes2 = await this.camera!.getAvailablePictureSizesAsync('16:9');
-    console.log('Available picSizes 16:9', picSizes2);
-
-    this.texture = await this.createCameraTexture();
-    const cameraTexture = this.texture;
-    const pixelRatio = PixelRatio.get();
-
-    let width = Math.floor(cameraPreviewWidth * pixelRatio);
-    let height = Math.floor(cameraPreviewHeight * pixelRatio);
-    const depth = 4;
-
-    // 4032x3024
-    // width = Math.floor(3024);
-    // height = Math.floor(4032);
-
-    console.log('onContextCreate.pixelRatio', pixelRatio);
-    console.log('onContextCreate.w:h:d', width, height, depth);
-    console.log('onContextCreate.gl dims',
-      gl.drawingBufferWidth, gl.drawingBufferHeight);
-    console.log('onContextCreate.gl viewport', gl.getParameter(gl.VIEWPORT));
-
-    const targetWidth = width / 4;
-    const targetHeight = height/ 4;
-    const targetDepth = depth;
-
-    const image = require('../../assets/images/catsmall.jpg');
-    const imageAssetPath = Image.resolveAssetSource(image);
-    const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
-    const rawImageData = await response.arrayBuffer();
-
-    const imageTensor = decodeJpeg(new Uint8Array(rawImageData), 3);
-    const imageTensorWAlpha = imageTensor.pad([[0,0],[0,0],[0,1]], 255);
-    console.log('imagetensorshape', imageTensor.shape);
-    console.log('imagetensordata', Array.from(imageTensor.dataSync().slice(0, 10)));
-    console.log('imageTensor4d shape', imageTensorWAlpha.shape);
-    console.log('imageTensor4d', Array.from(imageTensorWAlpha.dataSync().slice(0, 10)));
-
-    // Render loop
-    let start;
-    let end;
-    const loop = async () => {
-      // this._rafID = requestAnimationFrame(loop);
-
-      start = Date.now();
-      const resizedCamTensor = fromTexture(
-        gl,
-        cameraTexture,
-        // Source
-        {width, height, depth},
-        // Target
-        {width: targetWidth, height: targetHeight, depth: targetDepth},
-        {alignCorners: false},
-      );
-      // console.log('from rexture res', resizedCamTensor.shape);
-      // console.log('from rexture res data', Array.from(resizedCamTensor.dataSync().slice(0, 100)));
-      end = Date.now();
-      // console.log('fromTexture:time', end - start);
-
-      start = Date.now();
-      const resizedCamTexture = await toTexture(gl, resizedCamTensor);
-      end = Date.now();
-      // console.log('toTexture:time', end - start);
-
-
-
-      resizedCamTensor.dispose();
-
-      // renderToGLView(gl, resizedCamTexture, { width, height });
-
-      const catTexture = await toTexture(gl, imageTensorWAlpha);
-      const catT = fromTexture(gl, catTexture,
-        {height: imageTensorWAlpha.shape[0], width: imageTensorWAlpha.shape[1], depth: imageTensorWAlpha.shape[2]},
-        {height: 4, width: 24, depth: 3},
-        {alignCorners: false, interpolation: 'nearest_neighbor'}
-      );
-      console.log('catT', catT.shape);
-      catT.print();
-      const smallCat = await toTexture(gl, catT);
-      renderToGLView(gl, smallCat, { width, height });
-
-      gl.endFrameEXP();
-    };
-
-    setInterval(() => {
-      loop();
-    }, 800);
-
-  }
-
   renderCameraCapture() {
     const {hasCameraPermission} = this.state;
 
@@ -735,7 +198,6 @@ export class WebcamDemo extends React.Component<ScreenProps,ScreenState> {
         <Camera
           style={styles.camera}
           type={this.state.cameraType}
-          pictureSize='320x240'
           ref={ref => { this.camera = ref; }}>
         </Camera>
         <View style={styles.cameraControls}>
@@ -804,39 +266,20 @@ export class WebcamDemo extends React.Component<ScreenProps,ScreenState> {
   }
 
   render() {
-    const {isLoading} = this.state;
-    const camV = <View style={styles.cameraContainer}>
-        <Camera
-          style={styles.camera}
-          type={this.state.cameraType}
-          zoom={0}
-          ref={ref => this.camera = ref!}
-          onLayout={this.onCameraLayout.bind(this)}
-        />
-        <GLView
-          style={styles.camera}
-          onLayout={this.onGLViewLayout.bind(this)}
-          onContextCreate={this.onContextCreate.bind(this)}
-          ref={ref => this.glView = ref!}
-        />
-        </View>;
+    const {mode, isLoading} = this.state;
     return (
       <View style={{width:'100%'}}>
         {isLoading ? <View style={[styles.loadingIndicator]}>
           <ActivityIndicator size='large' color='#FF0266' />
-        </View> : camV}
-        {/* {mode === 'results' ?
-              this.renderResults() : this.renderCameraCapture()} */}
+        </View> : null}
+        {mode === 'results' ?
+              this.renderResults() : this.renderCameraCapture()}
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-   container: {
-    flex: 1,
-    flexDirection: 'column',
-  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24
@@ -870,12 +313,12 @@ const styles = StyleSheet.create({
   },
   camera : {
     display: 'flex',
-    width: '60%',
-    height: '40%',
-    // backgroundColor: '#f0F',
+    width: '92%',
+    height: '64%',
+    backgroundColor: '#f0F',
     zIndex: 1,
-    borderWidth: 2,
-    borderRadius: 2,
+    borderWidth: 20,
+    borderRadius: 40,
     borderColor: '#f0f',
   },
   cameraControls: {
