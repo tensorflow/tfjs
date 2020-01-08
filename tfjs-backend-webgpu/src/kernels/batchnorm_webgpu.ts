8d5360629b4d13dd754bcc5c620d47c0a444321a
NALLEIN <hao1.jiang@intel.com>
2020-01-08 13:59:23 +0800

    webgpu Add batchnormal kernel

--- tfjs-backend-webgpu/src/kernels/batchnorm_webgpu.ts
+++ tfjs-backend-webgpu/src/kernels/batchnorm_webgpu.ts
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
-import {backend_util} from '@tensorflow/tfjs-core'
+import {backend_util} from '@tensorflow/tfjs-core';
 
 import {computeDispatch} from '../webgpu_util';
 
