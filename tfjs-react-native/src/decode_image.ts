0dd1be5a7d04d428b79dd034e8780d9f3f94971d
Yannick Assogba <yassogba@google.com>
2020-01-22 13:44:15 -0500

    code review fixes

--- tfjs-react-native/src/decode_image.ts
+++ tfjs-react-native/src/decode_image.ts
@@ -12,7 +12,7 @@ enum ImageType {
  * Decode a JPEG-encoded image to a 3D Tensor of dtype `int32`.
  *
  * ```js
- * const image = require("path/to/img.jpg");
+ * const image = require('path/to/img.jpg');
  * const imageAssetPath = Image.resolveAssetSource(image);
  * const response = await fetch(imageAssetPath.uri, {}, { isBinary: true });
  * const rawImageData = await response.arrayBuffer();
