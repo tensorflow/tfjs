63f9fdaf1621d9d0031531540b9001894881b26b
Na Li <linazhao@google.com>
2020-01-23 10:18:20 -0800

    Extend timeout

--- tfjs-core/karma.conf.js
+++ tfjs-core/karma.conf.js
@@ -110,6 +110,8 @@ module.exports = function(config) {
     },
     captureTimeout: 120000,
     reportSlowerThan: 500,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 5,
     browserNoActivityTimeout: 180000,
     customLaunchers: {
       // For browserstack configs see:
