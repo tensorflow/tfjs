ce3ac1d465f7a6bef654b8b62251226633ddcef9
Na Li <linazhao@google.com>
2020-01-23 13:42:38 -0800

    Revert again to test browserstack

--- tfjs-core/src/io/io_utils.ts
+++ tfjs-core/src/io/io_utils.ts
@@ -250,12 +250,13 @@ export function arrayBufferToBase64String(buffer: ArrayBuffer): string {
   if (useNodeBuffer) {
     return Buffer.from(buffer).toString('base64');
   }
-  const buf = new Uint8Array(buffer);
-  let s = '';
-  for (let i = 0; i < buf.length; i++) {
-    s += String.fromCharCode(buf[i]);
-  }
-  return btoa(s);
+  // const buf = new Uint8Array(buffer);
+  // let s = '';
+  // for (let i = 0; i < buf.length; i++) {
+  //   s += String.fromCharCode(buf[i]);
+  // }
+  // return btoa(s);
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)));
 }
 
 /**
