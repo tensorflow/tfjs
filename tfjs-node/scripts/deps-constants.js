c5bbd78a796c672087c3db8014e41dee82f7d96d
Shanqing Cai <cais@google.com>
2020-01-30 10:13:10 -0500

    [tfjs-node] Upgrade libtensorflow version
    1.15.0 --> 2.1.0
    
    Without this update, the latest versions of tfjs-node (Python) tensorflow
    depend on different CUDA versions (10.0 vs. 10.1), which causes
    confusion and inconvenience to users who use TF in both languages.

--- tfjs-node/scripts/deps-constants.js
+++ tfjs-node/scripts/deps-constants.js
@@ -22,7 +22,7 @@ const modulePath =
     module_path_napi.replace('{napi_build_version}', process.versions.napi);
 
 /** Version of the libtensorflow shared library to depend on. */
-const LIBTENSORFLOW_VERSION = '1.15.0';
+const LIBTENSORFLOW_VERSION = '2.1.0';
 
 /** Map the os.arch() to arch string in a file name */
 const ARCH_MAPPING = {
