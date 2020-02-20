e59bcde459a044907d1fa0fa9c790baa8f57cd99
Na Li <linazhao@google.com>
2020-02-20 10:58:24 -0800

    Add build script to tfjs.

--- tfjs-node/scripts/test.sh
+++ tfjs-node/scripts/test.sh
@@ -1,15 +1,3 @@
-echo "=====Build tfjs-core.====="
-cd ../tfjs-core && yarn && yarn build
-
-echo "=====Build tfjs-converter.====="
-cd ../tfjs-converter && yarn && yarn build
-
-echo "=====Build tfjs-layers.====="
-cd ../tfjs-layers && yarn && yarn build
-
-echo "=====Build tfjs-data.====="
-cd ../tfjs-data && yarn && yarn build
-
 echo "=====Build tfjs.====="
 cd ../tfjs && yarn && yarn build
 
