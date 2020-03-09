eda97a305479cd422e28b4e6eb0b50d45133caa0
Na Li <linazhao@google.com>
2020-03-09 10:54:44 -0700

    [tfjs]Test against head.

--- tfjs/scripts/test-ci.sh
+++ tfjs/scripts/test-ci.sh
@@ -17,7 +17,7 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-yarn build
+yarn build-ci
 yarn lint
 yarn karma start --browsers='bs_firefox_mac,bs_chrome_mac' --singleRun
 cd integration_tests
