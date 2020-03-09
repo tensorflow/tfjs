c14ffef60f2e6e96a690a2fd7815c8dad60f71b5
Ann Yuan <annyuan@gmail.com>
2020-03-09 09:44:37 -0400

    got test to pass

--- tfjs-backend-wasm/src/cc/kernels/FusedBatchMatMul_test.cc
+++ tfjs-backend-wasm/src/cc/kernels/FusedBatchMatMul_test.cc
@@ -50,31 +50,39 @@ TEST(FUSEDBATCH_MATMUL, xnn_operator_lfietime) {
   ASSERT_EQ(5, tfjs::backend::num_tensors());
   ASSERT_EQ(0, tfjs::backend::xnn_operator_count);
 
+  const FusableActivation activation = FusableActivation::LINEAR;
+  size_t bias_id = 0;
+  size_t prelu_weights_id = 0;
+
   // One new xnn_operator should be created for the first call to BatchMatMul.
-  tfjs::wasm::FusedBatchMatMul(
-      a0_id, a_shape_ptr, a_shape.size(), b0_id, b_shape_ptr, b_shape.size(),
-      false /* transpose_a */, false /* transpose_b */, out_id);
+  tfjs::wasm::FusedBatchMatMul(a0_id, a_shape_ptr, a_shape.size(), b0_id,
+                               b_shape_ptr, b_shape.size(),
+                               false /* transpose_a */, false /* transpose_b */,
+                               activation, bias_id, prelu_weights_id, out_id);
   ASSERT_EQ(1, tfjs::backend::xnn_operator_count);
 
   // No new xnn_operators should be created for the second call to BatchMatMul
   // with the same b's.
-  tfjs::wasm::FusedBatchMatMul(
-      a0_id, a_shape_ptr, a_shape.size(), b0_id, b_shape_ptr, b_shape.size(),
-      false /* transpose_a */, false /* transpose_b */, out_id);
+  tfjs::wasm::FusedBatchMatMul(a0_id, a_shape_ptr, a_shape.size(), b0_id,
+                               b_shape_ptr, b_shape.size(),
+                               false /* transpose_a */, false /* transpose_b */,
+                               activation, bias_id, prelu_weights_id, out_id);
   ASSERT_EQ(1, tfjs::backend::xnn_operator_count);
 
   // One new xnn_operator should be created for another call to BatchMatMul with
   // new b's.
-  tfjs::wasm::FusedBatchMatMul(
-      a0_id, a_shape_ptr, a_shape.size(), b1_id, b_shape_ptr, b_shape.size(),
-      false /* transpose_a */, false /* transpose_b */, out_id);
+  tfjs::wasm::FusedBatchMatMul(a0_id, a_shape_ptr, a_shape.size(), b1_id,
+                               b_shape_ptr, b_shape.size(),
+                               false /* transpose_a */, false /* transpose_b */,
+                               activation, bias_id, prelu_weights_id, out_id);
   ASSERT_EQ(2, tfjs::backend::xnn_operator_count);
 
   // No new xnn_operators should be created for the next call to BatchMatMul
   // with the same b's.
-  tfjs::wasm::FusedBatchMatMul(
-      a0_id, a_shape_ptr, a_shape.size(), b1_id, b_shape_ptr, b_shape.size(),
-      false /* transpose_a */, false /* transpose_b */, out_id);
+  tfjs::wasm::FusedBatchMatMul(a0_id, a_shape_ptr, a_shape.size(), b1_id,
+                               b_shape_ptr, b_shape.size(),
+                               false /* transpose_a */, false /* transpose_b */,
+                               activation, bias_id, prelu_weights_id, out_id);
   ASSERT_EQ(2, tfjs::backend::xnn_operator_count);
 
   // Disposing a's should not remove xnn operators.
