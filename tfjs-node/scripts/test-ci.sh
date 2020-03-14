8501ef9bb0fa6ef43b9153a96b6b74b70375d9f2
Na Li <linazhao@google.com>
2020-03-13 17:32:24 -0700

    Fix integration test.

--- tfjs-node/scripts/test-ci.sh
+++ tfjs-node/scripts/test-ci.sh
@@ -10,6 +10,11 @@
 set -e
 
 yarn build-addon-from-source
+yarn build-core-ci
+yarn build-layers-ci
+yarn build-converter-ci
+yarn build-data-ci
+yarn build-union-ci
 yarn build-ci
 yarn lint
 yarn test
