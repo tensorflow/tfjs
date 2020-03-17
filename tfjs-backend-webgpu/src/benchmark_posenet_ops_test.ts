c425193413ef9e8d07708e59bb5aab55f2a3a747
Ann Yuan <annyuan@gmail.com>
2020-03-17 15:15:11 -0400

    simplify

--- tfjs-backend-webgpu/src/benchmark_posenet_ops_test.ts
+++ tfjs-backend-webgpu/src/benchmark_posenet_ops_test.ts
@@ -3,60 +3,30 @@ import * as tf from '@tensorflow/tfjs-core';
 import {ops} from './benchmark_util/posenetReadout';
 import {benchmarkAndLog, describeWebGPU} from './test_util';
 
-const opNameMap: any = {
-  'PadV2': 'pad',
-  'Conv2D': 'conv2d'
-};
-
 describeWebGPU('benchmark-posenet', () => {
   beforeEach(() => {
     tf.setBackend('webgl');
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 999999999;
   });
 
-  ops.filter(obj => {
-       return obj.name !== 'FromPixels';
-     })
-      .forEach(obj => {
-        let opName =
-            `${obj.name.substring(0, 1).toLowerCase()}${obj.name.substring(1)}`;
-        if (opNameMap[obj.name]) {
-          opName = opNameMap[obj.name];
-        }
+  ops.forEach(obj => {
+    const opName = obj.name;
 
-        fit(opName, async () => {
-          const inputs = (obj.inputs as any).map((input: number[]) => {
-            if (typeof input[0] === 'object') {
-              console.log('THIS SHOULD ONLY HAPPEN WITH CONCAT');
-              return (input as any).map((nested: any) => {
-                return tf.randomNormal(nested);
-              });
-            }
-            return tf.randomNormal(input);
-          });
-
-          let additionalArgs: any = [];
-          if ((obj as any).args) {
-            additionalArgs = (obj as any).args;
-          } else {
-            if (opName === 'conv2d') {
-              additionalArgs = [1, 'same'];
-            } else if (opName === 'pad') {
-              const paddings = [];
-              for (let i = 0; i < (obj.inputs as any)[0].length; i++) {
-                paddings.push([0, 1]);
-              }
-              additionalArgs = [paddings];
-            } else if (opName === 'cast') {
-              additionalArgs = ['float32'];
-            }
-          }
-
-          await benchmarkAndLog(
-              `${obj.name}_${(obj.inputs as any).join('|')}`, () => {
-                const f = (tf as any)[opName];
-                return f.call(tf, ...inputs.concat(additionalArgs));
-              });
-        });
+    fit(opName, async () => {
+      const inputs = (obj.inputs as any).map((input: number[]) => {
+        return tf.randomNormal(input);
       });
+
+      let additionalArgs: any = [];
+      if ((obj as any).args) {
+        additionalArgs = (obj as any).args;
+      }
+
+      await benchmarkAndLog(
+          `${obj.name}_${(obj.inputs as any).join('|')}`, () => {
+            const f = (tf as any)[opName];
+            return f.call(tf, ...inputs.concat(additionalArgs));
+          });
+    });
+  });
 });
--- tfjs-backend-webgpu/src/benchmark_util/posenetReadout.ts
+++ tfjs-backend-webgpu/src/benchmark_util/posenetReadout.ts
@@ -1,345 +1,345 @@
 export const ops = [
   {
-    'name': 'PadV2',
+    'name': 'pad',
     'inputs': [[1, 15, 15, 256]],
     'args': [[[0, 0], [1, 1], [1, 1], [0, 0]]]
   },
   {
-    'name': 'PadV2',
+    'name': 'pad',
     'inputs': [[1, 29, 29, 128]],
     'args': [[[0, 0], [1, 1], [1, 1], [0, 0]]]
   },
   {
-    'name': 'PadV2',
+    'name': 'pad',
     'inputs': [[1, 57, 57, 64]],
     'args': [[[0, 0], [1, 1], [1, 1], [0, 0]]]
   },
   {
-    'name': 'PadV2',
+    'name': 'pad',
     'inputs': [[1, 113, 113, 64]],
     'args': [[[0, 0], [1, 1], [1, 1], [0, 0]]]
   },
   {
-    'name': 'PadV2',
+    'name': 'pad',
     'inputs': [[1, 225, 225, 3]],
     'args': [[[0, 0], [3, 3], [3, 3], [0, 0]]]
   },
   {
-    'name': 'PadV2',
+    'name': 'pad',
     'inputs': [[500, 600, 3]],
     'args': [[[50, 50], [0, 0], [0, 0]]]
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 8, 8, 256], [1, 1, 256, 1024]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 8, 8, 512], [1, 1, 512, 2048]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 8, 8, 1024], [1, 1, 1024, 2048]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 8, 8, 1024], [1, 1, 1024, 512]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 8, 8, 2048], [1, 1, 2048, 17]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 8, 8, 2048], [1, 1, 2048, 32]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 8, 8, 2048], [1, 1, 2048, 34]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 8, 8, 2048], [1, 1, 2048, 512]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 15, 15, 128], [1, 1, 128, 512]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 15, 15, 256], [1, 1, 256, 1024]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 15, 15, 512], [1, 1, 512, 256]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 15, 15, 512], [1, 1, 512, 1024]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 15, 15, 1024], [1, 1, 1024, 256]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 29, 29, 64], [1, 1, 64, 256]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 29, 29, 128], [1, 1, 128, 512]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 29, 29, 256], [1, 1, 256, 128]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 29, 29, 256], [1, 1, 256, 512]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 29, 29, 512], [1, 1, 512, 128]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 57, 57, 64], [1, 1, 64, 64]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 57, 57, 64], [1, 1, 64, 256]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 57, 57, 256], [1, 1, 256, 64]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 8, 8, 512], [3, 3, 512, 512]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 15, 15, 256], [3, 3, 256, 256]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 17, 17, 256], [3, 3, 256, 256]],
     'args': [2, 'valid']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 29, 29, 128], [3, 3, 128, 128]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 31, 31, 128], [3, 3, 128, 128]],
     'args': [2, 'valid']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 57, 57, 64], [3, 3, 64, 64]],
     'args': [1, 'same']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 59, 59, 64], [3, 3, 64, 64]],
     'args': [2, 'valid']
   },
   {
-    'name': 'Conv2D',
+    'name': 'conv2d',
     'inputs': [[1, 231, 231, 3], [7, 7, 3, 64]],
     'args': [2, 'valid']
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 15, 15, 128]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 15, 15, 256]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 15, 15, 512]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 15, 15, 1024]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 29, 29, 64]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 29, 29, 128]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 29, 29, 256]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 29, 29, 512]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 57, 57, 64]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 57, 57, 256]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 8, 8, 256]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 8, 8, 512]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 8, 8, 1024]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 8, 8, 2048]],
   },
   {
-    'name': 'Relu',
+    'name': 'relu',
     'inputs': [[1, 113, 113, 64]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 8, 8, 17], [17]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 8, 8, 32], [32]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 8, 8, 34], [34]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 8, 8, 256], [256]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 8, 8, 512], [512]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 8, 8, 1024], [1, 8, 8, 1024]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 8, 8, 1024], [1024]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 8, 8, 2048], [1, 8, 8, 2048]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 8, 8, 2048], [2048]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 15, 15, 128], [128]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 15, 15, 256], [256]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 15, 15, 512], [1, 15, 15, 512]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 15, 15, 512], [512]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 15, 15, 1024], [1, 15, 15, 1024]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 15, 15, 1024], [1024]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 29, 29, 64], [64]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 29, 29, 128], [128]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 29, 29, 256], [1, 29, 29, 256]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 29, 29, 256], [256]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 29, 29, 512], [1, 29, 29, 512]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 29, 29, 512], [512]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 57, 57, 64], [64]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 57, 57, 256], [1, 57, 57, 256]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 57, 57, 256], [256]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[1, 113, 113, 64], [64]],
   },
   {
-    'name': 'Add',
+    'name': 'add',
     'inputs': [[256, 256, 3], [3]],
   },
-  {'name': 'MaxPool', 'inputs': [[1, 15, 15, 1024]], 'args': [1, 2, 'same']},
-  {'name': 'MaxPool', 'inputs': [[1, 29, 29, 512]], 'args': [1, 2, 'same']},
-  {'name': 'MaxPool', 'inputs': [[1, 57, 57, 256]], 'args': [1, 2, 'same']},
-  {'name': 'MaxPool', 'inputs': [[1, 115, 115, 64]], 'args': [3, 2, 'valid']},
+  {'name': 'maxPool', 'inputs': [[1, 15, 15, 1024]], 'args': [1, 2, 'same']},
+  {'name': 'maxPool', 'inputs': [[1, 29, 29, 512]], 'args': [1, 2, 'same']},
+  {'name': 'maxPool', 'inputs': [[1, 57, 57, 256]], 'args': [1, 2, 'same']},
+  {'name': 'maxPool', 'inputs': [[1, 115, 115, 64]], 'args': [3, 2, 'valid']},
 ];
