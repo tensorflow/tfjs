97c166071988f8043e55a5d27092120770412bd3
Na Li <linazhao@google.com>
2020-03-18 18:24:32 -0700

    Infer release branch.

--- scripts/release-util.ts
+++ scripts/release-util.ts
@@ -2,6 +2,105 @@ import chalk from 'chalk';
 import * as readline from 'readline';
 import * as shell from 'shelljs';
 
+export interface Phase {
+  // The list of packages that will be updated with this change.
+  packages: string[];
+  // The list of dependencies that all of the packages will update to.
+  deps?: string[];
+  // An ordered map of scripts, key is package name, value is an object with two
+  // optional fields: `before-yarn` with scripts to run before `yarn`, and
+  // `after-yarn` with scripts to run after yarn is called and before the pull
+  // request is sent out.
+  scripts?: {[key: string]: {[key: string]: string[]}};
+  // Whether to leave the version of the package alone. Defaults to false
+  // (change the version).
+  leaveVersion?: boolean;
+  title?: string;
+}
+
+export interface ReleaseUnit {
+  // A human-readable name. Used for generating release branch.
+  name: string;
+  // The phases in this release unit.
+  phases: Phase[];
+  // The repository *only if it is not the same as tfjs*.
+  repo?: string;
+}
+
+export const CORE_PHASE: Phase = {
+  packages: ['tfjs-core']
+};
+
+export const LAYERS_CONVERTER_PHASE: Phase = {
+  packages: ['tfjs-layers', 'tfjs-converter'],
+  deps: ['tfjs-core']
+};
+
+export const DATA_PHASE: Phase = {
+  packages: ['tfjs-data'],
+  deps: ['tfjs-core', 'tfjs-layers']
+}
+
+export const UNION_PHASE: Phase = {
+  packages: ['tfjs'],
+  deps: ['tfjs-core', 'tfjs-layers', 'tfjs-converter', 'tfjs-data']
+};
+
+export const NODE_PHASE: Phase = {
+  packages: ['tfjs-node', 'tfjs-node-gpu'],
+  deps: ['tfjs', 'tfjs-core'],
+  scripts: {'tfjs-node-gpu': {'before-yarn': ['yarn prep-gpu']}}
+};
+
+export const WASM_PHASE: Phase = {
+  packages: ['tfjs-backend-wasm'],
+  deps: ['tfjs-core']
+};
+
+export const VIS_PHASE: Phase = {
+  packages: ['tfjs-vis']
+};
+
+export const REACT_NATIVE_PHASE: Phase = {
+  packages: ['tfjs-react-native']
+};
+
+export const WEBSITE_PHASE: Phase = {
+  packages: ['tfjs-website'],
+  deps: ['tfjs', 'tfjs-node', 'tfjs-vis', 'tfjs-react-native'],
+  scripts: {'tfjs-website': {'after-yarn': ['yarn build-prod']}},
+  leaveVersion: true,
+  title: 'Update website to latest dependencies.'
+};
+
+export const TFJS_RELEASE_UNIT: ReleaseUnit = {
+  name: 'tfjs',
+  phases: [
+    CORE_PHASE, LAYERS_CONVERTER_PHASE, DATA_PHASE, UNION_PHASE, NODE_PHASE,
+    WASM_PHASE
+  ]
+};
+
+export const VIS_RELEASE_UNIT: ReleaseUnit = {
+  name: 'vis',
+  phases: [VIS_PHASE]
+};
+
+export const REACT_NATIVE_RELEASE_UNIT: ReleaseUnit = {
+  name: 'react-native',
+  phases: [REACT_NATIVE_PHASE]
+};
+
+export const WEBSITE_RELEASE_UNIT: ReleaseUnit = {
+  name: 'website',
+  phases: [WEBSITE_PHASE],
+  repo: 'tfjs-website'
+};
+
+export const RELEASE_UNITS: ReleaseUnit[] = [
+  TFJS_RELEASE_UNIT, VIS_RELEASE_UNIT, REACT_NATIVE_RELEASE_UNIT, WEBSITE_RELEASE_UNIT
+];
+
 const rl =
   readline.createInterface({input: process.stdin, output: process.stdout});
 
@@ -24,4 +123,11 @@ export function $(cmd: string) {
     process.exit(1);
   }
   return result.stdout.trim();
+}
+
+export function printReleaseUnit(id: number) {
+  const releaseUnit = RELEASE_UNITS[id];
+  console.log(chalk.green(`Release unit ${id}:`));
+  console.log(` packages: ${chalk.blue(releaseUnit.phases.map(
+    phase => phase.packages.join(', ')).join(', '))}`);
 }
--- scripts/release.ts
+++ scripts/release.ts
@@ -32,105 +32,7 @@ import * as fs from 'fs';
 import * as mkdirp from 'mkdirp';
 import * as readline from 'readline';
 import * as shell from 'shelljs';
-
-interface Phase {
-  // The list of packages that will be updated with this change.
-  packages: string[];
-  // The list of dependencies that all of the packages will update to.
-  deps?: string[];
-  // An ordered map of scripts, key is package name, value is an object with two
-  // optional fields: `before-yarn` with scripts to run before `yarn`, and
-  // `after-yarn` with scripts to run after yarn is called and before the pull
-  // request is sent out.
-  scripts?: {[key: string]: {[key: string]: string[]}};
-  // Whether to leave the version of the package alone. Defaults to false
-  // (change the version).
-  leaveVersion?: boolean;
-  title?: string;
-}
-
-interface ReleaseUnit {
-  // A human-readable name. Used for generating release branch.
-  name: string;
-  // The phases in this release unit.
-  phases: Phase[];
-  // The repository *only if it is not the same as tfjs*.
-  repo?: string;
-}
-
-const CORE_PHASE: Phase = {
-  packages: ['tfjs-core']
-};
-
-const LAYERS_CONVERTER_PHASE: Phase = {
-  packages: ['tfjs-layers', 'tfjs-converter'],
-  deps: ['tfjs-core']
-};
-
-const DATA_PHASE: Phase = {
-  packages: ['tfjs-data'],
-  deps: ['tfjs-core', 'tfjs-layers']
-}
-
-const UNION_PHASE: Phase = {
-  packages: ['tfjs'],
-  deps: ['tfjs-core', 'tfjs-layers', 'tfjs-converter', 'tfjs-data']
-};
-
-const NODE_PHASE: Phase = {
-  packages: ['tfjs-node', 'tfjs-node-gpu'],
-  deps: ['tfjs', 'tfjs-core'],
-  scripts: {'tfjs-node-gpu': {'before-yarn': ['yarn prep-gpu']}}
-};
-
-const WASM_PHASE: Phase = {
-  packages: ['tfjs-backend-wasm'],
-  deps: ['tfjs-core']
-};
-
-const VIS_PHASE: Phase = {
-  packages: ['tfjs-vis']
-};
-
-const REACT_NATIVE_PHASE: Phase = {
-  packages: ['tfjs-react-native']
-};
-
-const WEBSITE_PHASE: Phase = {
-  packages: ['tfjs-website'],
-  deps: ['tfjs', 'tfjs-node', 'tfjs-vis', 'tfjs-react-native'],
-  scripts: {'tfjs-website': {'after-yarn': ['yarn build-prod']}},
-  leaveVersion: true,
-  title: 'Update website to latest dependencies.'
-};
-
-const TFJS_RELEASE_UNIT: ReleaseUnit = {
-  name: 'tfjs',
-  phases: [
-    CORE_PHASE, LAYERS_CONVERTER_PHASE, DATA_PHASE, UNION_PHASE, NODE_PHASE,
-    WASM_PHASE
-  ]
-};
-
-const VIS_RELEASE_UNIT: ReleaseUnit = {
-  name: 'vis',
-  phases: [VIS_PHASE]
-};
-
-const REACT_NATIVE_RELEASE_UNIT: ReleaseUnit = {
-  name: 'react-native',
-  phases: [REACT_NATIVE_PHASE]
-};
-
-const WEBSITE_RELEASE_UNIT: ReleaseUnit = {
-  name: 'website',
-  phases: [WEBSITE_PHASE],
-  repo: 'tfjs-website'
-};
-
-const RELEASE_UNITS: ReleaseUnit[] = [
-  TFJS_RELEASE_UNIT, VIS_RELEASE_UNIT, REACT_NATIVE_RELEASE_UNIT, WEBSITE_RELEASE_UNIT
-];
+import {RELEASE_UNITS, Phase, $, question, printReleaseUnit} from './release-util';
 
 const TMP_DIR = '/tmp/tfjs-release';
 
@@ -140,13 +42,6 @@ parser.addArgument('--git-protocol', {
   action: 'storeTrue',
   help: 'Use the git protocal rather than the http protocol when cloning repos.'
 });
-
-function printReleaseUnit(id: number) {
-  const releaseUnit = RELEASE_UNITS[id];
-  console.log(chalk.green(`Release unit ${id}:`));
-  console.log(` packages: ${chalk.blue(releaseUnit.phases.map(
-    phase => phase.packages.join(', ')).join(', '))}`);
-}
 
 function printPhase(phases: Phase[], phaseId: number) {
   const phase = phases[phaseId];
@@ -355,30 +250,6 @@ async function main() {
     'a new package version');
 
   process.exit(0);
-}
-
-/**
- * A wrapper around shell.exec for readability.
- * @param cmd The bash command to execute.
- * @returns stdout returned by the executed bash script.
- */
-function $(cmd: string) {
-  const result = shell.exec(cmd, {silent: true});
-  if (result.code > 0) {
-    console.log('$', cmd);
-    console.log(result.stderr);
-    process.exit(1);
-  }
-  return result.stdout.trim();
-}
-
-const rl =
-  readline.createInterface({input: process.stdin, output: process.stdout});
-
-async function question(questionStr: string): Promise<string> {
-  console.log(chalk.bold(questionStr));
-  return new Promise<string>(
-    resolve => rl.question('> ', response => resolve(response)));
 }
 
 main();
