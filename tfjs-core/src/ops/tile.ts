1530a9d30f1c9c2aeba8a0768e5573638844cc64
Yannick Assogba <yassogba@google.com>
2020-03-23 17:15:44 -0400

    add modular tile gradient

--- tfjs-core/src/ops/tile.ts
+++ tfjs-core/src/ops/tile.ts
@@ -25,7 +25,6 @@ import {DataType, TensorLike} from '../types';
 import * as util from '../util';
 
 import {op} from './operation';
-import {zerosLike} from './tensor_ops';
 
 /**
  * Construct a tensor by repeating it the number of times given by reps.
@@ -59,59 +58,6 @@ function tile_<T extends Tensor>(x: T|TensorLike, reps: number[]): T {
       () => `Error in transpose: rank of input ${$x.rank} ` +
           `must match length of reps ${reps}.`);
 
-  const grad = (dy: T, saved: Tensor[]) => {
-    const [$x] = saved;
-    const derX = () => {
-      let xGrad = zerosLike($x);
-      // TODO(cais): Maybe reduce memory footprint by avoiding repeated
-      // slicing.
-      if ($x.rank === 1) {
-        for (let i = 0; i < reps[0]; ++i) {
-          xGrad = xGrad.add(dy.slice([i * $x.shape[0]], [$x.shape[0]]));
-        }
-      } else if ($x.rank === 2) {
-        for (let i = 0; i < reps[0]; ++i) {
-          for (let j = 0; j < reps[1]; ++j) {
-            xGrad = xGrad.add(dy.slice(
-                [i * $x.shape[0], j * $x.shape[1]],
-                [$x.shape[0], $x.shape[1]]));
-          }
-        }
-      } else if ($x.rank === 3) {
-        for (let i = 0; i < reps[0]; ++i) {
-          for (let j = 0; j < reps[1]; ++j) {
-            for (let k = 0; k < reps[2]; ++k) {
-              xGrad = xGrad.add(dy.slice(
-                  [i * $x.shape[0], j * $x.shape[1], k * $x.shape[2]],
-                  [$x.shape[0], $x.shape[1], $x.shape[2]]));
-            }
-          }
-        }
-      } else if ($x.rank === 4) {
-        for (let i = 0; i < reps[0]; ++i) {
-          for (let j = 0; j < reps[1]; ++j) {
-            for (let k = 0; k < reps[2]; ++k) {
-              for (let l = 0; l < reps[3]; ++l) {
-                xGrad = xGrad.add(dy.slice(
-                    [
-                      i * $x.shape[0], j * $x.shape[1], k * $x.shape[2],
-                      l * $x.shape[3]
-                    ],
-                    [$x.shape[0], $x.shape[1], $x.shape[2], $x.shape[3]]));
-              }
-            }
-          }
-        }
-      } else {
-        throw new Error(
-            `Gradient for tile operation is not implemented for rank-` +
-            `${$x.rank} tensors yet.`);
-      }
-      return xGrad as T;
-    };
-    return {x: derX};
-  };
-
   const forward: ForwardFunc<T> = (backend, save) => {
     const res = backend.tile($x, reps);
     save([$x]);
@@ -123,7 +69,7 @@ function tile_<T extends Tensor>(x: T|TensorLike, reps: number[]): T {
   const attrs: TileAttrs = {reps};
 
   return ENGINE.runKernelFunc(
-      forward, inputs as unknown as NamedTensorMap, grad, Tile,
+      forward, inputs as unknown as NamedTensorMap, null /* grad */, Tile,
       attrs as unknown as NamedAttrMap, inputsToSave);
 }
 
