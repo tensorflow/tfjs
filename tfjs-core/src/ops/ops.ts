7f6c78a094074caf3163c2ae57a623a2d4a5dbcc
Yannick Assogba <yassogba@google.com>
2020-03-23 10:14:45 -0400

    Modularize random distribution tensor creation ops (#2939)
    INTERNAL

--- tfjs-core/src/ops/ops.ts
+++ tfjs-core/src/ops/ops.ts
@@ -17,8 +17,14 @@
 
 // Modularized ops.
 export {broadcastTo} from './broadcast_to';
+export {multinomial} from './multinomial';
+export {rand} from './rand';
+export {randomGamma} from './random_gamma';
+export {randomNormal} from './random_normal';
+export {randomUniform} from './random_uniform';
 export {square} from './square';
 export {squaredDifference} from './squared_difference';
+export {truncatedNormal} from './truncated_normal';
 
 export * from './batchnorm';
 export * from './boolean_mask';
