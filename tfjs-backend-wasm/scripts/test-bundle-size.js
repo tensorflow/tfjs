68723db82ef68eeead8be2f10d08ac02effebf00
Yannick Assogba <yassogba@google.com>
2020-03-24 12:03:55 -0400

    fix wasm test-bundle-size

--- tfjs-backend-wasm/scripts/test-bundle-size.js
+++ tfjs-backend-wasm/scripts/test-bundle-size.js
@@ -36,9 +36,7 @@ exec(
 
 shell.cd(dirName);
 shell.cd(wasmDirName);
-exec(
-    `yarn && yarn build-core && ./scripts/build-ci.sh && yarn rollup -c`,
-    {silent: false});
+exec(`yarn && ./scripts/build-ci.sh && yarn rollup -c`, {silent: false});
 
 const masterMinBundleSize = getFileSizeBytes(bundleFilename);
 const masterWasmSize = getFileSizeBytes(wasmFileName);
