e91c9f1a0c12c83d3214fcb4a01dd090bd9a8f8f
Yannick Assogba <yassogba@google.com>
2020-03-24 11:08:57 -0400

    save

--- tfjs-core/src/gradients/OneHot_grad.ts
+++ tfjs-core/src/gradients/OneHot_grad.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2019 Google Inc. All Rights Reserved.
+ * Copyright 2020 Google Inc. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
