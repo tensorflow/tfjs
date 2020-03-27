3048859a2c903033a57f735dfa3b4ba4cdcd6fa3
Na Li <linazhao@google.com>
2020-03-27 12:51:51 -0700

    Fix release.ts

--- scripts/release-util.ts
+++ scripts/release-util.ts
@@ -52,7 +52,7 @@ export const UNION_PHASE: Phase = {
 // the test to tf.layers.
 export const NODE_PHASE: Phase = {
   packages: ['tfjs-node', 'tfjs-node-gpu'],
-  deps: ['tfjs', 'tfjs-core'],
+  deps: ['tfjs', 'tfjs-core', 'tfjs-layers'],
   scripts: {'tfjs-node-gpu': {'before-yarn': ['yarn prep-gpu']}}
 };
 
