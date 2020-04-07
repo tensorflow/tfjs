035a8835f8ad07b778675b9639ed0cbad33ac238
Xu Xing <xing.xu@intel.com>
2020-04-07 10:00:19 +0800

    Use await before nonMaxSuppression

--- tfjs-backend-webgpu/src/setup_test.ts
+++ tfjs-backend-webgpu/src/setup_test.ts
@@ -145,13 +145,7 @@ const TEST_FILTERS: TestFilter[] = [
                           // 'CanvasRenderingContext2D': The source width is 0
     ]
   },
-  {
-    include: 'nonMaxSuppression',
-    excludes: [
-      'works with other operators, div'  // TODO(xing.xu@intel.com):
-                                         // https://github.com/tensorflow/tfjs/issues/2976.
-    ]
-  },
+  {include: 'nonMaxSuppression', excludes: []},
   {
     include: 'argmax',
     excludes: [
