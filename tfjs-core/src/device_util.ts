3f07941c60d6531eeed4e12a9f048328084cc158
Ping Yu <4018+pyu10055@users.noreply.github.com>
2020-04-13 08:15:07 -0700

    make sure nagvigator is available

--- tfjs-core/src/device_util.ts
+++ tfjs-core/src/device_util.ts
@@ -16,8 +16,11 @@
  */
 
 export function isMobile(): boolean {
-  // tslint:disable-next-line:no-any
-  const a = navigator.userAgent || navigator.vendor || (window as any).opera;
+  const a = (typeof navigator !== 'undefined' && navigator != null &&
+                 navigator.userAgent ||
+             navigator.vendor) ||
+      // tslint:disable-next-line:no-any
+      (window as any).opera;
   // tslint:disable-next-line:max-line-length
   return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i
              .test(a) ||
