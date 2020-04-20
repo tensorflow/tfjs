b5727280191d2d7c736ba77c4633df0abf5e228c
Xu Xing <xing.xu@intel.com>
2020-04-20 10:51:09 +0800

    Full match in test ruls

--- tfjs-backend-wasm/src/setup_test.ts
+++ tfjs-backend-wasm/src/setup_test.ts
@@ -119,18 +119,11 @@ const TEST_FILTERS: TestFilter[] = [
       'gradient',          // Gradients not defined yet.
       'NCHW',              // xnn pack does not support channels first.
       // Issue: https://github.com/tensorflow/tfjs/issues/3104.
-      'relu bias stride 2 x=[1,8,8,16] f=[3,3,16,1] s=[2,2] d=4',   // Actual
-                                                                    // !=
-                                                                    // expected.
-      'relu bias stride 2 x=[1,8,8,16] f=[3,3,16,1] s=[2,2] d=8',   // Actual
-                                                                    // !=
-                                                                    // expected.
-      'relu bias stride 2 x=[1,8,8,16] f=[3,3,16,1] s=[2,2] d=32',  // Actual
-                                                                    // !=
-                                                                    // expected.
-      'prelu bias stride 2 x=[1,8,8,16] f=[3,3,16,1] s=[2,2] d=8',  // Actual
-                                                                    // !=
-                                                                    // expected.
+      // Actual != expected.
+      'relu bias stride 2 x=[1,8,8,16] f=[3,3,16,1] s=[2,2] d=4 p=same',
+      'relu bias stride 2 x=[1,8,8,16] f=[3,3,16,1] s=[2,2] d=8 p=same',
+      'relu bias stride 2 x=[1,8,8,16] f=[3,3,16,1] s=[2,2] d=32 p=same',
+      'prelu bias stride 2 x=[1,8,8,16] f=[3,3,16,1] s=[2,2] d=8 p=same',
     ]
   },
   {
