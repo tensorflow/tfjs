ff6d59e8f2855f17d1e07c3f4763d221881ff09d
Yannick Assogba <yassogba@google.com>
2020-05-05 13:59:15 -0400

    save

--- tfjs-core/karma.conf.js
+++ tfjs-core/karma.conf.js
@@ -55,8 +55,8 @@ const browserstackConfig = {
 const webworkerConfig = {
   ...browserstackConfig,
   files: [
-    'dist/setup_test.ts',
-    'dist/worker_test.ts',
+    'dist/setup_test.js',
+    'dist/worker_test.js',
     // Serve dist/tf-core.min.js and tf-backend-cpu.min.js as a static
     // resource, but do not include in the test runner
     {pattern: 'dist/tf-core.min.js', included: false},
