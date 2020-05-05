e26e400b92e5ddf69f56d96c3885cf8587fb7c50
Yannick Assogba <yassogba@google.com>
2020-05-05 13:21:47 -0400

    save

--- tfjs-core/scripts/test-ci.sh
+++ tfjs-core/scripts/test-ci.sh
@@ -32,7 +32,7 @@ then
   # Make a dist/tf-core.min.js file to be imported by the web worker.
   yarn rollup -c --ci
   # copy core & cpu backend bundle somewhere the test can access it
-  cp -v ../tfjs-backend-cpu/dist/tf-core.min.js dist/
+  cp -v ../tfjs-core/dist/tf-core.min.js dist/
   cp -v ../tfjs-backend-cpu/dist/tf-backend-cpu.min.js dist/
   yarn test-webworker --browsers=bs_safari_mac,bs_chrome_mac
 else
