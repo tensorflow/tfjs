2022b62498601cd7e212e0b544d050b841f76902
David Dengg <david@anyline.com>
2020-05-13 11:04:43 +0200

    4b0ede47 tfs-converter: Adds loadSync function to GraphModel

--- tfjs-converter/src/executor/graph_model.ts
+++ tfjs-converter/src/executor/graph_model.ts
@@ -108,6 +108,7 @@ export class GraphModel implements InferenceModel {
    * Loads the model and weight files, construct the in memory weight map and
    * compile the inference graph.
    */
+
   async load(): Promise<boolean> {
     this.findIOHandler();
     if (this.handler.load == null) {
@@ -115,7 +116,18 @@ export class GraphModel implements InferenceModel {
           'Cannot proceed with model loading because the IOHandler provided ' +
           'does not have the `load` method implemented.');
     }
-    this.artifacts = await this.handler.load();
+    const artifacts = await this.handler.load();
+
+    return this.loadSync(artifacts)
+  }
+
+  /**
+   * Synchronously construct the in memory weight map and
+   * compile the inference graph.
+   */
+
+  loadSync(artifacts:io.ModelArtifacts) {    
+    this.artifacts = artifacts;
     const graph = this.artifacts.modelTopology as tensorflow.IGraphDef;
     let signature = {};
     if (this.artifacts.userDefinedMetadata != null) {
