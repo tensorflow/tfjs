8719988676d660c4e7984681a5a7dffc68d93dcf
Ping Yu <4018+pyu10055@users.noreply.github.com>
2020-05-28 15:51:38 -0700

    Adding support for Control flow v2 in converter and executor (#3346)
    FEATURE
    * make sure graph def library fields are not removed during optimization
    
    * added support function mapping and subgraph executor mapping
    
    * add support both StatelessIf and StatelessWhile ops
    
    * fix test error
    
    * require different version of pylint based python version
    
    * fix import
    
    * make sure function field exist in library
    
    * address comments

--- tfjs-converter/src/operations/operation_mapper_test.ts
+++ tfjs-converter/src/operations/operation_mapper_test.ts
@@ -150,6 +150,96 @@ const SIMPLE_MODEL: tensorflow.IGraphDef = {
       attr: {DstT: {type: tensorflow.DataType.DT_UINT8}}
     },
   ],
+  library: {
+    function: [
+      {
+        signature: {
+          name: '__inference_while_cond_10_49_frozen',
+          inputArg: [
+            {name: 'while_loop_counter', type: tensorflow.DataType.DT_INT32}, {
+              name: 'while_maximum_iterations',
+              type: tensorflow.DataType.DT_INT32
+            },
+            {name: 'placeholder', type: tensorflow.DataType.DT_INT32},
+            {name: 'less_y', type: tensorflow.DataType.DT_INT32}, {
+              name: 'while_cond_10___redundant_placeholder0',
+              type: tensorflow.DataType.DT_INT32
+            }
+          ],
+          outputArg: [{name: 'identity', type: tensorflow.DataType.DT_BOOL}]
+        },
+        nodeDef: [{
+          name: 'Less',
+          op: 'Less',
+          input: ['placeholder', 'less_y'],
+          attr: {T: {type: tensorflow.DataType.DT_INT32}}
+        }],
+        ret: {identity: 'Less:z:0'}
+      },
+      {
+        signature: {
+          name: '__inference_while_body_11_40_frozen',
+          inputArg: [
+            {name: 'while_loop_counter', type: tensorflow.DataType.DT_INT32}, {
+              name: 'while_maximum_iterations',
+              type: tensorflow.DataType.DT_INT32
+            },
+            {name: 'placeholder', type: tensorflow.DataType.DT_INT32},
+            {name: 'y_0', type: tensorflow.DataType.DT_INT32},
+            {name: 'add_1_z_0', type: tensorflow.DataType.DT_INT32}
+          ],
+          outputArg: [
+            {name: 'identity', type: tensorflow.DataType.DT_INT32},
+            {name: 'identity_1', type: tensorflow.DataType.DT_INT32},
+            {name: 'identity_2', type: tensorflow.DataType.DT_INT32},
+            {name: 'y', type: tensorflow.DataType.DT_INT32},
+            {name: 'add_1_z', type: tensorflow.DataType.DT_INT32}
+          ]
+        },
+        nodeDef: [
+          {
+            name: 'add_2/y',
+            op: 'Const',
+            attr: {
+              dtype: {type: tensorflow.DataType.DT_INT32},
+              value: {
+                tensor: {
+                  dtype: tensorflow.DataType.DT_INT32,
+                  tensorShape: {},
+                  intVal: [1]
+                }
+              }
+            }
+          },
+          {
+            name: 'add',
+            op: 'AddV2',
+            input: ['placeholder', 'y_0'],
+            attr: {T: {type: tensorflow.DataType.DT_INT32}}
+          },
+          {
+            name: 'add_2',
+            op: 'AddV2',
+            input: ['add_2/y:output:0', 'while_loop_counter'],
+            attr: {T: {type: tensorflow.DataType.DT_INT32}}
+          },
+          {
+            name: 'add_1',
+            op: 'AddV2',
+            input: ['add:z:0', 'add_1_z_0'],
+            attr: {T: {type: tensorflow.DataType.DT_INT32}}
+          }
+        ],
+        ret: {
+          identity_1: 'while_maximum_iterations',
+          identity_2: 'add_1:z:0',
+          y: 'y_0',
+          identity: 'add_2:z:0',
+          add_1_z: 'add_1_z_0'
+        }
+      }
+    ]
+  },
   versions: {producer: 1.0}
 };
 
@@ -222,6 +312,72 @@ describe('operationMapper without signature', () => {
       });
     });
 
+    describe('function level', () => {
+      it('should convert the functions', () => {
+        expect(Object.keys(convertedGraph.functions)).toEqual([
+          '__inference_while_cond_10_49_frozen',
+          '__inference_while_body_11_40_frozen'
+        ]);
+      });
+      it('should find the graph input nodes', () => {
+        expect(convertedGraph.functions['__inference_while_cond_10_49_frozen']
+                   .inputs.map(node => node.name))
+            .toEqual([
+              'while_loop_counter', 'while_maximum_iterations', 'placeholder',
+              'less_y', 'while_cond_10___redundant_placeholder0'
+            ]);
+      });
+
+      it('should find the graph output nodes', () => {
+        expect(convertedGraph.functions['__inference_while_cond_10_49_frozen']
+                   .outputs.map(node => node.name))
+            .toEqual(['Less']);
+      });
+
+      it('should find the graph weight nodes', () => {
+        expect(convertedGraph.functions['__inference_while_cond_10_49_frozen']
+                   .weights.map(node => node.name))
+            .toEqual([]);
+      });
+
+      it('should convert nodes', () => {
+        expect(Object.keys(convertedGraph
+                               .functions['__inference_while_cond_10_49_frozen']
+                               .nodes))
+            .toEqual([
+              'Less', 'while_loop_counter', 'while_maximum_iterations',
+              'placeholder', 'less_y', 'while_cond_10___redundant_placeholder0'
+            ]);
+      });
+      it('should convert signature', () => {
+        expect(convertedGraph.functions['__inference_while_cond_10_49_frozen']
+                   .signature)
+            .toEqual({
+              methodName: '__inference_while_cond_10_49_frozen',
+              inputs: {
+                while_loop_counter: {
+                  name: 'while_loop_counter',
+                  dtype: tensorflow.DataType.DT_INT32
+                },
+                while_maximum_iterations: {
+                  name: 'while_maximum_iterations',
+                  dtype: tensorflow.DataType.DT_INT32
+                },
+                placeholder:
+                    {name: 'placeholder', dtype: tensorflow.DataType.DT_INT32},
+                less_y: {name: 'less_y', dtype: tensorflow.DataType.DT_INT32},
+                while_cond_10___redundant_placeholder0: {
+                  name: 'while_cond_10___redundant_placeholder0',
+                  dtype: tensorflow.DataType.DT_INT32
+                }
+              },
+              outputs: {
+                identity:
+                    {name: 'Less:z:0', dtype: tensorflow.DataType.DT_BOOL}
+              }
+            });
+      });
+    });
     describe('node level', () => {
       it('should find the input nodes', () => {
         expect(convertedGraph.nodes['Fill'].inputs.map(node => node.name))
