bcb7511083d8719af6ce5b4b79e8b36247d8350b
Ann Yuan <annyuan@gmail.com>
2020-05-28 14:53:32 -0400

    change v

--- tfjs-backend-wasm/scripts/build-ci.sh
+++ tfjs-backend-wasm/scripts/build-ci.sh
@@ -29,10 +29,10 @@ do
   [ $i -gt 0 ] && echo "Retry in 15 seconds, count: $i" && sleep 15
   # If install is successful, $? will hold 0 and execution will break from the
   # loop.
-  ./emsdk install 1.39.16 && break
+  ./emsdk install 1.39.13 && break
 done
 
-./emsdk activate 1.39.16
+./emsdk activate 1.39.13
 source ./emsdk_env.sh
 cd ..
 
