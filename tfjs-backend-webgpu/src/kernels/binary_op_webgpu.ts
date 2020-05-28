4280eb42498b3f6853e01e2fd18f0af2f2da76b3
Jiajia Qin <jiajia.qin@intel.com>
2020-05-29 03:52:27 +0800

    webgpu: Use shared memory for binary op (#3320)
    PERF
    Co-authored-by: Ann Yuan <annyuan@gmail.com>

--- tfjs-backend-webgpu/src/kernels/binary_op_webgpu.ts
+++ tfjs-backend-webgpu/src/kernels/binary_op_webgpu.ts
@@ -52,16 +52,25 @@ export class BinaryOpProgram implements WebGPUProgram {
   workGroupSize: [number, number, number];
 
   constructor(op: string, aShape: number[], bShape: number[]) {
-    const workGroupSizeX = 16;
+    // This is an experimental value when using shared memory.
+    const workGroupSizeX = 512;
     this.workGroupSize = [workGroupSizeX, 1, 1];
     this.outputShape = backend_util.assertAndGetBroadcastShape(aShape, bShape);
     this.dispatchLayout = flatDispatchLayout(this.outputShape);
     const size = util.sizeFromShape(this.outputShape);
     const fit = util.arraysEqual(aShape, bShape) && size % workGroupSizeX === 0;
-    this.workPerThread = fit ? 1 : 4;
+    this.workPerThread = fit ? 1 : 2;
     this.dispatch = computeDispatch(
         this.dispatchLayout, this.outputShape, this.workGroupSize,
         [this.workPerThread, 1, 1]);
+
+    // TODO(jiajia.qin@intel.com): More tuning on this. Maybe split the shared
+    // memory shader into a new class.
+    const useSharedMemoryWithA =
+        aShape.length === 1 && bShape.length > 1 && aShape[0] < 2048;
+    const useSharedMemoryWithB =
+        bShape.length === 1 && aShape.length > 1 && bShape[0] < 2048;
+
     if (fit) {
       this.userCode = `
           float binaryOperation(float a, float b) {
@@ -77,6 +86,50 @@ export class BinaryOpProgram implements WebGPUProgram {
           }
         `;
       this.shaderKey = `binary2${op}`;
+    } else if (useSharedMemoryWithA || useSharedMemoryWithB) {
+      const type = getCoordsDataType(this.outputShape.length);
+      const sharedMemorySize = useSharedMemoryWithB ? bShape[0] : aShape[0];
+      const sharedIndexSnippet =
+          sharedMemorySize > 1 ? `coords[${this.outputShape.length - 1}]` : '0';
+      const accessDataSnippet = useSharedMemoryWithB ?
+          `            float a = getAAtOutCoords(coords);
+                       float b = sharedBuf[${sharedIndexSnippet}];` :
+          `            float a = sharedBuf[${sharedIndexSnippet}];
+                       float b = getBAtOutCoords(coords);
+      `;
+      this.userCode = `
+      float binaryOperation(float a, float b) {
+        ${op}
+      }
+
+      shared float sharedBuf[${sharedMemorySize}];
+      void main() {
+        int index = int(gl_GlobalInvocationID.x);
+        int localIndex = int(gl_LocalInvocationIndex);
+
+        // Fill in the shared memory buffer. Here we need a loop to make sure
+        // that all data in A|B are uploaded when |sharedMemorySize| is larger
+        // than work group size.
+        while(localIndex < ${sharedMemorySize})
+        {
+          sharedBuf[localIndex] = ${
+          useSharedMemoryWithB ? 'B' : 'A'}[localIndex];
+          localIndex += int(gl_WorkGroupSize.x);
+        }
+        barrier();
+
+        for(int i = 0; i < ${this.workPerThread}; i++) {
+          int flatIndex = index * ${this.workPerThread} + i;
+
+          if(flatIndex < ${size}) {
+            ${type} coords = getCoordsFromFlatIndex(flatIndex);
+
+            ${accessDataSnippet}
+            setOutput(flatIndex, binaryOperation(a, b));
+          }
+        }
+      }
+      `;
     } else {
       const type = getCoordsDataType(this.outputShape.length);
 
