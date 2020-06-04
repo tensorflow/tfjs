32ff635e8b77fa3002a0de41eb23774bbc3378e5
Jiajia Qin <jiajia.qin@intel.com>
2020-06-04 16:20:30 +0800

    webgpu: Refactor binary ops program
    This patch makes shared binary shader as a seperate program to make the logic more clear.
    
    It also increaces the work group size of unary/binary to a larger one. The previously value 16 is not good for thread concurrency.

---
+++ tfjs-backend-webgpu/src/kernels/binary_ops.ts
@@ -0,0 +1,35 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+export const MUL = 'return a * b;';
+export const ADD = 'return a + b;';
+export const SUB = 'return a - b;';
+export const DIV = 'return a / b;';
+export const GREATER = 'return float(a > b);';
+export const GREATER_EQUAL = 'return float(a >= b);';
+export const LESS = `return float(a < b);`;
+export const LESS_EQUAL = `return float(a <= b);`;
+export const SQUARED_DIFFERENCE = 'return (a - b) * (a - b);';
+
+export const INT_DIV = `
+  float s = sign(a) * sign(b);
+  int ia = int(round(a));
+  int ib = int(round(b));
+  return float(idiv(ia, ib, s));
+`;
+
+export const PRELU = `return (a < 0.) ? b * a : a;`;
