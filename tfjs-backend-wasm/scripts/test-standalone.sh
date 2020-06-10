5660a1fc3e2e41bb92a70d6dc43819e0546aec51
Ann Yuan <annyuan@gmail.com>
2020-06-10 14:11:43 -0400

    ad

--- tfjs-backend-wasm/scripts/test-standalone.sh
+++ tfjs-backend-wasm/scripts/test-standalone.sh
@@ -3,3 +3,4 @@ yarn rollup -c
 
 cp dist/tf-backend-wasm.js ../tfjs-core/benchmarks/
 cp wasm-out/tfjs-backend-wasm.wasm ../tfjs-core/benchmarks/
+cp wasm-out/tfjs-backend-wasm-simd.wasm ../tfjs-core/benchmarks/
