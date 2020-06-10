ada6f1f97023f207875e90b17999df76fda95439
Yannick Assogba <yassogba@google.com>
2020-06-10 18:15:20 -0400

    save

--- tfjs-core/src/public/chained_ops/resize_bilinear.ts
+++ tfjs-core/src/public/chained_ops/resize_bilinear.ts
@@ -21,7 +21,7 @@ import {Rank} from '../../types';
 declare module '../../tensor' {
   interface Tensor<R extends Rank = Rank> {
     resizeBilinear<T extends Tensor3D|Tensor4D>(
-        newShape2D: [number, number], alignCorners: boolean): T;
+        newShape2D: [number, number], alignCorners?: boolean): T;
   }
 }
 
