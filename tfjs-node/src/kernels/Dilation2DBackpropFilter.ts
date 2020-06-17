a1336a068fbe199088c15a898e48b19da6018e7f
Na Li <linazhao@google.com>
2020-06-16 21:03:50 -0700

    Register kernel for node.;

--- tfjs-node/src/kernels/Dilation2DBackpropFilter.ts
+++ tfjs-node/src/kernels/Dilation2DBackpropFilter.ts
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
-import {backend_util, Dilation2DAttrs, Dilation2DBackpropFilter, Dilation2DBackpropFilterInputs, KernelConfig, registerKernel} from '@tensorflow/tfjs';
+import {backend_util, Dilation2DAttrs, Dilation2DBackpropFilter, Dilation2DBackpropFilterInputs, KernelConfig} from '@tensorflow/tfjs';
 
 import {createTensorsTypeOpAttr, NodeJSKernelBackend} from '../nodejs_kernel_backend';
 
@@ -51,5 +51,3 @@ export const dilation2dBackpropFilterConfig: KernelConfig = {
         Dilation2DBackpropFilter, opAttrs, [x, filter, dy]);
   }
 };
-
-registerKernel(dilation2dBackpropFilterConfig);
