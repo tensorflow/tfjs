b5d1cf315b584a766256901fa7540ef59873a45f
Na Li <linazhao@google.com>
2020-06-18 14:57:21 -0700

    Move kernel registration to one file.

--- tfjs-node/src/register_all_kernels.ts
+++ tfjs-node/src/register_all_kernels.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2019 Google Inc. All Rights Reserved.
+ * Copyright 2020 Google Inc. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,10 +14,19 @@
  * limitations under the License.
  * =============================================================================
  */
-
 // We explicitly import the modular kernels so they get registered in the
 // global registry when we compile the library. A modular build would replace
 // the contents of this file and import only the kernels that are needed.
-import './non_max_suppression_v5';
-import './Softmax';
-import './SquaredDifference';
+import {KernelConfig, registerKernel} from '@tensorflow/tfjs-core';
+
+import {nonMaxSuppressionV5Config} from './kernels/NonMaxSuppressionV5';
+import {softmaxConfig} from './kernels/Softmax';
+import {squaredDifferenceConfig} from './kernels/SquaredDifference';
+
+// List all kernel configs here
+const kernelConfigs: KernelConfig[] =
+    [nonMaxSuppressionV5Config, softmaxConfig, squaredDifferenceConfig];
+
+for (const kernelConfig of kernelConfigs) {
+  registerKernel(kernelConfig);
+}
