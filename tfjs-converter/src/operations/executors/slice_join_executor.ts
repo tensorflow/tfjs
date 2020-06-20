e95251e4cf3b39b01cb88a4c66ca12c04df81d23
Ping Yu <4018+pyu10055@users.noreply.github.com>
2020-06-19 17:11:18 -0700

    support negative split value

--- tfjs-converter/src/operations/executors/slice_join_executor.ts
+++ tfjs-converter/src/operations/executors/slice_join_executor.ts
@@ -24,9 +24,9 @@ import {InternalOpExecutor, Node} from '../types';
 import {getParamValue} from './utils';
 
 export const executeOp: InternalOpExecutor = (node: Node,
-                                            tensorMap: NamedTensorsMap,
-                                            context: ExecutionContext):
-                                               tfc.Tensor[] => {
+                                              tensorMap: NamedTensorsMap,
+                                              context: ExecutionContext):
+                                                 tfc.Tensor[] => {
   switch (node.op) {
     case 'ConcatV2':
     case 'Concat': {
@@ -66,9 +66,9 @@ export const executeOp: InternalOpExecutor = (node: Node,
       const end = getParamValue('end', node, tensorMap, context) as number[];
       const strides =
           getParamValue('strides', node, tensorMap, context) as number[];
-      const beginMask =
+      let beginMask =
           getParamValue('beginMask', node, tensorMap, context) as number;
-      const endMask =
+      let endMask =
           getParamValue('endMask', node, tensorMap, context) as number;
       const ellipsisMask =
           getParamValue('ellipsisMask', node, tensorMap, context) as number;
@@ -77,13 +77,17 @@ export const executeOp: InternalOpExecutor = (node: Node,
       const shrinkAxisMask =
           getParamValue('shrinkAxisMask', node, tensorMap, context) as number;
       const tensor = getParamValue('x', node, tensorMap, context) as tfc.Tensor;
-      if (begin.length === 1 && tensor.shape.length > 1) {
-        for (let i = 1; i < tensor.shape.length; i++) {
-          begin.push(0);
-          end.push(tensor.shape[i]);
-          strides.push(strides[0]);
+      if (begin.length < tensor.rank) {
+        begin.unshift(0);
+        end.unshift(tensor.shape[0]);
+        if (end[1] === 0) {
+          end[1] = tensor.shape[1];
         }
+        strides.unshift(1);
+        beginMask *= 2;
+        endMask *= 2;
       }
+
       return [tfc.stridedSlice(
           tensor, begin, end, strides, beginMask, endMask, ellipsisMask,
           newAxisMask, shrinkAxisMask)];
@@ -126,9 +130,18 @@ export const executeOp: InternalOpExecutor = (node: Node,
       const numOrSizeSplits =
           getParamValue('numOrSizeSplits', node, tensorMap, context) as number |
           number[];
-      return tfc.split(
-          getParamValue('x', node, tensorMap, context) as tfc.Tensor,
-          numOrSizeSplits, axis);
+      const tensor = getParamValue('x', node, tensorMap, context) as tfc.Tensor;
+
+      // Allow the last number of split array to be -1, which indicates the rest
+      // of dimension is allocated to the last split.
+      if (Array.isArray(numOrSizeSplits)) {
+        if (numOrSizeSplits[numOrSizeSplits.length - 1] === -1) {
+          const total = numOrSizeSplits.reduce((a, b) => b > 0 ? a + b : a);
+          numOrSizeSplits[numOrSizeSplits.length - 1] =
+              tensor.shape[axis] - total;
+        }
+      }
+      return tfc.split(tensor, numOrSizeSplits, axis);
     }
     case 'ScatterNd': {
       const indices =
