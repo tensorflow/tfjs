f9affee01f45b61ce67d6219f348790cec21bb92
Na Li <linazhao@google.com>
2020-06-29 15:51:50 -0700

    Refactor python test.

--- tfjs-inference/python/inference_test.py
+++ tfjs-inference/python/inference_test.py
@@ -30,46 +30,15 @@ import inference
 
 class InferenceTest(tf.test.TestCase):
 
-  def testCpuInference(self):
-    binary_path = os.path.join('../binaries', 'tfjs-inference-linux')
-    model_path = os.path.join('../test_data', 'model.json')
-    test_data_dir = os.path.join('../test_data')
-    tmp_dir = tempfile.mkdtemp()
-
-    result = inference.predict(binary_path, model_path, test_data_dir, tmp_dir)
-    print(result)
-
-    with open(os.path.join(tmp_dir, 'data.json'), 'rt') as f:
-      ys_values = json.load(f)
-
-      # The output is a list of tensor data in the form of dict.
-      # Example output:
-      # [{"0":0.7567615509033203,"1":-0.18349379301071167,"2":0.7567615509033203,"3":-0.18349379301071167}]
-      ys_values = [list(y.values()) for y in ys_values]
-
-    with open(os.path.join(tmp_dir, 'shape.json'), 'rt') as f:
-      ys_shapes = json.load(f)
-
-    with open(os.path.join(tmp_dir, 'dtype.json'), 'rt') as f:
-      ys_dtypes = json.load(f)
-
-    self.assertAllClose(ys_values[0], [
-        0.7567615509033203, -0.18349379301071167, 0.7567615509033203,
-        -0.18349379301071167
-    ])
-    self.assertAllEqual(ys_shapes[0], [2, 2])
-    self.assertEqual(ys_dtypes[0], 'float32')
-    # Cleanup tmp dir.
-    shutil.rmtree(tmp_dir)
-
-  def testWasmInference(self):
+  def testInference(self):
+    backends = ['cpu', 'wasm']
+    for backend in backends:
       binary_path = os.path.join('../binaries', 'tfjs-inference-linux')
       model_path = os.path.join('../test_data', 'model.json')
       test_data_dir = os.path.join('../test_data')
       tmp_dir = tempfile.mkdtemp()
 
-      result = inference.predict(binary_path, model_path, test_data_dir, tmp_dir, 'wasm')
-      print(result)
+      result = inference.predict(binary_path, model_path, test_data_dir, tmp_dir, backend)
 
       with open(os.path.join(tmp_dir, 'data.json'), 'rt') as f:
         ys_values = json.load(f)
