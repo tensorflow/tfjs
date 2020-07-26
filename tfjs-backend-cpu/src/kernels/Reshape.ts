950db62afab650b9e336a778dba6ce861eef13d3
Na Li <linazhao@google.com>
2020-07-26 15:39:11 -0700

    Modularize kernel reshape and introduce refCounter to cpu backend.

---
+++ tfjs-backend-cpu/src/kernels/Reshape.ts
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import {Reshape, ReshapeAttrs, ReshapeInputs} from '@tensorflow/tfjs-core';
+import {KernelConfig} from '@tensorflow/tfjs-core';
+
+export const reshapeConfig: KernelConfig = {
+  kernelName: Reshape,
+  backendName: 'cpu',
+  kernelFunc: ({inputs, attrs}) => {
+    const {x} = inputs as ReshapeInputs;
+    const {shape} = attrs as {} as ReshapeAttrs;
+
+    // Todo(linazhao): use reshapeImpl once the `TensorInfo` refCounter
+    // mechanism are deprecated. Right now, the engine will take care of
+    // increase refCount.
+
+    return {dataId: x.dataId, shape, dtype: x.dtype};
+  }
+};
