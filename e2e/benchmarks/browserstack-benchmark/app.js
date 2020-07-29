e65371b6b6336af4372c0aec7b5b149fe1cc5400
Linchenn <40653845+Linchenn@users.noreply.github.com>
2020-07-29 10:44:28 -0700

    ini workflow

---
+++ e2e/benchmarks/browserstack-benchmark/app.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const socketio = require('socket.io');
+const fs = require('fs');
+const {exec} = require('child_process');
+
+const port = process.env.PORT || 8001;
+
+const app = http.createServer(function(request, response) {
+  const url = request.url === '/' ? '/index.html' : request.url;
+  fs.readFile(__dirname + url, function(err, data) {
+    if (err) {
+      response.writeHead(404);
+      response.end(JSON.stringify(err));
+      return;
+    }
+    response.writeHead(200);
+    response.end(data);
+  });
+});
+
+const io = socketio(app);
+
+app.listen(port, () => {
+  console.log(`  > Running socket on port: ${port}`);
+});
+
+io.on('connection', function(socket) {
+  socket.on('run', benchmark);
+});
+
+function benchmark(config) {
+  // TODO:
+  // 1. Write browsers.json.
+  // 2. Write benchmark parameter config.
+  console.log(`Start benchmarking.`);
+  exec('yarn test', (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const re = /.*\<benchmark\>(.*)\<\/benchmark\>/;
+    const benchmarkResultStr = stdout.match(re)[1];
+    const benchmarkResult = JSON.parse(benchmarkResultStr);
+    io.emit('benchmarkComplete', benchmarkResult);
+  });
+}
---
+++ e2e/benchmarks/browserstack-benchmark/index.js
@@ -0,0 +1,24 @@
+
+const socket = io();
+const state = {
+  run: () => {
+    // Disable the button.
+    benchmarkButton.__li.style.pointerEvents = 'none';
+    benchmarkButton.__li.style.opacity = .5;
+
+    socket.emit('run', true);
+  }
+};
+
+socket.on('benchmarkComplete', function(benchmarkResult) {
+  const {timeInfo, memoryInfo} = benchmarkResult;
+  document.getElementById('results').innerHTML +=
+      JSON.stringify(timeInfo, null, 2);
+
+  // Enable the button.
+  benchmarkButton.__li.style.pointerEvents = '';
+  benchmarkButton.__li.style.opacity = 1;
+});
+
+const gui = new dat.gui.GUI();
+const benchmarkButton = gui.add(state, 'run').name('Run benchmark');
