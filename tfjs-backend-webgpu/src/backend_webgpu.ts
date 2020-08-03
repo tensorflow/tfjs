d0f856cdd0b4bf6843eb13ffc9802fb592f7bf11
Yan, Shaobo <shaobo.yan@intel.com>
2020-08-03 17:02:28 +0800

    WebGPU: Add ImageBitmap as input of fromPixel for tfjs webgpu
    This patch support imageBitmap as input type of fromPixel. By calling
    copyImageBitmapToTexture to do upload.

--- tfjs-backend-webgpu/src/backend_webgpu.ts
+++ tfjs-backend-webgpu/src/backend_webgpu.ts
@@ -33,6 +33,7 @@ import {Conv2DNaiveProgram} from './kernels/conv2d_naive_webgpu';
 import {CropAndResizeProgram} from './kernels/crop_and_resize_webgpu';
 import {DepthwiseConv2DProgram} from './kernels/depthwise_conv2d_webgpu';
 import {FillProgram} from './kernels/fill_webgpu';
+import {FromPixelsProgram} from './kernels/from_pixels_webgpu';
 import {Im2ColProgram} from './kernels/im2col_webgpu';
 import {MatMulPackedProgram} from './kernels/matmul_packed_webgpu';
 import {MatMulProgram} from './kernels/matmul_webgpu';
@@ -100,6 +101,7 @@ export class WebGPUBackend extends KernelBackend {
   queue: GPUQueue;
   glslang: Glslang;
   commandQueue: GPUCommandEncoder[];
+  fromPixelProgram: FromPixelsProgram = null;
 
   private commandQueueOwnedIds = new WeakSet<DataId>();
   private binaryCache: {[key: string]: WebGPUBinary};
@@ -1160,7 +1162,7 @@ export class WebGPUBackend extends KernelBackend {
 
   fromPixels(
       pixels: backend_util.PixelData|ImageData|HTMLImageElement|
-      HTMLCanvasElement|HTMLVideoElement,
+      HTMLCanvasElement|HTMLVideoElement|ImageBitmap,
       numChannels: number): Tensor3D {
     if (pixels == null) {
       throw new Error(
@@ -1168,20 +1170,57 @@ export class WebGPUBackend extends KernelBackend {
     }
 
     const outShape = [pixels.height, pixels.width, numChannels];
+    const size = util.sizeFromShape(outShape);
+    const uniformData: [number, number] = [size, numChannels];
     let imageData = (pixels as ImageData | backend_util.PixelData).data;
 
     if (env().getBool('IS_BROWSER')) {
-      if (!(pixels instanceof HTMLVideoElement) &&
+      if (!(pixels instanceof ImageBitmap) &&
+          !(pixels instanceof HTMLVideoElement) &&
           !(pixels instanceof HTMLImageElement) &&
           !(pixels instanceof HTMLCanvasElement) &&
           !(pixels instanceof ImageData) &&
           !(pixels.data instanceof Uint8Array)) {
         throw new Error(
             'pixels passed to tf.browser.fromPixels() must be either an ' +
-            `HTMLVideoElement, HTMLImageElement, HTMLCanvasElement, ImageData` +
-            ` or {data: Uint32Array, width: number, height: number}, ` +
+            `ImageBitmap, HTMLVideoElement, HTMLImageElement, ` +
+            `HTMLCanvasElement, ImageData` +
+            ` or {data: Uint8Array, width: number, height: number}, ` +
             `but was ${(pixels as {}).constructor.name}`);
       }
+
+      if (pixels instanceof ImageBitmap) {
+        const output = this.makeOutputArray(outShape, 'int32');
+        if (!this.fromPixelProgram) {
+          this.fromPixelProgram = new FromPixelsProgram(outShape);
+
+          const {bindGroupLayout, pipeline} = webgpu_program.compileProgram(
+              this.glslang, this.device, this.fromPixelProgram, [], output);
+
+          // TODO(tfjs-optimization): Replace the uniform buffer with fixed
+          // numbers.
+          this.fromPixelProgram.setUniform(this.device, uniformData);
+
+          this.fromPixelProgram.setWebGPUBinary(bindGroupLayout, pipeline);
+        }
+
+        this.queue.copyImageBitmapToTexture(
+            {imageBitmap: pixels, origin: {x: 0, y: 0}}, {
+              texture: this.fromPixelProgram.makeInputTexture(
+                  this.device, pixels.width, pixels.height)
+            },
+            {width: pixels.width, height: pixels.height, depth: 1});
+
+        const info = this.tensorMap.get(output.dataId);
+
+        info.bufferInfo.buffer = this.acquireBuffer(info.bufferInfo.byteSize);
+
+        this.commandQueue.push(this.fromPixelProgram.generateEncoder(
+            this.device, info.bufferInfo.buffer, uniformData));
+        this.submitQueue();
+        return output as Tensor3D;
+      }
+
       if (pixels instanceof HTMLVideoElement ||
           pixels instanceof HTMLImageElement ||
           pixels instanceof HTMLCanvasElement) {
@@ -1242,6 +1281,7 @@ export class WebGPUBackend extends KernelBackend {
       return;
     }
     this.bufferManager.dispose();
+    this.fromPixelProgram.dispose();
     this.disposed = true;
   }
 }
--- tfjs-backend-webgpu/src/kernels/from_pixels_webgpu.ts
+++ tfjs-backend-webgpu/src/kernels/from_pixels_webgpu.ts
@@ -26,35 +26,144 @@ export class FromPixelsProgram implements WebGPUProgram {
   variableNames = ['A'];
   dispatchLayout: {x: number[]};
   dispatch: [number, number, number];
+  pipeline: GPUComputePipeline;
+  bindGroupLayout: GPUBindGroupLayout;
+
+  uniform: GPUBuffer;
+  lastUniformContent = [0, 0, 0];
+
+  inputTexture: GPUTexture = null;
+  lastPixelSize = {width: 0, height: 0};
+
+  private disposed = false;
 
   constructor(outputShape: number[]) {
     this.outputShape = outputShape;
     this.dispatchLayout = flatDispatchLayout(this.outputShape);
     this.dispatch = computeDispatch(this.dispatchLayout, this.outputShape);
-
+    const workGroupSize = 256;  // The empirical value.
     this.userCode = `
-      void main() {
-        ivec3 coords = getOutputCoords();
-        int texR = coords[0];
-        int texC = coords[1];
-        int depth = coords[2];
-        vec2 uv = (vec2(texC, texR) + halfCR) / vec2(outShape.yx);
+    layout (local_size_x = ${workGroupSize},
+      local_size_y = 1,
+      local_size_z = 1) in;
+    layout(set = 0, binding = 1, rgba8) uniform readonly image2D srcImage;
+    layout(set = 0, binding = 2) uniform Meta {
+      int size;
+      int numChannels;
+    } outShape;
 
-        vec4 values = texelFetch(A, uv);
-        float value;
-        if (depth == 0) {
-          value = values.r;
-        } else if (depth == 1) {
-          value = values.g;
-        } else if (depth == 2) {
-          value = values.b;
-        } else if (depth == 3) {
-          value = values.a;
+    void main() {
+      ivec3 coords = getCoordsFromFlatIndex(
+        int(gl_GlobalInvocationID.x) * outShape.numChannels);
+      int texR = coords[0];
+      int texC = coords[1];
+      int depth = coords[2];
+      vec4 values = imageLoad(srcImage, ivec2(texC, texR));
+
+      for(int i = 0; i < outShape.numChannels; i++) {
+        float value = values[i];
+        int flatIndex =
+          int(gl_GlobalInvocationID.x) * outShape.numChannels + i;
+        if (flatIndex < outShape.size) {
+          result[flatIndex] = int(floor(255.0 * value));
         }
-
-        setOutput(floor(value * 255.0 + 0.5));
       }
+    }
     `;
-    this.shaderKey = 'fromPixel';
+    this.shaderKey = `fromPixel${workGroupSize}`;
+  }
+
+  setWebGPUBinary(
+      bindGroupLayout: GPUBindGroupLayout, pipeline: GPUComputePipeline) {
+    this.bindGroupLayout = bindGroupLayout;
+    this.pipeline = pipeline;
+  }
+
+  setUniform(device: GPUDevice, uniformData: number[]) {
+    if (this.uniform) {
+      this.uniform.destroy();
+    }
+
+    const [uniformBuffer, uniformMapping] =
+        device.createBufferMapped({size: 4 * 2, usage: GPUBufferUsage.UNIFORM});
+    new Uint32Array(uniformMapping).set(uniformData);
+    uniformBuffer.unmap();
+    this.uniform = uniformBuffer;
+  }
+
+  makeInputTexture(device: GPUDevice, pixelWidth: number, pixelHeight: number):
+      GPUTexture {
+    if (!this.inputTexture || this.lastPixelSize.width !== pixelWidth ||
+        this.lastPixelSize.height !== pixelHeight) {
+      if (this.inputTexture) {
+        this.inputTexture.destroy();
+      }
+
+      this.inputTexture = device.createTexture({
+        size: {
+          width: pixelWidth,
+          height: pixelHeight,
+          depth: 1,
+        },
+        format: 'rgba8unorm',
+        usage: GPUTextureUsage.COPY_DST | GPUTextureUsage.STORAGE,
+      });
+      this.lastPixelSize.width = pixelWidth;
+      this.lastPixelSize.height = pixelHeight;
+    }
+    return this.inputTexture;
+  }
+
+  generateEncoder(device: GPUDevice, output: GPUBuffer, uniformData: [
+    number, number
+  ]): GPUCommandEncoder {
+    if (uniformData &&
+        (uniformData[0] !== this.lastUniformContent[0] ||
+         uniformData[1] !== this.lastUniformContent[1])) {
+      // TODO(tfjs-optimization): using new mapAsync API to update the buffer.
+      this.setUniform(device, uniformData);
+    }
+    const bindGroup = device.createBindGroup({
+      layout: this.bindGroupLayout,
+      entries: [
+        {
+          binding: 0,
+          resource: {
+            buffer: output,
+          }
+        },
+        {
+          binding: 1,
+          resource: this.inputTexture.createView(),
+        },
+        {
+          binding: 2,
+          resource: {
+            buffer: this.uniform,
+          }
+        }
+      ],
+    });
+
+    const commandEncoder = device.createCommandEncoder({});
+    const passEncoder = commandEncoder.beginComputePass();
+    passEncoder.setPipeline(this.pipeline);
+    passEncoder.setBindGroup(0, bindGroup);
+    passEncoder.dispatch(this.dispatch[0], this.dispatch[1], this.dispatch[2]);
+    passEncoder.endPass();
+    return commandEncoder;
+  }
+
+  dispose() {
+    if (this.disposed) {
+      return;
+    }
+    if (this.uniform) {
+      this.uniform.destroy();
+    }
+    if (this.inputTexture) {
+      this.inputTexture.destroy();
+    }
+    this.disposed = true;
   }
 }
