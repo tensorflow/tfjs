d3e297cf1aa3a0424c8fb6c0477dddc60b26a274
Ann Yuan <annyuan@gmail.com>
2020-08-03 15:40:08 -0400

    clean

--- tfjs-backend-wasm/src/backend_wasm.ts
+++ tfjs-backend-wasm/src/backend_wasm.ts
@@ -20,8 +20,7 @@ import {backend_util, BackendTimingInfo, DataStorage, DataType, deprecationWarn,
 
 import {BackendWasmModule, WasmFactoryConfig} from '../wasm-out/tfjs-backend-wasm';
 import wasmFactoryThreadedSimd from '../wasm-out/tfjs-backend-wasm-threaded-simd.js';
-// @ts-ignore
-import {wasmWorkerContents} from '../wasm-out/tfjs-backend-wasm-threaded-simd.worker.ts';
+import {wasmWorkerContents} from '../wasm-out/tfjs-backend-wasm-threaded-simd.worker';
 import wasmFactory from '../wasm-out/tfjs-backend-wasm.js';
 
 const WASM_PRIORITY = 2;
