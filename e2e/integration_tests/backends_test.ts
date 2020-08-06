97aa0dac4ad5606cdf3f3dd50bda88c94840330f
Na Li <linazhao@google.com>
2020-08-06 12:41:30 -0700

    Refactor test.

--- e2e/integration_tests/backends_test.ts
+++ e2e/integration_tests/backends_test.ts
@@ -35,69 +35,111 @@ describe(`${SMOKE} backends`, () => {
       // A backend with no refCounter.
       await tfc.setBackend('webgl');
 
-      // numDataIds = 0
-      const before = tfc.engine().backend.numDataIds();
+      const webglBefore = tfc.engine().backend.numDataIds();
 
-      // input tensorData is stored in webgl backend.
+      // input is stored in webgl backend. tensorInfo refCount = 1.
       const input = tfc.tensor2d([1, 1, 1, 1], [2, 2], 'float32');
 
       // input tensorInfo refCount = 2;
       const inputReshaped = tfc.reshape(input, [2, 2]);
 
+      const webglAfter = tfc.engine().backend.numDataIds();
+
+      expect(webglAfter).toEqual(webglBefore + 1);
+
       await tfc.setBackend('cpu');
 
-      // inputReshaped tensorData is reshaped, during which it is moved to cpu
-      // backend with a refCount 1. After reshape, it has a refCount 2. The
-      // tensorInfo refCount becomes 3 because inputReshaped2 is an output.
+      const cpuBefore = tfc.engine().backend.numDataIds();
+
+      // input moved to cpu. tensorData refCount = 1.
       const inputReshaped2 = tfc.reshape(inputReshaped, [2, 2]);
 
+      const cpuAfter = tfc.engine().backend.numDataIds();
+
+      // input tensorData refCount = 3, reshape increase by 1, then output
+      // tensor increase by 1.
+      expect(cpuAfter).toEqual(cpuBefore + 1);
+
+      await tfc.setBackend('webgl');
+
+      // Because input is moved to cpu, data should be deleted from webgl.
+      expect(tfc.engine().backend.numDataIds()).toEqual(webglAfter - 1);
+
+      await tfc.setBackend('cpu');
+
+      // After dispose, tensorData refCount = 2.
       input.dispose();
+
+      // Input is not deleted, because refCount is 2.
+      expect(tfc.engine().backend.numDataIds()).toEqual(cpuAfter);
+
+      // After dispose, tensorData refCount = 1.
       inputReshaped.dispose();
+
+      // Input is not deleted, because refCount is 1.
+      expect(tfc.engine().backend.numDataIds()).toEqual(cpuAfter);
+
+      // After dispose, tensorData refCount = 0, data deleted.
       inputReshaped2.dispose();
 
       const after = tfc.engine().backend.numDataIds();
 
-      expect(after).toBe(before);
-
-      // There should be no more data in webgl, because they are all moved to
-      // cpu even if the tensorInfo refCount before moving is 2.
-      await tfc.setBackend('webgl');
-      const webglAfter = tfc.engine().backend.numDataIds();
-      expect(webglAfter).toBe(0);
+      expect(after).toBe(cpuBefore);
     });
 
     it(`from cpu to webgl.`, async () => {
       await tfc.setBackend('cpu');
 
-      // numDataIds = 0.
-      const before = tfc.engine().backend.numDataIds();
+      const cpuBefore = tfc.engine().backend.numDataIds();
 
-      // input tensorData is stored in cpu backend.
+      // input is stored in cpu backend. tensorData refCount = 1, tensorInfo
+      // refCount = 1.
       const input = tfc.tensor2d([1, 1, 1, 1], [2, 2], 'float32');
 
-      // input tensorData refCount = 2, tensorInfo refCount = 1.
+      // input tensorData refCount = 3, reshape increase by 1, then output
+      // tensor increase by 1. tensorInfo refCount = 1.
       const inputReshaped = tfc.reshape(input, [2, 2]);
+
+      const cpuAfter = tfc.engine().backend.numDataIds();
+
+      expect(cpuAfter).toEqual(cpuBefore + 1);
 
       await tfc.setBackend('webgl');
 
-      // inputReshaped tensorData is reshaped, during which it is moved to webgl
-      // backend with a refCount 2. Then tensorInfo refCount is also 2. After
-      // reshape, the tensorInfo refCount becomes 3.
+      const webglBefore = tfc.engine().backend.numDataIds();
+
+      // input moved to webgl. tensorInfo refCount = 3.
       const inputReshaped2 = tfc.reshape(inputReshaped, [2, 2]);
 
+      const webglAfter = tfc.engine().backend.numDataIds();
+
+      expect(webglAfter).toEqual(webglBefore + 1);
+
+      await tfc.setBackend('cpu');
+
+      // Because input is moved to webgl, data should be deleted from cpu.
+      expect(tfc.engine().backend.numDataIds()).toEqual(cpuAfter - 1);
+
+      await tfc.setBackend('webgl');
+
+      // After dispose, tensorInfo = 2.
       input.dispose();
+
+      // Data is not deleted, because tensorInfo is 2.
+      expect(tfc.engine().backend.numDataIds()).toEqual(webglAfter);
+
+      // After dipose, tensorInfo = 1.
       inputReshaped.dispose();
+
+      // Data is not deleted, because tensorInfo is 1.
+      expect(tfc.engine().backend.numDataIds()).toEqual(webglAfter);
+
+      // After dipose, tensorInfo = 1, data deleted.
       inputReshaped2.dispose();
 
       const after = tfc.engine().backend.numDataIds();
 
-      expect(after).toBe(before);
-
-      // There should be no more data in cpu, because they are all moved to
-      // webgl.
-      await tfc.setBackend('cpu');
-      const cpuAfter = tfc.engine().backend.numDataIds();
-      expect(cpuAfter).toBe(0);
+      expect(after).toBe(webglBefore);
     });
   });
 });
