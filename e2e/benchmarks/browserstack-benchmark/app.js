042d4b634d11943c964c1d30700454e383d4ba3c
Linchenn <40653845+Linchenn@users.noreply.github.com>
2020-08-05 20:19:19 -0700

    ini tool

--- e2e/benchmarks/browserstack-benchmark/app.js
+++ e2e/benchmarks/browserstack-benchmark/app.js
@@ -19,84 +19,123 @@ const http = require('http');
 const socketio = require('socket.io');
 const fs = require('fs');
 const path = require('path');
-const {exec} = require('child_process');
 
 const port = process.env.PORT || 8001;
+let io;
+let availableBrowsers;
 
-const app = http.createServer((request, response) => {
-  const url = request.url === '/' ? '/index.html' : request.url;
-  let filePath = path.join(__dirname, url);
-  if (!fs.existsSync(filePath)) {
-    filePath = path.join(__dirname, '../', url);
+function setAvailableBrowsers() {
+  const BrowserStack = require('browserstack');
+  if (process.env.BROWSERSTACK_USERNAME == null ||
+      process.env.BROWSERSTACK_ACCESS_KEY == null) {
+    throw new Error(
+        `Please export your BrowserStack username and access key by running` +
+        `the following commands in the terminal:
+          export BROWSERSTACK_USERNAME=YOUR_USERNAME
+          export BROWSERSTACK_ACCESS_KEY=YOUR_ACCESS_KEY`);
   }
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      response.writeHead(404);
-      response.end(JSON.stringify(err));
-      return;
-    }
-    response.writeHead(200);
-    response.end(data);
-  });
-});
-
-const io = socketio(app);
-
-app.listen(port, () => {
-  console.log(`  > Running socket on port: ${port}`);
-});
-
-io.on('connection', socket => {
-  socket.on('run', benchmark);
-});
-
-function benchmark(config) {
-  console.log('Preparing configuration files for the test runner.');
-  // TODO:
-  // 1. Write browsers.json.
-  // Write the browsers to benchmark to `./browsers.json`.
-  config.browsers.forEach(browser => {
-    browser.base = 'BrowserStack';
-    // For mobile devices, we would use real devices instead of emulators.
-    if (browser.os === 'ios' || browser.os === 'android') {
-      browser.real_mobile = true;
-    }
-  });
-  fs.writeFileSync('./browsers.json', JSON.stringify(config.browsers, null, 2));
-
-  // 2. Write benchmark parameter config.
-  fs.writeFileSync(
-      './benchmark_parameters.json', JSON.stringify(config.benchmark, null, 2));
-
-  console.log(`Start benchmarking.`);
-  exec('yarn test --browserstack', (error, stdout, stderr) => {
-    console.log(`benchmark completed.`);
-    if (error) {
+  const browserStackCredentials = {
+    username: process.env.BROWSERSTACK_USERNAME,
+    password: process.env.BROWSERSTACK_ACCESS_KEY
+  };
+  const automateClient =
+      BrowserStack.createAutomateClient(browserStackCredentials);
+  automateClient.getBrowsers((error, browsers) => {
+    if (error != null) {
       console.log(error);
-      io.emit(
-          'benchmarkComplete',
-          {error: 'Failed to run yarn test --browserstack.'});
-      return;
+      throw new Error('Failed to authenticate BrowserStack.');
+    } else {
+      availableBrowsers = browsers;
+      selectBrowsers();
+      runServer();
+      console.log('Successfully authenticated BrowserStack.');
     }
-
-    const errorReg = /.*\<tfjs_error\>(.*)\<\/tfjs_error\>/;
-    const matchedError = stdout.match(errorReg);
-    if (matchedError != null) {
-      io.emit('benchmarkComplete', {error: matchedError[1]});
-      return;
-    }
-
-    const resultReg = /.*\<tfjs_benchmark\>(.*)\<\/tfjs_benchmark\>/;
-    const matchedResult = stdout.match(resultReg);
-    if (matchedResult != null) {
-      const benchmarkResult = JSON.parse(matchedResult[1]);
-      io.emit('benchmarkComplete', benchmarkResult);
-      return;
-    }
-
-    io.emit('benchmarkComplete', {
-      error: 'Did not find benchmark results from the logs ' +
-          'of the benchmark test (benchmark_models.js).'
-    });
   });
 }
+
+function runServer() {
+  const app = http.createServer((request, response) => {
+    const url = request.url === '/' ? '/index.html' : request.url;
+    let filePath = path.join(__dirname, url);
+    if (!fs.existsSync(filePath)) {
+      filePath = path.join(__dirname, '../', url);
+    }
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        response.writeHead(404);
+        response.end(JSON.stringify(err));
+        return;
+      }
+      response.writeHead(200);
+      response.end(data);
+    });
+  });
+  app.listen(port, () => {
+    console.log(`  > Running socket on port: ${port}`);
+  });
+
+  io = socketio(app);
+  io.on('connection', socket => {
+    socket.emit('availableBrowsers', availableBrowsers);
+  });
+}
+
+function selectBrowsers() {
+  const selectedBrowsers = [];
+  // Array `availableBrowsers` is a list of BrowserStack's supported browser
+  // configurations (Automate service).
+  for (const combination of availableBrowsers) {
+    const androidOsVersions = ['9.0', '10.0', '8.1'];
+    if (combination.os === 'android' &&
+        androidOsVersions.indexOf(combination.os_version) > -1) {
+      selectedBrowsers.push(combination);
+    }
+
+    const iosOsVersions = ['13', '12'];
+    if (combination.os === 'ios' &&
+        iosOsVersions.indexOf(combination.os_version) > -1) {
+      selectedBrowsers.push(combination);
+    }
+
+    if (combination.os === 'Windows' && combination.os_version === '10') {
+      if (combination.browser === 'chrome' &&
+          combination.browser_version === '84.0') {
+        selectedBrowsers.push(combination);
+      }
+      if (combination.browser === 'firefox' &&
+          combination.browser_version === '79.0') {
+        selectedBrowsers.push(combination);
+      }
+      if (combination.browser === 'edge' &&
+          combination.browser_version === '84.0') {
+        selectedBrowsers.push(combination);
+      }
+      if (combination.browser === 'ie' &&
+          combination.browser_version === '11.0') {
+        selectedBrowsers.push(combination);
+      }
+    }
+
+    if (combination.os === 'OS X' && combination.os_version === 'Catalina') {
+      if (combination.browser === 'chrome' &&
+          combination.browser_version === '84.0') {
+        selectedBrowsers.push(combination);
+      }
+      if (combination.browser === 'firefox' &&
+          combination.browser_version === '79.0') {
+        selectedBrowsers.push(combination);
+      }
+      if (combination.browser === 'safari' &&
+          combination.browser_version === '13.1') {
+        selectedBrowsers.push(combination);
+      }
+    }
+  }
+  console.log(`${
+      selectedBrowsers
+          .length} combinations are selected in ./browser_list.json`);
+  fs.writeFileSync(
+      'browser_list.json', JSON.stringify(selectedBrowsers, null, 2));
+}
+
+setAvailableBrowsers();
--- e2e/benchmarks/browserstack-benchmark/index.js
+++ e2e/benchmarks/browserstack-benchmark/index.js
@@ -15,79 +15,198 @@
  * =============================================================================
  */
 
-const socket = io();
+const TUNABLE_BROWSER_FIELDS =
+    ['os', 'os_version', 'browser', 'browser_version', 'device'];
 const state = {
-  run: () => {
-    // Disable the button.
-    benchmarkButton.__li.style.pointerEvents = 'none';
-    benchmarkButton.__li.style.opacity = .5;
-
-    // Send the benchmark configuration to the server to start the benchmark.
-    if (state.browser.device === 'null') {
-      state.browser.device = null;
-    }
-    const benchmark = {...state.benchmark};
-    if (state.benchmark.model !== 'custom') {
-      delete benchmark['modelUrl'];
-    }
-    socket.emit('run', {benchmark, browsers: [state.browser]});
+  add: () => {
+    const browserConfig = getBrowserFullConfig(state.browser);
+    document.getElementById('config').innerHTML +=
+        document.getElementById('config').innerHTML === '' ? '' : ',';
+    document.getElementById('config').innerHTML +=
+        JSON.stringify(browserConfig, null, 2);
+  },
+  clean: () => {
+    document.getElementById('config').innerHTML = '';
   },
   browser: {
-    base: 'BrowserStack',
-    browser: 'chrome',
-    browser_version: '84.0',
-    os: 'OS X',
-    os_version: 'Catalina',
-    device: 'null'
+    browser: 'android',
+    browser_version: 'null',
+    os: 'android',
+    os_version: '9.0',
+    device: 'Xiaomi Redmi Note 8'
   },
   benchmark: {model: 'mobilenet_v2', modelUrl: '', numRuns: 1, backend: 'wasm'}
 };
 
-socket.on('benchmarkComplete', benchmarkResult => {
-  if (benchmarkResult.error != null) {
-    document.getElementById('results').innerHTML += benchmarkResult.error;
-  } else {
-    const {timeInfo, memoryInfo} = benchmarkResult;
-    document.getElementById('results').innerHTML +=
-        JSON.stringify(timeInfo, null, 2);
+
+function getBrowserFullConfig(browser) {
+  let curNode = browserTreeRoot;
+  for (const fieldName of TUNABLE_BROWSER_FIELDS) {
+    const nextNode = curNode[browser[fieldName]];
+    curNode = nextNode;
   }
-
-  // Enable the button.
-  benchmarkButton.__li.style.pointerEvents = '';
-  benchmarkButton.__li.style.opacity = 1;
-});
-
-const gui = new dat.gui.GUI();
-showModelSelection();
-showParameterSettings();
-const benchmarkButton = gui.add(state, 'run').name('Run benchmark');
-
-function showModelSelection() {
-  const modelFolder = gui.addFolder('Model');
-  let modelUrlController = null;
-
-  modelFolder.add(state.benchmark, 'model', Object.keys(benchmarks))
-      .name('model name')
-      .onChange(async model => {
-        if (model === 'custom') {
-          if (modelUrlController === null) {
-            modelUrlController = modelFolder.add(state.benchmark, 'modelUrl');
-            modelUrlController.domElement.querySelector('input').placeholder =
-                'https://your-domain.com/model-path/model.json';
-          }
-        } else if (modelUrlController != null) {
-          modelFolder.remove(modelUrlController);
-          modelUrlController = null;
-        }
-      });
-  modelFolder.open();
-  return modelFolder;
+  return curNode;
 }
 
-function showParameterSettings() {
-  const parameterFolder = gui.addFolder('Parameters');
-  parameterFolder.add(state.benchmark, 'numRuns');
-  parameterFolder.add(state.benchmark, 'backend', ['wasm', 'webgl', 'cpu']);
-  parameterFolder.open();
-  return parameterFolder;
+let socket;
+
+// UI controllers.
+let gui;
+let browserFolder;
+let benchmarkButton;
+let browserSettingControllers = [];
+
+// Available BrowserStack's browsers will be collected in a tree when the array
+// of available browsers is recieved in runtime.
+let browserTreeRoot;
+
+/**
+ * Collect all given browsers to a tree. The field of each level is defined by
+ * `TUNABLE_BROWSER_FIELDS`.
+ *
+ * The tree node is implemented by Map/Object:
+ * - For non-leaf nodes, each node stores a map: each key is the index to a
+ * child node and the correspoding value is the child node.
+ * - For leaf nodes, each leaf node stores the full configuration for a certain
+ * browser.
+ *
+ * @param {Array<object>} browsersArray An array of browser configurations.
+ */
+function constructBrowserTree(browsersArray) {
+  const browserTreeRoot = {};
+  browsersArray.forEach(browser => {
+    let currentNode = browserTreeRoot;
+
+    // Route through non-leaf nodes.
+    for (let fieldIndex = 0; fieldIndex <= TUNABLE_BROWSER_FIELDS.length - 2;
+         fieldIndex++) {
+      const fieldName = TUNABLE_BROWSER_FIELDS[fieldIndex];
+      if (currentNode[browser[fieldName]] == null) {
+        currentNode[browser[fieldName]] = {};
+      }
+      currentNode = currentNode[browser[fieldName]];
+    }
+
+    // Set the full configuration as the leaf node.
+    const leafFieldName =
+        TUNABLE_BROWSER_FIELDS[TUNABLE_BROWSER_FIELDS.length - 1];
+    const leafFieldValue = browser[leafFieldName];
+    if (currentNode[leafFieldValue] == null) {
+      currentNode[leafFieldValue] = browser;
+    } else {
+      console.warn(
+          `The browser ${browser} shares the same ` +
+          'configuration with another browser.');
+    }
+  });
+  return browserTreeRoot;
+}
+
+/**
+ * Once the value of a certain browser field is changed, the values and options
+ * of the following fields will be invalid. This function updates the following
+ * fields recursively and does nothing for the leaf nodes.
+ *
+ * @param {number} currentFieldIndex
+ * @param {string} currentFieldValue
+ * @param {object} currentNode
+ */
+function updateFollowingFields(
+    currentFieldIndex, currentFieldValue, currentNode) {
+  const nextFieldIndex = currentFieldIndex + 1;
+  if (nextFieldIndex === TUNABLE_BROWSER_FIELDS.length) {
+    return;
+  }
+
+  const nextFieldName = TUNABLE_BROWSER_FIELDS[nextFieldIndex];
+  const nextNode = currentNode[currentFieldValue];
+  const nextFieldAvailableValues = Object.keys(nextNode);
+  let nextFieldValue = state.browser[nextFieldName];
+
+  // Update the value of the next field, if the old value is not applicable.
+  if (nextNode[nextFieldValue] == null) {
+    nextFieldValue = nextFieldAvailableValues[0];
+  }
+
+  // Update the options for the next field.
+  const nextFieldController = browserSettingControllers[nextFieldIndex].options(
+      nextFieldAvailableValues);
+
+  // When updating options for a dat.gui controller, a new controller instacne
+  // will be created, so we need to bind the event again and record the new
+  // controller.
+  nextFieldController.onFinishChange(() => {
+    const newValue = state.browser[nextFieldName];
+    updateFollowingFields(nextFieldIndex, newValue, nextNode);
+  });
+  browserSettingControllers[nextFieldIndex] = nextFieldController;
+
+  nextFieldController.setValue(nextFieldValue);
+
+  if (nextFieldValue === 'null') {
+    nextFieldController.__li.hidden = true;
+  } else {
+    nextFieldController.__li.hidden = false;
+  }
+
+  updateFollowingFields(nextFieldIndex, nextFieldValue, nextNode);
+}
+
+/**
+ * This is a wrapper function of `dat.gui.GUI.add()` with:
+ * - Binds the `finishChange` event to update the value and options for the
+ * controller of its child field.
+ * - Hides the dropdown menu, if the field is not applicable for this browser.
+ *
+ * @param {number} fieldIndex The index of the browser field to be shown.
+ * @param {object} currentNode The keys of this map are available values
+ *     for this field.
+ */
+function showBrowserField(fieldIndex, currentNode) {
+  const fieldName = TUNABLE_BROWSER_FIELDS[fieldIndex];
+  const fieldController =
+      browserFolder.add(state.browser, fieldName, Object.keys(currentNode));
+
+  fieldController.onFinishChange(() => {
+    const newValue = state.browser[fieldName];
+    updateFollowingFields(fieldIndex, newValue, currentNode);
+  });
+
+  // Null represents the field is not applicable for this browser. For example,
+  // `browser_version` is normally not applicable for mobile devices.
+  if (state.browser[fieldName] === 'null') {
+    fieldController.__li.hidden = true;
+  }
+
+  // The controller will be used to reset options when the value of its parent
+  // field is changed.
+  browserSettingControllers.push(fieldController);
+}
+
+function onPageLoad() {
+  socket = io();
+
+  // Once the server is connected, the server will send back all
+  // BrowserStack's available browsers in an array.
+  socket.on('availableBrowsers', availableBrowsersArray => {
+    if (browserTreeRoot == null) {
+      // Initialize the browser tree.
+      browserTreeRoot = constructBrowserTree(availableBrowsersArray);
+
+      // Show browser settings.
+      let currentNode = browserTreeRoot;
+      TUNABLE_BROWSER_FIELDS.forEach((field, index) => {
+        showBrowserField(index, currentNode);
+        currentNode = currentNode[state.browser[field]];
+      });
+      browserFolder.open();
+
+      // Enable users to benchmark.
+      benchmarkButton = gui.add(state, 'add').name('get JSON config');
+      benchmarkButton = gui.add(state, 'clean').name('clean config');
+    }
+  });
+
+  gui = new dat.gui.GUI({width: 400});
+  browserFolder = gui.addFolder('Browser');
 }
