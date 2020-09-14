50519e255223541c531e74fc9cd527f907fc92c0
Ann Yuan <annyuan@gmail.com>
2020-09-14 11:46:39 -0400

    rm

--- tfjs-backend-webgl/src/kernels/Max_impl.ts
+++ tfjs-backend-webgl/src/kernels/Max_impl.ts
@@ -19,7 +19,7 @@ import {TensorInfo, util} from '@tensorflow/tfjs-core';
 
 import {MathBackendWebGL} from '../backend_webgl';
 import {reduce} from '../kernel_utils/reduce';
-import {reshape} from '../kernel_utils/reshape';
+import {reshape} from '../kernels/Reshape';
 
 export function maxImpl(
     x: TensorInfo, reduceShape: number[], outShape: number[],
@@ -27,7 +27,9 @@ export function maxImpl(
   const inSize = util.sizeFromShape(reduceShape);
   const xSize = util.sizeFromShape(x.shape);
   const batchSize = xSize / inSize;
-  const reshapedInput = reshape(x, [batchSize, inSize], backend);
+  const reshapedInput =
+      reshape({inputs: {x}, attrs: {shape: [batchSize, inSize]}, backend});
+
   const reduced = reduce(reshapedInput, x.dtype, 'max', backend);
 
   if (reshapedInput.dataId !== x.dataId) {
@@ -35,5 +37,5 @@ export function maxImpl(
     backend.disposeData(reshapedInput.dataId);
   }
 
-  return reshape(reduced, outShape, backend);
+  return reshape({inputs: {x: reduced}, attrs: {shape: outShape}, backend});
 }
