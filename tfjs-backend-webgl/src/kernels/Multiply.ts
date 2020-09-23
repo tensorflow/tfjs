b54b1428cd3c84b0f7caac381d2cdee099c733ef
Ann Yuan <annyuan@gmail.com>
2020-09-23 12:52:17 -0400

    add

--- tfjs-backend-webgl/src/kernels/Multiply.ts
+++ tfjs-backend-webgl/src/kernels/Multiply.ts
@@ -18,12 +18,13 @@
 
 import {BinaryInputs, env, KernelConfig, Multiply, TensorInfo} from '@tensorflow/tfjs-core';
 
-// import {binaryKernelFunc} from '../kernel_utils/kernel_funcs_utils';
 import {MathBackendWebGL} from '../backend_webgl';
+import * as binaryop_complex_gpu from '../binaryop_complex_gpu';
+import {BinaryOpComplexProgram} from '../binaryop_complex_gpu';
 import {BinaryOpProgram} from '../binaryop_gpu';
 import {BinaryOpPackedProgram} from '../binaryop_packed_gpu';
-// import * as binaryop_complex_gpu from './binaryop_complex_gpu';
-// import {BinaryOpComplexProgram} from './binaryop_complex_gpu';
+
+import {complex} from './Complex';
 
 const MUL = 'return a * b;';
 
@@ -33,6 +34,47 @@ export function multiply(
   const {a, b} = inputs;
 
   if (a.dtype === 'complex64') {
+    const aData = backend.texData.get(a.dataId);
+    const bData = backend.texData.get(b.dataId);
+
+    const realProgram = new BinaryOpComplexProgram(
+        binaryop_complex_gpu.COMPLEX_MULTIPLY.REAL, a.shape, b.shape);
+    const imagProgram = new BinaryOpComplexProgram(
+        binaryop_complex_gpu.COMPLEX_MULTIPLY.IMAG, a.shape, b.shape);
+
+    const inputs = [
+      {
+        dataId: aData.complexTensorInfos.real.dataId,
+        dtype: aData.complexTensorInfos.real.dtype,
+        shape: a.shape
+      },
+      {
+        dataId: aData.complexTensorInfos.imag.dataId,
+        dtype: aData.complexTensorInfos.imag.dtype,
+        shape: a.shape
+      },
+      {
+        dataId: bData.complexTensorInfos.real.dataId,
+        dtype: bData.complexTensorInfos.real.dtype,
+        shape: b.shape
+      },
+      {
+        dataId: bData.complexTensorInfos.imag.dataId,
+        dtype: bData.complexTensorInfos.imag.dtype,
+        shape: b.shape
+      }
+    ];
+
+    const realPart = backend.runWebGLProgram(realProgram, inputs, 'float32');
+    const imagPart = backend.runWebGLProgram(imagProgram, inputs, 'float32');
+
+    const complexOutput =
+        complex({inputs: {real: realPart, imag: imagPart}, backend});
+
+    backend.disposeIntermediateTensorInfo(realPart);
+    backend.disposeIntermediateTensorInfo(imagPart);
+
+    return complexOutput;
   }
 
   // const shouldExecuteOnCPU = backend.shouldExecuteOnCPU([a, b]);
