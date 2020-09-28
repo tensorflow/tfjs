246c7d7ae36d57fd469d586d23bb314b157ac556
Ann Yuan <annyuan@gmail.com>
2020-09-28 15:16:56 -0400

    fix

--- tfjs-backend-webgl/src/kernels/Concat.ts
+++ tfjs-backend-webgl/src/kernels/Concat.ts
@@ -15,16 +15,10 @@
  * =============================================================================
  */
 
-import {backend_util, Concat, ConcatAttrs, ConcatInputs, env, KernelConfig, KernelFunc, TensorInfo, util} from '@tensorflow/tfjs-core';
+import {backend_util, Concat, ConcatAttrs, ConcatInputs, KernelConfig, KernelFunc, TensorInfo, util} from '@tensorflow/tfjs-core';
 
 import {MathBackendWebGL} from '../backend_webgl';
-import {ConcatProgram} from '../concat_gpu';
-import {ConcatPackedProgram} from '../concat_packed_gpu';
-
-import {complex} from './Complex';
-import {imag} from './Imag';
-import {real} from './Real';
-import {reshape} from './Reshape';
+import {concatImpl} from './Concat_impl';
 
 // TODO: CPU FORWARDING
 // if (this.shouldExecuteOnCPU(tensors)) {
@@ -39,7 +33,8 @@ export function concat(
   const {axis} = attrs;
 
   const $axis = util.parseAxisParam(axis, inputs[0].shape)[0];
-  let outShape = backend_util.computeOutShape(inputs.map(t => t.shape), $axis);
+  const outShape =
+      backend_util.computeOutShape(inputs.map(t => t.shape), $axis);
 
   if (util.sizeFromShape(outShape) === 0) {
     return backend.makeTensorInfo(outShape, inputs[0].dtype);
@@ -54,74 +49,7 @@ export function concat(
   const shapes = $inputs.map(t => t.shape);
   backend_util.assertParamsConsistent(shapes, $axis);
 
-  const dtype = $inputs[0].dtype;
-  if (dtype === 'complex64') {
-    const reals = $inputs.map((t) => real({inputs: {input: t}, backend}));
-    const imags = $inputs.map((t) => imag({inputs: {input: t}, backend}));
-
-    const realConcated = concat({inputs: reals, backend, attrs: {axis: $axis}});
-    const imagConcated = concat({inputs: imags, backend, attrs: {axis: $axis}});
-
-    const result =
-        complex({inputs: {real: realConcated, imag: imagConcated}, backend});
-
-    reals.forEach(r => backend.disposeIntermediateTensorInfo(r));
-    imags.forEach(i => backend.disposeIntermediateTensorInfo(i));
-    backend.disposeIntermediateTensorInfo(realConcated);
-    backend.disposeIntermediateTensorInfo(imagConcated);
-
-    return result;
-  }
-
-  if ($inputs.length > env().getNumber('WEBGL_MAX_TEXTURES_IN_SHADER')) {
-    const midIndex = Math.floor($inputs.length / 2);
-    const leftSide = concat(
-        {inputs: $inputs.slice(0, midIndex), backend, attrs: {axis: $axis}});
-    const rightSide = concat(
-        {inputs: $inputs.slice(midIndex), backend, attrs: {axis: $axis}});
-
-    const result =
-        concat({inputs: [leftSide, rightSide], backend, attrs: {axis: $axis}});
-
-    backend.disposeIntermediateTensorInfo(leftSide);
-    backend.disposeIntermediateTensorInfo(rightSide);
-
-    return result;
-  }
-
-  if (env().getBool('WEBGL_PACK_ARRAY_OPERATIONS') &&
-      $inputs[0].shape.length > 1) {
-    const program = new ConcatPackedProgram($inputs.map(t => t.shape), $axis);
-    return backend.runWebGLProgram(program, $inputs, dtype);
-  }
-
-  // Any concat of n-dimensional tensors across any axis can be reduced to
-  // a concatenation of two-dimensional tensors across the axis 1 by first
-  // partitioning the axes of the original tensors into those less than the
-  // axis to be concatenated and the rest. Then reshape the tensors
-  // into a two-dimensional tensor by collapsing these two sets of axes and
-  // concatenate the resulting matrices across the axis 1, finally reshaping
-  // the result to have the proper shape.
-  const tensors2D = $inputs.map(
-      x => reshape({
-        inputs: {x},
-        attrs: {shape: [-1, util.sizeFromShape(x.shape.slice($axis))]},
-        backend
-      }));
-  // Concats 2d tensors along axis=1.
-  outShape =
-      backend_util.computeOutShape(tensors2D.map(t => t.shape), 1 /* axis */);
-
-  const program =
-      new ConcatProgram(tensors2D.map(t => t.shape as [number, number]));
-  const result = backend.runWebGLProgram(program, tensors2D, dtype);
-
-  tensors2D.forEach(r => backend.disposeIntermediateTensorInfo(r));
-  const reshapedResult =
-      reshape({inputs: {x: result}, attrs: {shape: outShape}, backend});
-  backend.disposeIntermediateTensorInfo(result);
-
-  return reshapedResult;
+  return concatImpl($inputs, $axis, backend);
 }
 
 export const concatConfig: KernelConfig = {
