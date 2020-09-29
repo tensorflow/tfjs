5a937b77625c7510a377f21064060b1a3bdced2a
Yannick Assogba <yassogba@google.com>
2020-09-29 00:13:06 -0400

    mirror webgl backend device config in tests

--- e2e/integration_tests/setup_test.ts
+++ e2e/integration_tests/setup_test.ts
@@ -15,18 +15,50 @@
  * =============================================================================
  */
 
+// Import core for side effects
+// TODO(yassogba) this should be removable after tfjs 3.x. As is removing
+// this line can cause tests to fail on the webgl backend as chained ops
+// are not added to the tensor class by the time the tests run.
+import '@tensorflow/tfjs-core';
+
 // tslint:disable-next-line: no-imports-from-dist
-import {registerTestEnv} from '@tensorflow/tfjs-core/dist/jasmine_util';
+import {parseTestEnvFromKarmaFlags, setTestEnvs, TEST_ENVS} from '@tensorflow/tfjs-core/dist/jasmine_util';
 
 import {TAGS} from './constants';
 
-registerTestEnv({name: 'cpu', backendName: 'cpu'});
-registerTestEnv({name: 'webgl', backendName: 'webgl'});
+setTestEnvs([
+  {
+    name: 'webgl1',
+    backendName: 'webgl',
+    flags: {
+      'WEBGL_VERSION': 1,
+      'WEBGL_CPU_FORWARD': false,
+      'WEBGL_SIZE_UPLOAD_UNIFORM': 0
+    },
+    isDataSync: true
+  },
+  {
+    name: 'webgl2',
+    backendName: 'webgl',
+    flags: {
+      'WEBGL_VERSION': 2,
+      'WEBGL_CPU_FORWARD': false,
+      'WEBGL_SIZE_UPLOAD_UNIFORM': 0
+    },
+    isDataSync: true
+  },
+  {name: 'cpu', backendName: 'cpu', isDataSync: true}
+]);
 
 // tslint:disable-next-line:no-any
 declare let __karma__: any;
 if (typeof __karma__ !== 'undefined') {
   const args = __karma__.config.args || [];
+
+  const testEnv = parseTestEnvFromKarmaFlags(__karma__.config.args, TEST_ENVS);
+  if (testEnv != null) {
+    setTestEnvs([testEnv]);
+  }
 
   let tags;
 
--- e2e/scripts/test-ci.sh
+++ e2e/scripts/test-ci.sh
@@ -53,7 +53,8 @@ fi
 
 if [[ "$NIGHTLY" = true || "$RELEASE" = true ]]; then
   yarn run-browserstack --browsers=bs_chrome_mac --tags $TAGS
-  yarn run-browserstack --browsers=bs_safari_mac,bs_firefox_mac,win_10_chrome,bs_ios_11,bs_android_9 --tags $TAGS
+  yarn run-browserstack --browsers=bs_firefox_mac,win_10_chrome,bs_android_9 --tags $TAGS
+  yarn run-browserstack --browsers=bs_safari_mac,bs_ios_11 --tags $TAGS --testEnv webgl1 --flags '{"WEBGL_CPU_FORWARD": false, "WEBGL_SIZE_UPLOAD_UNIFORM": 0}'
 
   # Test script tag bundles
   karma start ./script_tag_tests/karma.conf.js --browserstack --browsers=bs_chrome_mac --testBundle tf.min.js
