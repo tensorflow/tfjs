38bfa73ea74182fbc918dd13a2f6e1768d38f297
Yannick Assogba <yassogba@google.com>
2020-09-30 22:07:01 -0400

    Move e2e custom bundling test into regression tests/nightly (#3983)
    INTERNAL
    
    This PR does a few things to reduce flakiness of e2e test suite as well as increase the speed of e2e for PRs:
    
    - Move the custom bundle test into regression/nightly. This primarily speeds up the regular PR tests by not building tfjs-backend-wasm
    - Restrict the final step custom bundle e2e test to chrome. It was flaky on mobile safari probably due to webworker loading (which was needed to provide isolation), and for the flow we want to check testing in chrome is sufficient to meet our needs here.
    - Refactoring the tests to use describeWithFlags (and testenvs). This standardizes our testing approach with what is used in the other packages, makes configuring the environment more deterministic and provides better isolation for tests.

--- e2e/scripts/test-ci.sh
+++ e2e/scripts/test-ci.sh
@@ -19,9 +19,6 @@ set -e
 # Smoke tests run in PR and nightly builds.
 TAGS="#SMOKE"
 
-# Generate custom bundle files for tests
-./scripts/run-custom-builds.sh
-
 # Regression tests run in nightly builds.
 if [[ "$NIGHTLY" = true || "$RELEASE" = true ]]; then
     TAGS="${TAGS},#REGRESSION"
@@ -46,11 +43,17 @@ if [[ "$TAGS" == *"#REGRESSION"*  ]]; then
   source ../scripts/cleanup-py-env.sh
 
   cd ..
+
+  # Build the wasm backend
+  yarn build-backend-wasm-ci
+
+  # Generate custom bundle files for tests
+  ./scripts/run-custom-builds.sh
 fi
 
 if [[ "$NIGHTLY" = true || "$RELEASE" = true ]]; then
-  yarn run-browserstack --browsers=bs_chrome_mac --tags $TAGS
-  yarn run-browserstack --browsers=bs_safari_mac,bs_firefox_mac,win_10_chrome,bs_ios_11,bs_android_9 --tags $TAGS
+  yarn run-browserstack --browsers=bs_safari_mac,bs_ios_11 --tags $TAGS --testEnv webgl --flags '{"WEBGL_VERSION": 1, "WEBGL_CPU_FORWARD": false, "WEBGL_SIZE_UPLOAD_UNIFORM": 0}'
+  yarn run-browserstack --browsers=bs_firefox_mac,bs_chrome_mac,win_10_chrome,bs_android_9 --tags $TAGS
 
   # Test script tag bundles
   karma start ./script_tag_tests/karma.conf.js --browserstack --browsers=bs_chrome_mac --testBundle tf.min.js
