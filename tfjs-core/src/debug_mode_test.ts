0ed75c5d7ac9ad18bcee1d5638283907a606886a
Yannick Assogba <yassogba@google.com>
2020-10-09 10:32:33 -0400

    reenable failing test

--- tfjs-core/src/debug_mode_test.ts
+++ tfjs-core/src/debug_mode_test.ts
@@ -99,7 +99,7 @@ describeWithFlags('debug on', SYNC_BACKEND_ENVS, () => {
   });
 
   // tslint:disable-next-line: ban
-  xit('A x B', async () => {
+  it('A x B', async () => {
     const a = tf.tensor2d([1, 2, 3, 4, 5, 6], [2, 3]);
     const b = tf.tensor2d([0, 1, -3, 2, 2, 1], [3, 2]);
 
