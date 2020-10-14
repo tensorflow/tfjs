f8376f64ed3ef35e0a02f71283f8e90fc8e36796
Ann Yuan <annyuan@gmail.com>
2020-10-14 09:17:29 -0400

    hbn

--- tfjs-backend-webgl/src/backend_webgl.ts
+++ tfjs-backend-webgl/src/backend_webgl.ts
@@ -493,9 +493,9 @@ export class MathBackendWebGL extends KernelBackend {
     const {shape, dtype, isPacked} = this.texData.get(dataId);
     const size = util.sizeFromShape(shape);
 
-    // if (shape.includes(0)) {
-    //   return new Float32Array(0);
-    // }
+    if (shape.includes(0)) {
+      return util.getTypedArrayFromDType(dtype as 'float32', 0);
+    }
 
     if (env().getBool('WEBGL_DOWNLOAD_FLOAT_ENABLED')) {
       const tmpTarget = this.decode(dataId);
@@ -520,6 +520,10 @@ export class MathBackendWebGL extends KernelBackend {
     const output = this.runWebGLProgram(
         program, [{shape: outputShape, dtype, dataId}], 'float32');
     const tmpData = this.texData.get(output.dataId);
+    if (tmpData.values) {
+      return tmpData.values as Float32Array;
+    }
+
     const vals =
         this.gpgpu
             .downloadByteEncodedFloatMatrixFromOutputTexture(
