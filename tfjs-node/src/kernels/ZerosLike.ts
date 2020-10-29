3d701a8eec6b6a843a2f64dbb78ddcfaedc69be5
Yannick Assogba <yassogba@google.com>
2020-10-29 11:17:55 -0400

    modularize node kernels (#4154)
    Modularizes onesLike, zerosLike, unpack/unstack, batchMatMul, slice, isFinite, isInf, IsNan, reciprocal, squaredDifference
    
    DEV

--- tfjs-node/src/kernels/ZerosLike.ts
+++ tfjs-node/src/kernels/ZerosLike.ts
@@ -15,17 +15,21 @@
  * =============================================================================
  */
 
-import {KernelConfig, SquaredDifference, SquaredDifferenceInputs} from '@tensorflow/tfjs';
-import {createTensorsTypeOpAttr, NodeJSKernelBackend} from '../nodejs_kernel_backend';
+import {KernelConfig, ZerosLike, ZerosLikeInputs} from '@tensorflow/tfjs';
+import {NodeJSKernelBackend} from '../nodejs_kernel_backend';
 
-export const squaredDifferenceConfig: KernelConfig = {
-  kernelName: SquaredDifference,
+export const zerosLikeConfig: KernelConfig = {
+  kernelName: ZerosLike,
   backendName: 'tensorflow',
   kernelFunc: (args) => {
-    const {a, b} = args.inputs as SquaredDifferenceInputs;
+    const {x} = args.inputs as ZerosLikeInputs;
     const backend = args.backend as NodeJSKernelBackend;
 
-    const opAttrs = [createTensorsTypeOpAttr('T', a.dtype)];
-    return backend.executeSingleOutput(SquaredDifference, opAttrs, [a, b]);
+    const opAttrs = [{
+      name: 'T',
+      type: backend.binding.TF_ATTR_TYPE,
+      value: backend.getDTypeInteger(x.dtype)
+    }];
+    return backend.executeSingleOutput(ZerosLike, opAttrs, [x]);
   }
 };
