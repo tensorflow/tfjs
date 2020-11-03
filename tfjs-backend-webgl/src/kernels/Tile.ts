6ac2950bdf3a8e30f8745fc7c2cb09c569517042
Ann Yuan <annyuan@gmail.com>
2020-11-03 14:24:21 -0500

    setup

---
+++ tfjs-backend-webgl/src/kernels/Tile.ts
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import {buffer, kernel_impls, KernelConfig, KernelFunc, TensorInfo, Tile, TileAttrs, TileInputs, util} from '@tensorflow/tfjs-core';
+
+import {MathBackendWebGL} from '../backend_webgl';
+import {TileProgram} from '../tile_gpu';
+
+export const tile:
+    (params:
+         {inputs: TileInputs, backend: MathBackendWebGL, attrs: TileAttrs}) =>
+        TensorInfo = ({inputs, backend, attrs}) => {
+          const {x} = inputs;
+          const {reps} = attrs;
+
+          if (x.dtype === 'string') {
+            const data = backend.texData.get(x.dataId).values as Uint8Array[];
+            const decodedData = data.map(d => util.decodeString(d));
+            const buf = buffer(x.shape, x.dtype, decodedData);
+            return kernel_impls.tile(buf, reps);
+          }
+
+          const program = new TileProgram(x.shape, reps);
+          const output = backend.runWebGLProgram(program, [x], x.dtype);
+
+          return output;
+        };
+
+export const tileConfig: KernelConfig = {
+  kernelName: Tile,
+  backendName: 'webgl',
+  kernelFunc: tile as {} as KernelFunc,
+};
