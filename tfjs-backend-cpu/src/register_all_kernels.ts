44324b830a9a27433257a15b9902ccd2398fac4a
Na Li <linazhao@google.com>
2020-11-20 13:00:42 -0800

    Modularize resize kernels. (#4274)
    DEV

--- tfjs-backend-cpu/src/register_all_kernels.ts
+++ tfjs-backend-cpu/src/register_all_kernels.ts
@@ -121,6 +121,10 @@ import {reciprocalConfig} from './kernels/Reciprocal';
 import {reluConfig} from './kernels/Relu';
 import {relu6Config} from './kernels/Relu6';
 import {reshapeConfig} from './kernels/Reshape';
+import {resizeBilinearConfig} from './kernels/ResizeBilinear';
+import {resizeBilinearGradConfig} from './kernels/ResizeBilinearGrad';
+import {resizeNearestNeighborConfig} from './kernels/ResizeNearestNeighbor';
+import {resizeNearestNeighborGradConfig} from './kernels/ResizeNearestNeighborGrad';
 import {reverseConfig} from './kernels/Reverse';
 import {rotateWithOffsetConfig} from './kernels/RotateWithOffset';
 import {roundConfig} from './kernels/Round';
@@ -255,6 +259,10 @@ const kernelConfigs: KernelConfig[] = [
   reluConfig,
   relu6Config,
   reshapeConfig,
+  resizeBilinearConfig,
+  resizeBilinearGradConfig,
+  resizeNearestNeighborConfig,
+  resizeNearestNeighborGradConfig,
   reverseConfig,
   rotateWithOffsetConfig,
   roundConfig,
