8ce30a2ae51b9d635ceb86117ea53c3b3d163533
Na Li <linazhao@google.com>
2020-11-30 13:57:02 -0800

    Clean up.

--- tfjs-backend-wasm/starter/parcel/index.js
+++ tfjs-backend-wasm/starter/parcel/index.js
@@ -1,77 +1,22 @@
+// Copyright 2019 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
 import '@tensorflow/tfjs-backend-wasm';
-
-import * as tfconv from '@tensorflow/tfjs-converter';
 import * as tf from '@tensorflow/tfjs-core';
 
-const IMAGE_SIZE = 224;
-const filesElement = document.getElementById('files');
-filesElement.addEventListener('change', evt => {
-  let files = evt.target.files;
-  // Display thumbnails & issue call to predict each image.
-  for (let i = 0, f; f = files[i]; i++) {
-    // Only process image files (skip non image files)
-    if (!f.type.match('image.*')) {
-      continue;
-    }
-    let reader = new FileReader();
-    reader.onload = e => {
-      // Fill the image & call predict.
-      let img = document.createElement('img');
-      img.src = e.target.result;
-      img.width = IMAGE_SIZE;
-      img.height = IMAGE_SIZE;
-      img.onload = () => predict(img);
-    };
-
-    // Read in the image file as a data URL.
-    reader.readAsDataURL(f);
-  }
-});
-
-let global_model = null;
 async function run() {
   await tf.setBackend('wasm');
-  const modelUrl = 'http://localhost:8080/tmp_web_model/model.json';
-  global_model = await tfconv.loadGraphModel(modelUrl);
+  tf.add(5, 3).print();
 }
 run();
-
-async function predict(imgElement) {
-  // The first start time includes the time it takes to extract the image
-  // from the HTML and preprocess it, in additon to the predict() call.
-  const startTime1 = performance.now();
-  // The second start time excludes the extraction and preprocessing and
-  // includes only the predict() call.
-  let startTime2;
-  const result = tf.tidy(() => {
-    // tf.browser.fromPixels() returns a Tensor from an image element.
-    const img = tf.browser.fromPixels(imgElement).toFloat();
-
-    const offset = tf.scalar(127.5);
-    // Normalize the image from [0, 255] to [-1, 1].
-    const normalized = img.sub(offset).div(offset);
-
-    // Reshape to a single-element batch so we can pass it to predict.
-    const batched = normalized.reshape([1, IMAGE_SIZE, IMAGE_SIZE, 3]);
-
-    startTime2 = performance.now();
-    // Make a prediction through mobilenet.
-    return global_model.predict(batched);
-  });
-
-  const totalTime1 = performance.now() - startTime1;
-  const totalTime2 = performance.now() - startTime2;
-  console.log(totalTime1);
-  console.log(totalTime2);
-
-  await showResults(imgElement, result);
-}
-
-async function showResults(imgElement, result) {
-  const canvas = document.getElementById('result');
-  const [batch, height, width, channel] = result.shape;
-  const resultReshaped = result.reshape([height, width, channel]);
-  const offset = tf.scalar(127.5);
-  const denormalized = resultReshaped.mul(offset).add(offset).cast('int32');
-  await tf.browser.toPixels(denormalized, canvas);
-}
