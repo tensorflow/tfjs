cc55bb6c75877f8d9aec83ef2f8cbd8c1317b32e
Yannick Assogba <yassogba@google.com>
2020-12-09 21:30:59 -0500

    change broadcastTo to use runKernel and logSoftmax to use customGrad

--- tfjs-core/src/ops/broadcast_to.ts
+++ tfjs-core/src/ops/broadcast_to.ts
@@ -15,9 +15,8 @@
  * =============================================================================
  */
 
-import {KernelBackend} from '../backends/backend';
 import {ENGINE} from '../engine';
-import {BroadcastTo, BroadCastToAttrs, BroadcastToInputs} from '../kernel_names';
+import {Tile, TileAttrs, TileInputs} from '../kernel_names';
 import {NamedAttrMap} from '../kernel_registry';
 import {Tensor} from '../tensor';
 import {NamedTensorMap} from '../tensor_types';
@@ -27,7 +26,6 @@ import {Rank, ShapeMap, TensorLike} from '../types';
 import {clone} from './clone';
 import {op} from './operation';
 import {reshape} from './reshape';
-import {tile} from './tile';
 
 /**
  * Broadcast an array to a compatible shape NumPy-style.
@@ -81,14 +79,11 @@ function broadcastTo_<R extends Rank>(
     return clone(input) as Tensor<R>;
   }
 
-  const forward = (backend: KernelBackend) => tile(input, reps);
-
-  const inputs: BroadcastToInputs = {x: input};
-  const attrs: BroadCastToAttrs = {shape, inputShape};
-
-  return ENGINE.runKernelFunc(
-             forward, inputs as unknown as NamedTensorMap, null /* grad */,
-             BroadcastTo, attrs as unknown as NamedAttrMap) as Tensor<R>;
+  // TODO call broadcastTo kernel directly once backends implement broadcstTo
+  const inputs: TileInputs = {x: input};
+  const attrs: TileAttrs = {reps};
+  return ENGINE.runKernel(
+      Tile, inputs as {} as NamedTensorMap, attrs as unknown as NamedAttrMap)
 }
 
 export const broadcastTo = op({broadcastTo_});
