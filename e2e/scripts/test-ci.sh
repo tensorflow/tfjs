9534d4d8450b218a807afded81fda53c2f5df3bf
Matthew Soulanille <msoulanille@google.com>
2020-12-10 09:49:50 -0800

    Revert "Temporarily disable android in nightly CI (#4336)"
    This reverts commit 029690070c54f3fd7dd739531c970505d01c565c.

--- e2e/scripts/test-ci.sh
+++ e2e/scripts/test-ci.sh
@@ -53,7 +53,7 @@ fi
 
 if [[ "$NIGHTLY" = true || "$RELEASE" = true ]]; then
   yarn run-browserstack --browsers=bs_safari_mac,bs_ios_11 --tags $TAGS --testEnv webgl --flags '{"WEBGL_VERSION": 1, "WEBGL_CPU_FORWARD": false, "WEBGL_SIZE_UPLOAD_UNIFORM": 0}'
-  yarn run-browserstack --browsers=bs_firefox_mac,bs_chrome_mac,win_10_chrome --tags $TAGS
+  yarn run-browserstack --browsers=bs_firefox_mac,bs_chrome_mac,win_10_chrome,bs_android_9 --tags $TAGS
 
   # Test script tag bundles
   karma start ./script_tag_tests/karma.conf.js --browserstack --browsers=bs_chrome_mac --testBundle tf.min.js
