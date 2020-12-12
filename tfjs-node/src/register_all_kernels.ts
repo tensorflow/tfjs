2ef4babc1a299592ec2ee14a56e4bd5210faf6cd
Yannick Assogba <yassogba@google.com>
2020-12-12 12:26:59 -0500

    Add range kernel to wasm and node (#4385)
    DEV

--- tfjs-node/src/register_all_kernels.ts
+++ tfjs-node/src/register_all_kernels.ts
@@ -125,6 +125,7 @@ import {padV2Config} from './kernels/PadV2';
 import {powConfig} from './kernels/Pow';
 import {preluConfig} from './kernels/Prelu';
 import {prodConfig} from './kernels/Prod';
+import {rangeConfig} from './kernels/Range';
 import {realConfig} from './kernels/Real';
 import {realDivConfig} from './kernels/RealDiv';
 import {reciprocalConfig} from './kernels/Reciprocal';
@@ -275,6 +276,7 @@ const kernelConfigs: KernelConfig[] = [
   powConfig,
   preluConfig,
   prodConfig,
+  rangeConfig,
   realConfig,
   realDivConfig,
   reciprocalConfig,
