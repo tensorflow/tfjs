788ad7636825e84a3b47b2afb8fd8c3ac0f7a40a
Yannick Assogba <yassogba@google.com>
2021-01-05 15:30:21 -0500

    save

--- tfjs-converter/src/operations/executors/transformation_executor_test.ts
+++ tfjs-converter/src/operations/executors/transformation_executor_test.ts
@@ -28,6 +28,26 @@ describe('transformation', () => {
   const input2 = [tfOps.tensor1d([1, 1])];
   const context = new ExecutionContext({}, {}, {});
 
+  beforeAll(() => {
+    // Set all properties of tfOps to writable
+    // See https://github.com/jasmine/jasmine/issues/1817 as of
+    // ts > 3.9 module properties are effectively frozen (and thus made
+    // immutable as per module spec). Thus spyOn does not work. This
+    // is an attempt to workaroudn that.
+    const props = Object.getOwnPropertyNames(tfOps);
+    // console.log('tf ops proeperties', props);
+    console.log('tfops isFrozen', Object.isFrozen(tfOps));
+    console.log('tfops isSealed', Object.isSealed(tfOps));
+    console.log('tfops isExtensible', Object.isExtensible(tfOps));
+    props.filter(p => !p.startsWith('_')).forEach((propName) => {
+      let descriptor = Object.getOwnPropertyDescriptor(tfOps, propName);
+
+      console.log('descriptor for', propName, descriptor);
+      // console.log('redefining property', propName);
+      Object.defineProperty(tfOps, propName, {writable: true});
+    })
+  })
+
   beforeEach(() => {
     node = {
       name: 'test',
@@ -158,7 +178,7 @@ describe('transformation', () => {
     });
     describe('DepthToSpace', () => {
       it('should call tfOps.depthToSpace', () => {
-        spyOn(tfOps, 'depthToSpace');
+        spyOnProperty(tfOps, 'depthToSpace');
         node.op = 'DepthToSpace';
         node.attrParams.blockSize = createNumberAttr(1);
         node.attrParams.dataFormat = createStrAttr('nhwc');
@@ -170,7 +190,7 @@ describe('transformation', () => {
     });
     describe('BroadcastTo', () => {
       it('should call tfOps.broadcastTo', () => {
-        spyOn(tfOps, 'broadcastTo');
+        spyOnProperty(tfOps, 'broadcastTo');
         node.op = 'BroadcastTo';
         node.inputParams.shape = createNumericArrayAttrFromIndex(1);
         node.inputNames = ['input1', 'input2'];
