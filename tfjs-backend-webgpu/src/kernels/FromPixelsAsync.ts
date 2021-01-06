94d45cbe76e8d6b2ebdc0fbf2fd5c86a122c56a3
Yan, Shaobo <shaobo.yan@intel.com>
2021-01-06 13:42:56 +0800

    Use CopyImageBitmapToTexture for webgpu backend (#4374)
    * Use CopyImageBitmapToTexture for webgpu backend
    
    ImageBitmap is also a possible input from HTML. Adding ImageBitmap as
    fromPixels valid input type.
    
    CopyImageBitmapToTexture is usual API for uploading in WebGPU.
    Integrate this API into fromPixels in tfjs webgpu backend.
    
    * Address comments
    
    * Address comments and fix errors
    
    * Address comments

--- tfjs-backend-webgpu/src/kernels/FromPixelsAsync.ts
+++ tfjs-backend-webgpu/src/kernels/FromPixelsAsync.ts
@@ -17,15 +17,12 @@
 
 import {env, KernelConfig, KernelFunc} from '@tensorflow/tfjs-core';
 import {FromPixelsAttrs, FromPixelsInputs} from '@tensorflow/tfjs-core';
-import {Tensor3D} from '@tensorflow/tfjs-core';
-import {util} from '@tensorflow/tfjs-core';
 
 import {WebGPUBackend} from '../backend_webgpu';
 import {FromPixelsAsync} from '../ops/from_pixels_async';
 
 import {fromPixels} from './FromPixels';
-import {FromPixelsProgram} from './FromPixels_utils/from_pixels_webgpu';
-import * as webgpu_program from './webgpu_program';
+import {fromPixelsImageBitmap} from './FromPixelsImageBitmap';
 
 // Not many diffs with fromPixel, keep it here
 export const fromPixelsAsyncConfig: KernelConfig = {
@@ -41,15 +38,10 @@ async function fromPixelsAsync(args: {
 }) {
   const {inputs, backend, attrs} = args;
   const {pixels} = inputs;
-  const {numChannels} = attrs;
 
   if (pixels == null) {
     throw new Error('pixels passed to fromPixelsAsync can not be null');
   }
-
-  const outShape = [pixels.height, pixels.width, numChannels];
-  const size = util.sizeFromShape(outShape);
-  const uniformData: [number, number] = [size, numChannels];
 
   if (env().getBool('IS_BROWSER')) {
     // TODO: pixels.data instance of Uint8Array is not ImageBitmapSource,
@@ -76,32 +68,5 @@ async function fromPixelsAsync(args: {
     return fromPixels({inputs, backend, attrs});
   }
 
-  const output = backend.makeOutputArray(outShape, 'int32');
-  if (!backend.fromPixelProgram ||
-      backend.fromPixelProgram.outputShape !== outShape) {
-    backend.fromPixelProgram = new FromPixelsProgram(outShape);
-
-    const {bindGroupLayout, pipeline} = webgpu_program.compileProgram(
-        backend.glslang, backend.device, backend.fromPixelProgram, [], output);
-
-    backend.fromPixelProgram.setWebGPUBinary(bindGroupLayout, pipeline);
-  }
-
-  backend.queue.copyImageBitmapToTexture(
-      {imageBitmap, origin: {x: 0, y: 0}}, {
-        texture: backend.fromPixelProgram.makeInputTexture(
-            backend.device, pixels.width, pixels.height)
-      },
-      {width: pixels.width, height: pixels.height, depth: 1});
-
-  const info = backend.tensorMap.get(output.dataId);
-
-  info.bufferInfo.buffer = backend.acquireBuffer(info.bufferInfo.byteSize);
-
-  backend.fromPixelProgram.setUniform(backend.device, uniformData);
-
-  backend.commandQueue.push(backend.fromPixelProgram.generateEncoder(
-      backend.device, info.bufferInfo.buffer));
-  backend.submitQueue();
-  return output as Tensor3D;
+  return fromPixelsImageBitmap({imageBitmap, backend, attrs});
 }
