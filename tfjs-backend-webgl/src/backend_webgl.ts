7fbd5547b52f4afe6c5f545cdcee481694c789e9
Ping Yu <4018+pyu10055@users.noreply.github.com>
2021-01-14 15:51:21 -0800

    fix typo

--- tfjs-backend-webgl/src/backend_webgl.ts
+++ tfjs-backend-webgl/src/backend_webgl.ts
@@ -565,14 +565,14 @@ export class MathBackendWebGL extends KernelBackend {
     this.releaseGPUData(dataId);
     const {complexTensorInfos} = this.texData.get(dataId);
     if (complexTensorInfos != null) {
-      const realDataInfo = this.texData.get(complexTensorInfos.real.dataId)
+      const realDataInfo = this.texData.get(complexTensorInfos.real.dataId);
       realDataInfo.complexParentRefCount--;
       realDataInfo.refCount--;
       if (realDataInfo.refCount < 1) {
         this.disposeData(complexTensorInfos.real.dataId);
       }
 
-      const imagDataInfo = this.texData.get(complexTensorInfos.imag.dataId)
+      const imagDataInfo = this.texData.get(complexTensorInfos.imag.dataId);
       imagDataInfo.complexParentRefCount--;
       imagDataInfo.refCount--;
       if (imagDataInfo.refCount < 1) {
