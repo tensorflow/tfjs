e75ba61cc3ece8f05f65b81382d837b3fdbaabce
Yannick Assogba <yassogba@google.com>
2021-01-19 16:00:33 -0500

    import from core rather than union

--- tfjs-vis/src/show/tensor_test.ts
+++ tfjs-vis/src/show/tensor_test.ts
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
-import * as tf from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs-core';
 import {valuesDistribution} from './tensor';
 
 describe('perClassAccuracy', () => {
