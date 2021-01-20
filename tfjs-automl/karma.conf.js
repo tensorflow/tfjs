143a4048c6b6f4f2cc0a656f5095fbb2e04103b7
Ping Yu <4018+pyu10055@users.noreply.github.com>
2021-01-20 14:39:52 -0800

    update the package deps

--- tfjs-automl/karma.conf.js
+++ tfjs-automl/karma.conf.js
@@ -56,13 +56,12 @@ module.exports = function(config) {
     port: 9866,
     colors: true,
     browsers: ['Chrome'],
-    reportSlowerThan: 5000,
     client: {jasmine: {random: false}, args: args},
     browserStack: {
       username: process.env.BROWSERSTACK_USERNAME,
       accessKey: process.env.BROWSERSTACK_KEY,
       tunnelIdentifier:
-          `tfjs_vis_${Date.now()}_${Math.floor(Math.random() * 1000)}`
+          `tfjs_automl_${Date.now()}_${Math.floor(Math.random() * 1000)}`
     },
     captureTimeout: 120000,
     reportSlowerThan: 500,
