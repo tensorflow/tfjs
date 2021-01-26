20834374e360573dc9577a20e859d98dc47c22cc
Yannick Assogba <yassogba@google.com>
2021-01-26 16:48:42 -0500

    fix slice

--- tfjs-core/src/ops/cumsum_test.ts
+++ tfjs-core/src/ops/cumsum_test.ts
@@ -100,12 +100,12 @@ describeWithFlags('cumsum', ALL_ENVS, () => {
 
     expect(res.shape).toEqual([1, 32, 46, 4]);
 
-    const earlySlice = tf.slice(res, [0, 0, 0, 0], [1, 1, 42, 1]);
-    const lateSlice = tf.slice(res, [0, 31, 0, 0], [1, 1, 42, 1]);
+    const earlySlice = tf.slice(res, [0, 0, 0, 0], [1, 1, 46, 1]);
+    const lateSlice = tf.slice(res, [0, 31, 0, 0], [1, 1, 46, 1]);
     const expectedDataInEachSlice = [
-      1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14,
-      15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
-      29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+      1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16,
+      17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+      33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46
     ];
     expectArraysClose(await earlySlice.data(), expectedDataInEachSlice);
     expectArraysClose(await lateSlice.data(), expectedDataInEachSlice);
