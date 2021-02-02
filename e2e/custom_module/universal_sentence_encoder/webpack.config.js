91c2b366626c4964b6b22698517585730aa3c6f2
Yannick Assogba <yassogba@google.com>
2021-02-02 16:12:43 -0500

    fix alias pattern in rollup/webpack configs for custom builds.

--- e2e/custom_module/universal_sentence_encoder/webpack.config.js
+++ e2e/custom_module/universal_sentence_encoder/webpack.config.js
@@ -63,7 +63,7 @@ module.exports = function(env, argv) {
             path.resolve(__dirname, './custom_tfjs/custom_tfjs.js'),
         '@tensorflow/tfjs-core$':
             path.resolve(__dirname, './custom_tfjs/custom_tfjs_core.js'),
-        'tensorflow/tfjs-core/dist/ops/ops_for_converter': path.resolve(
+        '@tensorflow/tfjs-core/dist/ops/ops_for_converter': path.resolve(
             __dirname, './custom_tfjs/custom_ops_for_converter.js'),
       }
     }
