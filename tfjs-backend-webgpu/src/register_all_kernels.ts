e23128cc5807a5f5140f0c82901b7f75490cc6f5
Xu Xing <xing.xu@intel.com>
2021-02-05 08:52:26 +0800

    [WebGPU] Add minimum
    Currently, NAN is not supported in WebGPU backend:
    https://gpuweb.github.io/gpuweb/wgsl.html#floating-point-evaluation

--- tfjs-backend-webgpu/src/register_all_kernels.ts
+++ tfjs-backend-webgpu/src/register_all_kernels.ts
@@ -48,6 +48,7 @@ import {lessEqualConfig} from './kernels/LessEqual';
 import {logConfig} from './kernels/Log';
 import {maxConfig} from './kernels/Max';
 import {maximumConfig} from './kernels/Maximum';
+import {minimumConfig} from './kernels/Minimum';
 import {maxPoolConfig} from './kernels/MaxPool';
 import {minConfig} from './kernels/Min';
 import {mirrorPadConfig} from './kernels/MirrorPad';
@@ -111,6 +112,7 @@ const kernelConfigs: KernelConfig[] = [
   logConfig,
   maxConfig,
   maximumConfig,
+  minimumConfig,
   maxPoolConfig,
   minConfig,
   mirrorPadConfig,
