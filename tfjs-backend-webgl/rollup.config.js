tensorflow/tfjs

commit d28e229344cf15e3138abd9bb60f89362733e2ef

Merge 3.x_dev branch (#4537)
BREAKING
FEATURE

Summary of breaking changes

  Removal of *strict variants of ops
  Remove deprecated tensor.depthwiseConv2D (use tensor.depthwiseConv2d)
  Remove dilations param from maxPool3d and avgPool3d
  Remove default registration of chaining api from core
  Remove default registration of gradients from core

Other Changes

  Add .fesm bundles to npm package
  Rename custom-bundle tool to custom-module

Internal changes
  refactor runKernel/runKernelFunc to remove legacy code after modularization of all kernels

tfjs-backend-webgl/rollup.config.js
@@ -18,8 +18,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import {terser} from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
+import {getBrowserBundleConfigOptions} from '../rollup.config.helpers';
 
 const PREAMBLE = `/**
  * @license
@@ -96,10 +96,8 @@ function config({
 module.exports = cmdOptions => {
   const bundles = [];
 
-  const terserPlugin = terser({output: {preamble: PREAMBLE, comments: false}});
   const name = 'tf';
   const extend = true;
-  const browserFormat = 'umd';
   const fileName = 'tf-backend-webgl';
 
   // Node
@@ -115,58 +113,16 @@ module.exports = cmdOptions => {
     external: ['@tensorflow/tfjs-backend-cpu']
   }));
 
-  if (cmdOptions.ci || cmdOptions.npm) {
-    // Browser default minified (ES5)
-    bundles.push(config({
-      plugins: [terserPlugin],
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.min.js`,
-        freeze: false
-      },
-      tsCompilerOptions: {target: 'es5'},
-      visualize: cmdOptions.visualize
-    }));
+  if (cmdOptions.ci) {
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, true /* CI */);
+    bundles.push(...browserBundles);
   }
 
   if (cmdOptions.npm) {
-    // Browser default unminified (ES5)
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.js`,
-        freeze: false
-      },
-      tsCompilerOptions: {target: 'es5'}
-    }));
-
-    // Browser ES2017
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.js`
-      },
-      tsCompilerOptions: {target: 'es2017'}
-    }));
-
-    // Browser ES2017 minified
-    bundles.push(config({
-      plugins: [terserPlugin],
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.min.js`
-      },
-      tsCompilerOptions: {target: 'es2017'},
-      visualize: cmdOptions.visualize
-    }));
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, false /* CI */);
+    bundles.push(...browserBundles);
   }
 
   return bundles;

tfjs-backend-cpu/rollup.config.js
@@ -18,8 +18,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import {terser} from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
+import {getBrowserBundleConfigOptions} from '../rollup.config.helpers';
 
 const PREAMBLE = `/**
  * @license
@@ -96,10 +96,8 @@ function config({
 module.exports = cmdOptions => {
   const bundles = [];
 
-  const terserPlugin = terser({output: {preamble: PREAMBLE, comments: false}});
   const name = 'tf';
   const extend = true;
-  const browserFormat = 'umd';
   const fileName = 'tf-backend-cpu';
 
   // Node
@@ -114,57 +112,16 @@ module.exports = cmdOptions => {
     tsCompilerOptions: {target: 'es5'}
   }));
 
-  if (cmdOptions.ci || cmdOptions.npm) {
-    // Browser default minified (ES5)
-    bundles.push(config({
-      plugins: [terserPlugin],
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.min.js`,
-        freeze: false
-      },
-      tsCompilerOptions: {target: 'es5'},
-      visualize: cmdOptions.visualize
-    }));
+  if (cmdOptions.ci) {
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, true /* CI */);
+    bundles.push(...browserBundles);
   }
 
   if (cmdOptions.npm) {
-    // Browser default unminified (ES5)
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.js`,
-        freeze: false
-      },
-      tsCompilerOptions: {target: 'es5'}
-    }));
-
-    // Browser ES2017
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.js`
-      },
-      tsCompilerOptions: {target: 'es2017'}
-    }));
-
-    // Browser ES2017 minified
-    bundles.push(config({
-      plugins: [terserPlugin],
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.min.js`
-      },
-      tsCompilerOptions: {target: 'es2017'}
-    }));
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, false /* CI */);
+    bundles.push(...browserBundles);
   }
 
   return bundles;

tfjs-backend-wasm/rollup.config.js
@@ -17,9 +17,10 @@
 
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
 import node from '@rollup/plugin-node-resolve';
-import {terser} from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import visualizer from 'rollup-plugin-visualizer';
+import {getBrowserBundleConfigOptions} from '../rollup.config.helpers';
 
 const PREAMBLE = `/**
  * @license
@@ -38,7 +39,20 @@ const PREAMBLE = `/**
  * =============================================================================
  */`;
 
-function config({plugins = [], output = {}, tsCompilerOptions = {}}) {
+function config({
+  plugins = [],
+  output = {},
+  external = [],
+  visualize = false,
+  tsCompilerOptions = {}
+}) {
+  if (visualize) {
+    const filename = output.file + '.html';
+    plugins.push(visualizer(
+        {sourcemap: true, filename, template: 'sunburst', gzipSize: true}));
+    console.log(`Will output a bundle visualization in ${filename}`);
+  }
+
   const defaultTsOptions = {
     include: ['src/**/*.ts'],
     module: 'ES2015',
@@ -48,8 +62,7 @@ function config({plugins = [], output = {}, tsCompilerOptions = {}}) {
   return {
     input: 'src/index.ts',
     plugins: [
-      typescript(tsoptions), resolve(),
-      node({preferBuiltins: true}),
+      typescript(tsoptions), resolve(), node({preferBuiltins: true}),
       // Polyfill require() from dependencies.
       commonjs({
         ignore: ['crypto', 'node-fetch', 'util'],
@@ -60,10 +73,24 @@ function config({plugins = [], output = {}, tsCompilerOptions = {}}) {
     output: {
       banner: PREAMBLE,
       sourcemap: true,
-      globals: {'@tensorflow/tfjs-core': 'tf', 'fs': 'fs', 'path': 'path', 'worker_threads': 'worker_threads', 'perf_hooks': 'perf_hooks'},
+      globals: {
+        '@tensorflow/tfjs-core': 'tf',
+        'fs': 'fs',
+        'path': 'path',
+        'worker_threads': 'worker_threads',
+        'perf_hooks': 'perf_hooks'
+      },
       ...output,
     },
-    external: ['crypto', '@tensorflow/tfjs-core', 'fs', 'path', 'worker_threads', 'perf_hooks'],
+    external: [
+      'crypto',
+      '@tensorflow/tfjs-core',
+      'fs',
+      'path',
+      'worker_threads',
+      'perf_hooks',
+      ...external,
+    ],
     onwarn: warning => {
       let {code} = warning;
       if (code === 'CIRCULAR_DEPENDENCY' || code === 'CIRCULAR' ||
@@ -78,10 +105,8 @@ function config({plugins = [], output = {}, tsCompilerOptions = {}}) {
 module.exports = cmdOptions => {
   const bundles = [];
 
-  const terserPlugin = terser({output: {preamble: PREAMBLE, comments: false}});
   const name = 'tf.wasm';
   const extend = true;
-  const browserFormat = 'umd';
   const fileName = 'tf-backend-wasm';
 
   // Node
@@ -96,55 +121,14 @@ module.exports = cmdOptions => {
     tsCompilerOptions: {target: 'es5'}
   }));
 
-  if (!cmdOptions.ci || cmdOptions.npm) {
-    // tf-backend-wasm.min.js
-  bundles.push(config({
-    plugins: [terserPlugin],
-    output: {
-      format: 'umd',
-      name,
-      extend,
-      file: `dist/${fileName}.min.js`,
-    },
-  }));
-
-  }
-
   if (cmdOptions.npm) {
-    // Browser default unminified (ES5)
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.js`,
-        freeze: false
-      },
-      tsCompilerOptions: {target: 'es5'}
-    }));
-
-    // Browser ES2017
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.js`
-      },
-      tsCompilerOptions: {target: 'es2017'}
-    }));
-
-    // Browser ES2017 minified
-    bundles.push(config({
-      plugins: [terserPlugin],
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.min.js`
-      },
-      tsCompilerOptions: {target: 'es2017'}
-    }));
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, false /* CI */);
+    bundles.push(...browserBundles);
+  } else {
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, true /* CI */);
+    bundles.push(...browserBundles);
   }
 
   return bundles;

tfjs-converter/rollup.config.js
@@ -21,6 +21,7 @@ import replace from '@rollup/plugin-replace';
 import typescript from '@rollup/plugin-typescript';
 import {terser} from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
+import {getBrowserBundleConfigOptions} from '../rollup.config.helpers';
 
 const PREAMBLE = `/**
  * @license
@@ -126,57 +127,16 @@ module.exports = cmdOptions => {
     tsCompilerOptions: {target: 'es5'}
   }));
 
-  if (cmdOptions.ci || cmdOptions.npm) {
-    // Browser default minified (ES5)
-    bundles.push(config({
-      plugins: [terserPlugin],
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.min.js`,
-        freeze: false
-      },
-      tsCompilerOptions: {target: 'es5'},
-      visualize: cmdOptions.visualize
-    }));
+  if (cmdOptions.ci) {
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, true /* CI */);
+    bundles.push(...browserBundles);
   }
 
   if (cmdOptions.npm) {
-    // Browser default unminified (ES5)
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.js`,
-        freeze: false
-      },
-      tsCompilerOptions: {target: 'es5'}
-    }));
-
-    // Browser ES2017
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.js`
-      },
-      tsCompilerOptions: {target: 'es2017'}
-    }));
-
-    // Browser ES2017 minified
-    bundles.push(config({
-      plugins: [terserPlugin],
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.min.js`
-      },
-      tsCompilerOptions: {target: 'es2017'}
-    }));
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, false /* CI */);
+    bundles.push(...browserBundles);
 
     // Miniprogram entry (minified es5)
     bundles.push(config({

tfjs-core/rollup.config.js
@@ -18,8 +18,8 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import {terser} from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
+import {getBrowserBundleConfigOptions} from '../rollup.config.helpers';
 
 const PREAMBLE = `/**
  * @license
@@ -91,10 +91,8 @@ function config({
 module.exports = cmdOptions => {
   const bundles = [];
 
-  const terserPlugin = terser({output: {preamble: PREAMBLE, comments: false}});
   const name = 'tf';
   const extend = true;
-  const browserFormat = 'umd';
   const fileName = 'tf-core';
 
   // Node
@@ -109,57 +107,16 @@ module.exports = cmdOptions => {
     tsCompilerOptions: {target: 'es5'}
   }));
 
-  if (cmdOptions.ci || cmdOptions.npm) {
-    // Browser default minified (ES5)
-    bundles.push(config({
-      plugins: [terserPlugin],
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.min.js`,
-        freeze: false
-      },
-      tsCompilerOptions: {target: 'es5'},
-      visualize: cmdOptions.visualize
-    }));
+  if (cmdOptions.ci) {
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, true /* CI */);
+    bundles.push(...browserBundles);
   }
 
   if (cmdOptions.npm) {
-    // Browser default unminified (ES5)
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.js`,
-        freeze: false
-      },
-      tsCompilerOptions: {target: 'es5'}
-    }));
-
-    // Browser ES2017
-    bundles.push(config({
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.js`
-      },
-      tsCompilerOptions: {target: 'es2017'}
-    }));
-
-    // Browser ES2017 minified
-    bundles.push(config({
-      plugins: [terserPlugin],
-      output: {
-        format: browserFormat,
-        name,
-        extend,
-        file: `dist/${fileName}.es2017.min.js`
-      },
-      tsCompilerOptions: {target: 'es2017'}
-    }));
+    const browserBundles = getBrowserBundleConfigOptions(
+        config, name, fileName, PREAMBLE, cmdOptions.visualize, false /* CI */);
+    bundles.push(...browserBundles);
   }
 
   return bundles;

commit eb378781beb438a39605adbea9d8d10a0c387f77

[webgl] Add more config in rollup
Currently yarn build will not generated tf-backend-webgl.*js.
This problem is introduced by https://github.com/tensorflow/tfjs/pull/4537.

tfjs-backend-webgl/rollup.config.js
@@ -18,6 +18,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import {terser} from 'rollup-plugin-terser';
 import visualizer from 'rollup-plugin-visualizer';
 import {getBrowserBundleConfigOptions} from '../rollup.config.helpers';
 
@@ -98,6 +99,7 @@ module.exports = cmdOptions => {
 
   const name = 'tf';
   const extend = true;
+  const browserFormat = 'umd';
   const fileName = 'tf-backend-webgl';
 
   // Node
@@ -111,6 +113,28 @@ module.exports = cmdOptions => {
     },
     tsCompilerOptions: {target: 'es5'},
     external: ['@tensorflow/tfjs-backend-cpu']
+  }));
+
+  // Browser default unminified
+  bundles.push(config({
+    output: {
+      format: browserFormat,
+      name,
+      extend,
+      file: `dist/${fileName}.js`,
+    }
+  }));
+
+  // Browser default minified
+  bundles.push(config({
+    plugins: [terser({output: {preamble: PREAMBLE}})],
+    output: {
+      format: browserFormat,
+      name,
+      extend,
+      file: `dist/${fileName}.min.js`,
+    },
+    visualize: cmdOptions.visualize
   }));
 
   if (cmdOptions.ci) {