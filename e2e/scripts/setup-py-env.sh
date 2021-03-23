4f2d443bc6d1c2a10f0753d5c4ee4775eecd6403
Matthew Soulanille <msoulanille@google.com>
2021-03-23 08:46:57 -0700

    [e2e] Fix e2e virtualenv in nightly CI

--- e2e/scripts/setup-py-env.sh
+++ e2e/scripts/setup-py-env.sh
@@ -38,12 +38,13 @@ fi
 
 VENV_DIR="$(mktemp -d)_venv"
 echo "Creating virtualenv at ${VENV_DIR} ..."
-python -m virtualenv -p python3 "${VENV_DIR}"
 PLATFORM="$(python -m platform)"
 if [[ $PLATFORM =~ "Windows" ]]
 then
+  python -m virtualenv -p python3 "${VENV_DIR}"
   source "${VENV_DIR}/Scripts/activate"
 else
+  virtualenv -p python3 "${VENV_DIR}"
   source "${VENV_DIR}/bin/activate"
 fi
 
