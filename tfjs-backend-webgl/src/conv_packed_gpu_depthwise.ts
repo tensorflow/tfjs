8b940bbbd5854a26cac6818f8201e85f6fca26e9
Ping Yu <4018+pyu10055@users.noreply.github.com>
2021-04-08 17:55:36 -0700

    added more checks for unused channels

--- tfjs-backend-webgl/src/conv_packed_gpu_depthwise.ts
+++ tfjs-backend-webgl/src/conv_packed_gpu_depthwise.ts
@@ -31,7 +31,7 @@ export class DepthwiseConvPacked2DProgram implements GPGPUProgram {
       activation: string = null, hasPreluActivation = false,
       hasLeakyReluAlpha = false) {
     this.outputShape = convInfo.outShape;
-
+    const channelMul = convInfo.outChannels / convInfo.inChannels;
     const xNumRows = convInfo.inHeight;
     const xNumCols = convInfo.inWidth;
     const padTop = convInfo.padInfo.top;
@@ -44,14 +44,14 @@ export class DepthwiseConvPacked2DProgram implements GPGPUProgram {
     const filterWidth = convInfo.filterWidth;
     const texelsAcross = filterWidth;
 
-    let mainLoop = `int xR; int xC; int xCOffset;`;
+    let mainLoop = `
+      int xR; int xC; int xCOffset;
+      vec4 wTexel; vec4 previous; vec4 final;`;
 
-    for (let r = 0; r < filterHeight; r++) {
-      for (let c = 0; c < filterWidth; c++) {
-        mainLoop += `
-          vec4 xTexelR${r}C${c * 2} = vec4(0.);
-          vec4 xR${r}C${c} = vec4(0.);`;
-      }
+    for (let c = 0; c < filterWidth; c++) {
+      mainLoop += `
+          vec4 xTexelC${c * 2};
+          vec4 xC${c};`;
     }
 
     /**
@@ -63,13 +63,22 @@ export class DepthwiseConvPacked2DProgram implements GPGPUProgram {
      * values from a texture2D call at once.
      */
     for (let r = 0; r < filterHeight; r++) {
+      for (let c = 0; c < filterWidth; c++) {
+        mainLoop += `
+          xTexelC${c * 2} = vec4(0.);
+          xC${c} = vec4(0.);`;
+      }
+      mainLoop += `
+        xR = xRCorner + ${r * dilationHeight};
+        if (xR >=0 && xR < ${xNumRows}) {
+      `;
+
       for (let texelC = 0; texelC < (texelsAcross / 2 + 1); texelC++) {
         const c = texelC * 2;
 
         mainLoop += `
-          xR = xRCorner + ${r * dilationHeight};
           xC = xCCorner + ${c * dilationWidth};
-        `;
+          `;
 
         if (strideWidth === 1) {
           if (c < filterWidth) {
@@ -86,55 +95,50 @@ export class DepthwiseConvPacked2DProgram implements GPGPUProgram {
 
               mainLoop += `
                 xCOffset = xC + 1;
-                if(xR >= 0 && xR < ${xNumRows} && xCOffset >= 0 && xCOffset < ${
-                  xNumCols}) {
-                  xTexelR${r}C${c} = getX(batch, xR, xCOffset, d1);
+                if (xCOffset >= 0 && xCOffset < ${xNumCols}) {
+                  xTexelC${c} = getX(batch, xR, xCOffset, d1);
 
                   // Need to manually clear unused channels in case
                   // we're reading from recycled texture.
-                  if(xCOffset + 1 >= ${xNumCols}) {
-                    xTexelR${r}C${c}.zw = vec2(0.);
+                  if (xCOffset + 1 >= ${xNumCols}) {
+                    xTexelC${c}.zw = vec2(0.);
                   }
                 }
-               `;
+              `;
               // This texel has been read in previous iteration if the dilation
               // is 1.
               if (dilationWidth === 1 && c > 0) {
                 mainLoop += `
-                  xR${r}C${c} = vec4(xTexelR${r}C${c - 2}.zw, xTexelR${r}C${
-                    c}.xy);
-                 `;
+                xC${c} = vec4(xTexelC${c - 2}.zw, xTexelC${c}.xy);
+                `;
               } else {
                 mainLoop += `
-                 xCOffset = xC + 1 - 2;
+                  xCOffset = xC + 1 - 2;
 
-                 if(xR >= 0 && xR < ${
-                    xNumRows} && xCOffset >= 0 && xCOffset < ${xNumCols}) {
-                   vec4 previous = getX(batch, xR, xCOffset, d1);
+                  if (xCOffset >= 0 && xCOffset < ${xNumCols}) {
+                    previous = getX(batch, xR, xCOffset, d1);
 
-                   // Need to manually clear unused channels in case
-                   // we're reading from recycled texture.
-                   if(xCOffset + 1 >= ${xNumCols}) {
-                     previous.zw = vec2(0.);
-                   }
+                    // Need to manually clear unused channels in case
+                    // we're reading from recycled texture.
+                    if (xCOffset + 1 >= ${xNumCols}) {
+                      previous.zw = vec2(0.);
+                    }
 
-                   xR${r}C${c} = vec4(previous.zw, xTexelR${r}C${c}.xy);
-                 } else {
-                   xR${r}C${c} = vec4(0, 0, xTexelR${r}C${c}.xy);
-                 }
-               `;
+                    xC${c} = vec4(previous.zw, xTexelC${c}.xy);
+                  } else {
+                    xC${c} = vec4(0., 0., xTexelC${c}.xy);
+                  }
+                  `;
               }
             } else {
               // Padding is even, so xRC corresponds to a single texel.
               mainLoop += `
-                if(xR >= 0 && xR < ${xNumRows} && xC >= 0 && xC < ${xNumCols}) {
-                  xTexelR${r}C${c} = getX(batch, xR, xC, d1);
-                } else {
-                  xTexelR${r}C${c} = vec4(0.);
+                if (xC >= 0 && xC < ${xNumCols}) {
+                  xTexelC${c} = getX(batch, xR, xC, d1);
                 }
 
-                xR${r}C${c} = xTexelR${r}C${c};
-              `;
+                xC${c} = xTexelC${c};
+                `;
             }
 
             if (c + 1 < filterWidth) {
@@ -153,122 +157,135 @@ export class DepthwiseConvPacked2DProgram implements GPGPUProgram {
                 mainLoop += `
                   xCOffset = xC + ${padLeft % 2} + ${nextTexelOffset};
 
-                  if(xR >= 0 && xR < ${xNumRows} &&
-                    xCOffset >= 0 && xCOffset < ${xNumCols}) {
-                    xTexelR${r}C${c + 2} = getX(batch, xR, xCOffset, d1);
+                  if (xCOffset >= 0 && xCOffset < ${xNumCols}) {
+                    xTexelC${c + 2} = getX(batch, xR, xCOffset, d1);
+
+                    // Need to manually clear unused channels in case
+                    // we're reading from recycled texture.
+                    if (xCOffset + 1 >= ${xNumCols}) {
+                      xTexelC${c + 2}.zw = vec2(0.);
+                    }
                   }
-                `;
+                  `;
 
                 // If dilation > 1 then the xRC's will not be able to share any
                 // values, so each xRC will require two unique calls to getX.
                 if (dilationWidth > 1) {
                   mainLoop += `
                     xCOffset -= 2;
-                    if(xR >= 0 && xR < ${xNumRows} &&
-                      xCOffset >= 0 && xCOffset < ${xNumCols}) {
-                      xTexelR${r}C${c} = getX(batch, xR, xCOffset, d1);
-                    } else {
-                      xTexelR${r}C${c} = vec4(0.);
+                    if (xCOffset >= 0 && xCOffset < ${xNumCols}) {
+                      xTexelC${c} = getX(batch, xR, xCOffset, d1);
                     }
-                  `;
+                    `;
                 }
 
                 mainLoop += `
-                  xR${r}C${c + 1} = vec4(
-                    xTexelR${r}C${c}.zw, xTexelR${r}C${c + 2}.xy);
-                `;
+                  xC${c + 1} = vec4(xTexelC${c}.zw, xTexelC${c + 2}.xy);
+                  `;
               } else {
                 // If dialtion is 1 and padding is odd, we have already read the
                 // texel when constructing the previous x value. Here we can
                 // simply skip the texture read.
                 if (nextTexelOffset === 1) {
                   mainLoop += `
-                  xR${r}C${c + 1} = xTexelR${r}C${c};
-                  `;
+                    xC${c + 1} = xTexelC${c};
+                    `;
                 } else {
                   mainLoop += `
-                  xCOffset = xC + ${nextTexelOffset};
+                    xCOffset = xC + ${nextTexelOffset};
 
-                  if(xR >= 0 && xR < ${xNumRows} &&
-                    xCOffset >= 0 && xCOffset < ${xNumCols}) {
-                    xTexelR${r}C${c + 2} = getX(batch, xR, xCOffset, d1);
-                  }
+                    if (xCOffset >= 0 && xCOffset < ${xNumCols}) {
+                      xTexelC${c + 2} = getX(batch, xR, xCOffset, d1);
 
-                  xR${r}C${c + 1} = xTexelR${r}C${c + 2};
-                `;
+                      // Need to manually clear unused channels in case
+                      // we're reading from recycled texture.
+                      if (xCOffset + 1 >= ${xNumCols}) {
+                        xTexelC${c + 2}.zw = vec2(0.);
+                      }
+                    }
+
+                    xC${c + 1} = xTexelC${c + 2};
+                    `;
                 }
               }
             }
           }
-        } else {  // stride > 1
+        } else {  // stride === 2
           if (c < filterWidth) {
-            mainLoop += `
-              if(xR >= 0 && xR < ${xNumRows}) {
-            `;
-
             // Depending on whether padLeft is even or odd, we want either the
-            // xy or zw channels from X texels for xR${r}C${c}. If padLeft is
-            // even, xR${r}C${c + 1} is simply the zw channels of texels we've
-            // already sampled. But if padLeft is odd, xR${r}C{$c + 1}.zw will
-            // need to come from the xy channels of a new texel, hence the `vec4
+            // xy or zw channels from X texels for xC${c}. If padLeft is
+            // even, xC${c + 1} is simply the zw channels of texels we've
+            // already sampled. But if padLeft is odd, xC{$c + 1}.zw will
+            // need to come from the xy channels of a new texel, hence the `
+            // vec4
             // final` initialized below.
             if (padLeft % 2 === 1) {
               mainLoop += `
                 xCOffset = xC + 1 - ${strideWidth};
                 if(xCOffset >= 0 && xCOffset < ${xNumCols}) {
-                  xTexelR${r}C${c} = getX(batch, xR, xCOffset, d1);
-                } else {
-                  xTexelR${r}C${c} = vec4(0.);
+                  xTexelC${c} = getX(batch, xR, xCOffset, d1);
+                  // Need to manually clear unused channels in case
+                  // we're reading from recycled texture.
+                  if (xCOffset + 1 >= ${xNumCols}) {
+                    xTexelC${c}.zw = vec2(0.);
+                  }
                 }
 
                 if(xC + 1 >= 0 && xC + 1 < ${xNumCols}) {
-                  xTexelR${r}C${c + 2} = getX(batch, xR, xC + 1, d1);
-                } else {
-                  xTexelR${r}C${c + 2} = vec4(0.);
+                  xTexelC${c + 2} = getX(batch, xR, xC + 1, d1);
+                  // Need to manually clear unused channels in case
+                  // we're reading from recycled texture.
+                  if (xCOffset + 1 >= ${xNumCols}) {
+                    xTexelC${c + 2}.zw = vec2(0.);
+                  }
                 }
 
-                xR${r}C${c} = vec4(
-                  xTexelR${r}C${c}.zw, xTexelR${r}C${c + 2}.zw);
+                xC${c} = vec4(
+                  xTexelC${c}.zw, xTexelC${c + 2}.zw);
               `;
 
               if (c + 1 < filterWidth) {
                 mainLoop += `
-                  vec4 final = vec4(0.);
+                  final = vec4(0.);
                   xCOffset = xC + 1 + ${strideWidth};
                   if(xCOffset >= 0 && xCOffset < ${xNumCols}) {
                     final = getX(batch, xR, xCOffset, d1);
                   }
-                  xR${r}C${c + 1} = vec4(xTexelR${r}C${c + 2}.xy, final.xy);
+                  xC${c + 1} = vec4(xTexelC${c + 2}.xy, final.xy);
                 `;
               }
             } else {
               mainLoop += `
                 if(xC >= 0 && xC < ${xNumCols}) {
-                  xTexelR${r}C${c} = getX(batch, xR, xC, d1);
-                } else {
-                  xTexelR${r}C${c} = vec4(0.);
+                  xTexelC${c} = getX(batch, xR, xC, d1);
+                  // Need to manually clear unused channels in case
+                  // we're reading from recycled texture.
+                  if (xCOffset + 1 >= ${xNumCols}) {
+                    xTexelC${c}.zw = vec2(0.);
+                  }
                 }
 
                 xCOffset = xC + ${strideWidth};
                 if(xCOffset >= 0 && xCOffset < ${xNumCols}) {
-                  xTexelR${r}C${c + 2} = getX(batch, xR, xCOffset, d1);
-                } else {
-                  xTexelR${r}C${c + 2} = vec4(0.);
+                  xTexelC${c + 2} = getX(batch, xR, xCOffset, d1);
+                  // Need to manually clear unused channels in case
+                  // we're reading from recycled texture.
+                  if (xCOffset + 1 >= ${xNumCols}) {
+                    xTexelC${c + 2}.zw = vec2(0.);
+                  }
                 }
 
-                xR${r}C${c} = vec4(
-                  xTexelR${r}C${c}.xy, xTexelR${r}C${c + 2}.xy);
+                xC${c} = vec4(
+                  xTexelC${c}.xy, xTexelC${c + 2}.xy);
               `;
 
               if (c + 1 < filterWidth) {
                 mainLoop += `
-                  xR${r}C${c + 1} = vec4(
-                    xTexelR${r}C${c}.zw, xTexelR${r}C${c + 2}.zw);
+                  xC${c + 1} = vec4(
+                    xTexelC${c}.zw, xTexelC${c + 2}.zw);
                 `;
               }
             }
-
-            mainLoop += `}`;
           }
         }
 
@@ -278,19 +295,21 @@ export class DepthwiseConvPacked2DProgram implements GPGPUProgram {
         // 50 variables)
         if (c < filterWidth) {
           mainLoop += `
-            vec4 wTexelR${r}C${c} = getW(${r}, ${c}, d1, q);
-            dotProd += vec4(xR${r}C${c}.xy * wTexelR${r}C${c}.xz, xR${r}C${
-              c}.zw * wTexelR${r}C${c}.xz);
+            wTexel = getW(${r}, ${c}, d1, q);
+            dotProd += vec4(xC${c}.xy * wTexel.xz, xC${c}.zw * wTexel.xz);
           `;
 
           if (c + 1 < filterWidth) {
             mainLoop += `
-              vec4 wTexelR${r}C${c + 1} = getW(${r}, ${c + 1}, d1, q);
-              dotProd += vec4(xR${r}C${c + 1}.xy * wTexelR${r}C${c + 1}.xz, xR${
-                r}C${c + 1}.zw * wTexelR${r}C${c + 1}.xz);`;
+              wTexel = getW(${r}, ${c + 1}, d1, q);
+              dotProd += vec4(xC${c + 1}.xy * wTexel.xz, xC${
+                c + 1}.zw * wTexel.xz);`;
           }
         }
       }
+      mainLoop += `
+        }
+      `;
     }
 
     let activationSnippet = '', applyActivationSnippet = '';
@@ -338,8 +357,8 @@ export class DepthwiseConvPacked2DProgram implements GPGPUProgram {
         int batch = coords.x;
         ivec2 xRCCorner = coords.yz * strides - pads;
         int d2 = coords.w;
-        int d1 = d2;
-        int q = 0;
+        int d1 = d2 / ${channelMul};
+        int q = d2 - d1 * ${channelMul};
         int xRCorner = xRCCorner.x;
         int xCCorner = xRCCorner.y;
 
