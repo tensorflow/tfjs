53f35a58b1bd548dd43ed71f0f85afc89545fe50
eric.lwh <eric.lwh@alibaba-inc.com>
2021-04-09 13:15:23 +0800

    fix

--- tfjs-node/src/run_tests.ts
+++ tfjs-node/src/run_tests.ts
@@ -94,16 +94,16 @@ const IGNORE_LIST: string[] = [
   'denseBincount',
   // only available in tf addon.
   'image.transform',
-  // Breaking change in NaN processing 
+  // Breaking change in NaN processing
   // See https://github.com/tensorflow/tfjs/issues/4883
   'max test-tensorflow {} ignores NaNs',
   'min test-tensorflow {} ignores NaNs',
   'sign test-tensorflow {} basic',
   'sign test-tensorflow {} does not propagate NaNs',
-  'sign test-tensorflow {} accepts a tensor-like object'
+  'sign test-tensorflow {} accepts a tensor-like object',
   // Node kernel for einsum is yet to be implemented.
   // See: ttps://github.com/tensorflow/tfjs/issues/2349
-  'einsum',
+  'einsum'
 ];
 
 if (process.platform === 'win32') {
