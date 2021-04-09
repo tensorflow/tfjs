0be6eb80b522bce502e5538711bc5815747bcf48
eric.lwh <eric.lwh@alibaba-inc.com>
2021-04-09 11:26:26 +0800

    ignore nan

--- tfjs-node/src/run_tests.ts
+++ tfjs-node/src/run_tests.ts
@@ -93,7 +93,13 @@ const IGNORE_LIST: string[] = [
   // not available in tf yet.
   'denseBincount',
   // only available in tf addon.
-  'image.transform'
+  'image.transform',
+  // breaking change in NaN processing see https://github.com/tensorflow/tfjs/issues/4883
+  'max test-tensorflow {} ignores NaNs',
+  'min test-tensorflow {} ignores NaNs',
+  'sign test-tensorflow {} basic',
+  'sign test-tensorflow {} does not propagate NaNs',
+  'sign test-tensorflow {} accepts a tensor-like object'
 ];
 
 if (process.platform === 'win32') {
