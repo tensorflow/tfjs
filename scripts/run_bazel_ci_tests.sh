c0e5075ad9c7af3be9ae26e53beb66c6e580d909
Matthew Soulanille <msoulanille@google.com>
2021-04-09 11:45:52 -0700

    Pass BrowserStack credentials in .bazelrc correctly

--- scripts/run_bazel_ci_tests.sh
+++ scripts/run_bazel_ci_tests.sh
@@ -18,4 +18,4 @@
 set -e
 
 # Run `bazel test` on all targets that have the tag "ci"
-yarn bazel test --action_env=BROWSERSTACK_USERNAME=$BROWSERSTACK_USERNAME --action_env=BROWSERSTACK_KEY=$BROWSERSTACK_KEY --config=ci --flaky_test_attempts=3 --test_output=all `./node_modules/.bin/bazel query --output label 'attr("tags", "ci", ...)'`
+yarn bazel test --config=ci --flaky_test_attempts=3 --test_output=all `./node_modules/.bin/bazel query --output label 'attr("tags", "ci", ...)'`
