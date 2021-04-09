0ab1df1cd14ab3e3b8b97ac73cb38e15ddb31619
Roman <iskullbreakeri@gmail.com>
2021-04-09 05:34:39 +0300

    Update threshold.ts
    Code changed according to review suggestions.

--- tfjs-core/src/ops/image/threshold.ts
+++ tfjs-core/src/ops/image/threshold.ts
@@ -1,104 +1,126 @@
-import { Tensor3D } from '../../tensor';
-import { TensorLike } from '../../types';
-import { op } from '../operation';
-import { tensor3d } from '../tensor3d';
-import * as util from '../../util';
-import {convertToTensor} from '../../tensor_util_env';
-
 /**
- * Performs threshold algorithms on Tensors
- * @param image a 2d image tensor of shape `[x , y, 3]`.
- * @param method Optional string from `'binary' | 'otsu'`,
- *     defaults to binary, which specifies type of the threshold operation
- * @param coeff Optional number which defines thresh coefficient from 0 to 1.
- * @param inverted Optional boolian which
- *                specifies if colours should be inverted
+ * @license
+ * Copyright 2021 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
  */
 
-function otsu_alg(histData: number[], total: number) {
-  let sum = 0;
-  for (let t = 0; t < 256; t++) {
-    sum += t * histData[t];
-  }
-  let sumB = 0;
-  let wB = 0;
-  let wF = 0;
-  let varMax = 0;
-  let thresh = 0;
+import { Tensor1D, Tensor3D } from '../../tensor';
+import { tensor1d } from '../tensor1d';
+import { TensorLike } from '../../types';
+import { op } from '../operation';
+import { cast } from '../cast';
+import { split } from '../split';
+import { bincount } from '../bincount';
+import { lessEqual } from '../less_equal';
+import { greater } from '../greater';
+import { sum } from '../sum';
+import { add } from '../add';
+import { fill } from '../fill';
+import { range } from '../range';
+import { tensor } from '../tensor';
+import * as util from '../../util';
+import { convertToTensor } from '../../tensor_util_env';
 
-  for (let t = 0; t < 256; t++) {
-    wB += histData[t];               // Weight Background
-    if (wB === 0) {
-      continue;
-    }
-
-    wF = total - wB;                 // Weight Foreground
-    if (wF === 0) {
-      break;
-    }
-
-    sumB += t * histData[t];
-    const mB = sumB / wB;            // Mean Background
-    const mF = (sum - sumB) / wF;    // Mean Foreground
-
-    // Calculate Between Class Variance
-    const varBetween = wB * wF * (mB - mF) * (mB - mF);
-    // Check if new maximum found
-    if (varBetween > varMax) {
-      varMax = varBetween;
-      thresh = t;
-    }
-  }
-
-  return thresh;
-}
+/**
+ * Performs image binarization with corresponding threshold
+ * (depends on the method)value, which creates a binary image from a grayscale image.
+ * @param image 3D tensor of shape [batch, imageHeight, imageWidth, depth]. The image color range should be [0, 255].
+ * @param method Optional string from `'binary' | 'otsu'`,
+ *     defaults to binary, which specifies the method for thresholding.
+ * @param inverted Optional boolean which
+ *                specifies if colours should be inverted
+ * @param threshValue Optional number which defines threshold value from 0 to 1.
+ * @return A 3D tensor which contains binarized image.
+ */
 
 function threshold_(
-  image: Tensor3D | TensorLike,
-  method: 'binary' | 'otsu' = 'binary',
-  coeff?: number,
-  inverted?: boolean
+    image: Tensor3D | TensorLike,
+    method: 'binary' | 'otsu' = 'binary',
+    inverted: boolean = false,
+    threshValue: number = 0.5
 ): Tensor3D {
-    
-  const $image = convertToTensor(image, 'image', 'threshold');
-  const threshold = coeff * 255;
-  const arrayedImage = Array.from($image.dataSync());
+    const $image = convertToTensor(image, 'image', 'threshold');
+    const redIntencityCoef = 0.2126;
+    const greenIntencityCoef = 0.7152;
+    const blueIntencityCoef = 0.0722;
+    const totalPixelsInImage = $image.shape[0] * $image.shape[1];
 
-  util.assert(
-    $image.rank === 3,
-    () => 'Error in threshold: image must be rank 3,' +
-      `but got rank ${$image.rank}.`);
+    let $threshold = tensor1d([threshValue]).mul(255);
+    let r,
+        g,
+        b,
+        grayscale;
 
-  util.assert(
-    method === 'otsu' || method === 'binary',
-    () => `method must be binary or otsu, but was ${method}`);
+    util.assert(
+        $image.rank === 3,
+        () => 'Error in threshold: image must be rank 3,' +
+            `but got rank ${$image.rank}.`);
 
-  if (method === 'binary') {
-    arrayedImage.forEach((item, i) => {
-      if (!inverted) {
-        arrayedImage[i] = (item < threshold) ? 0 : 255;
-      } else {
-        arrayedImage[i] = (item > threshold) ? 0 : 255;
-      }
-    });
+    util.assert(
+        method === 'otsu' || method === 'binary',
+        () => `Method must be binary or otsu, but was ${method}`);
 
-  } else if (method === 'otsu') {
-    const histogram = Array(256).fill(0);
-
-    for (let i = 0; i < arrayedImage.length; i++) {
-      const gray = arrayedImage[i];
-      histogram[Math.round(gray)] += 1;
+    if ($image.shape[2] === 3) {
+        [r, g, b] = split($image, [1, 1, 1], -1);
+        grayscale = r.mul(redIntencityCoef).add(g.mul(greenIntencityCoef)).add(b.mul(blueIntencityCoef));//if image RGB
+    } else {
+        grayscale = image;
     }
 
-    const threshold = otsu_alg(histogram, arrayedImage.length);
+    if (method === 'otsu') {
+        const $histogram = bincount(cast(grayscale.round(), 'int32'), tensor([]), 256);
+        $threshold = otsu($histogram, totalPixelsInImage);
+    }
 
-    arrayedImage.forEach((item, i) => {
-      arrayedImage[i] = (item < threshold) ? 0 : 255;
-    });
+    const invCondition = inverted ? lessEqual(grayscale, $threshold) : greater(grayscale, $threshold);
+    const result = cast(invCondition.mul(255), 'int32');
+    return result;
+}
 
-  }
+function otsu(histogram: Tensor1D, total: number) {
 
-  return tensor3d(arrayedImage, $image.shape, 'int32');
+    let bestThreshold = tensor1d([-1]) //bestThreshold
+    let bestInBetweenVariance = tensor([0]); // bestVariance
+    let curInBetweenVariance = tensor1d([0])
+    let classFirst, classSecond, meanFirst, meanSecond, weightForeground, weightBackground;
+
+    for (let index = 0; index < histogram.shape[0]; index++) {
+        if (index != histogram.shape[0] - 1) {
+
+            classFirst = histogram.slice(0, index + 1);
+
+            classSecond = histogram.slice(index + 1);
+
+            weightForeground = sum(classFirst).div(total);
+
+            weightBackground = sum(classSecond).div(total);
+
+            meanFirst = sum(classFirst.mul(range(0, classFirst.shape[0]))).div(classFirst.sum());
+
+            meanSecond = sum(classSecond.mul(add(range(0, classSecond.shape[0]), fill(classSecond.shape, classFirst.shape[0])))).div(classSecond.sum())
+
+            curInBetweenVariance = (weightForeground.mul(weightBackground).mul(meanFirst.sub(meanSecond)).mul(meanFirst.sub(meanSecond))).reshape([1])  // currentVariance
+
+            let condition = curInBetweenVariance.greater(bestInBetweenVariance);
+
+            bestInBetweenVariance = curInBetweenVariance.where(condition, bestInBetweenVariance);
+
+            bestThreshold = tensor1d([index]).where(condition, bestThreshold);
+
+        }
+    }
+    return bestThreshold;
 }
 
 export const threshold = op({ threshold_ });
