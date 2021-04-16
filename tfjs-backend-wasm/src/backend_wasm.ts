867e5c9f3255bad66c9132512c4051f52b0c5cc2
Xu Xing <xing.xu@intel.com>
2021-04-16 09:45:56 +0800

    [wasm] Add flag WASM_THREAD_POOL_SIZE
    This flag tells how many threads can be used in multi-thread mode.

--- tfjs-backend-wasm/src/backend_wasm.ts
+++ tfjs-backend-wasm/src/backend_wasm.ts
@@ -44,6 +44,12 @@ export class BackendWasm extends KernelBackend {
   constructor(public wasm: BackendWasmModule) {
     super();
     this.wasm.tfjs.init();
+    // Register the used thread pool size flag. Done it here to avoid circular
+    // import.
+    env().registerFlag('WASM_THREAD_POOL_SIZE', () => {
+      return this.getThreadPoolSize();
+    });
+
     this.dataIdMap = new DataStorage(this, engine());
   }
 
@@ -155,6 +161,11 @@ export class BackendWasm extends KernelBackend {
   // testing.
   getMemoryOffset(dataId: DataId): number {
     return this.dataIdMap.get(dataId).memoryOffset;
+  }
+
+  // Return the used thread pool size.
+  getThreadPoolSize(): number {
+    return this.wasm.tfjs.getThreadPoolSize();
   }
 
   dispose() {
@@ -336,7 +347,7 @@ export async function init(): Promise<{wasm: BackendWasmModule}> {
       initialized = true;
       initAborted = false;
 
-      const voidReturnType: string = null;
+      const voidType: string = null;
       // Using the tfjs namespace to avoid conflict with emscripten's API.
       module.tfjs = {
         init: module.cwrap('init', null, []),
@@ -347,8 +358,10 @@ export async function init(): Promise<{wasm: BackendWasmModule}> {
               'number',  // size
               'number',  // memoryOffset
             ]),
-        disposeData: module.cwrap('dispose_data', voidReturnType, ['number']),
-        dispose: module.cwrap('dispose', voidReturnType, []),
+        getThreadPoolSize:
+            module.cwrap('get_thread_pool_size', 'number', [voidType]),
+        disposeData: module.cwrap('dispose_data', voidType, ['number']),
+        dispose: module.cwrap('dispose', voidType, []),
       };
 
       resolve({wasm: module});
