3136dde6fc2ae4b9fb29a406643ba7f29d4d39d5
Ping Yu <4018+pyu10055@users.noreply.github.com>
2021-06-07 16:30:58 -0700

    remove the imports for chained ops

--- tfjs-layers/src/index.ts
+++ tfjs-layers/src/index.ts
@@ -9,12 +9,6 @@
  */
 
 import '@tensorflow/tfjs-core';
-// tslint:disable-next-line:no-imports-from-dist
-// import
-// '@tensorflow/tfjs-core/dist/public/chained_ops/register_all_chained_ops';
-// tslint:disable-next-line: no-imports-from-dist
-// import '@tensorflow/tfjs-core/dist/register_all_gradients';
-
 // This file lists all exports of TensorFlow.js Layers
 
 import * as constraints from './exports_constraints';
