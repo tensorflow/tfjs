78c86a4f65eef095fc2a91433505ddd28ed0cb73
Ping Yu <4018+pyu10055@users.noreply.github.com>
2021-06-08 16:32:18 -0700

    [tfjs-layers] remove deps of chained ops in layers (#5188)
    BUG
    PERF
    * remove deps of chained ops in layers
    
    * fix failed tests
    
    * fix failed tests
    
    * remove the imports for chained ops
    
    * fix tfjs-data snippets test error
    
    * added readme and fixed integration tests
    
    * leave the gradient registration in the layers

--- tfjs-layers/src/index.ts
+++ tfjs-layers/src/index.ts
@@ -9,8 +9,6 @@
  */
 
 import '@tensorflow/tfjs-core';
-// tslint:disable-next-line:no-imports-from-dist
-import '@tensorflow/tfjs-core/dist/public/chained_ops/register_all_chained_ops';
 // tslint:disable-next-line: no-imports-from-dist
 import '@tensorflow/tfjs-core/dist/register_all_gradients';
 
