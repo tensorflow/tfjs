16b843db921bdbb7c76bdc00541f59b3fa42bf4c
Yunfei Hao <yunfei.hao@intel.com>
2021-06-16 09:51:09 +0800

    Address comments from Na

--- tfjs-backend-webgpu/src/index.ts
+++ tfjs-backend-webgpu/src/index.ts
@@ -18,41 +18,41 @@
 import './flags_webgpu';
 import './register_all_kernels';
 
-import {env, registerBackend} from '@tensorflow/tfjs-core';
+import {device_util, env, registerBackend} from '@tensorflow/tfjs-core';
 import glslangInit from '@webgpu/glslang/dist/web-devel/glslang.onefile';
 
 import {WebGPUBackend} from './backend_webgpu';
 import * as webgpu from './webgpu';
 
-registerBackend('webgpu', async () => {
-  if (!env().getBool('HAS_WEBGPU')) {
-    throw new Error('WebGPU is not supported on this device');
-  }
-  // Remove it once we figure out how to correctly read the tensor data before
-  // the tensor is disposed in profiling mode.
-  env().set('CHECK_COMPUTATION_FOR_ERRORS', false);
+if (device_util.isBrowser() && env().getBool('HAS_WEBGPU')) {
+  registerBackend('webgpu', async () => {
+    // Remove it once we figure out how to correctly read the tensor data
+    // before the tensor is disposed in profiling mode.
+    env().set('CHECK_COMPUTATION_FOR_ERRORS', false);
 
-  const glslang = await glslangInit();
-  const gpuDescriptor: GPURequestAdapterOptions = {
-    powerPreference: env().get('WEBGPU_USE_LOW_POWER_GPU') ? 'low-power' :
-                                                             'high-performance'
-  };
+    const glslang = await glslangInit();
+    const gpuDescriptor: GPURequestAdapterOptions = {
+      powerPreference: env().get('WEBGPU_USE_LOW_POWER_GPU') ?
+          'low-power' :
+          'high-performance'
+    };
 
-  const adapter = await navigator.gpu.requestAdapter(gpuDescriptor);
-  let deviceDescriptor: GPUDeviceDescriptor = {};
-  const supportTimeQuery = adapter.features.has('timestamp-query');
+    const adapter = await navigator.gpu.requestAdapter(gpuDescriptor);
+    let deviceDescriptor: GPUDeviceDescriptor = {};
+    const supportTimeQuery = adapter.features.has('timestamp-query');
 
-  if (supportTimeQuery) {
-    deviceDescriptor = {nonGuaranteedFeatures: ['timestamp-query' as const ]};
-  } else {
-    console.warn(
-        `This device doesn't support timestamp-query extension. ` +
-        `Zero will shown for the kernel time when profiling mode is enabled. ` +
-        `Using performance.now is not workable for webgpu since it doesn't ` +
-        `support synchronously to read data from GPU.`);
-  }
-  const device: GPUDevice = await adapter.requestDevice(deviceDescriptor);
-  return new WebGPUBackend(device, glslang, supportTimeQuery);
-}, 3 /*priority*/);
+    if (supportTimeQuery) {
+      deviceDescriptor = {nonGuaranteedFeatures: ['timestamp-query' as const ]};
+    } else {
+      console.warn(
+          `This device doesn't support timestamp-query extension. ` +
+          `Zero will shown for the kernel time when profiling mode is enabled. ` +
+          `Using performance.now is not workable for webgpu since it doesn't ` +
+          `support synchronously to read data from GPU.`);
+    }
+    const device: GPUDevice = await adapter.requestDevice(deviceDescriptor);
+    return new WebGPUBackend(device, glslang, supportTimeQuery);
+  }, 3 /*priority*/);
+}
 
 export {webgpu};
