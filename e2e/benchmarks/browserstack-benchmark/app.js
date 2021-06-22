abbe8192eefdf15bf8dc59ab2b315e4b18791a36
ClaireXP <cpoukey@google.com>
2021-06-22 17:49:54 +0000

    Added outfile Jasmine tests

--- e2e/benchmarks/browserstack-benchmark/app.js
+++ e2e/benchmarks/browserstack-benchmark/app.js
@@ -20,8 +20,8 @@ const socketio = require('socket.io');
 const fs = require('fs');
 const path = require('path');
 const {execFile} = require('child_process');
-const { argumentParser } = require('argparse');
-const { version } = require('./package.json');
+const {ArgumentParser} = require('argparse');
+const {version} = require('./package.json');
 
 const port = process.env.PORT || 8001;
 let io;
@@ -92,6 +92,9 @@ function setupBenchmarkEnv(config) {
 /**
  * Run model benchmark on BrowserStack.
  *
+ * Each browser-device pairing is benchmarked in parallel. Results are sent to
+ * the webpage staggered as they are returned to the server.
+ *
  * The benchmark configuration object contains two objects:
  * - `browsers`: Each key-value pair represents a browser instance to be
  * benchmarked. The key is a unique string id/tabId (assigned by the webpage)
@@ -108,12 +111,37 @@ function setupBenchmarkEnv(config) {
  *
  * @param {{browsers, benchmark}} config Benchmark configuration.
  */
-function benchmark(config) {
-  console.log('Preparing configuration files for the test runner.');
-  setupBenchmarkEnv(config);
+async function benchmark(config, benchmarkResult = runOneBenchmark) {
+  return new Promise((resolve) => {
+    console.log('Preparing configuration files for the test runner.');
+    setupBenchmarkEnv(config);
 
-  console.log(`Start benchmarking.`);
-  for (const tabId in config.browsers) {
+    console.log(`Start benchmarking.`);
+    let results = [];
+    for (const tabId in config.browsers) {
+      results.push(benchmarkResult(tabId));
+    }
+
+    /** Optional outfile written once all benchmarks have returned results. */
+    Promise.allSettled(results).then(results => {
+      if (process.argv.includes('--outfile')) {
+        write('./benchmark_results.json', results);
+      }
+      return resolve(results);
+    });
+  });
+}
+
+/**
+ * Run benchmark for singular browser-device combination.
+ *
+ * This function utilizes a promise that is fulfilled once the corresponding
+ * result is returned from BrowserStack.
+ *
+ * @param {config.browsers[index]} tabId.
+ */
+function runOneBenchmark(tabId) {
+  return new Promise((resolve, reject) => {
     const args = ['test', '--browserstack', `--browsers=${tabId}`];
     const command = `yarn ${args.join(' ')}`;
     console.log(`Running: ${command}`);
@@ -125,14 +153,14 @@ function benchmark(config) {
         io.emit(
             'benchmarkComplete',
             {tabId, error: `Failed to run ${command}:\n${error}`});
-        return;
+        return reject(`Failed to run ${command}:\n${error}`);
       }
 
       const errorReg = /.*\<tfjs_error\>(.*)\<\/tfjs_error\>/;
       const matchedError = stdout.match(errorReg);
       if (matchedError != null) {
         io.emit('benchmarkComplete', {tabId, error: matchedError[1]});
-        return;
+        return reject(matchedError[1]);
       }
 
       const resultReg = /.*\<tfjs_benchmark\>(.*)\<\/tfjs_benchmark\>/;
@@ -141,28 +169,44 @@ function benchmark(config) {
         const benchmarkResult = JSON.parse(matchedResult[1]);
         benchmarkResult.tabId = tabId;
         io.emit('benchmarkComplete', benchmarkResult);
-        return;
+        return resolve(benchmarkResult);
       }
 
-      io.emit('benchmarkComplete', {
-        error: 'Did not find benchmark results from the logs ' +
-            'of the benchmark test (benchmark_models.js).'
-      });
+      let errorMessage = 'Did not find benchmark results from the logs ' +
+          'of the benchmark test (benchmark_models.js).';
+      io.emit('benchmarkComplete', {error: errorMessage});
+      return reject(errorMessage);
     });
-  }
+  });
+}
+
+/**
+ * Writes a passed message to a passed JSON file.
+ *
+ * @param filePath, msg.
+ */
+function write(filePath, msg) {
+  fs.writeFile(
+      filePath, JSON.stringify(msg), 'utf8',
+      err => {console.log(err ? `Error: ${err}.` : 'Output written.')});
 }
 
 /** Set up --help menu for file description and available optional commands */
-function setUpHelpMessage() {
-  const parser = new argumentParser({
+function setupHelpMessage() {
+  const parser = new ArgumentParser({
     description: 'This file launches a server to connect to BrowserStack ' +
         'so that the performance of a TensorFlow model on one or more ' +
         'browsers can be benchmarked.'
   });
-  parser.add_argument('-v', '--version', { action: 'version', version });
+  parser.add_argument(
+      '--outfile', {help: 'write results to outfile', action: 'store_true'});
+  parser.add_argument('-v', '--version', {action: 'version', version});
   console.dir(parser.parse_args());
 }
 
-setUpHelpMessage();
+setupHelpMessage();
 checkBrowserStackAccount();
 runServer();
+
+exports.benchmark = benchmark;
+exports.write = write;
--- e2e/benchmarks/browserstack-benchmark/app_node_test.js
+++ e2e/benchmarks/browserstack-benchmark/app_node_test.js
@@ -1,5 +1,5 @@
 const {execFile} = require('child_process');
-
+const {benchmark, write} = require('./app.js');
 
 function exec(command, args) {
   return new Promise((fulfill, reject) => {
@@ -20,8 +20,7 @@ describe('benchmark app cli', () => {
   });
 
   it('runs a command line command', async () => {
-    expect(await exec('echo', ['-n', 'Hello, world!']))
-        .toMatch('Hello');
+    expect(await exec('echo', ['-n', 'Hello, world!'])).toMatch('Hello');
   });
 
   it('runs a command that takes a while', async () => {
@@ -30,5 +29,118 @@ describe('benchmark app cli', () => {
 
   it('runs a command that fails', async () => {
     await expectAsync(exec('exit 1')).toBeRejected();
+  });
+});
+
+describe('test outfile capabilities', () => {
+  const config = {
+    benchmark: {model: 'mobilenet_v2', numRuns: 1, backend: 'wasm'},
+    browsers: {
+      iPhone_XS_1: {
+        base: 'BrowserStack',
+        browser: 'iphone',
+        browser_version: 'null',
+        os: 'ios',
+        os_version: '12',
+        device: 'iPhone XS',
+        real_mobile: true
+      },
+      Samsung_Galaxy_S20_1: {
+        base: 'BrowserStack',
+        browser: 'android',
+        browser_version: 'null',
+        os: 'android',
+        os_version: '10.0',
+        device: 'Samsung Galaxy S20',
+        real_mobile: true
+      },
+      Windows_10_1: {
+        base: 'BrowserStack',
+        browser: 'chrome',
+        browser_version: '84.0',
+        os: 'Windows',
+        os_version: '10',
+        device: null
+      },
+      OS_X_Catalina_1: {
+        base: 'BrowserStack',
+        browser: 'chrome',
+        browser_version: '84.0',
+        os: 'OS X',
+        os_version: 'Catalina',
+        device: null
+      }
+    }
+  };
+  const results = {
+    'iPhone_XS_1': {
+      timeInfo: {
+        times: [216.00000000000045],
+        averageTime: 216.00000000000045,
+        minTime: 216.00000000000045,
+        maxTime: 216.00000000000045
+      },
+      tabId: 'iPhone_XS_1'
+    },
+    'Samsung_Galaxy_S20_1': {
+      timeInfo: {
+        times: [428.89999999897555],
+        averageTime: 428.89999999897555,
+        minTime: 428.89999999897555,
+        maxTime: 428.89999999897555
+      },
+      tabId: 'Samsung_Galaxy_S20_1'
+    },
+    'Windows_10_1': {
+      timeInfo: {
+        times: [395.8500000001095],
+        averageTime: 395.8500000001095,
+        minTime: 395.8500000001095,
+        maxTime: 395.8500000001095
+      },
+      tabId: 'Windows_10_1'
+    },
+    'OS_X_Catalina_1': {
+      timeInfo: {
+        times: [176.19500000728294],
+        averageTime: 176.19500000728294,
+        minTime: 176.19500000728294,
+        maxTime: 176.19500000728294
+      },
+      tabId: 'OS_X_Catalina_1'
+    }
+  };
+
+  beforeAll(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000000;
+  });
+
+  it('tests writing capabilities', function() {
+    const filePath = './benchmark_test_results.json';
+    const file = require(filePath);
+
+    write(filePath, results);
+    expect(file).toEqual(results);
+  });
+
+  it('tests promise capabilities', async () => {
+    const mockBenchmarkResult =
+        jasmine.createSpy('mockBenchmarkResult').and.callFake(function(tabId) {
+          return new Promise((resolve) => {resolve(results[tabId])});
+        });
+
+    let testResults = await benchmark(config, mockBenchmarkResult);
+    let formattedResults = {};
+    for (let i = 0; i < 4; i++) {
+      let result = testResults[i]['value']
+      formattedResults[result['tabId']] = result;
+    }
+
+    expect(mockBenchmarkResult.calls.count()).toEqual(4);
+    expect(mockBenchmarkResult)
+        .toHaveBeenCalledWith(
+            'iPhone_XS_1' || 'Samsung_Galaxy_S20_1' || 'Windows_10_1' ||
+            'OS_X_Catalina_1');
+    expect(formattedResults).toEqual(results);
   });
 });
