19ec6d02c8969b7c0532dbdf7230646a3eb76b2d
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-06-24 13:18:32 +0700

    fix: add missing semicolon

--- tfjs-backend-cpu/src/register_all_kernels.ts
+++ tfjs-backend-cpu/src/register_all_kernels.ts
@@ -84,7 +84,7 @@ import {fusedConv2DConfig} from './kernels/FusedConv2D';
 import {fusedDepthwiseConv2DConfig} from './kernels/FusedDepthwiseConv2D';
 import {gatherNdConfig} from './kernels/GatherNd';
 import {gatherV2Config} from './kernels/GatherV2';
-import {grayscaleToRGBConfig} from './kernels/GrayscaleToRGB'
+import {grayscaleToRGBConfig} from './kernels/GrayscaleToRGB';
 import {greaterConfig} from './kernels/Greater';
 import {greaterEqualConfig} from './kernels/GreaterEqual';
 import {identityConfig} from './kernels/Identity';
