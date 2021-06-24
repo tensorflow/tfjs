70184a0fa56b20b4a55367f3007842700aa7058c
Raffi Zulvian Muzhaffar <60030585+raffizulvian@users.noreply.github.com>
2021-06-24 13:30:52 +0700

    fix: add missing semicolon

--- tfjs-core/src/ops/image/grayscale_to_rgb_test.ts
+++ tfjs-core/src/ops/image/grayscale_to_rgb_test.ts
@@ -32,7 +32,7 @@ describeWithFlags('grayscaleToRGB', ALL_ENVS, () => {
   });
 
   it('should throw error because of input last dim is not 1', async () => {
-    const lastDim = 2
+    const lastDim = 2;
     const grayscale = tf.tensor4d(
       [1.0, 1.0, 2.0, 2.0, 3.0, 3.0], [1, 1, 3, lastDim]
     );
@@ -40,6 +40,6 @@ describeWithFlags('grayscaleToRGB', ALL_ENVS, () => {
     expect(() => tf.image.grayscaleToRGB(grayscale)).toThrowError(
       'Error in grayscaleToRGB: last dimension of a grayscale image should ' +
       `be size 1, but had size ${lastDim}.`
-    )
+    );
   });
 });
