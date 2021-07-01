4715aaf6e5580d54f5df3f9d7271e7f49905eb9d
Xu Xing <xing.xu@intel.com>
2021-07-01 10:49:24 +0800

    [webgpu] Add WGSL support (#5252)
    This is the general framework for WGSL support, currently only unary are tested.
    To test WGSL, change WEBGPU_USE_GLSL in tfjs-backend-webgpu/src/flags_webgpu.ts to false.

--- tfjs-backend-webgpu/src/backend_webgpu.ts
+++ tfjs-backend-webgpu/src/backend_webgpu.ts
@@ -533,13 +533,19 @@ export class WebGPUBackend extends KernelBackend {
     arrays.forEach(array => {
       const arrayData = array.data;
 
-      if (array.type !== 'int32' && array.type !== 'float32') {
+      if (array.type !== 'int32' && array.type !== 'float32' &&
+          array.type !== 'uint32') {
         throw new Error(`${array.type} not supported!`);
       }
 
       if (array.type === 'int32') {
         arrayData.forEach(d => {
           uniformDataView.setInt32(dataViewIndex * BYTES_PER_ELEMENT, d, true);
+          dataViewIndex++;
+        });
+      } else if (array.type === 'uint32') {
+        arrayData.forEach(d => {
+          uniformDataView.setUint32(dataViewIndex * BYTES_PER_ELEMENT, d, true);
           dataViewIndex++;
         });
       } else {
@@ -591,7 +597,7 @@ export class WebGPUBackend extends KernelBackend {
       padding = Math.ceil(currentOffset / baseAlignment) * baseAlignment -
           currentOffset;
       for (let p = 0; p < padding; ++p) {
-        dimUniformsData.push({type: 'int32', data: [0]});
+        dimUniformsData.push({type: d.type, data: [0]});
         dataViewIndex++;
       }
       dimUniformsData.push({type: d.type, data: d.data});
@@ -680,13 +686,17 @@ export class WebGPUBackend extends KernelBackend {
     let uniformsWithType: Array<{type: string; data: number[];}> =
         [{type: 'float32', data: [NaN]}];
     const bufferShapes = inputs.concat(output).map(d => d.shape);
+    let uniformsType = 'int32';
+    if (program.useWgsl) {
+      uniformsType = 'uint32';
+    }
     bufferShapes.map(d => {
-      uniformsWithType.push({type: 'int32', data: d});
+      uniformsWithType.push({type: uniformsType, data: d});
     });
     const strides = util.computeStrides(output.shape);
-    uniformsWithType.push({type: 'int32', data: strides});
+    uniformsWithType.push({type: uniformsType, data: strides});
     if (program.size != null) {
-      uniformsWithType.push({type: 'int32', data: [program.size]});
+      uniformsWithType.push({type: uniformsType, data: [program.size]});
     }
     if (programUniforms) {
       uniformsWithType = [...uniformsWithType, ...programUniforms];
--- tfjs-backend-webgpu/src/kernels/webgpu_program.ts
+++ tfjs-backend-webgpu/src/kernels/webgpu_program.ts
@@ -15,10 +15,11 @@
  * =============================================================================
  */
 
-import {DataType, Rank, ShapeMap, TensorInfo} from '@tensorflow/tfjs-core';
+import {DataType, env, Rank, ShapeMap, TensorInfo} from '@tensorflow/tfjs-core';
 import {Glslang} from '@webgpu/glslang/dist/web-devel/glslang.onefile';
 
 import * as shader_preprocessor from '../shader_preprocessor';
+import * as shader_preprocessor_wgsl from '../shader_preprocessor_wgsl';
 
 export interface WebGPUProgram {
   // The unique key to distinguish different shader source code.
@@ -31,6 +32,7 @@ export interface WebGPUProgram {
   dispatch: [number, number, number];
   variableNames: string[];
   uniforms?: string;
+  uniformsWgsl?: string;
   // Size of register cache in one dimension (assumes square cache).
   // Each thread writes to workPerThread * workPerThread locations in the output
   // buffer.
@@ -39,6 +41,7 @@ export interface WebGPUProgram {
   // in a thread group. Individual dimensions determines thread layout within
   // the group.
   workGroupSize?: [number, number, number];
+  useWgsl?: boolean;
   isVec4?: boolean;
   // size is used for bounds checking.
   size?: number;
@@ -70,27 +73,43 @@ export const compileProgram =
      isFromPixel = false): GPUComputePipeline => {
       const outputData = {dtype: output.dtype, shape: output.shape};
 
-      const source = shader_preprocessor.makeShader(
-          inputsData, outputData, program, isFromPixel);
-      const result = glslang.compileGLSLZeroCopy(source, 'compute', false);
-      if (result.data.length === 0) {
-        throw new Error('Shader compilation failed');
+      let source;
+      let module;
+      if (program.useWgsl) {
+        source = shader_preprocessor_wgsl.makeShader(
+            inputsData, outputData, program, isFromPixel);
+        module = device.createShaderModule({code: source});
+      } else {
+        source = shader_preprocessor.makeShader(
+            inputsData, outputData, program, isFromPixel);
+        const result = glslang.compileGLSLZeroCopy(source, 'compute', false);
+        if (result.data.length === 0) {
+          throw new Error('Shader compilation failed');
+        }
+        result.free();
+        module = device.createShaderModule({code: result.data});
       }
-
-      const module = device.createShaderModule({code: result.data});
       const pipeline = device.createComputePipeline(
           {layout: pipelineLayout, compute: {module, entryPoint: 'main'}});
 
-      result.free();
       return pipeline;
     };
 
 export function makeShaderKey<R extends Rank>(
     program: WebGPUProgram, shapes: Array<ShapeMap[R]>, types: string[],
     broadcastDimsKey = '', inputShapesEqualsOutShape = ''): string {
+  let useWgslKey = '';
+  if (program.useWgsl) {
+    useWgslKey = '_1';
+  }
   const key = (program.workGroupSize ? program.workGroupSize.join(',') : '') +
       shapes.map(shape => shape.length).join(',') + types.join(',') +
       program.variableNames.join(',') + broadcastDimsKey +
-      inputShapesEqualsOutShape + program.shaderKey;
+      inputShapesEqualsOutShape + program.shaderKey + useWgslKey;
   return key;
+}
+
+// This is global flag, but program may ignore this flag.
+export function getUseWgsl () {
+  return !env().getBool('WEBGPU_USE_GLSL');
 }
