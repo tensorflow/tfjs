f8499070ee4435310b21a4bbd07db3f3c68f2dfc
Kartikey <rawatkari554@gmail.com>
2021-07-09 15:53:39 +0530

    Updated Mod Kernel

--- tfjs-backend-wasm/src/cc/kernels/Mod.cc
+++ tfjs-backend-wasm/src/cc/kernels/Mod.cc
@@ -1,4 +1,4 @@
-/* Copyright 2021 Google LLC. All Rights Reserved.
+/* Copyright 2019 Google LLC. All Rights Reserved.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,19 +15,13 @@
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
-#include <xnnpack.h>
 
+#include <cmath>
 #include <cstddef>
 
+#include "tfjs-backend-wasm/src/cc/backend.h"
 #include "tfjs-backend-wasm/src/cc/binary.h"
 #include "tfjs-backend-wasm/src/cc/util.h"
-
-namespace {
-template <class T>
-inline T mod(T a, T b) {
-  return a % b;
-}
-}  // namespace
 
 namespace tfjs {
 namespace wasm {
@@ -37,24 +31,24 @@ extern "C" {
 #ifdef __EMSCRIPTEN__
 EMSCRIPTEN_KEEPALIVE
 #endif
-void Mod(const size_t a_id, const size_t* a_shape_ptr, const size_t a_shape_len,
-         const size_t b_id, const size_t* b_shape_ptr, const size_t b_shape_len,
-         const DType dtype, const size_t out_id) {
+void Mod(const size_t a_id, const size_t* a_shape_ptr,
+              const size_t a_shape_len, const size_t b_id,
+              const size_t* b_shape_ptr, const size_t b_shape_len,
+              const DType dtype, const size_t out_id) {
   switch (dtype) {
     case DType::float32:
-      binary_xnn_f32(a_id, a_shape_ptr, a_shape_len, b_id, b_shape_ptr,
-                     b_shape_len, out_id, xnn_create_mod_nd_f32,
-                     xnn_setup_mod_nd_f32);
+      binary_f32(a_id, b_id, out_id,
+                 [](float a, float b) { return mod(a % b); });
       break;
     case DType::int32:
-      binary_i32(a_id, b_id, out_id, mod<int32_t>);
-      break;
-    case DType::boolean:
-      binary_bool(a_id, b_id, out_id, mod<bool>);
+      binary_i32(a_id, b_id, out_id, [](int a, int b) {
+        return static_cast<int32_t>(mod(static_cast<float>(a) % b));
+      });
       break;
     default:
-      util::warn("Mod for tensor ids %d and %d failed. Unknown dtype %d", a_id,
-                 b_id, dtype);
+      util::warn(
+          "Mod for tensor ids %d and %d failed. Unsupported dtype %d",
+          a_id, b_id, dtype);
   }
 }
 
