ef82707c0b00d28ee683250c8346a311994ce704
Kartikey <rawatkari554@gmail.com>
2021-07-12 00:23:11 +0530

    Updated the mod file

--- tfjs-backend-wasm/src/cc/kernels/Mod.cc
+++ tfjs-backend-wasm/src/cc/kernels/Mod.cc
@@ -37,12 +37,12 @@ void Mod(const size_t a_id, const size_t* a_shape_ptr, const size_t a_shape_len,
   switch (dtype) {
     case DType::float32:
       binary_f32(a_id, b_id, out_id,
-                 [](float a, float b) { return (a - floor(a / b) * b); });
+                 [](float a, float b) { return mod(a % b); });
       break;
     case DType::int32:
       binary_i32(a_id, b_id, out_id, [](int a, int b) {
-        return static_cast<int32_t>(static_cast<float>(a) -
-                                    floor(static_cast<float>(a) / b) * b);
+        return static_cast<int32_t>(
+                                    mod(static_cast<float>(a) % b);
       });
       break;
     default:
