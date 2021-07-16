6d3a00d0edc97861ee99530f069d4541df050136
Xinghua Cao <xinghua.cao@intel.com>
2021-07-16 16:57:24 +0800

    webgpu: optimize matmul with small output size
    Current matmul algorithm dispatches logical threads based on
    output size, if output size is small and inner dimension size
    is large, there are few valid threads and every thread should
    handle large loads, so the performance is poor.
    This patch dispatches logical threads based on output size and
    inner dimension size, increases the logical threads, makes load
    balancing.
    This patches also introduces two shared memory buffers, some
    logical threads could handle arithmetic operations and others
    handle IO operations between barrier api, makes ALUs and load/store
    units work simultaneously, could improves the performance.

--- tfjs-backend-webgpu/src/kernels/BatchMatMul_impl.ts
+++ tfjs-backend-webgpu/src/kernels/BatchMatMul_impl.ts
@@ -19,6 +19,7 @@ import {backend_util, env, TensorInfo, util} from '@tensorflow/tfjs-core';
 
 import {WebGPUBackend} from '../backend_webgpu';
 
+import {MatMulSmallOutputSizeWidthProgram} from './matmul_small_output_size_webgpu';
 import {MatMulPackedVec4Program} from './matmul_packed_vec4_webgpu';
 import {MatMulPackedProgram} from './matmul_packed_webgpu';
 import {reshape} from './Reshape';
@@ -97,9 +98,14 @@ export function batchMatMulImpl({
 
   const useVec4 = a.shape[2] % 4 === 0 && b.shape[2] % 4 === 0 && !transposeA &&
       !transposeB && outerShapeB >= 32;
-  let program: MatMulPackedProgram|MatMulPackedVec4Program;
+  let program: MatMulPackedProgram|MatMulPackedVec4Program
+      |MatMulSmallOutputSizeWidthProgram;
   let dimensions = null;
-  if (useVec4) {
+  if ((a.shape[1] <= 16 || b.shape[2] <= 16) && !transposeA && !transposeB) {
+    program = new MatMulSmallOutputSizeWidthProgram(a3dShape, b3dShape,
+        [batchDim, outerShapeA, outerShapeB], bias, activation,
+        preluActivationWeights);
+  } else if (useVec4) {
     // TODO: Currently we need to make sure that a.shape[2] and b.shape[2]
     // are divisible by 4 since we use vec4 to get data. In future, we can
     // remove this limitation by insert 0 to pack data.
