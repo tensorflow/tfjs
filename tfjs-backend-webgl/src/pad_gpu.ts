5ac0e13102f74c21d2c267da8f2eabc8262829d4
Na Li <linazhao@google.com>
2021-07-20 16:23:27 -0700

    [webgl]Fix PadV2 with empty size.

--- tfjs-backend-webgl/src/pad_gpu.ts
+++ tfjs-backend-webgl/src/pad_gpu.ts
@@ -24,9 +24,7 @@ export class PadProgram implements GPGPUProgram {
   userCode: string;
   customUniforms = [{name: 'value', type: 'float' as UniformType}];
 
-  constructor(
-      xShape: number[], paddings: Array<[number, number]>,
-      constantValue: number) {
+  constructor(xShape: number[], paddings: Array<[number, number]>) {
     this.outputShape = paddings.map(
         (p, i) => p[0] /* beforePad */ + xShape[i] + p[1] /* afterPad */);
     const rank = xShape.length;
