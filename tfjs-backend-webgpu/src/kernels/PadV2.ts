c1b42e2e567ccb959b0c573e7e594cd5b8efa24f
Jiajia Qin <jiajia.qin@intel.com>
2021-07-23 08:15:35 +0800

    [webgpu] Fix PadV2 with empty size (#5363)
    This is a follow up for #5359
    
    Meanwhile, change the work gourp size to [64, 1, 1] to keep consistent
    with other flat dispatch layout programs.
    
    Co-authored-by: Na Li <linazhao@google.com>

--- tfjs-backend-webgpu/src/kernels/PadV2.ts
+++ tfjs-backend-webgpu/src/kernels/PadV2.ts
@@ -20,6 +20,7 @@ import {KernelConfig, KernelFunc, PadV2, PadV2Attrs, PadV2Inputs, TensorInfo, ut
 import {WebGPUBackend} from '../backend_webgpu';
 import {identity} from './Identity';
 import {PadProgram} from './pad_webgpu';
+import {fill} from './Fill';
 
 export const padV2 =
     (args: {inputs: PadV2Inputs,
@@ -30,6 +31,17 @@ export const padV2 =
       const {paddings, constantValue} = attrs;
       if (paddings.every(p => util.arraysEqual(p, [0, 0]))) {
         return identity({inputs: {x}, backend});
+      }
+      if (util.sizeFromShape(x.shape) === 0) {
+        // Short-circuit the computation, since x doesn't have value, only
+        // the shape is used to compute output shape to pad.
+        const outputShape = paddings.map(
+            (p, i) =>
+                p[0] /* beforePad */ + x.shape[i] + p[1] /* afterPad */);
+        return fill({
+          backend,
+          attrs: {shape: outputShape, value: constantValue, dtype: x.dtype}
+        });
       }
       const uniformData = [{type: 'float32', data: [constantValue]}];
       paddings.map(p => uniformData.push({type: 'int32', data: [p[0], p[1]]}));
