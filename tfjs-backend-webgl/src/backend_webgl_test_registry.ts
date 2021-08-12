75d0738b19447aa95c90f2693cc777f85eab0b4b
Matthew Soulanille <msoulanille@google.com>
2021-08-12 13:05:05 -0700

    Disable packed kernel tests on webgl1

--- tfjs-backend-webgl/src/backend_webgl_test_registry.ts
+++ tfjs-backend-webgl/src/backend_webgl_test_registry.ts
@@ -22,6 +22,7 @@ export const WEBGL_ENVS: Constraints = {
   predicate: testEnv => testEnv.backendName === 'webgl'
 };
 export const PACKED_ENVS: Constraints = {
+  predicate: testEnv => testEnv.name !== 'webgl1',
   flags: {'WEBGL_PACK': true}
 };
 
