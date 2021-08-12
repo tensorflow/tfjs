dd75fb2e2d4e00d37a3b973ab3735d4acce21542
Matthew Soulanille <msoulanille@google.com>
2021-08-12 07:56:12 -0700

    Revert "Build tfjs-backend-webgl with Bazel (#5339)"
    This reverts commit 86ad5e0ab69952fea3e910f2bae474a80812f6e8 and resolves conflicts.

--- e2e/scripts/publish-tfjs-ci.sh
+++ e2e/scripts/publish-tfjs-ci.sh
@@ -58,8 +58,7 @@ PACKAGES=("tfjs-core" "tfjs-backend-cpu" "tfjs-backend-webgl" \
 "tfjs-node" "tfjs-node-gpu")
 
 # Packages that build with Bazel
-BAZEL_PACKAGES=("tfjs-core" "tfjs-backend-cpu" "tfjs-backend-webgl" \
-"tfjs-tflite")
+BAZEL_PACKAGES=("tfjs-core" "tfjs-backend-cpu" "tfjs-tflite")
 
 for package in "${PACKAGES[@]}"
 do
