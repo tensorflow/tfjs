64ef5a31a3410c97041ad6daa1b162ab93078073
Jiajia Qin <jiajia.qin@intel.com>
2021-08-15 20:17:15 +0800

    webgl: Fix the incorrect result for blazeface (#5483)
    This is a partial roll back to get the correct result for blazeface. It's not the final solution.
    
    BUG #5447

--- tfjs-backend-webgl/src/im2col_packed_gpu.ts
+++ tfjs-backend-webgl/src/im2col_packed_gpu.ts
@@ -31,16 +31,16 @@ export class Im2ColPackedProgram implements GPGPUProgram {
     {name: 'pad', type: 'ivec2' as const },
     {name: 'stride', type: 'ivec2' as const },
     {name: 'dilation', type: 'ivec2' as const },
-    {name: 'inChannels', type: 'int' as const },
-    {name: 'itemsPerBlockRow', type: 'int' as const },
   ];
 
   constructor(outputShape: number[], convInfo: backend_util.Conv2DInfo) {
     this.outputShape = outputShape;
     this.enableShapeUniforms = useShapeUniforms(this.outputShape.length);
-    // TODO: Investigate why the result is not correct when using outWidth as a
-    // uniform.
-    const {outWidth, dataFormat} = convInfo;
+    // TODO: Investigate why the results of mobilenetv2/blazeface are not
+    // correct when using below values as uniforms.
+    // https://github.com/tensorflow/tfjs/issues/5447
+    const {filterWidth, inChannels, outWidth, dataFormat} = convInfo;
+    const itemsPerBlockRow = inChannels * filterWidth;
     const glsl = getGlslDifferences();
     const isChannelsLast = dataFormat === 'channelsLast';
     const rowDim = isChannelsLast ? 0 : 1;
@@ -59,18 +59,18 @@ export class Im2ColPackedProgram implements GPGPUProgram {
 
           ${boundsCheckingSnippet}
             offsetY = int(blockIndex / ${outWidth}) * stride[0] - pad[1];
-            d0 = offsetY + dilation[0] * (pos / itemsPerBlockRow);
+            d0 = offsetY + dilation[0] * (pos / ${itemsPerBlockRow});
 
             if(d0 < inputShape[${rowDim}] && d0 >= 0) {
 
               offsetX = int(mod(float(blockIndex), ${outWidth}.) *
                   float(stride[1]) - float(pad[0]));
               d1 = offsetX + dilation[1] * (int(mod(float(pos),
-                  float(itemsPerBlockRow)) / float(inChannels)));
+                  float(${itemsPerBlockRow})) / ${inChannels}.));
 
               if(d1 < inputShape[${colDim}] && d1 >= 0) {
 
-                ch = int(mod(float(pos), float(inChannels)));
+                ch = int(mod(float(pos), ${inChannels}.));
 
                 if (${isChannelsLast}) {
                   innerDims = vec2(d1, ch);
