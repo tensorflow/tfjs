14d990659514a7c9f6bc099f107ce575a128db03
Xu Xing <xing.xu@intel.com>
2021-09-01 13:43:46 +0800

    [webgpu] Add conv2dBackprop WGSL support

--- tfjs-backend-webgpu/src/kernels/conv_backprop_mm_webgpu.ts
+++ tfjs-backend-webgpu/src/kernels/conv_backprop_mm_webgpu.ts
@@ -19,8 +19,8 @@ import {backend_util, util} from '@tensorflow/tfjs-core';
 
 import {computeDispatch, computeWorkGroupSizeForConv2d, computeWorkPerThreadForConv2d} from '../webgpu_util';
 
-import {makeMatMulPackedSource} from './matmul_packed_webgpu';
-import {WebGPUProgram} from './webgpu_program';
+import {makeMatMulPackedSource, makeMatMulPackedSourceWgsl} from './matmul_packed_webgpu';
+import {getUseWgsl, WebGPUProgram} from './webgpu_program';
 
 export class Conv2DDerInputMMProgram implements WebGPUProgram {
   outputShape: number[];
@@ -29,8 +29,11 @@ export class Conv2DDerInputMMProgram implements WebGPUProgram {
   dispatch: [number, number, number];
   variableNames = ['x', 'W'];
   uniforms = 'ivec2 filterDims, pads, stride; ivec4 outBackprop;';
+  uniformsWgsl =
+      'filterDims : vec2<u32>; pads : vec2<u32>; stride : vec2<u32>; outBackprop : vec4<u32>; dimAOuter : u32; dimBOuter : u32; dimInner : u32;';
   workGroupSize: [number, number, number];
   elementsPerThread: [number, number, number];
+  useWgsl: boolean;
 
   constructor(convInfo: backend_util.Conv2DInfo) {
     this.outputShape = convInfo.inShape;
@@ -49,6 +52,7 @@ export class Conv2DDerInputMMProgram implements WebGPUProgram {
         this.elementsPerThread);
 
     this.shaderKey = `conv2DDerInputMM_${this.elementsPerThread}`;
+    this.useWgsl = getUseWgsl();
   }
 
   getUserCode(): string {
@@ -124,6 +128,72 @@ export class Conv2DDerInputMMProgram implements WebGPUProgram {
 
       mm_matMul(dimAOuter, dimInner, dimBOuter);
     }
+  `;
+    return userCode;
+  }
+
+  getUserCodeWgsl(): string {
+    const matMulSource =
+        makeMatMulPackedSourceWgsl(this.elementsPerThread, this.workGroupSize);
+
+    const readASnippet = `
+    let outRow = row / uniforms.outShape[2];
+    let outCol = row % uniforms.outShape[2];
+
+    let WRow = col / (uniforms.filterDims[1] * uniforms.outBackprop[3]);
+    let WCol = (col / uniforms.outBackprop[3]) % uniforms.filterDims[1];
+    let xR = f32(outRow - uniforms.pads[0] + WRow) / f32(uniforms.stride[0]);
+    let xC = f32(outCol - uniforms.pads[1] + WCol) / f32(uniforms.stride[1]);
+    if (xR < 0.0 || xR >= f32(uniforms.outBackprop[1]) || fract(xR) > 0.0) {
+      return 0.0;
+    }
+    if (xC < 0.0 || xC >= f32(uniforms.outBackprop[2]) || fract(xC) > 0.0) {
+      return 0.0;
+    }
+    let coord = vec4<u32>(
+        batch,
+        u32(xR),
+        u32(xC),
+        col % uniforms.outBackprop[3]);
+    return x.numbers[getFlatIndex4D(coord, uniforms.xShape)];`;
+
+    const sampleA = `if (row < uniforms.dimAOuter && col < uniforms.dimInner) {
+      ${readASnippet}
+    }
+    return 0.0;`;
+
+    const userCode = `
+    fn mm_readA(row : u32, col : u32, globalId : vec3<u32>) -> f32 {
+      var batch = globalId.z;
+      ${sampleA}
+    }
+
+    fn mm_readB(row : u32, col : u32, globalId : vec3<u32>) -> f32 {
+      let coordX = i32(uniforms.filterDims.x - 1u -
+          row / (uniforms.filterDims[1] * uniforms.outBackprop[3]));
+      let coordY = i32(uniforms.filterDims.y - 1u -
+          (row / uniforms.outBackprop[3]) % uniforms.filterDims[1]);
+      if (row < uniforms.dimInner && col < uniforms.dimBOuter &&
+          coordX >= 0 && coordY >= 0) {
+        let coord = vec4<u32>(u32(coordX), u32(coordY), col,
+            row % uniforms.outBackprop[3]);
+        return W.numbers[getFlatIndex4D(coord, uniforms.wShape)];
+      }
+      return 0.0;
+    }
+
+    fn mm_write(row : u32, col : u32, valueInput : f32, globalId : vec3<u32>) {
+      var batch = globalId.z;
+      var value = valueInput;
+      let outCoord = vec4<u32>(
+          batch,
+          row / uniforms.outShape[2],
+          row % uniforms.outShape[2],
+          col);
+      result.numbers[getFlatIndex4D(outCoord, uniforms.outShape)] = value;
+    }
+
+    ${matMulSource}
   `;
     return userCode;
   }
