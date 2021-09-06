76deb9170bc6fb6bac6e320e8fece92f89b9dbd4
Yan <shaobo.yan@intel.com>
2021-09-06 14:46:24 +0800

    Hide importExternalTexture behind flag
    importExternalTexture API will be in heavy development after WebGPU OT.
    It might break tfjs webgpu backend by accident.
    
    Hide this API behind flag until the API is in stable mode.
    
    This changes also fix FromPixels + HTMLVideoElement errors by drawing
    HTMLVideoElement to canvas first and then using copyExternalImageToTexture

--- tfjs-backend-webgpu/src/flags_webgpu.ts
+++ tfjs-backend-webgpu/src/flags_webgpu.ts
@@ -78,4 +78,4 @@ ENV.registerFlag('WEBGPU_USE_PROFILE_TOOL', () => false);
 /**
  * Whether to use import API.
  */
-ENV.registerFlag('WEBGPU_USE_IMPORT', () => true);
+ENV.registerFlag('WEBGPU_USE_IMPORT', () => false);
