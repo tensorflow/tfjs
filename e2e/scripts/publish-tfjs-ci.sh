7424d22915177b4ca7d8dae8d0fe520ff586c1d5
Matthew Soulanille <msoulanille@google.com>
2021-09-22 10:03:32 -0700

    [webgl] Build tfjs-backend-webgl with Bazel (#5562)
    PROCESS

--- e2e/scripts/publish-tfjs-ci.sh
+++ e2e/scripts/publish-tfjs-ci.sh
@@ -58,7 +58,8 @@ PACKAGES=("tfjs-core" "tfjs-backend-cpu" "tfjs-backend-webgl" \
 "tfjs-node" "tfjs-node-gpu")
 
 # Packages that build with Bazel
-BAZEL_PACKAGES=("tfjs-core" "tfjs-backend-cpu" "tfjs-tflite" "tfjs-converter")
+BAZEL_PACKAGES=("tfjs-core" "tfjs-backend-cpu" "tfjs-tflite" "tfjs-converter"
+"tfjs-backend-webgl")
 
 for package in "${PACKAGES[@]}"
 do
