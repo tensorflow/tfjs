34fd9947829bb846efb2a11a9eea5592f7f13098
Na Li <linazhao@google.com>
2021-11-15 10:12:29 -0800

    Fix test.

--- tfjs-backend-webgl/src/backend_webgl_test.ts
+++ tfjs-backend-webgl/src/backend_webgl_test.ts
@@ -751,8 +751,8 @@ describeWithFlags('WebGL backend has sync init', WEBGL_ENVS, () => {
   });
 });
 
-describeWithFlags('Parallel compilation', WEBGL_ENVS, async () => {
-  it('does not have memory leak.', async () => {
+describeWithFlags('Parallel compilation', WEBGL_ENVS, () => {
+  it('does not have memory leak.', () => {
     const savedWebGLCPUForward = tf.env().get('WEBGL_CPU_FORWARD');
     tf.env().set('WEBGL_CPU_FORWARD', false);
 
@@ -766,7 +766,7 @@ describeWithFlags('Parallel compilation', WEBGL_ENVS, async () => {
     const a0 = tf.tensor1d([1, 1, 1]);
     const b0 = tf.tensor1d([1, 1, 1]);
     const c0 = tf.add(a0, b0);
-    const data = await c0.data();
+    const data = c0.dataSync();
     const numOfBinaryCacheNoParallelCompillation =
         Object.keys(getBinaryCache(tf.ENV.getNumber('WEBGL_VERSION'))).length;
     console.log(data);
@@ -788,18 +788,18 @@ describeWithFlags('Parallel compilation', WEBGL_ENVS, async () => {
 
     // Pre-compile round.
     const c1 = tf.add(a1, b1);
-    await webGLBackend.checkCompileCompletionAsync();
+    webGLBackend.checkCompileCompletion();
     webGLBackend.getUniformLocations();
 
     // Warm-up upload and download round.
     tf.engine().state.compileOnly = false;
     const c2 = tf.add(a1, b1);
-    await c2.data();
+    c2.dataSync();
 
     // Actual inference.
     const c3 = tf.add(a1, b1);
 
-    console.log(await c3.data());
+    console.log(c3.dataSync());
     // expectArraysEqual(, [2, 2, 2]);
 
     tf.dispose([a1, b1, c1, c2, c3]);
