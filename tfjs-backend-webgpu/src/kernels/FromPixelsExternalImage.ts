13e0d46b4ff5d155576025f425ecbd84142745b9
Qin Jiajia <jiajia.qin@intel.com>
2021-11-16 12:59:30 +0800

    Save the promise of pipeline

--- tfjs-backend-webgpu/src/kernels/FromPixelsExternalImage.ts
+++ tfjs-backend-webgpu/src/kernels/FromPixelsExternalImage.ts
@@ -56,7 +56,7 @@ export function fromPixelsExternalImage(args: {
         backend.device, program, layout.pipelineLayout, [], output, true);
   });
 
-  program.setPipeline(pipeline);
+  program.setPipeline(pipeline as GPUComputePipeline);
 
   if (!useImport) {
     backend.queue.copyExternalImageToTexture(
