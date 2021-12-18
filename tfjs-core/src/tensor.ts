c9ff8929126dc87de462c1ca79364dc0bab48dd9
Na Li <linazhao@google.com>
2021-12-17 16:46:28 -0800

    fix test.

--- tfjs-core/src/tensor.ts
+++ tfjs-core/src/tensor.ts
@@ -164,6 +164,7 @@ export type DataToGPUOptions = DataToGPUWebGLOption;
 
 export interface GPUResource {
   dataId: DataId;
+  dtype: DataType;
   texture?: WebGLTexture;
   texShape?: [number, number];
 }
