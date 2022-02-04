fdc9475575a7aee5ea270976e4cdd0af04e56bb6
Na Li <linazhao@google.com>
2022-02-03 19:25:28 -0800

    fix lint.

--- tfjs-backend-webgl/src/canvas_util_test.ts
+++ tfjs-backend-webgl/src/canvas_util_test.ts
@@ -18,7 +18,7 @@ import * as tf from '@tensorflow/tfjs-core';
 // tslint:disable-next-line: no-imports-from-dist
 import {BROWSER_ENVS, describeWithFlags} from '@tensorflow/tfjs-core/dist/jasmine_util';
 
-import {clearWebGLContext, getWebGLContext} from './canvas_util';
+import {getWebGLContext} from './canvas_util';
 
 describeWithFlags('canvas_util', BROWSER_ENVS, () => {
   it('Returns a valid canvas', () => {
