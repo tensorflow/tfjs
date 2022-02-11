0e74d5856d6b7a3eda303a4a1e9753f65ed6f695
JuFeng Zhang <zjffun@gmail.com>
2022-02-11 20:45:05 +0800

    fix: the ssd demo of converter

--- tfjs-converter/demo/ssd/classes.js
+++ tfjs-converter/demo/ssd/classes.js
@@ -1,400 +1,419 @@
-export const CLASSES = [{
-  name: "/m/01g317",
-  id: 1,
-  display_name: "person",
-},
-{
-  name: "/m/0199g",
-  id: 2,
-  display_name: "bicycle",
-},
-{
-  name: "/m/0k4j",
-  id: 3,
-  display_name: "car",
-},
-{
-  name: "/m/04_sv",
-  id: 4,
-  display_name: "motorcycle",
-},
-{
-  name: "/m/05czz6l",
-  id: 5,
-  display_name: "airplane",
-},
-{
-  name: "/m/01bjv",
-  id: 6,
-  display_name: "bus",
-},
-{
-  name: "/m/07jdr",
-  id: 7,
-  display_name: "train",
-},
-{
-  name: "/m/07r04",
-  id: 8,
-  display_name: "truck",
-},
-{
-  name: "/m/019jd",
-  id: 9,
-  display_name: "boat",
-},
-{
-  name: "/m/015qff",
-  id: 10,
-  display_name: "traffic light",
-},
-{
-  name: "/m/01pns0",
-  id: 11,
-  display_name: "fire hydrant",
-},
-{
-  name: "/m/02pv19",
-  id: 13,
-  display_name: "stop sign",
-},
-{
-  name: "/m/015qbp",
-  id: 14,
-  display_name: "parking meter",
-},
-{
-  name: "/m/0cvnqh",
-  id: 15,
-  display_name: "bench",
-},
-{
-  name: "/m/015p6",
-  id: 16,
-  display_name: "bird",
-},
-{
-  name: "/m/01yrx",
-  id: 17,
-  display_name: "cat",
-},
-{
-  name: "/m/0bt9lr",
-  id: 18,
-  display_name: "dog",
-},
-{
-  name: "/m/03k3r",
-  id: 19,
-  display_name: "horse",
-},
-{
-  name: "/m/07bgp",
-  id: 20,
-  display_name: "sheep",
-},
-{
-  name: "/m/01xq0k1",
-  id: 21,
-  display_name: "cow",
-},
-{
-  name: "/m/0bwd_0j",
-  id: 22,
-  display_name: "elephant",
-},
-{
-  name: "/m/01dws",
-  id: 23,
-  display_name: "bear",
-},
-{
-  name: "/m/0898b",
-  id: 24,
-  display_name: "zebra",
-},
-{
-  name: "/m/03bk1",
-  id: 25,
-  display_name: "giraffe",
-},
-{
-  name: "/m/01940j",
-  id: 27,
-  display_name: "backpack",
-},
-{
-  name: "/m/0hnnb",
-  id: 28,
-  display_name: "umbrella",
-},
-{
-  name: "/m/080hkjn",
-  id: 31,
-  display_name: "handbag",
-},
-{
-  name: "/m/01rkbr",
-  id: 32,
-  display_name: "tie",
-},
-{
-  name: "/m/01s55n",
-  id: 33,
-  display_name: "suitcase",
-},
-{
-  name: "/m/02wmf",
-  id: 34,
-  display_name: "frisbee",
-},
-{
-  name: "/m/071p9",
-  id: 35,
-  display_name: "skis",
-},
-{
-  name: "/m/06__v",
-  id: 36,
-  display_name: "snowboard",
-},
-{
-  name: "/m/018xm",
-  id: 37,
-  display_name: "sports ball",
-},
-{
-  name: "/m/02zt3",
-  id: 38,
-  display_name: "kite",
-},
-{
-  name: "/m/03g8mr",
-  id: 39,
-  display_name: "baseball bat",
-},
-{
-  name: "/m/03grzl",
-  id: 40,
-  display_name: "baseball glove",
-},
-{
-  name: "/m/06_fw",
-  id: 41,
-  display_name: "skateboard",
-},
-{
-  name: "/m/019w40",
-  id: 42,
-  display_name: "surfboard",
-},
-{
-  name: "/m/0dv9c",
-  id: 43,
-  display_name: "tennis racket",
-},
-{
-  name: "/m/04dr76w",
-  id: 44,
-  display_name: "bottle",
-},
-{
-  name: "/m/09tvcd",
-  id: 46,
-  display_name: "wine glass",
-},
-{
-  name: "/m/08gqpm",
-  id: 47,
-  display_name: "cup",
-},
-{
-  name: "/m/0dt3t",
-  id: 48,
-  display_name: "fork",
-},
-{
-  name: "/m/04ctx",
-  id: 49,
-  display_name: "knife",
-},
-{
-  name: "/m/0cmx8",
-  id: 50,
-  display_name: "spoon",
-},
-{
-  name: "/m/04kkgm",
-  id: 51,
-  display_name: "bowl",
-},
-{
-  name: "/m/09qck",
-  id: 52,
-  display_name: "banana",
-},
-{
-  name: "/m/014j1m",
-  id: 53,
-  display_name: "apple",
-},
-{
-  name: "/m/0l515",
-  id: 54,
-  display_name: "sandwich",
-},
-{
-  name: "/m/0cyhj_",
-  id: 55,
-  display_name: "orange",
-},
-{
-  name: "/m/0hkxq",
-  id: 56,
-  display_name: "broccoli",
-},
-{
-  name: "/m/0fj52s",
-  id: 57,
-  display_name: "carrot",
-},
-{
-  name: "/m/01b9xk",
-  id: 58,
-  display_name: "hot dog",
-},
-{
-  name: "/m/0663v",
-  id: 59,
-  display_name: "pizza",
-},
-{
-  name: "/m/0jy4k",
-  id: 60,
-  display_name: "donut",
-},
-{
-  name: "/m/0fszt",
-  id: 61,
-  display_name: "cake",
-},
-{
-  name: "/m/01mzpv",
-  id: 62,
-  display_name: "chair",
-},
-{
-  name: "/m/02crq1",
-  id: 63,
-  display_name: "couch",
-},
-{
-  name: "/m/03fp41",
-  id: 64,
-  display_name: "potted plant",
-},
-{
-  name: "/m/03ssj5",
-  id: 65,
-  display_name: "bed",
-},
-{
-  name: "/m/04bcr3",
-  id: 67,
-  display_name: "dining table",
-},
-{
-  name: "/m/09g1w",
-  id: 70,
-  display_name: "toilet",
-},
-{
-  name: "/m/07c52",
-  id: 72,
-  display_name: "tv",
-},
-{
-  name: "/m/01c648",
-  id: 73,
-  display_name: "laptop",
-},
-{
-  name: "/m/020lf",
-  id: 74,
-  display_name: "mouse",
-},
-{
-  name: "/m/0qjjc",
-  id: 75,
-  display_name: "remote",
-},
-{
-  name: "/m/01m2v",
-  id: 76,
-  display_name: "keyboard",
-},
-{
-  name: "/m/050k8",
-  id: 77,
-  display_name: "cell phone",
-},
-{
-  name: "/m/0fx9l",
-  id: 78,
-  display_name: "microwave",
-},
-{
-  name: "/m/029bxz",
-  id: 79,
-  display_name: "oven",
-},
-{
-  name: "/m/01k6s3",
-  id: 80,
-  display_name: "toaster",
-},
-{
-  name: "/m/0130jx",
-  id: 81,
-  display_name: "sink",
-},
-{
-  name: "/m/040b_t",
-  id: 82,
-  display_name: "refrigerator",
-},
-{
-  name: "/m/0bt_c3",
-  id: 84,
-  display_name: "book",
-},
-{
-  name: "/m/01x3z",
-  id: 85,
-  display_name: "clock",
-},
-{
-  name: "/m/02s195",
-  id: 86,
-  display_name: "vase",
-},
-{
-  name: "/m/01lsmm",
-  id: 87,
-  display_name: "scissors",
-},
-{
-  name: "/m/0kmg4",
-  id: 88,
-  display_name: "teddy bear",
-},
-{
-  name: "/m/03wvsk",
-  id: 89,
-  display_name: "hair drier",
-},
-{
-  name: "/m/012xff",
-  id: 90,
-  display_name: "toothbrush",
-}];
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+export const CLASSES = {
+  1: {
+    name: '/m/01g317',
+    id: 1,
+    displayName: 'person',
+  },
+  2: {
+    name: '/m/0199g',
+    id: 2,
+    displayName: 'bicycle',
+  },
+  3: {
+    name: '/m/0k4j',
+    id: 3,
+    displayName: 'car',
+  },
+  4: {
+    name: '/m/04_sv',
+    id: 4,
+    displayName: 'motorcycle',
+  },
+  5: {
+    name: '/m/05czz6l',
+    id: 5,
+    displayName: 'airplane',
+  },
+  6: {
+    name: '/m/01bjv',
+    id: 6,
+    displayName: 'bus',
+  },
+  7: {
+    name: '/m/07jdr',
+    id: 7,
+    displayName: 'train',
+  },
+  8: {
+    name: '/m/07r04',
+    id: 8,
+    displayName: 'truck',
+  },
+  9: {
+    name: '/m/019jd',
+    id: 9,
+    displayName: 'boat',
+  },
+  10: {
+    name: '/m/015qff',
+    id: 10,
+    displayName: 'traffic light',
+  },
+  11: {
+    name: '/m/01pns0',
+    id: 11,
+    displayName: 'fire hydrant',
+  },
+  13: {
+    name: '/m/02pv19',
+    id: 13,
+    displayName: 'stop sign',
+  },
+  14: {
+    name: '/m/015qbp',
+    id: 14,
+    displayName: 'parking meter',
+  },
+  15: {
+    name: '/m/0cvnqh',
+    id: 15,
+    displayName: 'bench',
+  },
+  16: {
+    name: '/m/015p6',
+    id: 16,
+    displayName: 'bird',
+  },
+  17: {
+    name: '/m/01yrx',
+    id: 17,
+    displayName: 'cat',
+  },
+  18: {
+    name: '/m/0bt9lr',
+    id: 18,
+    displayName: 'dog',
+  },
+  19: {
+    name: '/m/03k3r',
+    id: 19,
+    displayName: 'horse',
+  },
+  20: {
+    name: '/m/07bgp',
+    id: 20,
+    displayName: 'sheep',
+  },
+  21: {
+    name: '/m/01xq0k1',
+    id: 21,
+    displayName: 'cow',
+  },
+  22: {
+    name: '/m/0bwd_0j',
+    id: 22,
+    displayName: 'elephant',
+  },
+  23: {
+    name: '/m/01dws',
+    id: 23,
+    displayName: 'bear',
+  },
+  24: {
+    name: '/m/0898b',
+    id: 24,
+    displayName: 'zebra',
+  },
+  25: {
+    name: '/m/03bk1',
+    id: 25,
+    displayName: 'giraffe',
+  },
+  27: {
+    name: '/m/01940j',
+    id: 27,
+    displayName: 'backpack',
+  },
+  28: {
+    name: '/m/0hnnb',
+    id: 28,
+    displayName: 'umbrella',
+  },
+  31: {
+    name: '/m/080hkjn',
+    id: 31,
+    displayName: 'handbag',
+  },
+  32: {
+    name: '/m/01rkbr',
+    id: 32,
+    displayName: 'tie',
+  },
+  33: {
+    name: '/m/01s55n',
+    id: 33,
+    displayName: 'suitcase',
+  },
+  34: {
+    name: '/m/02wmf',
+    id: 34,
+    displayName: 'frisbee',
+  },
+  35: {
+    name: '/m/071p9',
+    id: 35,
+    displayName: 'skis',
+  },
+  36: {
+    name: '/m/06__v',
+    id: 36,
+    displayName: 'snowboard',
+  },
+  37: {
+    name: '/m/018xm',
+    id: 37,
+    displayName: 'sports ball',
+  },
+  38: {
+    name: '/m/02zt3',
+    id: 38,
+    displayName: 'kite',
+  },
+  39: {
+    name: '/m/03g8mr',
+    id: 39,
+    displayName: 'baseball bat',
+  },
+  40: {
+    name: '/m/03grzl',
+    id: 40,
+    displayName: 'baseball glove',
+  },
+  41: {
+    name: '/m/06_fw',
+    id: 41,
+    displayName: 'skateboard',
+  },
+  42: {
+    name: '/m/019w40',
+    id: 42,
+    displayName: 'surfboard',
+  },
+  43: {
+    name: '/m/0dv9c',
+    id: 43,
+    displayName: 'tennis racket',
+  },
+  44: {
+    name: '/m/04dr76w',
+    id: 44,
+    displayName: 'bottle',
+  },
+  46: {
+    name: '/m/09tvcd',
+    id: 46,
+    displayName: 'wine glass',
+  },
+  47: {
+    name: '/m/08gqpm',
+    id: 47,
+    displayName: 'cup',
+  },
+  48: {
+    name: '/m/0dt3t',
+    id: 48,
+    displayName: 'fork',
+  },
+  49: {
+    name: '/m/04ctx',
+    id: 49,
+    displayName: 'knife',
+  },
+  50: {
+    name: '/m/0cmx8',
+    id: 50,
+    displayName: 'spoon',
+  },
+  51: {
+    name: '/m/04kkgm',
+    id: 51,
+    displayName: 'bowl',
+  },
+  52: {
+    name: '/m/09qck',
+    id: 52,
+    displayName: 'banana',
+  },
+  53: {
+    name: '/m/014j1m',
+    id: 53,
+    displayName: 'apple',
+  },
+  54: {
+    name: '/m/0l515',
+    id: 54,
+    displayName: 'sandwich',
+  },
+  55: {
+    name: '/m/0cyhj_',
+    id: 55,
+    displayName: 'orange',
+  },
+  56: {
+    name: '/m/0hkxq',
+    id: 56,
+    displayName: 'broccoli',
+  },
+  57: {
+    name: '/m/0fj52s',
+    id: 57,
+    displayName: 'carrot',
+  },
+  58: {
+    name: '/m/01b9xk',
+    id: 58,
+    displayName: 'hot dog',
+  },
+  59: {
+    name: '/m/0663v',
+    id: 59,
+    displayName: 'pizza',
+  },
+  60: {
+    name: '/m/0jy4k',
+    id: 60,
+    displayName: 'donut',
+  },
+  61: {
+    name: '/m/0fszt',
+    id: 61,
+    displayName: 'cake',
+  },
+  62: {
+    name: '/m/01mzpv',
+    id: 62,
+    displayName: 'chair',
+  },
+  63: {
+    name: '/m/02crq1',
+    id: 63,
+    displayName: 'couch',
+  },
+  64: {
+    name: '/m/03fp41',
+    id: 64,
+    displayName: 'potted plant',
+  },
+  65: {
+    name: '/m/03ssj5',
+    id: 65,
+    displayName: 'bed',
+  },
+  67: {
+    name: '/m/04bcr3',
+    id: 67,
+    displayName: 'dining table',
+  },
+  70: {
+    name: '/m/09g1w',
+    id: 70,
+    displayName: 'toilet',
+  },
+  72: {
+    name: '/m/07c52',
+    id: 72,
+    displayName: 'tv',
+  },
+  73: {
+    name: '/m/01c648',
+    id: 73,
+    displayName: 'laptop',
+  },
+  74: {
+    name: '/m/020lf',
+    id: 74,
+    displayName: 'mouse',
+  },
+  75: {
+    name: '/m/0qjjc',
+    id: 75,
+    displayName: 'remote',
+  },
+  76: {
+    name: '/m/01m2v',
+    id: 76,
+    displayName: 'keyboard',
+  },
+  77: {
+    name: '/m/050k8',
+    id: 77,
+    displayName: 'cell phone',
+  },
+  78: {
+    name: '/m/0fx9l',
+    id: 78,
+    displayName: 'microwave',
+  },
+  79: {
+    name: '/m/029bxz',
+    id: 79,
+    displayName: 'oven',
+  },
+  80: {
+    name: '/m/01k6s3',
+    id: 80,
+    displayName: 'toaster',
+  },
+  81: {
+    name: '/m/0130jx',
+    id: 81,
+    displayName: 'sink',
+  },
+  82: {
+    name: '/m/040b_t',
+    id: 82,
+    displayName: 'refrigerator',
+  },
+  84: {
+    name: '/m/0bt_c3',
+    id: 84,
+    displayName: 'book',
+  },
+  85: {
+    name: '/m/01x3z',
+    id: 85,
+    displayName: 'clock',
+  },
+  86: {
+    name: '/m/02s195',
+    id: 86,
+    displayName: 'vase',
+  },
+  87: {
+    name: '/m/01lsmm',
+    id: 87,
+    displayName: 'scissors',
+  },
+  88: {
+    name: '/m/0kmg4',
+    id: 88,
+    displayName: 'teddy bear',
+  },
+  89: {
+    name: '/m/03wvsk',
+    id: 89,
+    displayName: 'hair drier',
+  },
+  90: {
+    name: '/m/012xff',
+    id: 90,
+    displayName: 'toothbrush',
+  },
+};
--- tfjs-converter/demo/ssd/index.js
+++ tfjs-converter/demo/ssd/index.js
@@ -16,18 +16,17 @@
  */
 import 'babel-polyfill';
 import * as tf from '@tensorflow/tfjs';
-import {CLASSES} from './classes';
+import { CLASSES } from './classes';
 import imageURL from './image1.jpg';
 import image2URL from './image2.jpg';
 
 const GOOGLE_CLOUD_STORAGE_DIR =
-    'https://storage.googleapis.com/tfjs-models/savedmodel/';
-const MODEL_URL =
-    GOOGLE_CLOUD_STORAGE_DIR + 'coco-ssd-mobilenet_v1/model.json';
+  'https://storage.googleapis.com/tfjs-models/savedmodel/';
+const MODEL_URL = GOOGLE_CLOUD_STORAGE_DIR + 'ssd_mobilenet_v1/model.json';
 
 let modelPromise;
 
-window.onload = () => modelPromise = tf.loadGraphModel(MODEL_URL);
+window.onload = () => (modelPromise = tf.loadGraphModel(MODEL_URL));
 
 const button = document.getElementById('toggle');
 button.onclick = () => {
@@ -37,44 +36,150 @@ button.onclick = () => {
 const image = document.getElementById('image');
 image.src = imageURL;
 
+const buildDetectedObjects = (
+  width,
+  height,
+  boxes,
+  scores,
+  indexes,
+  classes
+) => {
+  const count = indexes.length;
+  const objects = [];
+  for (let i = 0; i < count; i++) {
+    const bbox = [];
+    for (let j = 0; j < 4; j++) {
+      bbox[j] = boxes[indexes[i] * 4 + j];
+    }
+    const minY = bbox[0] * height;
+    const minX = bbox[1] * width;
+    const maxY = bbox[2] * height;
+    const maxX = bbox[3] * width;
+    bbox[0] = minX;
+    bbox[1] = minY;
+    bbox[2] = maxX - minX;
+    bbox[3] = maxY - minY;
+    objects.push({
+      bbox: bbox,
+      class: CLASSES[classes[indexes[i]] + 1].displayName,
+      score: scores[indexes[i]],
+    });
+  }
+  return objects;
+};
+
+const calculateMaxScores = (scores, numBoxes, numClasses) => {
+  const maxes = [];
+  const classes = [];
+  for (let i = 0; i < numBoxes; i++) {
+    let max = Number.MIN_VALUE;
+    let index = -1;
+    for (let j = 0; j < numClasses; j++) {
+      if (scores[i * numClasses + j] > max) {
+        max = scores[i * numClasses + j];
+        index = j;
+      }
+    }
+    maxes[i] = max;
+    classes[i] = index;
+  }
+  return [maxes, classes];
+};
+
+/**
+ * Infers through the model.
+ * @param model The converted model.
+ * @param img The image to classify. Can be a tensor or a DOM element image,
+ * video, or canvas.
+ * @param maxNumBoxes The maximum number of bounding boxes of detected
+ * objects. There can be multiple objects of the same class, but at different
+ * locations. Defaults to 20.
+ * @param minScore The minimum score of the returned bounding boxes
+ * of detected objects. Value between 0 and 1. Defaults to 0.5.
+ */
+const infer = async (model, img, maxNumBoxes, minScore) => {
+  const batched = tf.tidy(() => {
+    if (!(img instanceof tf.Tensor)) {
+      img = tf.browser.fromPixels(img);
+    }
+    // Reshape to a single-element batch so we can pass it to executeAsync.
+    return tf.expandDims(img);
+  });
+  const height = batched.shape[1];
+  const width = batched.shape[2];
+  // model returns two tensors:
+  // 1. box classification score with shape of [1, 1917, 90]
+  // 2. box location with shape of [1, 1917, 1, 4]
+  // where 1917 is the number of box detectors, 90 is the number of classes.
+  // and 4 is the four coordinates of the box.
+  const result = await model.executeAsync(batched);
+  const scores = result[0].dataSync();
+  const boxes = result[1].dataSync();
+  // clean the webgl tensors
+  batched.dispose();
+  tf.dispose(result);
+  const [maxScores, classes] = calculateMaxScores(
+    scores,
+    result[0].shape[1],
+    result[0].shape[2]
+  );
+  const prevBackend = tf.getBackend();
+  // run post process in cpu
+  if (tf.getBackend() === 'webgl') {
+    tf.setBackend('cpu');
+  }
+  const indexTensor = tf.tidy(() => {
+    const boxes2 = tf.tensor2d(boxes, [result[1].shape[1], result[1].shape[3]]);
+    return tf.image.nonMaxSuppression(
+      boxes2,
+      maxScores,
+      maxNumBoxes,
+      minScore,
+      minScore
+    );
+  });
+  const indexes = indexTensor.dataSync();
+  indexTensor.dispose();
+  // restore previous backend
+  if (prevBackend !== tf.getBackend()) {
+    tf.setBackend(prevBackend);
+  }
+  return buildDetectedObjects(
+    width,
+    height,
+    boxes,
+    maxScores,
+    indexes,
+    classes
+  );
+};
+
 const runButton = document.getElementById('run');
 runButton.onclick = async () => {
   const model = await modelPromise;
-  const pixels = tf.browser.fromPixels(image);
   console.log('model loaded');
-  console.time('predict1');
-  const res1 = await model.executeAsync(pixels.reshape([1, ...pixels.shape]));
-  res1.map(t => t.dataSync());
-  console.timeEnd('predict1');
-  console.time('predict2');
-  const res2 = await model.executeAsync(pixels.reshape([1, ...pixels.shape]));
-  const count = res2[3].dataSync()[0];
-  const boxes = res2[0].dataSync();
-  const scores = res2[1].dataSync();
-  const classes = res2[2].dataSync();
-  console.timeEnd('predict2');
 
+  console.time('predict1');
+  const result = await infer(model, image, 20, 0.5);
+  console.timeEnd('predict1');
 
   const c = document.getElementById('canvas');
   const context = c.getContext('2d');
   context.drawImage(image, 0, 0);
   context.font = '10px Arial';
 
-  console.log('number of detections: ', count);
-  for (let i = 0; i < count; i++) {
-    const min_y = boxes[i * 4] * 399;
-    const min_x = boxes[i * 4 + 1] * 600;
-    const max_y = boxes[i * 4 + 2] * 399;
-    const max_x = boxes[i * 4 + 3] * 600;
-
+  console.log('number of detections: ', result.length);
+  for (let i = 0; i < result.length; i++) {
     context.beginPath();
-    context.rect(min_x, min_y, max_x - min_x, max_y - min_y);
+    context.rect(...result[i].bbox);
     context.lineWidth = 1;
-    context.strokeStyle = 'black';
+    context.strokeStyle = 'green';
+    context.fillStyle = 'green';
     context.stroke();
     context.fillText(
-        scores[i].toFixed(3) + ' ' + CLASSES.find(label => label.id === classes[i]).display_name,
-        min_x, min_y - 5);
+      result[i].score.toFixed(3) + ' ' + result[i].class,
+      result[i].bbox[0],
+      result[i].bbox[1] > 10 ? result[i].bbox[1] - 5 : 10
+    );
   }
 };
-
