6b18f456e44a3c8492eb4efb54d97bcae1e7c333
Matthew Soulanille <msoulanille@google.com>
2022-02-16 11:16:18 -0800

    Fix Apple silicon WASM development (#6098)
    Upgrade xnnpack and emsdk to support WASM development on Apple silicon.

--- tfjs-backend-wasm/src/setup_test.ts
+++ tfjs-backend-wasm/src/setup_test.ts
@@ -230,8 +230,9 @@ const TEST_FILTERS: TestFilter[] = [
     include: 'clip',
     excludes: [
       'gradient',
-      'propagates NaNs'  // clip delegates to XNNPACK which does not make
+      'propagates NaNs', // clip delegates to XNNPACK which does not make
                          // guarantees about behavior of nans.
+      'basic vec4'       // basic vec4 also includes nans.
     ]
   },
   {include: 'addN'},
