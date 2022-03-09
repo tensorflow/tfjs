c012c6d1b656856e43ce7b1ca7f651e299d2bdf6
Matthew Soulanille <msoulanille@google.com>
2022-03-09 14:06:46 -0800

    Update monorepo to example_release.

--- tfjs-backend-webgpu/src/version.ts
+++ tfjs-backend-webgpu/src/version.ts
@@ -1,5 +1,5 @@
 /** @license See the LICENSE file. */
 
 // This code is auto-generated, do not modify this file!
-const version = 'example_release';
+const version = 'example_webgpu_release';
 export {version};
