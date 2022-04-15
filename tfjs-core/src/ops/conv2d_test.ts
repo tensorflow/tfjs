8b613076d5b95603a74a14bfbea1aad951daac95
Qin Jiajia <jiajia.qin@intel.com>
2022-04-15 14:25:18 +0800

    Add nchw test for 1x1 conv2d

--- tfjs-core/src/ops/conv2d_test.ts
+++ tfjs-core/src/ops/conv2d_test.ts
@@ -228,6 +228,37 @@ describeWithFlags('conv2d', ALL_ENVS, () => {
     expectArraysClose(resultData, [20, 26, 13, 12]);
   });
 
+  it('x=[4,2,2] f=[1,1,4,4] s=1 d=1 p=same NCHW', async () => {
+    // Skip webgl backend due to bug
+    // https://github.com/tensorflow/tfjs/issues/6308.
+    // Skip tensorflow backend due to NCHW not supported.
+    if (tf.getBackend() === 'webgl' || tf.getBackend() === 'tensorflow') {
+      return;
+    }
+    const inputDepth = 4;
+    const inputShape: [number, number, number] = [inputDepth, 2, 2];
+    const outputDepth = 4;
+    const fSize = 1;
+    const pad = 'same';
+    const stride = 1;
+    const dataFormat = 'NCHW';
+    const dilation = 1;
+
+    const x = tf.tensor3d(
+        [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4], inputShape);
+    const w = tf.tensor4d(
+        [3, 3, 3, 3, 1, 1, 1, 1, 5, 5, 5, 5, 0, 0, 0, 0],
+        [fSize, fSize, inputDepth, outputDepth]);
+
+    const result = tf.conv2d(x, w, stride, pad, dataFormat, dilation);
+
+    const resultData = await result.data();
+    expect(result.shape).toEqual([4, 2, 2]);
+    expectArraysClose(
+        resultData,
+        [9, 18, 27, 36, 9, 18, 27, 36, 9, 18, 27, 36, 9, 18, 27, 36]);
+  });
+
   it('x=[4,2,2] f=[2,2,4,4] s=1 d=1 p=same NCHW', async () => {
     // Skip webgl backend due to bug
     // https://github.com/tensorflow/tfjs/issues/6308.
