1f484d38788b12cdd1c6c4a0277f194d1e7554bb
Linchenn <linchan@google.com>
2022-04-27 11:01:42 -0700

    Add batch test for the 1x1 conv2d branch

--- tfjs-core/src/ops/conv2d_test.ts
+++ tfjs-core/src/ops/conv2d_test.ts
@@ -285,6 +285,40 @@ describeWithFlags('conv2d', ALL_ENVS, () => {
         [9, 18, 27, 36, 9, 18, 27, 36, 9, 18, 27, 36, 9, 18, 27, 36]);
   });
 
+  it('x=[2,4,2,2] f=[1,1,4,4] s=1 d=1 p=same NCHW', async () => {
+    // Skip tensorflow backend due to NCHW not supported.
+    if (tf.getBackend() === 'tensorflow') {
+      return;
+    }
+    const inputDepth = 4;
+    const inputShape: [number, number, number, number] = [2, inputDepth, 2, 2];
+    const outputDepth = 4;
+    const fSize = 1;
+    const pad = 'same';
+    const stride = 1;
+    const dataFormat = 'NCHW';
+    const dilation = 1;
+
+    const x = tf.tensor4d(
+        [
+          1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4,
+          1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4
+        ],
+        inputShape);
+    const w = tf.tensor4d(
+        [3, 3, 3, 3, 1, 1, 1, 1, 5, 5, 5, 5, 0, 0, 0, 0],
+        [fSize, fSize, inputDepth, outputDepth]);
+
+    const result = tf.conv2d(x, w, stride, pad, dataFormat, dilation);
+
+    const resultData = await result.data();
+    expect(result.shape).toEqual([2, 4, 2, 2]);
+    expectArraysClose(resultData, [
+      9, 18, 27, 36, 9, 18, 27, 36, 9, 18, 27, 36, 9, 18, 27, 36,
+      9, 18, 27, 36, 9, 18, 27, 36, 9, 18, 27, 36, 9, 18, 27, 36
+    ]);
+  });
+
   it('x=[4,2,2] f=[2,2,4,4] s=1 d=1 p=same NCHW', async () => {
     // Skip tensorflow backend due to NCHW not supported.
     if (tf.getBackend() === 'tensorflow') {
