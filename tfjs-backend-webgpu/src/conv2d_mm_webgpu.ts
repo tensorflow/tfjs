fdcaa942dbf12daf57ad73e8e6ea6d03929f7de4
Qin Jiajia <jiajia.qin@intel.com>
2022-05-16 10:27:56 +0800

    nits

--- tfjs-backend-webgpu/src/conv2d_mm_webgpu.ts
+++ tfjs-backend-webgpu/src/conv2d_mm_webgpu.ts
@@ -20,7 +20,7 @@ import {backend_util, util} from '@tensorflow/tfjs-core';
 import {mapActivationToShaderProgram} from './activation_util';
 import {makeMatMulPackedSource} from './matmul_packed_webgpu';
 import {WebGPUProgram} from './webgpu_program';
-import {computeDispatch, tilesFitEvenlyIntoShape} from './webgpu_util';
+import {computeDispatch, computeWorkGroupSizeForConv2d, computeWorkPerThreadForConv2d, tilesFitEvenlyIntoShape} from './webgpu_util';
 
 export class Conv2DMMProgram implements WebGPUProgram {
   outputShape: number[];
@@ -48,8 +48,10 @@ export class Conv2DMMProgram implements WebGPUProgram {
     this.isChannelsLast = convInfo.dataFormat === 'channelsLast';
     this.dispatchLayout = this.isChannelsLast ? {x: [3], y: [1, 2], z: [0]} :
                                                 {x: [2, 3], y: [1], z: [0]};
-    this.workGroupSize = [16, 16, 1];
-    this.elementsPerThread = [2, 2, 1];
+    this.workGroupSize =
+        computeWorkGroupSizeForConv2d(this.dispatchLayout, this.outputShape);
+    this.elementsPerThread =
+        computeWorkPerThreadForConv2d(this.dispatchLayout, this.outputShape);
 
     this.dispatch = computeDispatch(
         this.dispatchLayout, this.outputShape, this.workGroupSize,
