9ae6a2636b524690df7bb3454b15dfffb401346e
Matthew Soulanille <msoulanille@google.com>
2022-05-18 17:28:46 -0700

    [core] Add a synchronous I/O handler (#6417)
    Add IOHandlerSync, which has the same interface as IOHandler but returns values instead of promises. Add fromMemorySync, the synchronous version of fromMemory, and withSaveHandlerSync, the synchronous version of withSavehandler.

--- tfjs-core/src/io/io.ts
+++ tfjs-core/src/io/io.ts
@@ -23,7 +23,7 @@ import './local_storage';
 import {browserFiles} from './browser_files';
 import {browserHTTPRequest, http, isHTTPScheme} from './http';
 import {concatenateArrayBuffers, decodeWeights, encodeWeights, getModelArtifactsForJSON, getModelArtifactsInfoForJSON} from './io_utils';
-import {fromMemory, withSaveHandler} from './passthrough';
+import {fromMemory, fromMemorySync, withSaveHandler, withSaveHandlerSync} from './passthrough';
 import {getLoadHandlers, getSaveHandlers, registerLoadRouter, registerSaveRouter} from './router_registry';
 import {IOHandler, LoadHandler, LoadOptions, ModelArtifacts, ModelArtifactsInfo, ModelJSON, ModelStoreManager, OnProgressCallback, RequestDetails, SaveConfig, SaveHandler, SaveResult, TrainingConfig, WeightGroup, WeightsManifestConfig, WeightsManifestEntry} from './types';
 import {loadWeights, weightsLoaderFactory} from './weights_loader';
@@ -36,6 +36,7 @@ export {
   decodeWeights,
   encodeWeights,
   fromMemory,
+  fromMemorySync,
   getLoadHandlers,
   getModelArtifactsForJSON,
   getModelArtifactsInfoForJSON,
@@ -62,5 +63,6 @@ export {
   weightsLoaderFactory,
   WeightsManifestConfig,
   WeightsManifestEntry,
-  withSaveHandler
+  withSaveHandler,
+  withSaveHandlerSync,
 };
