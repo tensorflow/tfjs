857f942459c8b8cadf53cf123129bb507d5076cc
Xinghua Cao <xinghua.cao@intel.com>
2022-05-26 10:21:36 +0800

    add flatDispatch to shader key

--- tfjs-backend-webgpu/src/webgpu_program.ts
+++ tfjs-backend-webgpu/src/webgpu_program.ts
@@ -90,10 +90,11 @@ export const compileProgram =
 export function makeShaderKey<R extends Rank>(
     program: WebGPUProgram, shapes: Array<ShapeMap[R]>, types: string[] = [],
     broadcastDimsKey = '', inputShapesEqualsOutShape = ''): string {
+  const flatDispatch = program.dispatch[1] === 1 && program.dispatch[2] === 1;
   const key = program.shaderKey + '_' +
       (program.workGroupSize ? program.workGroupSize.join(',') : '') +
       shapes.map(shape => shape.length).join(',') + types.join(',') +
       program.variableNames.join(',') + broadcastDimsKey +
-      inputShapesEqualsOutShape;
+      inputShapesEqualsOutShape + flatDispatch;
   return key;
 }
