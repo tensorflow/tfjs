7b0270b80d22e1441b9d8b1817c329e115a9eb75
Henry Bigelow <hrbigelow@gmail.com>
2022-06-01 14:07:19 -0700

    gatherND range check (#6464)
    BUG
    * add range checking for gatherMD webgl backend
    
    * added gathernd out of bounds tests
    
    * make all gatherNd the same square side length
    
    * add range checking for gatherMD webgl backend
    
    * added gathernd out of bounds tests
    
    * make all gatherNd the same square side length
    
    * fixing lint errors
    
    * add gatherNd scalar input throw test

--- tfjs-backend-webgl/src/webgl_ops_test.ts
+++ tfjs-backend-webgl/src/webgl_ops_test.ts
@@ -883,3 +883,65 @@ describeWithFlags('gather debug', WEBGL_ENVS, () => {
         .toThrowError(/GatherV2: the index value -1 is not in \[0, 1\]/);
   });
 });
+
+describeWithFlags('gatherNd', WEBGL_ENVS, () => {
+  it('throws for zero-rank input', async () => {
+    const x = tf.scalar(5, 'int32');
+    const ind = tf.tensor1d([5,2,1], 'int32');
+    expect(() => tf.gatherND(x, ind))
+        .toThrowError(/expects the input to be rank 1 or higher, but the rank was 0/);
+  });
+
+  it('works for out of bounds indices', async () => {
+    const x = tf.tensor1d([1, 2], 'int32');
+    const ind = tf.tensor2d([-1, 0, 1, 2], [4, 1], 'int32');
+    const g = tf.gatherND(x, ind);
+    expectArraysEqual(await g.data(), [0, 1, 2, 0]);
+  });
+
+  it('works for out of bounds indices 2d', async () => {
+    const x = tf.tensor2d([...Array(4).keys()].map(e => e + 1), 
+      [2, 2], 'int32');
+    const indices = [];
+    for (let y = -1; y !== 3; y+=1) {
+      for (let x = -1; x !== 3; x+=1) {
+        indices.push(y);
+        indices.push(x);
+      }
+    }
+    const ind = tf.tensor2d(indices, [16, 2], 'int32');
+    const g = tf.gatherND(x, ind);
+    const expected = [
+      0, 0, 0, 0,
+      0, 1, 2, 0,
+      0, 3, 4, 0,
+      0, 0, 0, 0
+    ];
+    expect(g.shape).toEqual([16]);
+    expectArraysEqual(await g.data(), expected);
+  });
+
+  it('works for out of bounds indices 3d', async () => {
+    const x = tf.tensor3d([...Array(8).keys()].map(e => e + 1), 
+      [2, 2, 2], 'int32');
+    const indices = [];
+    for (let z = -1; z !== 3; z+=1) {
+      for (let y = -1; y !== 3; y+=1) {
+        for (let x = -1; x !== 3; x+=1) {
+          indices.push(z);
+          indices.push(y);
+          indices.push(x);
+        }
+      }
+    }
+    const ind = tf.tensor2d(indices, [64, 3], 'int32');
+    const g = tf.gatherND(x, ind);
+    const expected = [
+      0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
+      0, 0, 0, 0,   0, 1, 2, 0,   0, 3, 4, 0,   0, 0, 0, 0,
+      0, 0, 0, 0,   0, 5, 6, 0,   0, 7, 8, 0,   0, 0, 0, 0,
+      0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
+    ];
+    expectArraysEqual(await g.data(), expected);
+  });
+});
