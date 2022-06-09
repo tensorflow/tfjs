418523d35ca10e26d06f7adb09a70b1e625a2251
Matthew Soulanille <msoulanille@google.com>
2022-06-09 16:29:21 -0700

    Display Jasmine test results in Karma browsers

--- tools/karma_template.conf.js
+++ tools/karma_template.conf.js
@@ -120,6 +120,12 @@ module.exports = function(config) {
   }
 
   config.set({
+    reporters: ['kjhtml'],
+    frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-jasmine-html-reporter'),
+    ],
     captureTimeout: 3e5,
     reportSlowerThan: 500,
     browserNoActivityTimeout: 3e5,
