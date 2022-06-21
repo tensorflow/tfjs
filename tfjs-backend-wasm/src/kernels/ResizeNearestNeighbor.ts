b285872db866aae8913829f218addf78acc0b687
Ricky Hartmann <hartmannr76@gmail.com>
2022-06-21 05:36:04 -0400

    Lint fix

--- tfjs-backend-wasm/src/kernels/ResizeNearestNeighbor.ts
+++ tfjs-backend-wasm/src/kernels/ResizeNearestNeighbor.ts
@@ -1,14 +1,14 @@
 /**
  * @license
  * Copyright 2019 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an 'AS IS' BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -23,11 +23,11 @@ import {
   ResizeNearestNeighborInputs,
   TensorInfo,
   util,
-} from "@tensorflow/tfjs-core";
+} from '@tensorflow/tfjs-core';
 
-import { BackendWasm } from "../backend_wasm";
+import { BackendWasm } from '../backend_wasm';
 
-import { cast } from "./Cast";
+import { cast } from './Cast';
 
 let wasmResizeNearestNeighbor: (
   xId: number,
@@ -47,16 +47,16 @@ function setup(backend: BackendWasm): void {
     ResizeNearestNeighbor,
     null /*void*/,
     [
-      "number", // xId
-      "number", // batch
-      "number", // oldHeight
-      "number", // oldWidth
-      "number", // numChannels
-      "number", // newHeight
-      "number", // newWidth
-      "number", // alignCorners
-      "number", // halfPixelCenters
-      "number", // outId
+      'number', // xId
+      'number', // batch
+      'number', // oldHeight
+      'number', // oldWidth
+      'number', // numChannels
+      'number', // newHeight
+      'number', // newWidth
+      'number', // alignCorners
+      'number', // halfPixelCenters
+      'number', // outId
     ]
   );
 }
@@ -77,17 +77,17 @@ function resizeNearestNeighbor(args: {
 
   let xData = backend.dataIdMap.get(images.dataId);
   let castedData;
-  if (xData.dtype !== "float32") {
+  if (xData.dtype !== 'float32') {
     castedData = cast({
       backend,
       inputs: { x: images },
-      attrs: { dtype: "float32" },
+      attrs: { dtype: 'float32' },
     });
     xData = backend.dataIdMap.get(castedData.dataId);
   }
   const xId = xData.id;
 
-  const out = backend.makeOutput(outShape, "float32");
+  const out = backend.makeOutput(outShape, 'float32');
   if (util.sizeFromShape(images.shape) === 0) {
     return out;
   }
@@ -115,7 +115,7 @@ function resizeNearestNeighbor(args: {
 
 export const resizeNearestNeighborConfig: KernelConfig = {
   kernelName: ResizeNearestNeighbor,
-  backendName: "wasm",
+  backendName: 'wasm',
   setupFunc: setup,
   kernelFunc: resizeNearestNeighbor as {} as KernelFunc,
 };
