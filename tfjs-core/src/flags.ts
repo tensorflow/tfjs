ea81161a777ec4aa5b8ac935bae9324ba2c98d55
Yang Gu <yang.gu@intel.com>
2022-06-30 17:22:03 +0800

    Remove fromPixelsAsync
    WebGPU no longer relies on async createImageBitmap for fromPixels, so
    the async version is no longer needed.

--- tfjs-core/src/flags.ts
+++ tfjs-core/src/flags.ts
@@ -74,9 +74,6 @@ ENV.registerFlag('IS_TEST', () => false);
 /** Whether to check computation result for errors. */
 ENV.registerFlag('CHECK_COMPUTATION_FOR_ERRORS', () => true);
 
-/** Whether the backend needs to wrap input to imageBitmap. */
-ENV.registerFlag('WRAP_TO_IMAGEBITMAP', () => false);
-
 /** Experimental flag, whether enter compile only phase. */
 ENV.registerFlag('ENGINE_COMPILE_ONLY', () => false);
 
