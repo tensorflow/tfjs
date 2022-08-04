b87bf71d2bdc357fe73328c2fe495ca2c2dc5b6c
Yang Gu <yang.gu@intel.com>
2022-08-05 07:59:10 +0800

    Move to platform_browser

--- tfjs-core/src/platforms/platform.ts
+++ tfjs-core/src/platforms/platform.ts
@@ -46,4 +46,6 @@ export interface Platform {
   encode(text: string, encoding: string): Uint8Array;
   /** Decode the provided bytes into a string using the provided encoding. */
   decode(bytes: Uint8Array, encoding: string): string;
+
+  setTimeoutCustom?(functionRef: Function, delay: number): void;
 }
