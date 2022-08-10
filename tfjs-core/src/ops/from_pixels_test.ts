4782aa836aabf9321dffc86b5dcb8c352cfcaa41
Jiajie Hu <jiajie.hu@intel.com>
2022-08-10 16:48:11 +0800

    what about this?

--- tfjs-core/src/ops/from_pixels_test.ts
+++ tfjs-core/src/ops/from_pixels_test.ts
@@ -222,14 +222,14 @@ describeWithFlags('fromPixels', BROWSER_ENVS, () => {
     video.appendChild(source);
     document.body.appendChild(video);
 
-    //// On mobile safari the ready state is ready immediately.
-    //if (video.readyState < 2) {
-    //  await new Promise(resolve => {
-    //    video.addEventListener('loadeddata', () => resolve(video));
-    //  });
-    //}
-
     await video.play();
+
+    // On mobile safari the ready state is ready immediately.
+    if (video.readyState < 2) {
+      await new Promise(resolve => {
+        video.addEventListener('loadeddata', () => resolve(video));
+      });
+    }
 
     expect(true).toBeTrue();
 
