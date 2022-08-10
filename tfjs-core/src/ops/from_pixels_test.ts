5db859c2b821c3518c4370f35a56af86a42cc4fe
Jiajie Hu <jiajie.hu@intel.com>
2022-08-10 13:36:44 +0800

    Hang???

--- tfjs-core/src/ops/from_pixels_test.ts
+++ tfjs-core/src/ops/from_pixels_test.ts
@@ -221,14 +221,14 @@ describeWithFlags('fromPixels', BROWSER_ENVS, () => {
     video.appendChild(source);
     document.body.appendChild(video);
 
-    expect(true).toBeTrue();
+    // On mobile safari the ready state is ready immediately.
+    if (video.readyState < 2) {
+      await new Promise(resolve => {
+        video.addEventListener('loadeddata', () => resolve(video));
+      });
+    }
 
-    //// On mobile safari the ready state is ready immediately.
-    //if (video.readyState < 2) {
-    //  await new Promise(resolve => {
-    //    video.addEventListener('loadeddata', () => resolve(video));
-    //  });
-    //}
+    expect(true).toBeTrue();
 
     //await video.play();
 
