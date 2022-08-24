61a82763bfe4a5410b577304ed9d5d001af204ee
Ivan Panchenko <39594356+ivan-pan@users.noreply.github.com>
2022-08-24 22:55:08 +0200

    Fix mistake

--- tfjs-data/src/readers.ts
+++ tfjs-data/src/readers.ts
@@ -207,7 +207,7 @@ export function generator<T extends TensorContainer>(
 }
 
 /**
- * Create an iterator that generate `Tensor`s from webcam video stream. This API
+ * Create an iterator that generates `Tensor`s from webcam video stream. This API
  * only works in Browser environment when the device has webcam.
  *
  * Note: this code snippet only works when the device has a webcam. It will
@@ -243,7 +243,7 @@ export async function webcam(
 }
 
 /**
- * Create an iterator that generate frequency-domain spectrogram `Tensor`s from
+ * Create an iterator that generates frequency-domain spectrogram `Tensor`s from
  * microphone audio stream with browser's native FFT. This API only works in
  * browser environment when the device has microphone.
  *
