559f0787e94b9bc9ab7a669512cccdb1e17e1056
Qin Jiajia <jiajia.qin@intel.com>
2022-08-26 16:46:38 +0800

    Use <=16 instead of < 32

--- tfjs-backend-webgpu/src/kernels/Conv2DBackpropInput.ts
+++ tfjs-backend-webgpu/src/kernels/Conv2DBackpropInput.ts
@@ -54,17 +54,17 @@ export function conv2DBackpropInput(args: {
     },
   ];
   let program: Conv2DDerInputProgram|Conv2DDerInputMMProgram;
-  const dimBOuter = convInfo.inChannels;
   const dimInner =
       convInfo.filterHeight * convInfo.filterWidth * convInfo.outChannels;
   if (env().getBool('WEBGPU_USE_NAIVE_CONV2D_TRANSPOSE') ||
-      dimInner <= 64 && dimBOuter < 32) {
+      dimInner <= 64 && convInfo.inChannels <= 16) {
     // When dimInner and dimBOuter are small,
     // Conv2DDerInputProgram is much faster than Conv2DDerInputMMProgram.
     program = new Conv2DDerInputProgram(convInfo);
   } else {
     program = new Conv2DDerInputMMProgram(convInfo);
     const dimAOuter = convInfo.inHeight * convInfo.inWidth;
+    const dimBOuter = convInfo.inChannels;
     dimensions.push(
         {type: 'uint32', data: [dimAOuter]},
         {type: 'uint32', data: [dimBOuter]},
