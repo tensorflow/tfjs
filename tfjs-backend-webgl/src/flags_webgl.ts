402f76e733c879774cae1396ae79b931ca1a8859
Qin Jiajia <jiajia.qin@intel.com>
2022-09-19 15:26:12 +0800

    Add flag WEBGL_ISNAN_CUSTOM

--- tfjs-backend-webgl/src/flags_webgl.ts
+++ tfjs-backend-webgl/src/flags_webgl.ts
@@ -268,3 +268,9 @@ ENV.registerFlag('WEBGL_MAX_SIZE_FOR_NARROW_TEXTURE', () => Infinity);
  * problem: https://github.com/tensorflow/tfjs/issues/6775.
  */
 ENV.registerFlag('WEBGL_AUTO_SQUARIFY_NARROW_TEXTURE_SHAPE', () => false);
+
+/**
+ * Whether to use the customed isnan. It's only useful for webgl2 since webgl1
+ * doens't have the builtin isnan.
+ */
+ENV.registerFlag('WEBGL_ISNAN_CUSTOM', () => true);
