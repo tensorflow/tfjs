51d13727a2a1406a8a2b7dce5d3a45d5e6412954
Qin Jiajia <jiajia.qin@intel.com>
2022-09-21 12:41:22 +0800

    Use builtin isnan by default

--- tfjs-backend-webgl/src/flags_webgl.ts
+++ tfjs-backend-webgl/src/flags_webgl.ts
@@ -270,7 +270,7 @@ ENV.registerFlag('WEBGL_MAX_SIZE_FOR_NARROW_TEXTURE', () => Infinity);
 ENV.registerFlag('WEBGL_AUTO_SQUARIFY_NARROW_TEXTURE_SHAPE', () => false);
 
 /**
- * Whether to use the customed isnan. It's only useful for webgl2 since webgl1
- * doens't have the builtin isnan.
+ * Whether to use the customized isnan. It's only useful for webgl2 since webgl1
+ * doesn't have the builtin isnan.
  */
-ENV.registerFlag('WEBGL_ISNAN_CUSTOM', () => true);
+ENV.registerFlag('WEBGL_ISNAN_CUSTOM', () => false);
