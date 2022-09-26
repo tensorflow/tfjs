bdc7a7267477427b84f6e1579f8b8625c76144ed
David Don Young Kim <koyykdy@gmail.com>
2022-09-26 15:19:04 -0700

    cleaning up commit diffs

--- tfjs-layers/src/layers/preprocessing/image_resizing.ts
+++ tfjs-layers/src/layers/preprocessing/image_resizing.ts
@@ -68,8 +68,7 @@ export class Resizing extends Layer {
 
   computeOutputShape(inputShape: Shape|Shape[]): Shape|Shape[] {
     inputShape = getExactlyOneShape(inputShape);
-    // inputShape.slice(inputShape.length - 1)[0];
-    const numChannels = inputShape.slice()[2];
+    const numChannels = inputShape[2];
     const outputShape = [];
     outputShape.push(this.height);
     outputShape.push(this.width);
