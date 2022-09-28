e1438e54965f32ae59331b02eb979121c04e3ee5
Ahmed Sabie <ahmedsabie@google.com>
2022-09-28 17:40:27 -0400

    Fix test

--- tfjs-converter/python/tensorflowjs/converters/tf_saved_model_conversion_v2_test.py
+++ tfjs-converter/python/tensorflowjs/converters/tf_saved_model_conversion_v2_test.py
@@ -498,8 +498,8 @@ class ConvertTest(tf.test.TestCase):
         'paths': ['group1-shard1of1.bin'],
         'weights': [
             {'name': 'unknown_0', 'shape': [], 'dtype': 'int32'},
-            {'name': '4589', 'shape': [2], 'dtype': 'string'},
-            {'name': '4591', 'shape': [2], 'dtype': 'int32'}
+            {'name': '4609', 'shape': [2], 'dtype': 'string'},
+            {'name': '4611', 'shape': [2], 'dtype': 'int32'}
         ]}]
 
     tfjs_path = os.path.join(self._tmp_dir, SAVED_MODEL_DIR, 'js')
