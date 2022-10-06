7bcabbb02492acaa6b99eb676140bf2ce3b1a086
AdamLang96 <45542095+AdamLang96@users.noreply.github.com>
2022-10-06 16:10:40 -0700

    Update center_crop.ts

--- tfjs-layers/src/layers/preprocessing/center_crop.ts
+++ tfjs-layers/src/layers/preprocessing/center_crop.ts
@@ -5,7 +5,7 @@
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
  * https://opensource.org/licenses/MIT.
- * ============================================================================= 
+ * =============================================================================
  */
 
 import {serialization,DataType,unstack,stack,tensor,Tensor,Tensor1D,Tensor2D, Tensor3D, Tensor4D, tidy, range} from '@tensorflow/tfjs-core';
