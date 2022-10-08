ff74b1aa68e656a62c46b0c8f40aa1b338617ab9
Xu Xing <xing.xu@intel.com>
2022-10-08 08:44:55 +0800

    Fix comments

--- e2e/benchmarks/local-benchmark/dump.js
+++ e2e/benchmarks/local-benchmark/dump.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2022 Google LLC. All Rights Reserved.
+ * Copyright 2022 Google LLC.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,20 +15,24 @@
  * =============================================================================
  */
 
-function compareData(data1, data2, level = 0) {
-  if (level == 0) {
-    let notMatch = false;
+const DUMP_LEVEL = {
+  CLOSE: 0,
+  ANY: 1,
+};
+
+function compareData(data1, data2, level = DUMP_LEVEL.CLOSE) {
+  if (level == DUMP_LEVEL.CLOSE) {
+    let match = true;
     try {
       expectObjectsClose(data1, data2);
     } catch (e) {
-      notMatch = true;
+      match = false;
     }
-    return notMatch;
-  } else if (level == 1) {
-    return JSON.stringify(data1) !== JSON.stringify(data2);
-  } else if (level == 2) {
-    return true;
+    return match;
+  } else if (level == DUMP_LEVEL.ANY) {
+    return JSON.stringify(data1) === JSON.stringify(data2);
   }
+  throw new Error(`Dump level ${level} is not supported!`);
 }
 
 function getGraphModel(model, benchmark) {
@@ -57,13 +61,11 @@ async function getIntermediateTensorInfo(tensorsMap) {
   const keysOfTensors = Object.keys(tensorsMap);
   for (let i = 0; i < keysOfTensors.length; i++) {
     const key = keysOfTensors[i];
-    const dataArray = [];
     jsonObject[key] = [];
     for (let j = 0; j < tensorsMap[key].length; j++) {
       const data = await (tensorsMap[key][j]).data();
-      dataArray.push(data);
       jsonObject[key].push({
-        value: dataArray,
+        value: data,
         shape: tensorsMap[key][j].shape,
         dtype: tensorsMap[key][j].dtype
       });
@@ -72,15 +74,15 @@ async function getIntermediateTensorInfo(tensorsMap) {
   return jsonObject;
 }
 
-async function saveObjectsToFile(
-    jsonObjects, backends, level, dumpCount, prefix) {
+async function saveObjectsToFile(jsonObjects, dumpCount, prefix) {
   let newPrefix = '';
   if (prefix !== '') {
     newPrefix = `${prefix.replace(/\//g, '-')}_`;
   }
-  if (((level < 2) && dumpCount) || (level === 2)) {
-    for (let i = 0; i < jsonObjects.length; i++) {
-      const object = jsonObjects[i];
+  const backends = Object.keys(jsonObjects);
+  if (dumpCount) {
+    for (let i = 0; i < backends.length; i++) {
+      const object = jsonObjects[backends[i]];
       const fileName = `${newPrefix}${backends[i]}.json`;
       const a = document.createElement('a');
       const file =
@@ -88,23 +90,58 @@ async function saveObjectsToFile(
       a.href = URL.createObjectURL(file);
       a.download = fileName;
       a.click();
-      await sleep(150);
       // This log informs tools file has been saved.
       console.log(fileName);
     }
   }
 }
 
+async function convertTensorToData(tensor, needInfo = false) {
+  const data = await tensor.data();
+  const info = {value: data, shape: tensor.shape, dtype: tensor.dtype};
+  tensor.dispose();
+  if (needInfo) {
+    return info;
+  }
+  return data;
+}
+
+async function getPredictionData(output, needInfo = false) {
+  if (output instanceof Promise) {
+    output = await output;
+  }
+
+  if (output instanceof tf.Tensor) {
+    output = [await convertTensorToData(output, needInfo)];
+  } else if (Array.isArray(output)) {
+    for (let i = 0; i < output.length; i++) {
+      if (output[i] instanceof tf.Tensor) {
+        output[i] = await convertTensorToData(output[i], needInfo);
+      }
+    }
+    return output;
+  } else if (output != null && typeof output === 'object') {
+    for (const property in output) {
+      if (output[property] instanceof tf.Tensor) {
+        output[property] =
+            await convertTensorToData(output[property], needInfo);
+      }
+    }
+  }
+  return output;
+}
+
+
 /**
  * Create a NamedTensorMap from an output node name.
- * @param outputNodeName output node name.
+ * @param outputNodeName Output node name.
  * @param modelJson The parsed model.json.
  * @param dumpedJson The dumped tensor infomation (including shape, dtype,
  *     value).
  *
  * @returns A NamedTensorMap.
  */
-async function createTensorMap(outputNodeName, modelJson, dumpedJson) {
+async function createNamedTensorMap(outputNodeName, modelJson, dumpedJson) {
   const modelNodes = modelJson['modelTopology']['node'];
   let inputs = [];
   for (let i = 0; i < modelNodes.length; i++) {
@@ -129,106 +166,120 @@ async function createTensorMap(outputNodeName, modelJson, dumpedJson) {
     }
     const tensorInfo = dumpedJson[key][0];
     const tensor = tf.tensor(
-        Object.values(tensorInfo.value[0]), tensorInfo.shape, tensorInfo.dtype);
+        Object.values(tensorInfo.value), tensorInfo.shape, tensorInfo.dtype);
     tensorMap[key] = tensor;
   }
 
   return tensorMap;
 }
 
+async function predictAndGetData(
+    predict, model, inferenceInput, benchmark, enableDump) {
+  const prediction = await predict(model, inferenceInput);
+  const graphModel = getGraphModel(model, benchmark);
+  let intermediateData = {};
+  enableDump = enableDump && !!graphModel;
+  if (enableDump) {
+    intermediateData =
+        await getIntermediateTensorInfo(graphModel.getIntermediateTensors());
+  }
+  const predictionData = await getPredictionData(prediction);
+  if (enableDump) {
+    graphModel.disposeIntermediateTensors();
+  }
+  return {data: predictionData, intermediateData};
+}
+
 async function predictOp(
     model, modelJson, dumpedJson, outputNodeName, backend) {
   await tf.setBackend(backend);
   const tensorMap =
-      await createTensorMap(outputNodeName, modelJson, dumpedJson);
+      await createNamedTensorMap(outputNodeName, modelJson, dumpedJson);
   if (tensorMap == null) {
     return null;
   }
   let prediction;
+  let savedKeepIntermediateTensors;
+  try {
+    savedKeepIntermediateTensors =
+        tf.env().getBool('KEEP_INTERMEDIATE_TENSORS');
+    tf.env().set('KEEP_INTERMEDIATE_TENSORS', false);
+  } catch (e) {
+    console.warn(e.message);
+  }
   try {
     // TODO(#6861): Support tensor with type conversion.
     prediction = await model.executeAsync(tensorMap, outputNodeName);
   } catch (e) {
+    tf.env().set('KEEP_INTERMEDIATE_TENSORS', savedKeepIntermediateTensors);
     console.warn(e.message);
     return null;
   }
 
-  let intermediateData =
-      await getIntermediateTensorInfo(model.getIntermediateTensors());
-  await getPredictionData(prediction);
-  model.disposeIntermediateTensors();
-  return intermediateData;
+  const predictObject = await getPredictionData(prediction, true);
+  tf.env().set('KEEP_INTERMEDIATE_TENSORS', savedKeepIntermediateTensors);
+  return predictObject;
 }
 
-async function dumpOp(model, dumpedJson, backends, outputNodeName, index) {
-  let objects1 = {};
-  let objects2 = {};
+/**
+ * Dump a single op specified by outputNodeName.
+ * @param model The loaded model.
+ * @param reference The reference result.
+ * @param backend The predict backend.
+ * @param outputNodeName Output node name.
+ * @param index Used as timestamp.
+ */
+async function dumpOp(model, reference, backend, outputNodeName, index) {
   const modelJson = model.artifacts;
-  const referenceObject = await predictOp(
-      model, modelJson, dumpedJson, outputNodeName, backends[1]);
-
-  const predictObject = await predictOp(
-      model, modelJson, dumpedJson, outputNodeName, backends[0]);
-  if (predictObject && referenceObject) {
-    objects1 = {index, ...predictObject[`${outputNodeName}`]};
-    objects2 = {index, ...referenceObject[`${outputNodeName}`]};
+  const predictObject =
+      await predictOp(model, modelJson, reference, outputNodeName, backend);
+  if (predictObject) {
+    return [{index, ...predictObject}, {index, ...reference[outputNodeName]}];
   }
-  return [objects1, objects2];
+  return [null, null];
 }
-
 
 /**
  * Dump the predict results of two backends and save diffs to files.
  * @param model The loaded model.
- * @param jsonObjects The predict results from backends.
+ * @param input The predict results from backends.
  * @param backends [predict backend, reference backend].
- * @param benchmark Used for generating dump file name.
- * @param level 0, dump close diffs. 1, dump any diffs. 2, dump all tensors.
+ * @param benchmark Used for getting graph model.
+ * @param prefix Used for generating dump file name.
+ * @param level 0, dump close diffs. 1, dump any diffs.
  * @param length Used for controlling how many tensors will be dumped. -1 dump
  *     all.
  */
 async function dump(
-    model, jsonObjects, backends, benchmark = '', level = 0, length = 1) {
-  const jsonObject1 = jsonObjects[0];
-  const jsonObject2 = jsonObjects[1];
+    model, input, benchmark = '', prefix = '', level = DUMP_LEVEL.CLOSE,
+    length = 1) {
+  const backends = Object.keys(input);
+  const jsonObject1 = input[backends[0]];
+  const jsonObject2 = input[backends[1]];
   const dumpObjects1 = {};
   const dumpObjects2 = {};
-  const keys = Object.keys(jsonObjects[0]);
-  const prefix = `dump_${benchmark}_${level}`;
+  const keys = Object.keys(jsonObject1);
+  prefix = `dump_${prefix}_${level}`;
   let dumpCount = 0;
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
-    if (compareData(jsonObject1[key], jsonObject2[key], level)) {
-      if (level == 2) {
-        // Dump all tensors.
-        if (jsonObject1[key]['index']) {
-          dumpObjects1[`${key}`] = jsonObject1[key];
-          dumpObjects2[`${key}`] = jsonObject2[key];
-        } else {
-          dumpObjects1[`${key}`] = {index: i, ...jsonObject1[key]};
-          dumpObjects2[`${key}`] = {index: i, ...jsonObject2[key]};
-        }
-      } else {
-        // Dump close diff or any diff.
-        const graphModel = getGraphModel(model, benchmark);
-        const [objects1, objects2] =
-            await dumpOp(graphModel, jsonObject2, backends, key, i, level);
-        if (compareData(objects1, objects2, level)) {
-          dumpObjects1[`${key}`] = objects1;
-          dumpObjects2[`${key}`] = objects2;
-        }
+    if (!compareData(jsonObject1[key], jsonObject2[key], level)) {
+      const graphModel = getGraphModel(model, benchmark);
+      const [objects1, objects2] =
+          await dumpOp(graphModel, jsonObject2, backends[0], key, i);
+      if (objects1 && objects2 && !compareData(objects1, objects2, level)) {
+        dumpObjects1[key] = objects1;
+        dumpObjects2[key] = objects2;
+        dumpCount++;
       }
-
-      dumpCount++;
     }
     // Break when diff count equals dumpLength to avoid downloading large file.
     if (length != -1 && dumpCount == length) {
       break;
     }
   }
-
-  await saveObjectsToFile(
-      [dumpObjects1, dumpObjects2], backends, level, dumpCount, prefix);
+  const dumpData = {[backends[0]]: dumpObjects1, [backends[1]]: dumpObjects2};
+  await saveObjectsToFile(dumpData, dumpCount, prefix);
   if (dumpCount) {
     console.warn(`Total dumped ${dumpCount} item(s).`);
   }
