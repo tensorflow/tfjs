c77b38baed4236f8cd0d1ab09486f2e0ab51053d
Ping Yu <4018+pyu10055@users.noreply.github.com>
2022-11-04 14:42:39 -0700

    remove redundant shape check

--- tfjs-core/src/ops/broadcast_to.ts
+++ tfjs-core/src/ops/broadcast_to.ts
@@ -47,11 +47,8 @@ function broadcastTo_<R extends Rank>(
   let input = convertToTensor(x, 'broadcastTo', 'x');
   const xShape = input.shape;
 
-  if (shape.some(d => !(d > 0) || d % 1 !== 0)) {
-    throw new Error(`broadcastTo(): Invalid broadcast shape [${shape}].`);
-  }
-
   assertNonNegativeIntegerDimensions(shape);
+
 
   if (shape.length < input.rank) {
     throw new Error(`broadcastTo(): shape.length=${shape.length} < input.rank=${
