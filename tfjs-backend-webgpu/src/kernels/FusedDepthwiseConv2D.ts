5fa9b4c4ff32efffc28dbddf70743c11a9c49915
Qin Jiajia <jiajia.qin@intel.com>
2022-11-07 10:40:56 +0800

    webgpu: Correct depthwise conv2d vec4 condition
    We should use the ouput width/hight instead of input width/hight as the
    condition of whether going to vec4 version since the task is splitted as
    output shape.

--- tfjs-backend-webgpu/src/kernels/FusedDepthwiseConv2D.ts
+++ tfjs-backend-webgpu/src/kernels/FusedDepthwiseConv2D.ts
@@ -64,7 +64,7 @@ export function fusedDepthwiseConv2D(args: {
   ];
 
   let program: DepthwiseConv2DProgram|DepthwiseConv2DVec4Program;
-  if (convInfo.inHeight > 4 && convInfo.inWidth > 4 &&
+  if (convInfo.outHeight > 4 && convInfo.outWidth > 4 &&
       convInfo.strideWidth <= 2 &&
       convInfo.inChannels === convInfo.outChannels &&
       convInfo.dilationHeight === 1 && convInfo.dilationWidth === 1 &&
