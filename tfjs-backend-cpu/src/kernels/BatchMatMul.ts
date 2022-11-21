e6ada3add5c7e3f6b2196ff071fe1c07aca500d2
Linchenn <40653845+Linchenn@users.noreply.github.com>
2022-11-21 13:04:40 -0800

    Fix batch matching for batch mat mul (#7062)
    BUG
    
    * fix
    
    * lint
    
    * fix

--- tfjs-backend-cpu/src/kernels/BatchMatMul.ts
+++ tfjs-backend-cpu/src/kernels/BatchMatMul.ts
@@ -106,12 +106,14 @@ export function batchMatMul(args: {
               let sum = 0.0;
 
               for (let k = k0; k < kBlock; k++) {
-                const batchOffsetA = Math.min(bi, batchDimA - 1) * aBatch;
-                const batchOffsetB = Math.min(bi, batchDimB - 1) * bBatch;
+                const batchIndexA = bi % batchDimA;
+                const batchIndexB = bi % batchDimB;
                 const aVal =
-                    a3dValues[batchOffsetA + i * aOuterStep + k * aInnerStep];
+                    // tslint:disable-next-line: max-line-length
+                    a3dValues[batchIndexA * aBatch + i * aOuterStep + k * aInnerStep];
                 const bVal =
-                    b3dValues[k * bInnerStep + j * bOuterStep + batchOffsetB];
+                    // tslint:disable-next-line: max-line-length
+                    b3dValues[k * bInnerStep + j * bOuterStep + batchIndexB * bBatch];
                 sum += aVal * bVal;
               }
               resVals[bi * size + (i * rightDim + j)] += sum;
