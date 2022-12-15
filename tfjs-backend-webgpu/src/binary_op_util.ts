5622b2fffb59addca4f9778b138ba35581822c40
Jiajie Hu <jiajie.hu@intel.com>
2022-12-15 14:46:19 +0800

    [webgpu] Inline `CHECK_NAN_SNIPPET_VEC4_INNER`
    What the heck is the `_INNER` suffix? How does it differ from
    `CHECK_NAN_SNIPPET_VEC4`? Why there isn't a `CHECK_NAN_SNIPPET_INNER`?
    
    I think this just creates more readability obstacles than it solves now.

--- tfjs-backend-webgpu/src/binary_op_util.ts
+++ tfjs-backend-webgpu/src/binary_op_util.ts
@@ -45,13 +45,9 @@ const CHECK_NAN_SNIPPET = `
   if (isnan(b)) { return b; }
   `;
 
-const CHECK_NAN_SNIPPET_VEC4_INNER = `
-  resultTemp = select(resultTemp, vec4<f32>(valueForNaN), isNaN);
-  `;
-
 const CHECK_NAN_SNIPPET_VEC4 = `
   let isNaN = isnanVec4(a) | isnanVec4(b);
-  ${CHECK_NAN_SNIPPET_VEC4_INNER}
+  resultTemp = select(resultTemp, vec4<f32>(valueForNaN), isNaN);
   `;
 
 const ADD = 'return a + b;';
@@ -202,7 +198,7 @@ const POW_VEC4 = `
   }
   let isNaN = (a < vec4<f32>(0.0)) & (floor(b) < b);
   let valueForNaN = uniforms.NAN;
-  ${CHECK_NAN_SNIPPET_VEC4_INNER}
+  resultTemp = select(resultTemp, vec4<f32>(valueForNaN), isNaN);
   return resultTemp;
 `;
 
