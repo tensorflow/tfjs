b96ea612f6e0f3e5cb65230fa264cb136743844b
Ping Yu <4018+pyu10055@users.noreply.github.com>
2022-12-19 11:08:53 -0800

    added support for TensorScatterUpdate op

--- tfjs-backend-wasm/src/register_all_kernels.ts
+++ tfjs-backend-wasm/src/register_all_kernels.ts
@@ -123,6 +123,7 @@ import {subConfig} from './kernels/Sub';
 import {sumConfig} from './kernels/Sum';
 import {tanConfig} from './kernels/Tan';
 import {tanhConfig} from './kernels/Tanh';
+import {tensorScatterUpdateConfig} from './kernels/TensorScatterUpdate';
 import {tileConfig} from './kernels/Tile';
 import {topKConfig} from './kernels/TopK';
 import {transformConfig} from './kernels/Transform';
@@ -236,6 +237,7 @@ const kernelConfigs: KernelConfig[] = [
   sumConfig,
   tanConfig,
   tanhConfig,
+  tensorScatterUpdateConfig,
   tileConfig,
   topKConfig,
   transformConfig,
