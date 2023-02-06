d676ae8ada74b3fd6c027873d2ed3c9c190e3540
Ryan <ryanwallace1396@gmail.com>
2023-02-06 09:35:40 -0800

    created baseRandomLayer Abstract layer, contains random generator functins for preprocessing layers
    Co-authored-by: Silvia Kocsis <silvia.kocsis@gmail.com>
    Co-authored-by: Natalie Umanzor <umanzor2949@gmail.com>

---
+++ tfjs-layers/src/engine/base_random_layers.ts
@@ -0,0 +1,52 @@
+/**
+ * @license
+ * Copyright 2022 CodeSmith LLC
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ * =============================================================================
+ */
+
+import { LayerArgs, Layer } from './topology';
+import { randomGamma, randomNormal} from '@tensorflow/tfjs-core';
+import { randomStandardNormal, randomUniform } from '@tensorflow/tfjs-core';
+import { ValueError } from '../errors';
+
+export declare interface BaseRandomLayerArgs extends LayerArgs {}
+
+export type RNGTypes = {
+  [key: string]: Function;
+};
+
+export abstract class BaseRandomLayer extends Layer {
+  // A layer handle the random number creation and savemodel behavior.
+  /** @nocollapse */
+  static className = 'RandomWidth';
+  randomGenerator: Function;
+  private rngType: string;
+
+  private readonly rngTypes: RNGTypes = {
+    gamma: randomGamma,
+    normal: randomNormal,
+    standardNormal: randomStandardNormal,
+    uniform: randomUniform
+  };
+
+  constructor(args: BaseRandomLayerArgs) {
+    super(args);
+  }
+
+  protected setRNGType = (rngType: string) => {
+    this.rngType = rngType;
+
+    if (this.rngTypes.hasOwnProperty(this.rngType)) {
+      this.randomGenerator = this.rngTypes[this.rngType];
+    } else {
+      throw new ValueError (
+        `Invalid rngType provided.
+        Received rngType=${this.rngType}`);
+    }
+    return this.randomGenerator;
+  }
+}
