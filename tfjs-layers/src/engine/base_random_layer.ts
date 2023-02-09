17ff308b470b919426983ba24880c18f72c4a04c
Silvia Kocsis <silvia.kocsis@gmail.com>
2023-02-08 21:32:56 -0500

    updated BaseRandomLayer to contain an instance of RandomGenerator class, got rid of rng_types as all random distribution functions seem to be stateless
    Co-authored-by: Natalie Umanzor <umanzor2949@gmail.com>
    Co-authored-by: Ryan Wallace <ryanwallace1396@gmail.com>

--- tfjs-layers/src/engine/base_random_layer.ts
+++ tfjs-layers/src/engine/base_random_layer.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2022 CodeSmith LLC
+ * Copyright 2023 CodeSmith LLC
  *
  * Use of this source code is governed by an MIT-style
  * license that can be found in the LICENSE file or at
@@ -9,44 +9,20 @@
  */
 
 import { LayerArgs, Layer } from './topology';
-import { randomGamma, randomNormal} from '@tensorflow/tfjs-core';
-import { randomStandardNormal, randomUniform } from '@tensorflow/tfjs-core';
-import { ValueError } from '../errors';
+import { RandomGenerator } from '../backend/random_generator';
 
-export declare interface BaseRandomLayerArgs extends LayerArgs {}
-
-export type RNGTypes = {
-  [key: string]: Function;
-};
+export declare interface BaseRandomLayerArgs extends LayerArgs {
+  seed?: number;
+}
 
 export abstract class BaseRandomLayer extends Layer {
   // A layer handle the random number creation and savemodel behavior.
   /** @nocollapse */
-  static className = 'RandomWidth';
-  randomGenerator: Function;
-  private rngType: string;
-
-  private readonly rngTypes: RNGTypes = {
-    gamma: randomGamma,
-    normal: randomNormal,
-    standardNormal: randomStandardNormal,
-    uniform: randomUniform
-  };
+  static className = 'BaseRandomLayer';
+  protected randomGenerator: RandomGenerator;
 
   constructor(args: BaseRandomLayerArgs) {
     super(args);
-  }
-
-  protected setRNGType = (rngType: string) => {
-    this.rngType = rngType;
-
-    if (this.rngTypes.hasOwnProperty(this.rngType)) {
-      this.randomGenerator = this.rngTypes[this.rngType];
-    } else {
-      throw new ValueError (
-        `Invalid rngType provided.
-        Received rngType=${this.rngType}`);
-    }
-    return this.randomGenerator;
+    this.randomGenerator = new RandomGenerator(args.seed);
   }
 }
