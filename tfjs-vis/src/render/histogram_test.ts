a49f444f7821852c7ad8b2a66e224450269759b5
Matthew Soulanille <msoulanille@google.com>
2023-02-22 10:51:07 -0800

    Update tfjs-vis to use tfjs 4.2.0 (#7402)
    Update tfjs-vis to use tfjs 4.2.0. Update other dependencies as well.
    
    Also, increase the OS X BrowserStack test version from High Sierra to Big Sur. This is needed because jasmine 4.x uses queueMicrotask, which is not available in High Sierra.

--- tfjs-vis/src/render/histogram_test.ts
+++ tfjs-vis/src/render/histogram_test.ts
@@ -133,7 +133,7 @@ describe('renderHistogram', () => {
     const statsEls = document.querySelectorAll('table tbody td');
     expect(statsEls.length).toBe(6);
     expect(statsEls[0].textContent).toEqual('8');
-    expect(statsEls[1].textContent).toEqual('-100');
+    expect(statsEls[1].textContent).toEqual('−100'); // Note the long dash '−'
     expect(statsEls[2].textContent).toEqual('200');
     expect(statsEls[3].textContent).toEqual('2 (25%)');
     expect(statsEls[4].textContent).toEqual('3 (37.5%)');
@@ -176,7 +176,7 @@ describe('renderHistogram', () => {
     const statsEls = document.querySelectorAll('table tbody td');
     expect(statsEls.length).toBe(5);
     expect(statsEls[0].textContent).toEqual('200');
-    expect(statsEls[1].textContent).toEqual('-30');
+    expect(statsEls[1].textContent).toEqual('−30'); // Note the long dash '−'
     expect(statsEls[2].textContent).toEqual('140');
     expect(statsEls[3].textContent).toEqual('2 (1%)');
     expect(statsEls[4].textContent).toEqual('5 (2.5%)');
