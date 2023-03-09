724a65d09a7d101243896a4ae42f3ec6fc85131e
Matthew Soulanille <msoulanille@google.com>
2023-03-09 13:41:04 -0800

    Add signature to declaration

--- tfjs-core/src/public/chained_ops/gather.ts
+++ tfjs-core/src/public/chained_ops/gather.ts
@@ -22,7 +22,8 @@ import {Rank, TensorLike} from '../../types';
 declare module '../../tensor' {
   interface Tensor<R extends Rank = Rank> {
     gather<T extends Tensor>(
-        this: T, indices: Tensor|TensorLike, axis?: number): T;
+        this: T, indices: Tensor|TensorLike, axis?: number,
+        batchDims?: number): T;
   }
 }
 
