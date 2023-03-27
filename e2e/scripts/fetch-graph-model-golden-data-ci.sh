24012d07d320932cd8ce25aa7e46c282ee8e6be8
Chunnien Chan <cnchan@google.com>
2023-03-27 15:27:49 -0700

    Remove parallel

--- e2e/scripts/fetch-graph-model-golden-data-ci.sh
+++ e2e/scripts/fetch-graph-model-golden-data-ci.sh
@@ -20,4 +20,4 @@ cd ./integration_tests/graph_model_golden_data
 
 golden_files=$(python -c "import json; print('\n'.join(json.load(open('./filenames.json'))))")
 
-echo "$golden_files" | parallel -j4 "curl -O $STORAGE_URL/{}"
+while read golden_file; do curl -O "$STORAGE_URL/$golden_file"; done <<<"$(echo "$golden_files")"
