250e080361c3bf446f275a396f0778a7daf1ba7e
Xu Xing <xing.xu@intel.com>
2023-03-30 08:52:43 +0800

    Nit

--- e2e/benchmarks/browserstack-benchmark/karma.conf.js
+++ e2e/benchmarks/browserstack-benchmark/karma.conf.js
@@ -75,7 +75,6 @@ module.exports = function(config) {
     'https://cdn.jsdelivr.net/npm/@tensorflow-models/speech-commands@latest/dist/speech-commands.min.js',
     'https://cdn.jsdelivr.net/npm/@tensorflow-models/posenet@latest/dist/posenet.min.js',
     'https://cdn.jsdelivr.net/npm/@tensorflow-models/body-pix@latest/dist/body-pix.min.js',
-    'https://cdn.jsdelivr.net/npm/@tensorflow-models/pose-detection@latest/dist/pose-detection.min.js',
     {pattern: './benchmark_parameters.json', included: false, served: true},
     '../util.js', '../benchmark_util.js', '../model_config.js',
     'benchmark_models.js'
