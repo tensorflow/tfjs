f865236a5db3f3adb70c422d37f26329f37b2427
Xinghua Cao <xinghua.cao@intel.com>
2023-04-13 15:05:52 +0800

    [WebGPU] support SparseReshape kernel

--- tfjs-backend-webgpu/src/register_all_kernels.ts
+++ tfjs-backend-webgpu/src/register_all_kernels.ts
@@ -155,6 +155,7 @@ import {sliceConfig} from './kernels/Slice';
 import {softmaxConfig} from './kernels/Softmax';
 import {softplusConfig} from './kernels/Softplus';
 import {spaceToBatchNDConfig} from './kernels/SpaceToBatchND';
+import {sparseReshapeConfig} from './kernels/SparseReshape';
 import {sparseToDenseConfig} from './kernels/SparseToDense';
 import {splitVConfig} from './kernels/SplitV';
 import {sqrtConfig} from './kernels/Sqrt';
@@ -320,6 +321,7 @@ const kernelConfigs: KernelConfig[] = [
   softmaxConfig,
   softplusConfig,
   spaceToBatchNDConfig,
+  sparseReshapeConfig,
   sparseToDenseConfig,
   splitVConfig,
   sqrtConfig,
