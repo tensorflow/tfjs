cfad73760cb5de5460bbb66ab701db33f60d7125
xhcao <xinghua.cao@intel.com>
2023-04-14 16:17:36 +0800

    [WebGPU] support ComplexAbs kernel (#7583)

---
+++ tfjs-backend-webgpu/src/complex_abs_webgpu.ts
@@ -0,0 +1,55 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import {getMainHeaderString as main, WebGPUProgram} from './webgpu_program';
+import {computeDispatch, flatDispatchLayout} from './webgpu_util';
+
+export class ComplexAbsProgram implements WebGPUProgram {
+  outputShape: number[] = [];
+  shaderKey: string;
+  dispatchLayout: {x: number[]};
+  dispatch: [number, number, number];
+  variableNames = ['real', 'imag'];
+  workgroupSize: [number, number, number] = [64, 1, 1];
+  size = true;
+
+  constructor(shape: number[]) {
+    this.outputShape = shape;
+    this.dispatchLayout = flatDispatchLayout(this.outputShape);
+    this.dispatch = computeDispatch(
+        this.dispatchLayout, this.outputShape, this.workgroupSize);
+
+    this.shaderKey = 'complexAbs';
+  }
+
+  getUserCode(): string {
+    const userCode = `
+    ${main('index')} {
+      if (index < uniforms.size) {
+        let re = abs(getRealByOutputIndex(index));
+        let im = abs(getImagByOutputIndex(index));
+        let mx = max(re, im);
+
+        // The length function in wgsl may be not underflow-safe on some GPUs.
+        // So the safe solution is to ensure underflow-safety in all cases.
+        setOutputAtIndex(index, select(mx * length(vec2<f32>(1, min(re, im)/mx)), 0.0, mx == 0.0));
+      }
+    }
+  `;
+    return userCode;
+  }
+}
