287dc7f84c9460352f9d82cda5ac5926c4f30192
chunnienc <121328115+chunnienc@users.noreply.github.com>
2023-05-11 10:45:28 -0700

    [wasm] Add AvgPoolGrad and MaxPoolGrad kernel (#7672)
    * wip
    
    * Add AvgPool3D kernel
    
    * Add MaxPool3D kernel
    
    * fix
    
    * Fix template types order
    
    * Update param channel_size
    
    * Update headers and build deps
    
    
    remove
    
    * Add AvgPool3DGrad
    
    * Add MaxPool3DGrad
    
    * Fix wasm func signature
    
    * Add AvgPoolGrad
    
    * Add MaxPoolGrad
    
    * Fix

--- tfjs-backend-wasm/src/setup_test.ts
+++ tfjs-backend-wasm/src/setup_test.ts
@@ -63,12 +63,7 @@ const TEST_FILTERS: TestFilter[] = [
     ]
   },
   {include: 'depthToSpace'},
-  {
-    include: 'avgPool ',
-    excludes: [
-      'gradient',  // Not yet implemented.
-    ]
-  },
+  {include: 'avgPool '},
   {
     include: 'relu',
     excludes: [
@@ -83,7 +78,6 @@ const TEST_FILTERS: TestFilter[] = [
   {
     include: 'maxPool',
     excludes: [
-      'maxPoolBackprop',   // Not yet implemented.
       'ignores NaNs',      // Actual != expected.
       'maxPoolWithArgmax'  // Not yet implemented.
     ]
