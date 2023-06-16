a00f45c76eaee3abdaf8bed63d76f21b62930689
Piero Orderique <orderique@google.com>
2023-06-16 00:38:24 +0000

    Add bytesToUnicode and HashTable for tensors.

--- tfjs-layers/src/layers/nlp/tokenizers_utils.ts
+++ tfjs-layers/src/layers/nlp/tokenizers_utils.ts
@@ -14,3 +14,85 @@
  * limitations under the License.
  * =============================================================================
  */
+
+/* Original source: keras-nlp/byte_pair_tokenizer.py */
+
+import { Tensor, tensor1d } from '@tensorflow/tfjs-core';
+import { ValueError } from '../../errors';
+
+export function bytesToUnicode(): [Uint8Array, string[]] {
+  const range = (start: number, end: number) =>
+    Array.from({ length: (end - start + 1) }, (v, k) => k + start);
+
+  const bs = [
+    ...range('!'.charCodeAt(0), '~'.charCodeAt(0)),
+    ...range('¡'.charCodeAt(0), '¬'.charCodeAt(0)),
+    ...range('®'.charCodeAt(0), 'ÿ'.charCodeAt(0))
+  ];
+
+  const cs = [...bs];
+  let n = 0;
+
+  // Removes mapping an int to a whitespace character
+  for (let b = 0; b < 2 ** 8; b++) {
+    if (!bs.includes(b)) {
+      bs.push(b);
+      cs.push(2 ** 8 + n);
+      n++;
+    }
+  }
+
+  const chars = cs.map(n => String.fromCharCode(n));
+
+  // TODO(orderique): Verify same functionality.
+  const bytes = Uint8Array.from(bs);
+
+  return [bytes, chars];
+}
+
+/**
+ * HashTable extends Map and includes a `lookup` function that
+ */
+class HashTable<K, V extends number|string> extends Map<K, V> {
+  constructor(keys: K[], values: V[], private readonly defaultValue: V) {
+    if (keys.length !== values.length) {
+      throw new ValueError(`keys and values arrays must be same length.
+        Instead got lengths ${keys.length} and ${values.length}.`
+      );
+    }
+    const keyValPairs: Array<[K, V]> = [];
+    for (let idx = 0; idx < keys.length; idx++) {
+      const key = keys[idx];
+      const val = values[idx];
+      keyValPairs.push([key, val]);
+    }
+
+    super(keyValPairs);
+  }
+
+  override get(key: K) {
+    if (!this.has(key)) {
+      this.set(key, this.defaultValue);
+    }
+    return super.get(key);
+  }
+
+  lookup(keys: Tensor[]): Tensor[] {
+    const values = keys.map(tensor => {
+      const innerValues: V[] = [];
+      for (const key of tensor.dataSync() as unknown as K[]) {
+        innerValues.push(this.get(key));
+      }
+
+      return tensor1d(innerValues as string[]|number[]);
+    });
+
+    return values;
+  }
+}
+
+export function createHashtable<T1, T2 extends number|string>(
+  keys: T1[], values: T2[], defaultVal: T2): HashTable<T1, T2> {
+
+  return new HashTable(keys, values, defaultVal);
+}
