a3edc697cf4f94dfaae09ebb86f68c1fc4cbc15b
Dedongala <133151251+Dedongala@users.noreply.github.com>
2023-06-27 11:29:52 -0700

    Update publish-pypi.ts

--- scripts/publish-pypi.ts
+++ scripts/publish-pypi.ts
@@ -1,25 +1,20 @@
+// publish-pypi.ts
 import * as argparse from 'argparse';
-import { checkoutReleaseBranch, question } from './release-util';
-const TMP_DIR = '/tmp/tfjs-pypi';
+import { checkoutReleaseBranch, question, $ } from './release-util';
 import * as shell from 'shelljs';
-import { execSync } from 'child_process';
-import chalk from 'chalk';
 import * as fs from 'fs';
+import chalk from 'chalk';
+
+const TMP_DIR = '/tmp/tfjs-pypi';
 
 const parser = new argparse.ArgumentParser();
 parser.addArgument('--git-protocol', {
   action: 'storeTrue',
   help: 'Use the git protocol rather than the http protocol when cloning repos.',
 });
-parser.addArgument(['--dry'], {
-  action: 'storeTrue',
-  help: 'Dry run. Stage all packages in verdaccio but do not publish them to the registry.',
-});
 
 async function getNewlyCreatedBranches(): Promise<string[]> {
-  const branchesStr = execSync(
-    `git branch -r --sort=-authordate --format='%(HEAD) %(refname:lstrip=-1)'`,
-  ).toString();
+  const branchesStr = $(`git branch -r --sort=-authordate --format='%(HEAD) %(refname:lstrip=-1)'`);
 
   const branches = branchesStr.split('\n').map((line: string) => line.trim());
 
@@ -39,37 +34,23 @@ async function main() {
     const latestBranch = branches[0];
     console.log('Latest Branch:', latestBranch);
 
-    const answer = await question(chalk.cyan.bold(`Is this the right branch '${latestBranch}' you are looking for? (y/n): `));
+    const answer = await question(chalk.cyan.bold(`Is this the right branch '${latestBranch}' you are looking for? (y/N): `));
 
     if (answer.toLowerCase() === 'y') {
       checkoutReleaseBranch(latestBranch, args.git_protocol, TMP_DIR);
       const targetDir = `${TMP_DIR}/tfjs-converter/python`;
       shell.cd(targetDir);
       console.log(chalk.blue.bold('Current directory:', shell.pwd().toString()));
-      const executeCommand = (code, stdout, stderr) => {
-        if (code !== 0) {
-          console.error(`Error executing the command: ${stderr}`);
-          return;
-        }
-        console.log(stdout);
-      };
-      shell.exec('bazel clean', executeCommand);
-      shell.exec('bazel build python3_wheel', executeCommand);
+      
+      $('bazel clean');
+      $('bazel build python3_wheel');
 
       // Remove dist folder if it exists
       if (fs.existsSync('./dist')) {
         fs.rmdirSync('./dist', { recursive: true });
       }
 
-      const command = './build-pip-package.sh --upload ./dist';
-      shell.exec(command, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error executing the command: ${error.message}`);
-          return;
-        }
-        console.log('stdout:', stdout);
-        console.error('stderr:', stderr);
-      });
+      $('./build-pip-package.sh --upload ./dist');
 
       // Command executed successfully
       console.log('Command executed successfully');
