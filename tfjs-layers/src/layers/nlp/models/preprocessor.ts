1556a6fe313aeb2035f151b9e48e0408a1f27452
Piero Orderique <orderique@google.com>
2023-06-29 16:54:31 +0000

    Remove uneeded args

--- tfjs-layers/src/layers/nlp/models/preprocessor.ts
+++ tfjs-layers/src/layers/nlp/models/preprocessor.ts
@@ -18,57 +18,10 @@
 /* Original source: keras-nlp/models/preprocessor.py */
 import { serialization } from '@tensorflow/tfjs-core';
 
-import { Layer, LayerArgs } from '../../../engine/topology';
+import { Layer } from '../../../engine/topology';
 import { Kwargs } from '../../../types';
 import { Tokenizer } from '../tokenizers';
 import { NotImplementedError } from 'tfjs-layers/src/errors';
-
-export declare interface StartEndPackerArgs extends LayerArgs {
-  /**
-   * Integer. The desired output length.
-   */
-  sequenceLength: number;
-
-  /**
-   * Integer or string. The ID or token that is to be placed at the start of
-   * each sequence. The dtype must match the dtype of the input tensors to the
-   * layer. If undefined, no start value will be added.
-   */
-  startValue?: number|string;
-
-  /**
-   * Integer or string. The ID or token that is to be placed at the end of each
-   * input segment. The dtype must match the dtype of the input tensors to the
-   * layer. If undefined, no end value will be added.
-   */
-  endValue?: number|string;
-
-  /**
-   * Integer or string. The ID or token that is to be placed into the unused
-   * positions after the last segment in the sequence. If undefined, 0 or ''
-   * will be added depending on the dtype of the input tensor.
-   */
-  padValue?: number|string;
-}
-
-export declare interface StartEndPackerOptions {
-  /**
-   * Pass to override the configured `sequenceLength` of the layer.
-   */
-  sequenceLength?: number;
-
-  /**
-   * Pass `false` to not append a start value for this input.
-   * Defaults to true.
-   */
-  addStartValue?: boolean;
-
-  /**
-   * Pass `false` to not append an end value for this input.
-   * Defaults to true.
-   */
-  addEndValue?: boolean;
-}
 
 /**
  * Base class for model Preprocessors.
