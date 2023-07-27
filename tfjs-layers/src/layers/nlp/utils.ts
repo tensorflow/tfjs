fca49f6797268449fceaea1aedb56008dea81bbd
Piero Orderique <porderiq@mit.edu>
2023-07-27 10:52:41 -0700

    Add slice update

--- tfjs-layers/src/layers/nlp/utils.ts
+++ tfjs-layers/src/layers/nlp/utils.ts
@@ -15,7 +15,7 @@
  * =============================================================================
  */
 
-import { Tensor } from '@tensorflow/tfjs-core';
+import { Tensor, tensorScatterUpdate } from '@tensorflow/tfjs-core';
 
 export function tensorToArr(input: Tensor): unknown[] {
   return Array.from(input.dataSync()) as unknown as unknown[];
@@ -23,4 +23,36 @@ export function tensorToArr(input: Tensor): unknown[] {
 
 export function tensorArrTo2DArr(inputs: Tensor[]): unknown[][] {
   return inputs.map(input => tensorToArr(input));
+}
+
+/**
+ * Returns a new Tensor with `updates` inserted into `inputs` starting at the
+ * index `startIndices`.
+ *
+ * @param inputs Tensor to "modify"
+ * @param startIndices the starting index to insert the slice.
+ *  Length must be equal to `inputs.rank`;
+ * @param updates the update tensor. Shape must fit within `inputs` shape.
+ * @returns a new tensor with the modification.
+ */
+export function sliceUpdate(
+  inputs: Tensor, startIndices: number[], updates: Tensor): Tensor {
+const indices: number[][] = [];
+function createIndices(idx: number, curr: number[]): void {
+  if (curr.length === startIndices.length) {
+    indices.push(curr.slice());
+    return;
+  }
+  const s = startIndices[idx];
+  const e = s + updates.shape[idx];
+  for (let i = s; i < e; i++) {
+    curr.push(i);
+    createIndices(idx + 1, curr);
+    curr.pop();
+  }
+}
+createIndices(0, []);
+// Flatten the updates to match length of its update indices.
+updates = updates.reshape([updates.size]);
+return tensorScatterUpdate(inputs, indices, updates);
 }
