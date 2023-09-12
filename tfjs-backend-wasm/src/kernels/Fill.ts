85056ec77d3877a50e00e0d2a9840285bc029e4d
Scott Klum <scott@applieddesigns.io>
2023-09-12 13:07:57 -0700

    Infer dtype in wasm fill

--- tfjs-backend-wasm/src/kernels/Fill.ts
+++ tfjs-backend-wasm/src/kernels/Fill.ts
@@ -15,13 +15,17 @@
  * =============================================================================
  */
 
-import {KernelConfig, KernelFunc} from '@tensorflow/tfjs-core';
+import {KernelConfig, KernelFunc, util} from '@tensorflow/tfjs-core';
 import {Fill, FillAttrs} from '@tensorflow/tfjs-core';
 
 import {BackendWasm} from '../backend_wasm';
 
 export function fill(args: {attrs: FillAttrs, backend: BackendWasm}) {
-  const {attrs: {shape, value, dtype}, backend} = args;
+  const {attrs: {shape, value}, backend} = args;
+  let {attrs: {dtype}} = args;
+
+  dtype = dtype || util.inferDtype(value);
+
   const out = backend.makeOutput(shape, dtype);
   const outVals = backend.typedArrayFromHeap(out);
   outVals.fill(value as number);
