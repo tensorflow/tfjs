5e3ab05fdf16e6bfa22ce45cdfffd45a5f851932
dbcp1 <150488501+dbcp1@users.noreply.github.com>
2024-05-28 15:06:08 -0700

    Update monorepo to 4.20.0-rc.0. (#8288)

--- tfjs-tfdf/src/version.ts
+++ tfjs-tfdf/src/version.ts
@@ -1,5 +1,5 @@
 /** @license See the LICENSE file. */
 
 // This code is auto-generated, do not modify this file!
-const version = '4.20.0-rc.0';
+const version = '0.0.1-alpha.25';
 export {version};
